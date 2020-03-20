4e1c3cfa5c8f9f3556508c8611460eb2610411a2
David Pacheco <dap@oxidecomputer.com>
2020-03-20 15:20:34 -0700

    more tests for config/logging

--- src/api_config.rs
+++ src/api_config.rs
@@ -48,11 +48,12 @@ impl ApiServerConfig {
 
 #[cfg(test)]
 mod test {
-    use super::ApiServerConfig;
-    use slog::Logger;
+    use serde::Deserialize;
     use std::fs;
     use std::net::IpAddr;
     use std::path::Path;
+    use std::path::PathBuf;
+    use super::ApiServerConfig;
 
     /*
      * Chunks of valid config file.  These are put together with invalid chunks
@@ -68,10 +69,7 @@ mod test {
             mode = "stderr-terminal"
         "##;
 
-    pub fn read_config(
-        label: &str,
-        contents: &str,
-    ) -> Result<ApiServerConfig, String> {
+    fn temp_path(label: &str) -> PathBuf {
         let arg0str = std::env::args().next().expect("expected process arg0");
         let arg0 = Path::new(&arg0str)
             .file_name()
@@ -81,6 +79,14 @@ mod test {
         let pid = std::process::id();
         let mut pathbuf = std::env::temp_dir();
         pathbuf.push(format!("{}.{}.{}", arg0, pid, label));
+        pathbuf
+    }
+
+    fn read_config(
+        label: &str,
+        contents: &str,
+    ) -> Result<ApiServerConfig, String> {
+        let pathbuf = temp_path(label);
         let path = pathbuf.as_path();
         eprintln!("writing test config {}", path.display());
         fs::write(path, contents).expect("write to tempfile failed");
@@ -244,8 +250,14 @@ mod test {
     /*
      * Working "mode = stderr-terminal" config
      *
-     * TODO-coverage: should use the logger and redirect stderr to a file and
-     * test its contents.
+     * TODO-coverage: It would be nice to redirect our own stderr to a file (or
+     * something else we can collect) and then use the logger that we get below.
+     * Then we could verify that it contains the content we expect.
+     * Unfortunately, while Rust has private functions to redirect stdout and
+     * stderr, there's no exposed function for doing that, nor is there a way to
+     * provide a specific stream to a terminal logger.  (We could always
+     * implement our own.)
+     *
      * TODO-coverage: other tests:
      * - failed to create file logger (filesystem failure of some kind)
      * - successful file logger, all three modes?
@@ -267,6 +279,261 @@ mod test {
         assert_eq!(config.bind_address.port(), 4567);
 
         config.log.to_logger().expect("expected logger");
+    }
+
+    /*
+     * Bad "mode = file" configurations
+     */
+
+    #[test]
+    pub fn test_config_bad_file_no_file() {
+        let bad_config = format!(
+            "{}{}",
+            CONFIG_VALID_BIND_ADDRESS,
+            r##"
+            [log]
+            mode = "file"
+            level = "warn"
+            "##
+        );
+        let error = read_config("bad_file_no_file", &bad_config)
+            .expect_err("expected failure");
+        assert!(error.starts_with("parse \""));
+        assert!(error.contains("\": missing field `path` for key `log`"));
+    }
+
+    #[test]
+    pub fn test_config_bad_file_no_level() {
+        let bad_config = format!(
+            "{}{}",
+            CONFIG_VALID_BIND_ADDRESS,
+            r##"
+            [log]
+            mode = "file"
+            path = "nonexistent"
+            "##
+        );
+        let error = read_config("bad_file_no_level", &bad_config)
+            .expect_err("expected failure");
+        assert!(error.starts_with("parse \""));
+        assert!(error.contains("\": missing field `level` for key `log`"));
+    }
+
+    #[test]
+    pub fn test_config_bad_file_bad_path_type() {
+        /*
+         * We create a path as a directory so that when we subsequently try to
+         * use it a file, we won't be able to.
+         */
+        let mut pathbuf = temp_path("bad_file_bad_path_type_dir");
+        pathbuf.push("dummy");
+
+        let path = pathbuf.as_path();
+        fs::create_dir_all(path).expect("create dummy directory");
+
+        let bad_config = format!(
+            "{}{}\"{}\"\n",
+            CONFIG_VALID_BIND_ADDRESS,
+            r##"
+            [log]
+            mode = "file"
+            level = "warn"
+            if_exists = "append"
+            path = "##,
+            path.display()
+        );
+
+        let config = read_config("bad_file_bad_path_type", &bad_config)
+            .expect("expected success");
+        let error = config.log.to_logger().expect_err("expected failure");
+        let message = format!("{}", error);
+        eprintln!("error message: {}", message);
+        assert!(message.starts_with(&format!(
+            "error creating API server: open log file \"{}\": Is a directory",
+            path.display()
+        )));
+
+        fs::remove_dir(path).expect("remove dummy directory");
+        fs::remove_dir(path.parent().expect("expected parent"))
+            .expect("remove test directory");
+    }
+
+    #[test]
+    pub fn test_config_bad_file_path_exists_fail() {
+        let mut pathbuf = temp_path("bad_file_path_exists_fail_dir");
+        pathbuf.push("log.out");
+
+        let path = pathbuf.as_path();
+        fs::create_dir_all(path.parent().unwrap()).expect("creating parent");
+        fs::write(path, "").expect("writing empty file");
+
+        let bad_config = format!(
+            "{}{}\"{}\"\n",
+            CONFIG_VALID_BIND_ADDRESS,
+            r##"
+            [log]
+            mode = "file"
+            level = "warn"
+            if_exists = "fail"
+            path = "##,
+            path.display()
+        );
+
+        let config = read_config("bad_file_bad_path_exists_fail", &bad_config)
+            .expect("expected success");
+        let error = config.log.to_logger().expect_err("expected failure");
+        let message = format!("{}", error);
+        eprintln!("error message: {}", message);
+        assert!(message.starts_with(&format!(
+            "error creating API server: open log file \"{}\": File exists",
+            path.display()
+        )));
+
+        fs::remove_file(path).expect("remove dummy directory");
+        fs::remove_dir(path.parent().expect("expected parent"))
+            .expect("remove test directory");
+    }
+
+    /*
+     * Working "mode = file" configuration.  The following test exercises
+     * successful file-based configurations for all three values of "if_exists",
+     * different log levels, and the bunyan log format.
+     * XXX refactor tests to avoid duplicating so much code?
+     */
+
+    #[test]
+    pub fn test_config_file() {
+        let mut pathbuf = temp_path("file_dir");
+        /* Use an extra level of directory to make sure that gets created. */
+        pathbuf.push("log.out");
+        let path = pathbuf.as_path();
+
+        /* The first attempt should succeed.  The log file doesn't exist yet. */
+        let bad_config = format!(
+            "{}{}\"{}\"\n",
+            CONFIG_VALID_BIND_ADDRESS,
+            r##"
+            [log]
+            mode = "file"
+            level = "warn"
+            if_exists = "fail"
+            path = "##,
+            path.display()
+        );
+
+        let config =
+            read_config("file", &bad_config).expect("expected success");
+        {
+            /*
+             * Construct the logger in a block so that it's flushed by the time
+             * we proceed.
+             */
+            let log = config.log.to_logger().expect("expected success");
+            debug!(log, "message1_debug");
+            warn!(log, "message1_warn");
+            error!(log, "message1_error");
+        }
+
+        /* Try again with if_exists = "append".  This should also work. */
+        let bad_config = format!(
+            "{}{}\"{}\"\n",
+            CONFIG_VALID_BIND_ADDRESS,
+            r##"
+            [log]
+            mode = "file"
+            level = "warn"
+            if_exists = "append"
+            path = "##,
+            path.display()
+        );
+
+        let config =
+            read_config("file", &bad_config).expect("expected success");
+        {
+            /* See above. */
+            let log = config.log.to_logger().expect("expected success");
+            warn!(log, "message2");
+        }
+
+        let log_contents_before = fs::read_to_string(path).unwrap();
+
+        /*
+         * TODO-cleanup log parsing code should be elsewhere and more general.
+         * TODO-coverage check hostname, timestamps, and level
+         */
+        #[derive(Deserialize)]
+        struct BunyanLogRecord {
+            name: String,
+            hostname: String,
+            pid: u32,
+            level: usize,
+            msg: String,
+            time: String,
+            v: usize,
+        }
+
+        let log_records = log_contents_before
+            .split("\n")
+            .filter(|line| line.len() > 0)
+            .map(|line| {
+                serde_json::from_str::<BunyanLogRecord>(line)
+                    .expect("invalid log record")
+            })
+            .collect::<Vec<BunyanLogRecord>>();
+        assert_eq!(log_records.len(), 3);
+        for record in log_records.iter() {
+            assert_eq!(record.name, "oxide-api");
+            assert_eq!(record.pid, std::process::id());
+            assert_eq!(record.v, 0);
+        }
+        for record in log_records.iter().skip(1) {
+            assert_eq!(log_records[0].hostname, record.hostname);
+        }
+        assert_eq!(log_records[0].msg, "message1_warn");
+        assert_eq!(log_records[1].msg, "message1_error");
+        assert_eq!(log_records[2].msg, "message2");
+
+        /*
+         * Try again with if_exists = "truncate".  This should also work, but
+         * remove the contents that's already there.
+         */
+        let bad_config = format!(
+            "{}{}\"{}\"\n",
+            CONFIG_VALID_BIND_ADDRESS,
+            r##"
+            [log]
+            mode = "file"
+            level = "trace"
+            if_exists = "truncate"
+            path = "##,
+            path.display()
+        );
+
+        let config = read_config("file", &bad_config).unwrap();
+        {
+            /* See above. */
+            let log = config.log.to_logger().unwrap();
+            debug!(log, "message3_debug");
+            warn!(log, "message3_warn");
+            error!(log, "message3_error");
+        }
+
+        let log_contents_after = fs::read_to_string(path).unwrap();
+        let log_records = log_contents_after
+            .split("\n")
+            .filter(|line| line.len() > 0)
+            .map(|line| {
+                serde_json::from_str::<BunyanLogRecord>(line)
+                    .expect("invalid log record")
+            })
+            .collect::<Vec<BunyanLogRecord>>();
+        assert_eq!(log_records.len(), 3);
+        assert_eq!(log_records[0].msg, "message3_debug");
+        assert_eq!(log_records[1].msg, "message3_warn");
+        assert_eq!(log_records[2].msg, "message3_error");
+
+        fs::remove_file(path).unwrap();
+        fs::remove_dir(path.parent().unwrap()).unwrap();
     }
 }
 
@@ -434,10 +701,9 @@ where
     T: slog::Drain + Send + 'static,
     <T as slog::Drain>::Err: std::fmt::Debug,
 {
-    let pid = std::process::id();
     let level_drain = slog::LevelFilter(drain, Level::from(level)).fuse();
     let async_drain = slog_async::Async::new(level_drain).build().fuse();
-    slog::Logger::root(async_drain, o!("pid" => pid))
+    slog::Logger::root(async_drain, o!())
 }
 
 fn log_drain_for_file(
