7f2c4cdb609021380d2ed8f043e90c71435df8f5
Adam H. Leventhal <ahl@oxide.computer>
2020-03-31 17:40:44 -0700

    validation

--- lib/openapi/src/lib.rs
+++ lib/openapi/src/lib.rs
@@ -4,15 +4,16 @@
 
 extern crate proc_macro;
 
-use proc_macro2::{Delimiter, TokenStream, TokenTree};
+use proc_macro2::{TokenStream, TokenTree};
 use quote::quote;
+use quote::ToTokens;
 
 use std::collections::HashMap;
 
 use serde::Deserialize;
 
 mod serde_tokenstream;
-use crate::serde_tokenstream::{from_tokenstream, Result};
+use crate::serde_tokenstream::from_tokenstream;
 
 // We use the `abort` macro to identify known, aberrant conditions while
 // processing macro parameters. This is based on `proc_macro_error::abort`
@@ -63,6 +64,7 @@ struct Parameter {
     deprecated: Option<bool>,
 }
 
+#[allow(non_snake_case)]
 #[derive(Deserialize, Debug)]
 enum MethodType {
     DELETE,
@@ -72,8 +74,20 @@ enum MethodType {
     PUT,
 }
 
+impl MethodType {
+    fn as_str(self) -> &'static str {
+        match self {
+            MethodType::DELETE => "DELETE",
+            MethodType::GET => "GET",
+            MethodType::PATCH => "PATCH",
+            MethodType::POST => "POST",
+            MethodType::PUT => "PUT",
+        }
+    }
+}
+
 #[derive(Deserialize, Debug)]
-struct MetaData {
+struct Metadata {
     method: MethodType,
     path: String,
     parameters: Vec<Parameter>,
@@ -86,38 +100,94 @@ pub fn endpoint(
     attr: proc_macro::TokenStream,
     item: proc_macro::TokenStream,
 ) -> proc_macro::TokenStream {
+    do_endpoint(attr, item).unwrap()
+}
+/// Conservative TokenStream equality.
+fn tokenstream_eq(a: &TokenStream, b: &TokenStream) -> bool {
+    let mut aa = a.clone().into_iter();
+    let mut bb = b.clone().into_iter();
+
+    loop {
+        match (aa.next(), bb.next()) {
+            (None, None) => break true,
+            (Some(TokenTree::Ident(at)), Some(TokenTree::Ident(bt)))
+                if at == bt =>
+            {
+                continue
+            }
+            (Some(TokenTree::Punct(at)), Some(TokenTree::Punct(bt)))
+                if at.as_char() == bt.as_char() =>
+            {
+                continue
+            }
+            _ => break false,
+        }
+    }
+}
+
+fn do_endpoint(
+    attr: proc_macro::TokenStream,
+    item: proc_macro::TokenStream,
+) -> Result<proc_macro::TokenStream, &'static str> {
     let attr2 = TokenStream::from(attr);
 
-    // new serde
-    let xxxx: Result<MetaData> = from_tokenstream(&attr2);
-
-    println!("{:?}", xxxx.unwrap());
-
-    // old working
-    let metadata = to_map(&attr2);
-
-    let method = match metadata.get("method") {
-        Some(MapEntry::Value(method)) => match method.as_str() {
-            "DELETE" | "GET" | "PATCH" | "POST" | "PUT" => method,
-            _ => abort!(attr2, r#"invalid method "{}""#, method),
-        },
-        None => abort!(
-            attr2,
-            r#""method" is a required field; valid values are DELETE, GET, PATCH, POST, and PUT"#
-        ),
-        _ => panic!("not done; needs test"),
+    let metadata = match from_tokenstream::<Metadata>(&attr2) {
+        Ok(value) => value,
+        Err(err) => panic!(err),
     };
 
-    let path = match metadata.get("path") {
-        Some(MapEntry::Value(path)) => path,
-        _ => panic!("not done; needs test"),
-    };
+    println!("{:?}", metadata);
 
-    // ast manipulation
+    let method = metadata.method.as_str();
+    let path = metadata.path;
+
+    let param_map = metadata
+        .parameters
+        .iter()
+        .map(|parameter| (&parameter.name, parameter))
+        .collect::<HashMap<_, _>>();
+
     let ast: syn::ItemFn = syn::parse(item).unwrap();
     let name = ast.sig.ident.clone();
 
+    let mut context = false;
+
+    let cty = quote! {
+        Arc<RequestContext>
+    };
+
+    println!("cty = {:?}", cty);
+
     let args = &ast.sig.inputs;
+
+    // Do validation of the fn paramters against the metadata we have.
+    for (i, arg) in args.iter().enumerate() {
+        match arg {
+            syn::FnArg::Typed(parameter) => match &*parameter.pat {
+                syn::Pat::Ident(id) => {
+                    let mut tt = TokenStream::new();
+                    parameter.ty.to_tokens(&mut tt);
+                    if tokenstream_eq(&cty, &tt) {
+                        context = true;
+                        if i != 0 {
+                            return Err("context parameter needs to be first");
+                        }
+                    } else {
+                        if param_map.get(&id.ident.to_string()).is_none() {
+                            println!("{:?}", id.ident.to_string());
+                            return Err("param not described");
+                        }
+                    }
+                }
+                _ => return Err("unexpected parameter type"),
+            },
+            syn::FnArg::Receiver(_selph) => {
+                return Err(
+                    "attribute cannot be applied to a method that uses self"
+                )
+            }
+        }
+    }
 
     let ins = args.iter().skip(1).map(|arg| match arg {
         syn::FnArg::Receiver(selph) => abort!(
@@ -162,141 +232,14 @@ pub fn endpoint(
             }
         }
     };
-    stream.into()
+    Ok(stream.into())
 }
 
-#[derive(Clone)]
+#[derive(Clone, Debug, Deserialize)]
 enum MapEntry {
     Value(String),
     Struct(HashMap<String, MapEntry>),
     Array(Vec<MapEntry>),
-}
-
-// Print out a MapEntry structure.
-#[allow(dead_code)]
-fn emit(depth: usize, metadata: MapEntry) {
-    match metadata {
-        MapEntry::Value(s) => println!("{}", s),
-        MapEntry::Struct(m) => {
-            println!("{}", '{');
-            for (key, value) in m {
-                print!("{:width$}{} = ", "", key, width = depth * 2 + 2);
-                emit(depth + 1, value);
-            }
-            println!("{:width$}{}", "", '}', width = depth * 2);
-        }
-        MapEntry::Array(v) => {
-            println!("[");
-            for value in v {
-                print!("{:width$}", "", width = depth * 2 + 2);
-                emit(depth + 1, value);
-            }
-            println!("{:width$}{}", "", ']', width = depth * 2);
-        }
-    }
-}
-
-fn parse_kv(tt: &TokenStream) -> MapEntry {
-    MapEntry::Struct(to_map(tt))
-}
-
-fn to_map(tt: &TokenStream) -> HashMap<String, MapEntry> {
-    let mut map = HashMap::new();
-    let mut iter = tt.clone().into_iter();
-
-    loop {
-        // Pull out the key; a None here means we've reached the end of input.
-        let key = match iter.next() {
-            None => break,
-            Some(ident @ TokenTree::Ident(_)) => ident,
-            Some(token) => {
-                abort!(token, "expected an identifier, but found `{}`", token)
-            }
-        };
-
-        // Verify we have an '=' delimiter.
-        let eq = match iter.next() {
-            Some(TokenTree::Punct(punct)) if punct.as_char() == '=' => punct,
-
-            Some(token) => abort!(token, "expected `=`, but found `{}`", token),
-            None => abort!(key, "expected `=` following `{}`", key),
-        };
-
-        let value = match iter.next() {
-            Some(TokenTree::Group(group)) => match group.delimiter() {
-                Delimiter::Parenthesis => {
-                    abort!(group, "parentheses not allowed")
-                }
-                Delimiter::Brace => parse_kv(&group.stream()),
-                Delimiter::Bracket => MapEntry::Array(to_vec(&group.stream())),
-                Delimiter::None => abort!(group, "invalid grouping"),
-            },
-            Some(TokenTree::Ident(ident)) => MapEntry::Value(ident.to_string()),
-            Some(TokenTree::Literal(lit)) => {
-                match syn::parse_str::<syn::ExprLit>(&lit.to_string()) {
-                    Ok(syn::ExprLit {
-                        lit: syn::Lit::Str(s), ..
-                    }) => MapEntry::Value(s.value()),
-                    _ => abort!(lit, "expected a value, but found `{}`", lit),
-                }
-            }
-
-            Some(token) => {
-                abort!(token, "expected a value, but found `{}`", token)
-            }
-            None => abort!(eq, "expected a value following `{}`", eq),
-        };
-
-        map.insert(key.to_string(), value);
-
-        let comma = iter.next();
-        match comma {
-            None => break,
-            Some(TokenTree::Punct(punct)) if punct.as_char() == ',' => continue,
-            Some(token) => abort!(token, "expected `,`, but found `{}`", token),
-        }
-    }
-
-    map
-}
-
-fn is_object(group: &proc_macro2::Group) -> bool {
-    match group.delimiter() {
-        Delimiter::Brace => true,
-        _ => false,
-    }
-}
-
-/// Process a TokenStream into an vector of MapEntry. We expect `tt` to match
-///   ({...},)* {...}?
-fn to_vec(tt: &TokenStream) -> Vec<MapEntry> {
-    let mut vec = Vec::new();
-
-    let mut iter = tt.clone().into_iter();
-    loop {
-        let item = match iter.next() {
-            None => break,
-            Some(TokenTree::Group(group)) if is_object(&group) => {
-                parse_kv(&group.stream())
-            }
-            Some(token) => abort!(
-                token,
-                "expected an object {{...}}, but found `{}`",
-                token
-            ),
-        };
-
-        vec.push(item);
-
-        // Look for a comma; terminate the loop if none is found.
-        match iter.next() {
-            None => break,
-            Some(TokenTree::Punct(punct)) if punct.as_char() == ',' => continue,
-            Some(token) => abort!(token, "expected `,`, but found `{}`", token),
-        }
-    }
-
-    vec
 }
 
 #[cfg(test)]
@@ -506,4 +449,18 @@ mod tests {
             .into(),
         );
     }
+    /*
+    #[test]
+    fn bad_bad_bad() {
+        use super::MapEntry;
+        use std::collections::HashMap;
+        let _: super::Result<HashMap<String, MapEntry>> =
+            super::serde_tokenstream::from_tokenstream(
+                &quote! {
+                    array = [{}<-]
+                }
+                .into(),
+            );
+    }
+    */
 }
