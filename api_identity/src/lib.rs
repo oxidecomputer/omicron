95442e7f73494b28a04d870f6b40ba098c481a03
David Pacheco <dap@oxidecomputer.com>
2020-08-21 15:16:33 -0700

    test for api_identity proc macro

--- api_identity/src/lib.rs
+++ api_identity/src/lib.rs
@@ -36,4 +36,28 @@ fn do_api_identity(item: TokenStream) -> Result<TokenStream, syn::Error> {
     Ok(stream)
 }
 
-/* TODO-coverage tests */
+#[cfg(test)]
+mod test {
+    use super::do_api_identity;
+    use quote::quote;
+
+    #[test]
+    fn test_identity() {
+        let ret = do_api_identity(
+            quote! {
+                struct Foo { identity: ApiIdentityMetadata }
+            }
+            .into(),
+        );
+
+        let expected = quote! {
+            impl ApiObjectIdentity for Foo {
+                fn identity(&self) -> &ApiIdentityMetadata {
+                    &self.identity
+                }
+            }
+        };
+
+        assert_eq!(expected.to_string(), ret.unwrap().to_string());
+    }
+}
