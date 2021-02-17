4a3dcef7a01c0e187a45f4d1b57397dd31056d59
Sean Klein <sean@oxide.computer>
2021-02-17 13:19:20 -0500

    Fix tests

--- tests/test_instances.rs
+++ tests/test_instances.rs
@@ -13,7 +13,6 @@ use oxide_api_prototype::api_model::ApiInstanceView;
 use oxide_api_prototype::api_model::ApiName;
 use oxide_api_prototype::api_model::ApiProjectCreateParams;
 use oxide_api_prototype::api_model::ApiProjectView;
-use oxide_api_prototype::ControllerServerContext;
 use oxide_api_prototype::OxideController;
 use oxide_api_prototype::OxideControllerTestInterfaces;
 use oxide_api_prototype::SledAgentTestInterfaces;
@@ -38,7 +37,7 @@ extern crate slog;
 async fn test_instances() {
     let cptestctx = test_setup("test_instances").await;
     let client = &cptestctx.external_client;
-    let apictx = ControllerServerContext::from_server(&cptestctx.server);
+    let apictx = &cptestctx.server.apictx;
     let controller = &apictx.controller;
 
     /* Create a project that we'll use for testing. */
