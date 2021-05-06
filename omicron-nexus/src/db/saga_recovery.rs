00c301484900ea6de3009d4b072a7bef648c7470
David Pacheco <dap@oxidecomputer.com>
2021-05-06 16:11:25 -0700

    remove unnecessary desugaring

--- omicron-nexus/src/db/saga_recovery.rs
+++ omicron-nexus/src/db/saga_recovery.rs
@@ -1,6 +1,5 @@
 /*!
  * Handles recovery of sagas
- * XXX The updating of saga rows needs to happen too, especially when done.
  */
 
 use crate::db;
@@ -10,7 +9,6 @@ use crate::db::sql_operations::sql_paginate;
 use omicron_common::error::ApiError;
 use std::collections::BTreeMap;
 use std::fmt;
-use std::future::Future;
 use std::sync::Arc;
 use steno::SagaTemplateGeneric;
 
@@ -54,15 +52,10 @@ where
 }
 
 /* XXX TODO-doc */
-/* This is regrettably desugared due to rust-lang/rust#63033. */
-fn list_sagas<'a, 'b, 'c>(
-    pool: &'a db::Pool,
-    sec_id: &'b db::SecId,
-) -> impl Future<Output = Result<Vec<db::saga_types::Saga>, ApiError>> + 'c
-where
-    'a: 'c,
-    'b: 'c,
-{
+async fn list_sagas(
+    pool: &db::Pool,
+    sec_id: &db::SecId,
+) -> Result<Vec<db::saga_types::Saga>, ApiError> {
     /*
      * For now, we do the simplest thing: we fetch all the sagas that the
      * caller's going to need before returning any of them.  This is easier to
@@ -74,22 +67,20 @@ where
      * consumers.  Anyway, having Nexus come up is really only blocked on this
      * step, not recovering all the individual sagas.
      */
-    async move {
-        let client = pool.acquire().await?;
+    let client = pool.acquire().await?;
 
-        let mut sql = db::sql::SqlString::new();
-        let saga_state_done = steno::SagaCachedState::Done.to_string();
-        let p1 = sql.next_param(&saga_state_done);
-        let p2 = sql.next_param(sec_id);
-        sql.push_str(&format!("saga_state != {} AND current_sec = {}", p1, p2));
+    let mut sql = db::sql::SqlString::new();
+    let saga_state_done = steno::SagaCachedState::Done.to_string();
+    let p1 = sql.next_param(&saga_state_done);
+    let p2 = sql.next_param(sec_id);
+    sql.push_str(&format!("saga_state != {} AND current_sec = {}", p1, p2));
 
-        sql_paginate::<
-            schema::LookupGenericByUniqueId,
-            schema::Saga,
-            <schema::Saga as Table>::ModelType,
-        >(&client, (), schema::Saga::ALL_COLUMNS, sql)
-        .await
-    }
+    sql_paginate::<
+        schema::LookupGenericByUniqueId,
+        schema::Saga,
+        <schema::Saga as Table>::ModelType,
+    >(&client, (), schema::Saga::ALL_COLUMNS, sql)
+    .await
 }
 
 async fn recover_saga<T>(
@@ -148,35 +139,28 @@ where
     Ok(())
 }
 
-/* This is regrettably desugared due to rust-lang/rust#63033. */
-pub fn load_saga_log<'a, 'b, 'c>(
-    pool: &'a db::Pool,
-    saga: &'b db::saga_types::Saga,
-) -> impl Future<Output = Result<Vec<steno::SagaNodeEvent>, ApiError>> + 'c
-where
-    'a: 'c,
-    'b: 'c,
-{
-    async move {
-        let client = pool.acquire().await?;
-        let mut extra_sql = db::sql::SqlString::new();
-        extra_sql.push_str("TRUE");
+pub async fn load_saga_log(
+    pool: &db::Pool,
+    saga: &db::saga_types::Saga,
+) -> Result<Vec<steno::SagaNodeEvent>, ApiError> {
+    let client = pool.acquire().await?;
+    let mut extra_sql = db::sql::SqlString::new();
+    extra_sql.push_str("TRUE");
 
-        let log_records = sql_paginate::<
-            schema::LookupSagaNodeEvent,
-            schema::SagaNodeEvent,
-            <schema::SagaNodeEvent as Table>::ModelType,
-        >(
-            &client,
-            (&saga.id.0,),
-            schema::SagaNodeEvent::ALL_COLUMNS,
-            extra_sql,
-        )
-        .await?;
+    let log_records = sql_paginate::<
+        schema::LookupSagaNodeEvent,
+        schema::SagaNodeEvent,
+        <schema::SagaNodeEvent as Table>::ModelType,
+    >(
+        &client,
+        (&saga.id.0,),
+        schema::SagaNodeEvent::ALL_COLUMNS,
+        extra_sql,
+    )
+    .await?;
 
-        Ok(log_records
-            .iter()
-            .map(steno::SagaNodeEvent::from)
-            .collect::<Vec<steno::SagaNodeEvent>>())
-    }
+    Ok(log_records
+        .iter()
+        .map(steno::SagaNodeEvent::from)
+        .collect::<Vec<steno::SagaNodeEvent>>())
 }
