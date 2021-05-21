09c9a4972462947838a1509d580706ac341c45ac
Sean Klein <sean@oxide.computer>
2021-05-21 09:05:24 -0400

    [wip] Skeleton of instance ensure, not working yet tho

--- omicron-sled-agent/src/sled_agent.rs
+++ omicron-sled-agent/src/sled_agent.rs
@@ -1,5 +1,6 @@
 /// Sled agent implementation
 
+use futures::lock::Mutex;
 use omicron_common::error::ApiError;
 use omicron_common::model::ApiDiskRuntimeState;
 use omicron_common::model::ApiDiskStateRequested;
@@ -7,13 +8,46 @@ use omicron_common::model::ApiInstanceRuntimeState;
 use omicron_common::model::ApiInstanceRuntimeStateRequested;
 use omicron_common::NexusClient;
 use slog::Logger;
+use std::collections::BTreeMap;
 use std::sync::Arc;
 use uuid::Uuid;
+
+use crate::common::instance::{InstanceState, Action as InstanceAction};
+
+// TODO: Could easily refactor this elsewhere...
+struct Instance {
+    id: Uuid,
+    state: InstanceState,
+}
+
+impl Instance {
+    fn new(id: Uuid, initial_runtime: ApiInstanceRuntimeState) -> Result<Self, ApiError> {
+        // TODO: Probably needs more config?
+        // TODO: Launch in a Zone?
+
+        // Launch a new SMF service running Propolis.
+        smf::Config::add("svc:/system/illumos/propolis-server")
+            .run(id.to_string())
+            .map_err(|e|
+                ApiError::InternalError {
+                    message: format!("Cannot create propolis service: {}", e)
+                }
+            )?;
+
+        // TODO: IP ADDRESS????
+
+        Ok(Instance {
+            id,
+            state: InstanceState::new(initial_runtime),
+        })
+    }
+}
 
 pub struct SledAgent {
     id: Uuid,
     log: Logger,
     nexus_client: Arc<NexusClient>,
+    instances: Mutex<BTreeMap<Uuid, Instance>>,
 }
 
 impl SledAgent {
@@ -24,10 +58,13 @@ impl SledAgent {
     ) -> SledAgent {
         info!(&log, "created sled agent"; "id" => ?id);
 
+        // TODO: Connection to propolis? Should this be done lazily?
+
         SledAgent {
             id: *id,
             log,
-            nexus_client
+            nexus_client,
+            instances: Mutex::new(BTreeMap::new()),
         }
     }
 
@@ -40,7 +77,31 @@ impl SledAgent {
         initial_runtime: ApiInstanceRuntimeState,
         target: ApiInstanceRuntimeStateRequested,
     ) -> Result<ApiInstanceRuntimeState, ApiError> {
-        todo!();
+        info!(&self.log, "instance_ensure {} -> {:#?}", instance_id, target);
+        let mut instances = self.instances.lock().await;
+
+        let instance = {
+            if let Some(instance) = instances.get_mut(&instance_id) {
+                // Instance already exists.
+                info!(&self.log, "instance already exists");
+                instance
+            } else {
+                // Instance does not exist - create it.
+                info!(&self.log, "new instance");
+                instances.insert(instance_id, Instance::new(instance_id, initial_runtime)?);
+                instances.get_mut(&instance_id).unwrap()
+            }
+        };
+
+        if let Some(action) = instance.state.request_transition(&target)? {
+            match action {
+                InstanceAction::Run => todo!(),
+                InstanceAction::Stop => todo!(),
+                InstanceAction::Reboot => todo!(),
+                InstanceAction::Destroy => todo!(),
+            }
+        }
+        Ok(instance.state.current().clone())
     }
 
     /// Idempotently ensures that the given API Disk (described by `api_disk`)
@@ -51,6 +112,6 @@ impl SledAgent {
         initial_state: ApiDiskRuntimeState,
         target: ApiDiskStateRequested,
     ) -> Result<ApiDiskRuntimeState, ApiError> {
-        todo!();
+        todo!("Disk attachment not yet implemented");
     }
 }
