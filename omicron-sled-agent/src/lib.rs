8bf730fe9dde2c5e43f6dc5d07794781869b1d0f
Sean Klein <sean@oxide.computer>
2021-07-19 09:19:05 -0400

    [sled-agent] Skeleton HTTP server (#150)

--- omicron-sled-agent/src/lib.rs
+++ omicron-sled-agent/src/lib.rs
@@ -1,17 +1,22 @@
-/*!
-* Library interface to the sled agent
- */
+//! Library interface to the sled agent
 
-/*
- * We only use rustdoc for internal documentation, including private items, so
- * it's expected that we'll have links to private items in the docs.
- */
+// We only use rustdoc for internal documentation, including private items, so
+// it's expected that we'll have links to private items in the docs.
 #![allow(private_intra_doc_links)]
-/* Clippy's style lints are useful, but not worth running automatically. */
+// Clippy's style lints are useful, but not worth running automatically.
 #![allow(clippy::style)]
 
-pub mod common;
+// Module for executing the simulated sled agent.
 pub mod sim;
+
+// Modules shared by both simulated and non-simulated sled agents.
+pub mod common;
+
+// Modules for the non-simulated sled agent.
+pub mod config;
+mod http_entrypoints;
+pub mod server;
+mod sled_agent;
 
 #[macro_use]
 extern crate slog;
