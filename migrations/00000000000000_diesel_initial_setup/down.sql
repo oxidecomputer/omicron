3e85aa38094e9f7455988f8cd07f55839ac6be3c
David Crespo <crespo.dm@gmail.com>
2021-08-07 14:32:32 -0500

    diesel successfully generates a schema.rs

---
+++ migrations/00000000000000_diesel_initial_setup/down.sql
@@ -0,0 +1,5 @@
+-- This file was automatically created by Diesel to setup helper functions
+-- and other internal bookkeeping. This file is safe to edit, any future
+-- changes will be added to existing projects as new migrations.
+
+SELECT 1;
