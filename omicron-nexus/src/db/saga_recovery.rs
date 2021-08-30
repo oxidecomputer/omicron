ed1f0504aa17af7d6584ec72865099adf58e779a
Sean Klein <sean@oxide.computer>
2021-08-30 15:46:33 -0400

    ... actually fix clippy lints

--- omicron-nexus/src/db/saga_recovery.rs
+++ omicron-nexus/src/db/saga_recovery.rs
@@ -145,7 +145,7 @@ async fn list_unfinished_sagas(
             Error::from_diesel(
                 e,
                 ResourceType::SagaDbg,
-                LookupType::ById(sec_id.0.into()),
+                LookupType::ById(sec_id.0),
             )
         })
 }
