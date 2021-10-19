7b26981a667d57e0e770b380955b842675949249
Tess Eisenberger <tess@oxidecomputer.com>
2021-10-19 16:42:05 -0700

    add new test

--- nexus/tests/test_projects.rs
+++ nexus/tests/test_projects.rs
@@ -20,7 +20,8 @@ async fn test_projects() {
     /* Create a project that we'll use for testing. */
     let p1_name = "springfield-squidport";
     let p2_name = "cairo-airport";
-    create_project(&client, &org_name, &p1_name).await;
+    let org_p1_id =
+        create_project(&client, &org_name, &p1_name).await.identity.id;
     create_project(&client, &org_name, &p2_name).await;
 
     let p1_url = format!("/organizations/{}/projects/{}", org_name, p1_name);
@@ -41,6 +42,24 @@ async fn test_projects() {
     // alphabetical order for now
     assert_eq!(projects[0].identity.name, p2_name);
     assert_eq!(projects[1].identity.name, p1_name);
+
+    /* Create a second organization and make sure we can have two projects with
+     * the same name across organizations */
+    let org2_name = "test-org2";
+    create_organization(&client, &org2_name).await;
+    let org2_p1_id =
+        create_project(&client, &org2_name, &p1_name).await.identity.id;
+    assert_ne!(org_p1_id, org2_p1_id);
+
+    // Make sure the list projects results for the new org make sense
+    let projects = objects_list_page::<Project>(
+        client,
+        &format!("/organizations/{}/projects", org2_name),
+    )
+    .await
+    .items;
+    assert_eq!(projects.len(), 1);
+    assert_eq!(projects[0].identity.name, p1_name);
 
     cptestctx.teardown().await;
 }
