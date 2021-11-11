b5db3d13755e287c441bf8374d56120eca81ce00
Tess Eisenberger <tess@oxidecomputer.com>
2021-11-11 12:30:54 -0800

    Update comment

--- nexus/src/db/model.rs
+++ nexus/src/db/model.rs
@@ -144,8 +144,9 @@ where
     }
 }
 
-/// Representation of an IP port in the database
-/// This handles converting from the database's i32 to the actual u16.
+/// Representation of a [u16] in the database.
+/// We need this because the database does not support unsigned types.
+/// This handles converting from the database's INT4 to the actual u16.
 #[derive(
     Copy, Clone, Debug, Eq, Ord, PartialEq, PartialOrd, AsExpression, FromSqlRow,
 )]
