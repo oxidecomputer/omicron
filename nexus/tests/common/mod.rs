edc8f62de6d8cd1a8c7dcdfcacb8e00d3b47afe1
David Pacheco <dap@oxidecomputer.com>
2021-11-18 18:19:01 -0800

    fix style

--- nexus/tests/common/mod.rs
+++ nexus/tests/common/mod.rs
@@ -19,8 +19,8 @@ use std::path::Path;
 use std::time::Duration;
 use uuid::Uuid;
 
-pub mod resource_helpers;
 pub mod http_testing;
+pub mod resource_helpers;
 
 const SLED_AGENT_UUID: &str = "b6d65341-167c-41df-9b5c-41cded99c229";
 const RACK_UUID: &str = "c19a698f-c6f9-4a17-ae30-20d711b8f7dc";
