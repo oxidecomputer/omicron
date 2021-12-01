6f87c143e05b47ecad3b975d3871020c3860221e
Tess Eisenberger <tess@oxidecomputer.com>
2021-11-30 18:28:53 -0800

    Add header to subnet_allocation.rs

--- nexus/src/db/subnet_allocation.rs
+++ nexus/src/db/subnet_allocation.rs
@@ -1,3 +1,9 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+//! Diesel queries used for subnet allocation
+
 use crate::db;
 use crate::db::identity::Resource;
 use crate::db::model::IncompleteNetworkInterface;
