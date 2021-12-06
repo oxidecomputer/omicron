974a9ec156e113525faeaca37d13a9220b23df0b
Andrew J. Stone <andrew@oxidecomputer.com>
2021-12-06 17:28:04 -0500

    Add missing MPL headers

--- sled-agent/src/bootstrap/spdm/error.rs
+++ sled-agent/src/bootstrap/spdm/error.rs
@@ -1,3 +1,7 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 //! Wrap errors returned from the `spdm` crate and std::io::Error.
 
 use spdm::{requester::RequesterError, responder::ResponderError};
--- sled-agent/src/bootstrap/trust_quorum/mod.rs
+++ sled-agent/src/bootstrap/trust_quorum/mod.rs
@@ -1,3 +1,7 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 //! The entry point for the trust quorum code
 //!
 //! The Trust quorum relies on IPv6 multicast discovery, rack secret handling,
