941f696a6c0c2e3a18fc408885eb1839eeec9e9c
Benjamin Naecker <ben@oxide.computer>
2021-12-11 21:21:22 +0000

    Update deps, make scripts MT safe

--- tools/dtrace/aggregate-query-latency.d
+++ tools/dtrace/aggregate-query-latency.d
@@ -1,23 +1,20 @@
-#!/usr/sbin/dtrace -qZs
+#!/usr/sbin/dtrace -qs
 
 /* Aggregate database query latency by connection, printing every 10 seconds. */
-uint64_t total_queries;
 
 dtrace:::BEGIN
 {
     printf("Tracing database query latency by connection ID for nexus PID %d, use Ctrl-C to exit\n", $target);
-    total_queries = 0;
 }
 
-diesel_db$target:::query_start
+diesel-db$target:::query-start
 {
-    total_queries++;
+    @total_queries = count();
     this->conn_id = json(copyinstr(arg1), "ok");
     self->ts[this->conn_id] = timestamp;
 }
 
-
-diesel_db$target:::query_end
+diesel-db$target:::query-done
 /self->ts[json(copyinstr(arg1), "ok")] != 0/
 {
     this->conn_id = json(copyinstr(arg1), "ok");
@@ -28,7 +25,7 @@ diesel_db$target:::query_end
 tick-10s
 {
     printf("%Y\n", walltimestamp);
-    printf("%d total queries\n", total_queries);
+    printa("%@d total queries\n", @total_queries);
     printa(@latency);
     printf("\n");
 }
--- tools/dtrace/slowest-queries.d
+++ tools/dtrace/slowest-queries.d
@@ -1,53 +1,41 @@
-#!/usr/sbin/dtrace -qZs
+#!/usr/sbin/dtrace -qs
 
 #pragma strsize=4k
 
-/* Print out the slowest query every 10 seconds. */
-
-struct query_info {
-    string conn_id;
-    string query;
-    uint64_t latency;
-};
-
-struct query_info slowest_query;
-self struct query_info queries[string];
+/* Print out the slowest 5 queries every 10 seconds. */
 
 dtrace:::BEGIN
 {
     printf("Tracing slowest queries for nexus PID %d, use Ctrl-C to exit\n", $target);
 }
 
-diesel_db$target:::query_start
+diesel-db$target:::query-start
 {
     this->conn_id = json(copyinstr(arg1), "ok");
-    self->queries[this->conn_id].conn_id = json(copyinstr(arg1), "ok");
-    self->queries[this->conn_id].query = copyinstr(arg2);
-    self->queries[this->conn_id].latency = timestamp;
+    ts[this->conn_id] = timestamp;
+    query[this->conn_id] = copyinstr(arg2);
 }
 
 
-diesel_db$target:::query_end
-/self->queries[json(copyinstr(arg1), "ok")].latency != 0/
+diesel-db$target:::query-done
 {
     this->conn_id = json(copyinstr(arg1), "ok");
-    this->latency = timestamp - self->queries[this->conn_id].latency;
-    if (this->latency > slowest_query.latency) {
-        slowest_query.conn_id = self->queries[this->conn_id].conn_id;
-        slowest_query.query = self->queries[this->conn_id].query;
-        slowest_query.latency = this->latency;
-    }
 }
 
-tick-10s
+diesel-db$target:::query-done
+/ts[this->conn_id]/
 {
-    printf("%Y\n", walltimestamp);
-    if (strlen(slowest_query.query) > 0) {
-        printf("latency: %lu us, conn_id: %s, query: '%s'\n", slowest_query.latency / 1000, slowest_query.conn_id, slowest_query.query);
-        slowest_query.query = "";
-        slowest_query.conn_id = "";
-        slowest_query.latency = 0;
-    } else {
-        printf("No new queries\n");
-    }
+    this->latency = timestamp - ts[this->conn_id];
+    @[this->conn_id, query[this->conn_id]] = max(this->latency);
+    ts[this->conn_id] = 0;
+    query[this->conn_id] = NULL;
+}
+
+tick-5s
+{
+    printf("\n%Y\n", walltimestamp);
+    trunc(@, 5);
+    normalize(@, 1000);
+    printa("latency: %@ld us, conn_id: %s, query: '%s'\n", @);
+    trunc(@);
 }
