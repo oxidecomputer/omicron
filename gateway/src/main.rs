b5835825180dcc8ebb52346afede8e15386c833d
Adam H. Leventhal <ahl@oxide.computer>
2022-02-08 10:43:43 -0800

    gateway API full draft

--- gateway/src/main.rs
+++ gateway/src/main.rs
@@ -2,10 +2,10 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
+// Copyright 2022 Oxide Computer Company
+
 mod http_entrypoints;
 
 pub struct GatewayService;
 
-fn main() {
-    println!("Hello, world!");
-}
+fn main() {}
