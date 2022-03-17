f6df2ed7b4733add5f02e79565e9feddbacaf6b1
David Pacheco <dap@oxidecomputer.com>
2022-03-17 16:56:43 -0700

    fix unused import

--- nexus/tests/integration_tests/unauthorized.rs
+++ nexus/tests/integration_tests/unauthorized.rs
@@ -20,7 +20,6 @@ use nexus_test_utils::resource_helpers::DiskTest;
 use nexus_test_utils::ControlPlaneTestContext;
 use nexus_test_utils_macros::nexus_test;
 use omicron_nexus::authn::external::spoof;
-use omicron_nexus::external_api::params;
 
 // This test hits a list Nexus API endpoints using both unauthenticated and
 // unauthorized requests to make sure we get the expected behavior (generally:
