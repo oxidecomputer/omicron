ee51a168b6f61128adcf214296e8e5e95e5de2b6
David Pacheco <dap@oxidecomputer.com>
2022-03-18 08:28:01 -0700

    update comment

--- nexus/tests/integration_tests/unauthorized_coverage.rs
+++ nexus/tests/integration_tests/unauthorized_coverage.rs
@@ -105,8 +105,9 @@ fn test_unauthorized_coverage() {
     println!("-----");
 
     // If you're here because this assertion failed, we found an endpoint tested
-    // by "unauthorized.rs" that's not in the OpenAPI spec.  It's not clear why
-    // this would ever happen.
+    // by "unauthorized.rs" that's not in the OpenAPI spec.  This could happen
+    // if you're adding an endpoint that's marked "hidden".  In that case, you
+    // might just allow expectorate to add it to the allowlist here.
     assert_contents(
         "tests/output/unexpected-authz-endpoints.txt",
         &unexpected_endpoints,
