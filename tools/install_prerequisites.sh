0e5d9846f1f2e29d3d5e60c6c3551ea5a2c7bb35
Sean Klein <sean@oxide.computer>
2022-04-02 22:28:12 -0400

    Merge update and install

--- tools/install_prerequisites.sh
+++ tools/install_prerequisites.sh
@@ -8,35 +8,34 @@ cd "${SOURCE_DIR}/.."
 
 HOST_OS=$(uname -s)
 if [[ "${HOST_OS}" == "Linux" ]]; then
-  echo "Linux"
   sudo apt-get install libpq-dev
   sudo apt-get install pkg-config
 elif [[ "${HOST_OS}" == "SunOS" ]]; then
-  echo "illumos"
   need=(
+    'pkg:/package/pkg'
     'build-essential'
     'library/postgresql-13'
     'pkg-config'
     'brand/omicron1/tools'
-    'pkg:/package/pkg'
   )
-  missing=()
-  for (( i = 0; i < ${#need[@]}; i++ )); do
-    p=${need[$i]}
-    if ! pkg info -q "$p"; then
-      missing+=( "$p" )
+
+  # Perform updates
+  if (( ${#need[@]} > 0 )); then
+    pfexec pkg install -v "${need[@]}" && rc=$? || rc=$?
+    # Return codes:
+    #  0: Normal Success
+    #  4: Failure because we're already up-to-date. Also acceptable.
+    if [ "$rc" -ne 4 ] && [ "$rc" -ne 0 ]; then
+      exit "$rc"
     fi
-  done
-  if (( ${#missing[@]} > 0 )); then
-    pfexec pkg install -v "${missing[@]}"
   fi
+
   pkg list -v "${need[@]}"
 elif [[ "${HOST_OS}" == "Darwin" ]]; then
-  echo "Mac"
   brew install postgresql
   brew install pkg-config
 else
-  echo "Unsupported OS"
+  echo "Unsupported OS: ${HOST_OS}"
   exit -1
 fi
 
