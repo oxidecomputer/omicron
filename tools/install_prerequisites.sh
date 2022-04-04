8d7aa809055605b22926e9dd85a855cbaed84cf9
Sean Klein <sean@oxide.computer>
2022-04-04 18:37:55 -0400

    Add confirmation prompt

--- tools/install_prerequisites.sh
+++ tools/install_prerequisites.sh
@@ -2,20 +2,41 @@
 
 set -eu
 
-on_exit () {
-    echo "Something went wrong, but this script is idempotent - If you can fix the issue, try re-running"
-}
-
-trap on_exit ERR
-
 # Set the CWD to Omicron's source.
 SOURCE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 cd "${SOURCE_DIR}/.."
 
+function on_exit
+{
+  echo "Something went wrong, but this script is idempotent - If you can fix the issue, try re-running"
+}
+
+trap on_exit ERR
+
+# Offers a confirmation prompt.
+#
+# Args:
+#  $1: Text to be displayed
+function confirm
+{
+  read -r -p "$1 (y/n): " response
+  case $response in
+    [yY])
+      true
+      ;;
+    *)
+      false
+      ;;
+  esac
+}
+
 HOST_OS=$(uname -s)
 if [[ "${HOST_OS}" == "Linux" ]]; then
-  sudo apt-get install libpq-dev
-  sudo apt-get install pkg-config
+  packages=(
+    'libpq-dev'
+    'pkg-config'
+  )
+  confirm "Install (or update) [${packages[*]}]?" && sudo apt-get install ${packages[@]}
 elif [[ "${HOST_OS}" == "SunOS" ]]; then
   packages=(
     'pkg:/package/pkg'
@@ -29,7 +50,7 @@ elif [[ "${HOST_OS}" == "SunOS" ]]; then
   # Explicitly manage the return code using "rc" to observe the result of this
   # command without exiting the script entirely (due to bash's "errexit").
   rc=0
-  pfexec pkg install -v "${packages[@]}" || rc=$?
+  confirm "Install (or update) [${packages[*]}]?" && { pfexec pkg install -v "${packages[@]}" || rc=$?; }
   # Return codes:
   #  0: Normal Success
   #  4: Failure because we're already up-to-date. Also acceptable.
@@ -39,8 +60,11 @@ elif [[ "${HOST_OS}" == "SunOS" ]]; then
 
   pkg list -v "${packages[@]}"
 elif [[ "${HOST_OS}" == "Darwin" ]]; then
-  brew install postgresql
-  brew install pkg-config
+  packages=(
+    'postgresql'
+    'pkg-config'
+  )
+  confirm "Install (or update) [${packages[*]}]?" && brew install ${packages[@]}
 else
   echo "Unsupported OS: ${HOST_OS}"
   exit -1
