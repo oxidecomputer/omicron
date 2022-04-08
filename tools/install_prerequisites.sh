9c9e913a9fef92ec2533a7c25e67b609f5deb3e3
Sean Klein <sean@oxide.computer>
2022-04-08 11:12:20 -0400

    Automate PATH verification in `install_prerequisites.sh`, simplify README (#858)
    Automating as much of the README as possible within the installation script to simplify the onboarding process.

--- tools/install_prerequisites.sh
+++ tools/install_prerequisites.sh
@@ -30,6 +30,22 @@ function confirm
   esac
 }
 
+# Packages to be installed on all OSes:
+#
+# - libpq, the PostgreSQL client lib.
+# We use Diesel's PostgreSQL support to connect to CockroachDB (which is
+# wire-compatible with PostgreSQL). Diesel uses the native libpq to do this.
+# `pg_config` is a utility which may be used to query for the installed
+# PostgreSQL libraries, and is expected by the Omicron build to exist in
+# the developer's PATH variable.
+# - pkg-config, a tool for querying installed libraries.
+#
+# Packages to be installed on Helios only:
+#
+# - pkg, the IPS client (though likely it will just be updated)
+# - build-essential: Common development tools
+# - brand/omicron1/tools: Oxide's omicron1-brand Zone
+
 HOST_OS=$(uname -s)
 if [[ "${HOST_OS}" == "Linux" ]]; then
   packages=(
@@ -70,5 +86,66 @@ else
   exit -1
 fi
 
+# CockroachDB and Clickhouse are used by Omicron for storage of
+# control plane metadata and metrics.
+#
+# They are used in a couple of spots within Omicron:
+#
+# - Test Suite: The test suite, regardless of host OS, builds temporary
+# databases for testing, and expects `cockroach` and `clickhouse` to
+# exist as a part of the PATH.
+# - Packaging: When constructing packages on Helios, these utilities
+# are packaged into zones which may be launched by the sled agent.
+
 ./tools/ci_download_cockroachdb
 ./tools/ci_download_clickhouse
+
+# Validate the PATH:
+expected_in_path=(
+  'pg_config'
+  'pkg-config'
+  'cockroach'
+  'clickhouse'
+)
+
+function show_hint
+{
+  case "$1" in
+    "pg_config")
+      if [[ "${HOST_OS}" == "SunOS" ]]; then
+        echo "On illumos, $1 is typically found in '/opt/ooce/bin'"
+      fi
+      ;;
+    "pkg-config")
+      if [[ "${HOST_OS}" == "SunOS" ]]; then
+        echo "On illumos, $1 is typically found in '/usr/bin'"
+      fi
+      ;;
+    "cockroach")
+      echo "$1 should have been installed to '$PWD/out/cockroachdb/bin'"
+      ;;
+    "clickhouse")
+      echo "$1 should have been installed to '$PWD/out/clickhouse'"
+      ;;
+    *)
+      ;;
+  esac
+}
+
+# Check all paths before returning an error.
+ANY_PATH_ERROR="false"
+for command in "${expected_in_path[@]}"; do
+  rc=0
+  which "$command" &> /dev/null || rc=$?
+  if [[ "$rc" -ne 0 ]]; then
+    echo "ERROR: $command seems installed, but was not found in PATH. Please add it."
+    show_hint "$command"
+    ANY_PATH_ERROR="true"
+  fi
+done
+
+if [[ "$ANY_PATH_ERROR" == "true" ]]; then
+  exit -1
+fi
+
+echo "All prerequisites installed successfully, and PATH looks valid"
