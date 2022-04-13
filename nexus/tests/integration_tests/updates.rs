4dcca263ed7caba634d1d34eea39e31029f9661b
David Pacheco <dap@oxidecomputer.com>
2022-04-13 09:53:29 -0700

    fix style

--- nexus/tests/integration_tests/updates.rs
+++ nexus/tests/integration_tests/updates.rs
@@ -225,7 +225,11 @@ fn generate_targets() -> (TempDir, Vec<&'static str>) {
     let dir = TempDir::new().unwrap();
 
     // The update artifact. This will someday be a tarball of some variety.
-    std::fs::write(dir.path().join("omicron-test-component-1"), TARGET_CONTENTS).unwrap();
+    std::fs::write(
+        dir.path().join("omicron-test-component-1"),
+        TARGET_CONTENTS,
+    )
+    .unwrap();
 
     // artifacts.json, which describes all available artifacts.
     let artifacts = ArtifactsDocument {
