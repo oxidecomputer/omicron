a4ff6bd65af0765786d85917859c3dde4e0262a5
Sean Klein <sean@oxide.computer>
2022-05-05 22:23:21 -0400

    doc comment for consistency

--- nexus/src/app/session.rs
+++ nexus/src/app/session.rs
@@ -2,6 +2,8 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
+//! Console session management.
+
 use crate::authn;
 use crate::authz;
 use crate::context::OpContext;
