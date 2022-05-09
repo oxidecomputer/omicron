1f3b13fe9cb8b18257bd0eff938936404f6d2325
David Pacheco <dap@oxidecomputer.com>
2022-05-09 11:11:20 -0700

    we could still test some of this for Silos

--- nexus/tests/integration_tests/role_assignments.rs
+++ nexus/tests/integration_tests/role_assignments.rs
@@ -20,12 +20,20 @@ use omicron_nexus::db::identity::Resource;
 use omicron_nexus::external_api::shared;
 use omicron_nexus::external_api::views;
 
-// TODO-coverage TODO-security We need a test that role assignments work on
-// Silos.  This isn't currently possible because there's not currently anything
-// you can do with a Silo as an admin user that you can't also do with an
-// unprivileged user.  In particular: even unprivileged users can see their own
-// Silo.  The only other operation we have is delete, which shouldn't work
-// within the Silo anyway.
+#[nexus_test]
+async fn test_role_assignments_silo(cptestctx: &ControlPlaneTestContext) {
+    let client = &cptestctx.external_client;
+    let silo_name = fixed_data::silo::DEFAULT_SILO.identity().name.to_string();
+    let silo_url = format!("/silos/{}", silo_name);
+    do_test::<authz::SiloRoles, views::Silo>(
+        client,
+        &silo_url,
+        &silo_name,
+        authz::SiloRoles::Admin,
+        true,
+    )
+    .await;
+}
 
 #[nexus_test]
 async fn test_role_assignments_organization(
@@ -40,6 +48,7 @@ async fn test_role_assignments_organization(
         &org_url,
         org_name,
         authz::OrganizationRoles::Admin,
+        false,
     )
     .await;
 }
@@ -58,17 +67,28 @@ async fn test_role_assignments_project(cptestctx: &ControlPlaneTestContext) {
         &project_url,
         project_name,
         authz::ProjectRoles::Admin,
+        false,
     )
     .await;
 }
 
+/// Helper function for running a role assignment test on the given resource
 // TODO-coverage A more comprehensive test would be useful when we have proper
 // Silo users
+// TODO-coverage The "skip_checks" flag is needed only for Silos.  When
+// "skip_checks" is false, this function verifies that an unprivileged user
+// cannot see the given resource at the start, then creates a policy that grants
+// that user access, then checks that they _can_ see it.  Then it revokes access
+// and checks again that they can't.  For Silos, an unprivileged user can always
+// see their own Silo today so this doesn't work as expected.  Once we flesh out
+// Silos with more operations (e.g., a "modify" operation), we could check
+// enforcement of that instead.
 async fn do_test<T, V>(
     client: &dropshot::test_util::ClientTestContext,
     resource_url: &str,
     resource_name: &str,
     admin_role: T,
+    skip_checks: bool,
 ) where
     T: Clone
         + std::fmt::Debug
@@ -85,16 +105,18 @@ async fn do_test<T, V>(
     // primarily tested in the separate "unauthorized" test, but we do it here
     // as a control to make sure that the checks below pass for the right
     // reasons.
-    NexusRequest::expect_failure(
-        client,
-        StatusCode::NOT_FOUND,
-        Method::GET,
-        resource_url,
-    )
-    .authn_as(AuthnMode::UnprivilegedUser)
-    .execute()
-    .await
-    .unwrap();
+    if !skip_checks {
+        NexusRequest::expect_failure(
+            client,
+            StatusCode::NOT_FOUND,
+            Method::GET,
+            resource_url,
+        )
+        .authn_as(AuthnMode::UnprivilegedUser)
+        .execute()
+        .await
+        .unwrap();
+    }
 
     // Make a new policy granting the unprivileged user access to this resource.
     // This is a little ugly, but we don't have a way of creating silo users yet
@@ -153,14 +175,16 @@ async fn do_test<T, V>(
 
     // Now that user ought to be able to fetch the resource.  (This is not
     // really a policy test so we're not going to check all possible actions.)
-    let resource: V = NexusRequest::object_get(client, resource_url)
-        .authn_as(AuthnMode::UnprivilegedUser)
-        .execute()
-        .await
-        .unwrap()
-        .parsed_body()
-        .unwrap();
-    assert_eq!(resource.identity().name, resource_name);
+    if !skip_checks {
+        let resource: V = NexusRequest::object_get(client, resource_url)
+            .authn_as(AuthnMode::UnprivilegedUser)
+            .execute()
+            .await
+            .unwrap()
+            .parsed_body()
+            .unwrap();
+        assert_eq!(resource.identity().name, resource_name);
+    }
 
     // The way we've defined things, the so-called unprivileged user ought to be
     // able to revoke their own access.
@@ -182,16 +206,18 @@ async fn do_test<T, V>(
     assert!(initial_policy.role_assignments.is_empty());
 
     // Now check that we enforce the change!
-    NexusRequest::expect_failure(
-        client,
-        StatusCode::NOT_FOUND,
-        Method::GET,
-        resource_url,
-    )
-    .authn_as(AuthnMode::UnprivilegedUser)
-    .execute()
-    .await
-    .unwrap();
+    if !skip_checks {
+        NexusRequest::expect_failure(
+            client,
+            StatusCode::NOT_FOUND,
+            Method::GET,
+            resource_url,
+        )
+        .authn_as(AuthnMode::UnprivilegedUser)
+        .execute()
+        .await
+        .unwrap();
+    }
 }
 
 async fn policy_fetch<T: serde::de::DeserializeOwned>(
