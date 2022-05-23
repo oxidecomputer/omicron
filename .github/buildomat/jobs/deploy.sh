ad541ab6d5026d3cc54a2c335d8c6409c6967c17
Joshua M. Clulow <jmc@oxide.computer>
2022-05-22 19:55:59 -0700

    XXX the lab-netdev ramdisk includes /opt/oxide/opte/bin/opteadm now

--- .github/buildomat/jobs/deploy.sh
+++ .github/buildomat/jobs/deploy.sh
@@ -16,8 +16,26 @@ set -o errexit
 set -o pipefail
 set -o xtrace
 
+if [[ -d /opt/oxide ]]; then
+	#
+	# The netdev ramdisk environment contains OPTE, which presently means
+	# /opt/oxide already exists as part of the ramdisk.  We want to create
+	# a tmpfs at that location so that we can unfurl a raft of extra files,
+	# so move whatever is already there out of the way:
+	#
+	pfexec mv /opt/oxide /opt/oxide-underneath
+fi
 pfexec mkdir /opt/oxide
 pfexec mount -F tmpfs -O swap /opt/oxide
+if [[ -d /opt/oxide-underneath ]]; then
+	#
+	# Copy the original /opt/oxide tree into the new tmpfs:
+	#
+	(cd /opt/oxide-underneath && pfexec tar czeEp@/f - .) |
+	    (cd /opt/oxide && pfexec tar xvzeEp@/f -)
+	rm -rf /opt/oxide-underneath
+fi
+
 pfexec mkdir /opt/oxide/work
 pfexec chown build:build /opt/oxide/work
 cd /opt/oxide/work
