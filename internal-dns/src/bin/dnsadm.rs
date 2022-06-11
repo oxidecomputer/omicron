40ab474a0ebb50f45057a6ff4cf51a45927c537b
Luqman Aden <luqman@oxide.computer>
2022-06-10 21:19:25 -0700

    [internal-dns] structopt -> clap

--- internal-dns/src/bin/dnsadm.rs
+++ internal-dns/src/bin/dnsadm.rs
@@ -3,33 +3,28 @@
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
 use anyhow::Result;
+use clap::{Args, Parser, Subcommand};
 use internal_dns_client::{
     types::{DnsKv, DnsRecord, DnsRecordKey, Srv},
     Client,
 };
 use slog::{Drain, Logger};
 use std::net::Ipv6Addr;
-use structopt::{clap::AppSettings::*, StructOpt};
 
-#[derive(Debug, StructOpt)]
-#[structopt(
-    name = "dnsadm",
-    about = "Administer DNS records",
-    global_setting(ColorAuto),
-    global_setting(ColoredHelp)
-)]
+#[derive(Debug, Parser)]
+#[clap(name = "dnsadm", about = "Administer DNS records")]
 struct Opt {
-    #[structopt(short, long)]
+    #[clap(short, long)]
     address: Option<String>,
 
-    #[structopt(short, long)]
+    #[clap(short, long)]
     port: Option<usize>,
 
-    #[structopt(subcommand)]
+    #[clap(subcommand)]
     subcommand: SubCommand,
 }
 
-#[derive(Debug, StructOpt)]
+#[derive(Debug, Subcommand)]
 enum SubCommand {
     ListRecords,
     AddAAAA(AddAAAACommand),
@@ -37,13 +32,13 @@ enum SubCommand {
     DeleteRecord(DeleteRecordCommand),
 }
 
-#[derive(Debug, StructOpt)]
+#[derive(Debug, Args)]
 struct AddAAAACommand {
     name: String,
     addr: Ipv6Addr,
 }
 
-#[derive(Debug, StructOpt)]
+#[derive(Debug, Args)]
 struct AddSRVCommand {
     name: String,
     prio: u16,
@@ -52,14 +47,14 @@ struct AddSRVCommand {
     target: String,
 }
 
-#[derive(Debug, StructOpt)]
+#[derive(Debug, Args)]
 struct DeleteRecordCommand {
     name: String,
 }
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let opt = Opt::from_args();
+    let opt = Opt::parse();
     let log = init_logger();
 
     let addr = match opt.address {
@@ -71,7 +66,6 @@ async fn main() -> Result<()> {
     let endpoint = format!("http://{}:{}", addr, port);
     let client = Client::new(&endpoint, log.clone());
 
-    let opt = Opt::from_args();
     match opt.subcommand {
         SubCommand::ListRecords => {
             let records = client.dns_records_get().await?;
