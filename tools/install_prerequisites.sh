5a7503f9fb21fe14805cb6f222dcbf68874a410f
David Crespo <david.crespo@oxidecomputer.com>
2022-06-15 11:08:46 -0500

    just for fun, remove sudo from install prereqs script

--- tools/install_prerequisites.sh
+++ tools/install_prerequisites.sh
@@ -80,11 +80,11 @@ if [[ "${HOST_OS}" == "Linux" ]]; then
     'libclang-dev'
     'libsqlite3-dev'
   )
-  sudo apt-get update
+  apt-get update
   if [[ "${ASSUME_YES}" == "true" ]]; then
-    sudo apt-get install -y ${packages[@]}
+    apt-get install -y ${packages[@]}
   else
-      confirm "Install (or update) [${packages[*]}]?" && sudo apt-get install ${packages[@]}
+      confirm "Install (or update) [${packages[*]}]?" && apt-get install ${packages[@]}
   fi
 elif [[ "${HOST_OS}" == "SunOS" ]]; then
   packages=(
