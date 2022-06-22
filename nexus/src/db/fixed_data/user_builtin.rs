d6e3c9de0d23cfe10e59bee4c23e20ee0ae847be
Sean Klein <sean@oxide.computer>
2022-06-22 12:12:38 -0400

    background-work -> service-balancer

--- nexus/src/db/fixed_data/user_builtin.rs
+++ nexus/src/db/fixed_data/user_builtin.rs
@@ -39,13 +39,13 @@ lazy_static! {
             "used for seeding initial database data",
         );
 
-    /// Internal user for performing operations driven by Nexus, rather
-    /// than any API request.
-    pub static ref USER_BACKGROUND_WORK: UserBuiltinConfig =
+    /// Internal user for performing operations to manage the
+    /// provisioning of services across the fleet.
+    pub static ref USER_SERVICE_BALANCER: UserBuiltinConfig =
         UserBuiltinConfig::new_static(
             "001de000-05e4-4000-8000-00000000bac3",
-            "background-work",
-            "used for Nexus-driven database operations",
+            "service-balancer",
+            "used for Nexus-driven service balancing",
         );
 
     /// Internal user used by Nexus when handling internal API requests
@@ -86,16 +86,16 @@ lazy_static! {
 #[cfg(test)]
 mod test {
     use super::super::assert_valid_uuid;
-    use super::USER_BACKGROUND_WORK;
     use super::USER_DB_INIT;
     use super::USER_EXTERNAL_AUTHN;
     use super::USER_INTERNAL_API;
     use super::USER_INTERNAL_READ;
     use super::USER_SAGA_RECOVERY;
+    use super::USER_SERVICE_BALANCER;
 
     #[test]
     fn test_builtin_user_ids_are_valid() {
-        assert_valid_uuid(&USER_BACKGROUND_WORK.id);
+        assert_valid_uuid(&USER_SERVICE_BALANCER.id);
         assert_valid_uuid(&USER_DB_INIT.id);
         assert_valid_uuid(&USER_INTERNAL_API.id);
         assert_valid_uuid(&USER_EXTERNAL_AUTHN.id);
