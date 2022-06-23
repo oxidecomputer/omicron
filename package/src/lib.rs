24b8e8bbed20b53b61cfde550d0bbfc64ca0f992
John Gallagher <john@oxidecomputer.com>
2022-06-23 17:20:26 -0400

    Add maghemite ddm_admin_api client (#1250)

--- package/src/lib.rs
+++ package/src/lib.rs
@@ -1,10 +1,7 @@
 //! Common code shared between `omicron-package` and `thing-flinger` binaries.
 
 use clap::Subcommand;
-use omicron_zone_package::package::Package;
 use serde::de::DeserializeOwned;
-use serde_derive::Deserialize;
-use std::collections::BTreeMap;
 use std::path::Path;
 use std::path::PathBuf;
 use thiserror::Error;
@@ -77,39 +74,4 @@ pub enum DeployCommand {
         #[clap(long = "out", default_value = "/opt/oxide", action)]
         install_dir: PathBuf,
     },
-}
-
-/// Describes the origin of an externally-built package.
-#[derive(Deserialize, Debug)]
-#[serde(tag = "type", rename_all = "lowercase")]
-pub enum ExternalPackageSource {
-    /// Downloads the package from the following URL:
-    ///
-    /// <https://buildomat.eng.oxide.computer/public/file/oxidecomputer/REPO/image/COMMIT/PACKAGE>
-    Prebuilt { repo: String, commit: String, sha256: String },
-    /// Expects that a package will be manually built and placed into the output
-    /// directory.
-    Manual,
-}
-
-/// Describes a package which originates from outside this repo.
-#[derive(Deserialize, Debug)]
-pub struct ExternalPackage {
-    #[serde(flatten)]
-    pub package: Package,
-
-    pub source: ExternalPackageSource,
-}
-
-/// Describes the configuration for a set of packages.
-#[derive(Deserialize, Debug)]
-pub struct Config {
-    /// Packages to be built and installed.
-    #[serde(default, rename = "package")]
-    pub packages: BTreeMap<String, Package>,
-
-    /// Packages to be installed, but which have been created outside this
-    /// repository.
-    #[serde(default, rename = "external_package")]
-    pub external_packages: BTreeMap<String, ExternalPackage>,
 }
