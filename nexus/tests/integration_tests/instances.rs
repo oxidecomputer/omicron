9ec9b8773c92270a53a135c79df2d4ab868a0487
Luqman Aden <luqman@oxide.computer>
2022-08-30 09:37:47 -0700

    Add test for getting serial data of Provisioned Instance.

--- nexus/tests/integration_tests/instances.rs
+++ nexus/tests/integration_tests/instances.rs
@@ -2550,6 +2550,133 @@ async fn test_instance_serial(cptestctx: &ControlPlaneTestContext) {
 }
 
 #[nexus_test]
+async fn test_instance_serial_provisioned(cptestctx: &ControlPlaneTestContext) {
+    let client = &cptestctx.external_client;
+    let apictx = &cptestctx.server.apictx;
+    let nexus = &apictx.nexus;
+
+    // Create a project that we'll use for testing.
+    create_ip_pool(client, POOL_NAME, None, None).await;
+    create_organization(client, ORGANIZATION_NAME).await;
+    let url_instances = format!(
+        "/organizations/{}/projects/{}/instances",
+        ORGANIZATION_NAME, PROJECT_NAME
+    );
+    let _ = create_project(client, ORGANIZATION_NAME, PROJECT_NAME).await;
+
+    // Make sure we get a 404 if we try to access the serial console before creation.
+    let instance_serial_url =
+        format!("{}/kris-picks/serial-console?from_start=0", url_instances);
+    let error: HttpErrorResponseBody = NexusRequest::expect_failure(
+        client,
+        StatusCode::NOT_FOUND,
+        Method::GET,
+        &instance_serial_url,
+    )
+    .authn_as(AuthnMode::PrivilegedUser)
+    .execute()
+    .await
+    .unwrap()
+    .parsed_body()
+    .unwrap();
+    assert_eq!(error.message, "not found: instance with name \"kris-picks\"");
+
+    // Create an instance in a stopped state.
+    let instance: Instance = object_create(
+        client,
+        &url_instances,
+        &params::InstanceCreate {
+            identity: IdentityMetadataCreateParams {
+                name: "kris-picks".parse().unwrap(),
+                description: "instance kris-picks".to_string(),
+            },
+            ncpus: InstanceCpuCount(4),
+            memory: ByteCount::from_gibibytes_u32(1),
+            hostname: String::from("the_host"),
+            user_data: vec![],
+            network_interfaces:
+                params::InstanceNetworkInterfaceAttachment::Default,
+            external_ips: vec![],
+            disks: vec![],
+            only_create: true,
+        },
+    )
+    .await;
+    assert_eq!(instance.runtime.run_state, InstanceState::Stopped);
+
+    // Provision the instance and verify state change
+    let instance_url = format!("{}/kris-picks", url_instances);
+    let instance =
+        instance_post(&client, &instance_url, InstanceOp::Provision).await;
+    assert_eq!(instance.runtime.run_state, InstanceState::Provisioned);
+
+    // Request serial buffer and make sure it doesn't 404 now that the instance is provisioned.
+    let serial_data: params::InstanceSerialConsoleData =
+        NexusRequest::object_get(client, &instance_serial_url)
+            .authn_as(AuthnMode::PrivilegedUser)
+            .execute()
+            .await
+            .expect("failed to make request")
+            .parsed_body()
+            .unwrap();
+    assert_eq!(serial_data.data.len(), 0);
+
+    // Start the instance, simulate completion of instance boot and check the state reported.
+    let instance =
+        instance_post(&client, &instance_url, InstanceOp::Start).await;
+    instance_simulate(nexus, &instance.identity.id).await;
+    let instance_next = instance_get(&client, &instance_url).await;
+    identity_eq(&instance.identity, &instance_next.identity);
+    assert_eq!(instance_next.runtime.run_state, InstanceState::Running);
+    assert!(
+        instance_next.runtime.time_run_state_updated
+            > instance.runtime.time_run_state_updated
+    );
+
+    // Grab the serial buffer again after the instance has started.
+    let serial_data: params::InstanceSerialConsoleData =
+        NexusRequest::object_get(client, &instance_serial_url)
+            .authn_as(AuthnMode::PrivilegedUser)
+            .execute()
+            .await
+            .expect("failed to make request")
+            .parsed_body()
+            .unwrap();
+
+    // FIXME: this is not necessarily going to always be the sled-agent-sim!
+    //  when it's reasonable to boot arbitrary images, perhaps we simply use one
+    //  that outputs something predictable like this
+    let expected = "This is simulated serial console output for ".as_bytes();
+    assert_eq!(&serial_data.data[..expected.len()], expected);
+
+    // Request a halt and verify both the immediate state and the finished state.
+    let instance = instance_next;
+    let instance_next =
+        instance_post(&client, &instance_url, InstanceOp::Stop).await;
+    assert_eq!(instance_next.runtime.run_state, InstanceState::Stopping);
+    assert!(
+        instance_next.runtime.time_run_state_updated
+            > instance.runtime.time_run_state_updated
+    );
+
+    let instance = instance_next;
+    instance_simulate(nexus, &instance.identity.id).await;
+    let instance_next = instance_get(&client, &instance_url).await;
+    assert_eq!(instance_next.runtime.run_state, InstanceState::Stopped);
+    assert!(
+        instance_next.runtime.time_run_state_updated
+            > instance.runtime.time_run_state_updated
+    );
+
+    // Delete the instance.
+    NexusRequest::object_delete(client, &instance_url)
+        .authn_as(AuthnMode::PrivilegedUser)
+        .execute()
+        .await
+        .unwrap();
+}
+
+#[nexus_test]
 async fn test_instance_ephemeral_ip_from_correct_project(
     cptestctx: &ControlPlaneTestContext,
 ) {
--- sled-agent/src/sim/sled_agent.rs
+++ sled-agent/src/sim/sled_agent.rs
@@ -11,7 +11,7 @@ use crate::params::{
 };
 use crate::serial::ByteOffset;
 use futures::lock::Mutex;
-use omicron_common::api::external::{Error, ResourceType};
+use omicron_common::api::external::{Error, InstanceState, ResourceType};
 use omicron_common::api::internal::nexus::DiskRuntimeState;
 use omicron_common::api::internal::nexus::InstanceRuntimeState;
 use slog::Logger;
@@ -283,9 +283,17 @@ impl SledAgent {
             return Err(format!("No such instance {}", instance_id));
         }
 
-        // TODO: if instance state isn't running {
-        //  return Ok(InstanceSerialConsoleData { data: vec![], last_byte_offset: 0 });
-        // }
+        let current = self
+            .instances
+            .sim_get_current_state(&instance_id)
+            .await
+            .map_err(|e| format!("{}", e))?;
+        if current.run_state != InstanceState::Running {
+            return Ok(InstanceSerialConsoleData {
+                data: vec![],
+                last_byte_offset: 0,
+            });
+        }
 
         let gerunds = [
             "Loading",
