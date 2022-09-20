c78ea250148e7bced7e39e314139360e83c98cf6
Justin Bennett <git@just-be.dev>
2022-09-19 22:40:24 -0400

    Adjust some language after feedback from @davepacheco

--- nexus/src/external_api/http_entrypoints.rs
+++ nexus/src/external_api/http_entrypoints.rs
@@ -2220,9 +2220,9 @@ async fn instance_disk_detach(
 
 // Images
 
-/// List global images
+/// List system-wide images
 ///
-/// Returns a list of all the global images. Global images are returned sorted
+/// Returns a list of all the system-wide images. System-wide images are returned sorted
 /// by creation date, with the most recent images appearing first.
 #[endpoint {
     method = GET,
@@ -2257,9 +2257,9 @@ async fn system_image_list(
     apictx.external_latencies.instrument_dropshot_handler(&rqctx, handler).await
 }
 
-/// Create a global image
+/// Create a system-wide image
 ///
-/// Create a new global image. This image can then be used by any user as a
+/// Create a new system-wide image. This image can then be used by any user in any silo as a
 /// base for instances.
 #[endpoint {
     method = POST,
@@ -2287,9 +2287,9 @@ struct GlobalImagePathParam {
     image_name: Name,
 }
 
-/// Fetch a global image
+/// Fetch a system-wide image
 ///
-/// Returns the details of a specific global image.
+/// Returns the details of a specific system-wide image.
 #[endpoint {
     method = GET,
     path = "/system/images/{image_name}",
@@ -2311,7 +2311,7 @@ async fn system_image_view(
     apictx.external_latencies.instrument_dropshot_handler(&rqctx, handler).await
 }
 
-/// Fetch a global image by id
+/// Fetch a system-wide image by id
 #[endpoint {
     method = GET,
     path = "/system/by-id/images/{id}",
@@ -2333,10 +2333,10 @@ async fn system_image_view_by_id(
     apictx.external_latencies.instrument_dropshot_handler(&rqctx, handler).await
 }
 
-/// Delete a global image
+/// Delete a system-wide image
 ///
-/// Permanently delete a global image. This operation cannot be undone. Any
-/// instances using the global image will continue to run, however new instances
+/// Permanently delete a system-wide image. This operation cannot be undone. Any
+/// instances using the system-wide image will continue to run, however new instances
 /// can not be created with this image.
 #[endpoint {
     method = DELETE,
