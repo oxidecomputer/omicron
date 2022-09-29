ef4284d0a6e8e8adfe00a5f1935316d776a9ccad
Andrew J. Stone <andrew.j.stone.1@gmail.com>
2022-09-29 21:18:48 +0000

    Change how we keep track of component rects in prep for mouse events

--- wicket/src/widgets/rack.rs
+++ wicket/src/widgets/rack.rs
@@ -4,10 +4,12 @@
 
 //! A rendering of the Oxide rack
 
+use crate::inventory::ComponentId;
 use crate::screens::make_even;
 use crate::screens::Height;
 use crate::screens::RectState;
 use slog::Logger;
+use std::collections::BTreeMap;
 use tui::buffer::Buffer;
 use tui::layout::Rect;
 use tui::style::Style;
@@ -62,7 +64,7 @@ impl Rack {
         self
     }
 
-    pub fn draw_sled(&self, buf: &mut Buffer, sled: &RectState, i: usize) {
+    pub fn draw_sled(&self, buf: &mut Buffer, sled: &RectState, i: u8) {
         let mut block =
             Block::default().title(format!("sled {}", i)).borders(Borders::ALL);
         if sled.tabbed {
@@ -95,7 +97,7 @@ impl Rack {
         }
     }
 
-    pub fn draw_switch(&self, buf: &mut Buffer, switch: &RectState, i: usize) {
+    pub fn draw_switch(&self, buf: &mut Buffer, switch: &RectState, i: u8) {
         let mut block = Block::default()
             .title(format!("switch {}", i))
             .borders(Borders::ALL);
@@ -122,7 +124,7 @@ impl Rack {
         &self,
         buf: &mut Buffer,
         power_shelf: &RectState,
-        i: usize,
+        i: u8,
         log: &Logger,
     ) {
         let mut block = Block::default()
@@ -159,16 +161,17 @@ impl StatefulWidget for Rack {
     type State = RackState;
 
     fn render(self, area: Rect, buf: &mut Buffer, state: &mut Self::State) {
-        for (i, s) in state.sleds.iter().enumerate() {
-            self.draw_sled(buf, s, i);
-        }
-
-        for (i, s) in state.switches.iter().enumerate() {
-            self.draw_switch(buf, s, i);
-        }
-
-        for (i, s) in state.power_shelves.iter().enumerate() {
-            self.draw_power_shelf(buf, s, i, state.log.as_ref().unwrap());
+        for (id, rect) in state.component_rects.iter() {
+            match id {
+                ComponentId::Sled(i) => self.draw_sled(buf, rect, *i),
+                ComponentId::Switch(i) => self.draw_switch(buf, rect, *i),
+                ComponentId::Psc(i) => self.draw_power_shelf(
+                    buf,
+                    rect,
+                    *i,
+                    state.log.as_ref().unwrap(),
+                ),
+            }
         }
     }
 }
@@ -176,16 +179,32 @@ impl StatefulWidget for Rack {
 // The visual state of the rack
 #[derive(Debug, Default)]
 pub struct RackState {
-    // The starting TabIndex for the Rack.
     pub log: Option<Logger>,
+    // The starting TabIndex for the Rack.
     pub tab_start: u16,
     pub rect: Rect,
-    pub sleds: [RectState; 32],
-    pub power_shelves: [RectState; 2],
-    pub switches: [RectState; 2],
+    pub component_rects: BTreeMap<ComponentId, RectState>,
 }
 
 impl RackState {
+    pub fn new() -> RackState {
+        let mut state = RackState::default();
+        for i in 0..32 {
+            state
+                .component_rects
+                .insert(ComponentId::Sled(i), RectState::default());
+        }
+
+        for i in 0..2 {
+            state
+                .component_rects
+                .insert(ComponentId::Switch(i), RectState::default());
+            state
+                .component_rects
+                .insert(ComponentId::Psc(i), RectState::default());
+        }
+        state
+    }
     pub fn set_logger(&mut self, log: Logger) {
         self.log = Some(log);
     }
@@ -216,12 +235,13 @@ impl RackState {
         let sled_width = rack.width / 2;
 
         // Top Sleds
-        for i in 0..16 {
+        for i in 0..16u8 {
             self.size_sled(i, &rack, sled_height, sled_width);
         }
 
         // Top Switch
-        let switch = &mut self.switches[0];
+        let switch =
+            self.component_rects.get_mut(&ComponentId::Switch(0)).unwrap();
         switch.rect.y = rack.y + sled_height * 8;
         switch.rect.x = rack.x;
         switch.rect.height = sled_height;
@@ -229,7 +249,10 @@ impl RackState {
 
         // Power Shelves
         for i in [17, 18] {
-            let shelf = &mut self.power_shelves[i - 17];
+            let shelf = self
+                .component_rects
+                .get_mut(&ComponentId::Psc(i - 17))
+                .unwrap();
             shelf.rect.y = rack.y + sled_height * (i as u16 / 2 + 1);
             shelf.rect.x = rack.x;
             shelf.rect.height = sled_height;
@@ -237,7 +260,8 @@ impl RackState {
         }
 
         // Bottom Switch
-        let switch = &mut self.switches[1];
+        let switch =
+            self.component_rects.get_mut(&ComponentId::Switch(1)).unwrap();
         switch.rect.y = rack.y + sled_height * (11);
         switch.rect.x = rack.x;
         switch.rect.height = sled_height;
@@ -252,14 +276,15 @@ impl RackState {
 
     fn size_sled(
         &mut self,
-        i: usize,
+        i: u8,
         rack: &Rect,
         sled_height: u16,
         sled_width: u16,
     ) {
         // The power shelves and switches are in between in the layout
         let index = if i < 16 { i } else { i - 8 };
-        let sled = &mut self.sleds[index];
+        let sled =
+            self.component_rects.get_mut(&ComponentId::Sled(index)).unwrap();
         sled.rect.y = rack.y + sled_height * (i as u16 / 2);
         if i % 2 == 0 {
             // left sled
