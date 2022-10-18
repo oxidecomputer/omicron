4886bf8a153f2b65b24ad7f8b87233368e5cbbbb
Andrew J. Stone <andrew.j.stone.1@gmail.com>
2022-10-18 15:45:08 -0400

    Introduce Wicket (#1815)
    This is the initial, and partial implementation of the technician port UI. Details are in the README.

---
+++ wicket/README.md
@@ -0,0 +1,124 @@
+# Overview
+
+Wicket is a TUI built for operator usage at the technician port. It is intended to support a limited set of responsibilities including:
+ * Rack Initialization
+ * Boundary service setup
+ * Disaster Recovery
+ * Minimal rack update / emergency update
+
+Wicket is built on top of [crossterm](https://github.com/crossterm-rs/crossterm) 
+and [tui-rs](https://github.com/fdehau/tui-rs), although the objects
+themselves, and the placement of objects on the screen is mostly custom.
+
+# Navigating
+
+* `banners` - Files containing "banner-like" output using `#` characters for
+glyph drawing
+* `src/lib.rs` - Contains the main `Wizard` type which manages the UI,
+downstream services, incoming events, and rendering
+* `src/mgs.rs` - Source code for interacting with the [Management Gatewway
+Service (MGS)](https://github.com/oxidecomputer/management-gateway-service)
+* `src/inventory.rs` - Contains rack related components to show in the `Component` `Screen`
+* `src/screens` - Each file represents a full terminal size view in the UI.
+`Screen`s manage specific events and controls, and render `Widget`s. 
+* `src/widgets` - These are implementations of a [`tui::Widget`](https://github.com/fdehau/tui-rs/blob/master/src/widgets/mod.rs#L63-L68) 
+specific to wicket. Widgets are created only to be rendered immediately into a
+[`tui::Frame`](https://github.com/fdehau/tui-rs/blob/9806217a6a4c240462bba3b32cb1bc59524f1bc2/src/terminal.rs#L58-L70). 
+Most widgets contain a reference to state which is mutated by input events, and
+defined in the same file as the widget. The state often implements a `Control`
+that allows it to be manipulated  by the rest of the system in a unified
+manner.
+
+# Design
+
+The main type of the wicket crate is the `Wizard`. The wizard is run by the `wicket` binary and is in charge of:
+ * Handling user input (mouse, keyboard, resize) events
+ * Sending requests to downstream services (MGS + RSS)
+ * Handling events from downstream services
+ * Managing the active screen and triggering terminal rendering
+
+There is a main thread that runs an infinite loop in the `Wizard::mainloop`
+method. The loop's job is to receive `Event`s from a single MPSC channel
+and update internal state, either directly or by forwarding events to the
+currently active screen by calling its `on` method. The active screen
+processes events, updates its internal state (possibly including global state
+passed in via the `on` method), and returns a list of `Action`s that instructs
+the wizard what to do next. Currently there are only two types of `Action`s:
+
+ * `Action::Redraw` - which instructs the Wizard to call the active screen's `draw` method
+ * `Action::SwitchScreen(ScreenId)` - which instructs the wizard to transition between screens
+
+It's important to notice that the internal state of the system is only updated
+upon event receipt, and that a screen never processes an event that can
+mutate state and render in the same method. This makes it very easy to test
+the internal state mutations and behavior of a screen. It also means that all
+drawing code is effectively stateless and fully immediate. While rendering
+`Widget`s relies on the current state of the system, the state of the  system
+does not change at all during rendering, and so an immutable borrow can be
+utilized for this state. This fits well with the `tui-rs` immediate drawing
+paradigm where widgets are created right before rendering and passed by value
+to the render function, which consumes them.
+
+Besides the main thread, which runs `mainloop`, there is a separate tokio
+runtime which is used to drive communications with MGS and RSS, and to manage
+inputs and timers. Requests are driven by MGS and RSS clients and all replies
+are handled by these clients in the tokio runtime. Any important information
+in these replies is forwarded as an `Event` over a channel to be received
+in `mainloop`. All `Event`s, whether respones from downstream services, user
+input, or timer ticks, are sent over the same channel in an `Event` enum. This
+keeps the `mainloop` simple and provides a total ordering of all events, which
+can allow for easier debugging.
+
+As mentioned above, a timer tick is sent as an `Event::Tick` message over
+a channel to the mainloop. Timers currently fire every 25ms, and help drive
+any animations. We don't redraw on every timer tick, since it's relatively
+expensive to calculate widget positions, and since the screens themselves
+return actions when they need to be redrawn. However, the wizard also doesn't
+know when a screen animation is ongoing, and so it forwards all ticks to the
+currently active screen which returns an `Action::Redraw` if the screen needs
+to be redrawn.
+
+# Screens, Widgets, and Controls
+
+A `Screen` represents the current visual state of the `Wizard` to the user, and
+what inputs are available to the user. Each `Screen` maintains its own internal
+state which can be mutated in response to events delivered to it via its `on`
+method. The `on` method also provides mutable access to a globl `State` which
+is relevant across sceens. As mentioned above, a `Screen::draw` method is
+called to render the current screen.
+
+Screens abstract the terminal display, or tty, which itself can be modeled
+as a buffer of characters or a rectangle with a width and height, and x
+and y coordinates for the upper left hand corner. This rectangle can be
+further divided into rectangles that can be independently styled and drawn.
+These rectangles can be manipulated directly, but in the common case this
+manipulation is abstracted into a drawable `Widget`. We have implemented
+several of our own widgets includng the rack view. Each screen has manual
+placement code for these widgets which allows full flexibility and responsive
+design.
+
+Widgets get consumed when drawn to the screen. And the placement code
+determines where the widget rectangles are drawn. However, how do we change the
+styling of the Widgets, such that we know when a mouse hover is occurring or a
+button was clicked? For this purpose, we must track the minimal state required
+to render the widgets and implement a `Control`. Controls provide two key
+things: access to the rectangle, or `Rect`, that we need in order to draw the
+widget on the next render, and a unique ID, that allows screens to keep track
+of which control is currently `active` or being hovered over. For example, when
+a mouse movement event comes in, we can use rectangle intersection to see if
+the mouse is currently over a given Control, and mark it as `hovered'.
+
+
+# What's left?
+
+There are currently 3 screens implemented:
+ * Splash screen
+ * Rack view screen
+ * Component (Sled, Switch, PSC) view
+
+Navigation and UI for these screens works well, but there is no backend
+functionality implemented. All the inventory and power data shown in the
+`Component` screen is fake. We also aren't currently really talking to the MGS
+and RSS. Lastly, we don't have a way to take rack updates and install them, or
+initialize the rack (including trust quorum). This is a lot of functionality
+that will be implemented incrementally.
---
+++ wicket/src/screens/component.rs
@@ -0,0 +1,338 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+//! Inventory and control of individual rack components: sled,switch,psc
+
+use super::colors::*;
+use super::Screen;
+use crate::widgets::Control;
+use crate::widgets::ControlId;
+use crate::widgets::HelpMenuState;
+use crate::widgets::{HelpButton, HelpButtonState, HelpMenu};
+use crate::widgets::{ScreenButton, ScreenButtonState};
+use crate::Action;
+use crate::Frame;
+use crate::ScreenEvent;
+use crate::ScreenId;
+use crate::State;
+use crossterm::event::Event as TermEvent;
+use crossterm::event::{
+    KeyCode, KeyEvent, KeyModifiers, MouseButton, MouseEvent, MouseEventKind,
+};
+use tui::layout::Alignment;
+use tui::style::Modifier;
+use tui::style::{Color, Style};
+use tui::text::{Span, Spans, Text};
+use tui::widgets::Block;
+use tui::widgets::Paragraph;
+
+pub struct ComponentScreen {
+    hovered: Option<ControlId>,
+    help_data: Vec<(&'static str, &'static str)>,
+    help_button_state: HelpButtonState,
+    help_menu_state: HelpMenuState,
+    rack_screen_button_state: ScreenButtonState,
+}
+
+impl ComponentScreen {
+    pub fn new() -> ComponentScreen {
+        let help_data = vec![
+            ("<TAB>", "Cycle forward through components"),
+            ("<SHIFT>-<TAB>", "Cycle backwards through components"),
+            ("<ESC>", "Go back to the rack screen"),
+            ("<CTRL-r", "Go back to the rack screen"),
+            ("<CTRL-h", "Toggle this help menu"),
+            ("<CTRL-c>", "Exit the program"),
+        ];
+        ComponentScreen {
+            hovered: None,
+            help_data,
+            help_button_state: HelpButtonState::new(1, 0),
+            help_menu_state: HelpMenuState::default(),
+            rack_screen_button_state: ScreenButtonState::new(
+                ScreenId::Rack,
+                // This get's reset on every draw
+                u16::MAX,
+                0,
+            ),
+        }
+    }
+
+    fn draw_background(&self, f: &mut Frame) {
+        let style = Style::default().fg(OX_GREEN_DARK).bg(Color::Black);
+        let block = Block::default().style(style);
+        f.render_widget(block, f.size());
+    }
+
+    fn draw_status_bar(&self, f: &mut Frame, state: &State) {
+        let mut rect = f.size();
+        rect.height = 5;
+
+        let style = Style::default().bg(OX_GREEN_DARK).fg(OX_GRAY);
+        let selected_style = Style::default().fg(OX_GREEN_LIGHT);
+        let help_menu_style =
+            Style::default().fg(OX_OFF_WHITE).bg(OX_GREEN_DARK);
+        let help_menu_command_style =
+            Style::default().fg(OX_GREEN_LIGHT).bg(OX_GREEN_DARK);
+        let button_style = Style::default().fg(OX_OFF_WHITE).bg(OX_GREEN_DARK);
+        let hovered_style = Style::default().fg(OX_PINK).bg(OX_GREEN_DARK);
+
+        let status_bar_block = Block::default().style(style);
+        f.render_widget(status_bar_block, rect);
+
+        let current = state.rack_state.get_current_component_id();
+
+        // Draw the components list
+        // TODO: Some sliding style animation?
+        let title = Spans::from(vec![
+            Span::styled(
+                state.rack_state.get_next_component_id().name(),
+                style,
+            ),
+            Span::raw("   "),
+            Span::styled(current.name(), selected_style),
+            Span::raw("   "),
+            Span::styled(
+                state.rack_state.get_next_component_id().name(),
+                style,
+            ),
+        ]);
+
+        let mut rect = f.size();
+        rect.height = 1;
+        rect.y = 1;
+        let title_block = Block::default()
+            .style(style)
+            .title(title)
+            .title_alignment(Alignment::Center);
+        f.render_widget(title_block, rect);
+
+        // Draw the power state
+        let title = match state.inventory.get_power_state(&current) {
+            Some(s) => {
+                format!(
+                    "⌁ Power State: {}",
+                    s.description().to_ascii_uppercase()
+                )
+            }
+            None => "⌁ Power State: UNKNOWN".to_string(),
+        };
+
+        let mut rect = f.size();
+        rect.height = 1;
+        rect.y = 3;
+        let power_state_block = Block::default()
+            .style(selected_style)
+            .title(title)
+            .title_alignment(Alignment::Center);
+        f.render_widget(power_state_block, rect);
+
+        // Draw the help button if the help menu is closed, otherwise draw the
+        // help menu
+        if !self.help_menu_state.is_closed() {
+            let menu = HelpMenu {
+                help: &self.help_data,
+                style: help_menu_style,
+                command_style: help_menu_command_style,
+                // Unwrap is safe because we check that the menu is open (and
+                // thus has an AnimationState).
+                state: self.help_menu_state.get_animation_state().unwrap(),
+            };
+            f.render_widget(menu, f.size());
+        } else {
+            let border_style =
+                if self.hovered == Some(self.help_button_state.id()) {
+                    hovered_style
+                } else {
+                    button_style
+                };
+            let button = HelpButton::new(
+                &self.help_button_state,
+                button_style,
+                border_style,
+            );
+
+            f.render_widget(button, f.size());
+        }
+
+        // Draw the RackSreenButton
+        let border_style =
+            if self.hovered == Some(self.rack_screen_button_state.id()) {
+                hovered_style
+            } else {
+                button_style
+            };
+        let button = ScreenButton::new(
+            &self.rack_screen_button_state,
+            button_style,
+            border_style,
+        );
+        f.render_widget(button, f.size());
+    }
+
+    fn draw_inventory(&self, f: &mut Frame, state: &State) {
+        // Draw the header
+        let selected_style = Style::default().fg(OX_GREEN_LIGHT);
+        let inventory_style = Style::default().fg(OX_YELLOW_DIM);
+
+        let mut header_style = selected_style;
+        header_style =
+            header_style.add_modifier(Modifier::UNDERLINED | Modifier::BOLD);
+
+        let text = Text::styled("INVENTORY\n\n", header_style);
+        let mut rect = f.size();
+        rect.y = 6;
+        rect.height -= 6;
+        let center = (rect.width - text.width() as u16) / 2;
+        rect.x += center;
+        rect.width -= center;
+        let header = Paragraph::new(text);
+        f.render_widget(header, rect);
+
+        // Draw the contents
+        let text = match state
+            .inventory
+            .get_inventory(&state.rack_state.get_current_component_id())
+        {
+            Some(inventory) => {
+                Text::styled(format!("{:#?}", inventory), inventory_style)
+            }
+            None => Text::styled("UNKNOWN", inventory_style),
+        };
+
+        let mut rect = f.size();
+        rect.y = 9;
+        rect.height -= 9;
+
+        let center = (rect.width - text.width() as u16) / 2;
+        rect.x += center;
+        rect.width -= center;
+
+        let inventory = Paragraph::new(text);
+        f.render_widget(inventory, rect);
+    }
+
+    fn handle_key_event(
+        &mut self,
+        state: &mut State,
+        event: KeyEvent,
+    ) -> Vec<Action> {
+        match event.code {
+            KeyCode::Tab => {
+                state.rack_state.inc_tab_index();
+            }
+            KeyCode::BackTab => {
+                state.rack_state.dec_tab_index();
+            }
+            KeyCode::Char('r') => {
+                if event.modifiers.contains(KeyModifiers::CONTROL) {
+                    return vec![Action::SwitchScreen(ScreenId::Rack)];
+                }
+            }
+            KeyCode::Char('h') => {
+                if event.modifiers.contains(KeyModifiers::CONTROL) {
+                    self.help_menu_state.toggle();
+                }
+            }
+            _ => (),
+        }
+        vec![Action::Redraw]
+    }
+
+    fn handle_mouse_event(
+        &mut self,
+        state: &mut State,
+        event: MouseEvent,
+    ) -> Vec<Action> {
+        match event.kind {
+            MouseEventKind::Moved => {
+                self.set_hover_state(state, event.column, event.row)
+            }
+            MouseEventKind::Down(MouseButton::Left) => {
+                self.handle_mouse_click(state)
+            }
+            _ => vec![],
+        }
+    }
+
+    fn handle_mouse_click(&mut self, _: &mut State) -> Vec<Action> {
+        match self.hovered {
+            Some(control_id) if control_id == self.help_button_state.id() => {
+                self.help_menu_state.open();
+                vec![]
+            }
+            Some(control_id)
+                if control_id == self.rack_screen_button_state.id() =>
+            {
+                vec![Action::SwitchScreen(ScreenId::Rack)]
+            }
+            _ => vec![],
+        }
+    }
+
+    fn set_hover_state(
+        &mut self,
+        _state: &mut State,
+        x: u16,
+        y: u16,
+    ) -> Vec<Action> {
+        let current_id = self.find_intersection(x, y);
+        if current_id == self.hovered {
+            // No change
+            vec![]
+        } else {
+            self.hovered = current_id;
+            vec![Action::Redraw]
+        }
+    }
+
+    // Return if the coordinates interesct a given control.
+    // This assumes disjoint control rectangles.
+    fn find_intersection(&self, x: u16, y: u16) -> Option<ControlId> {
+        if self.help_button_state.intersects_point(x, y) {
+            Some(self.help_button_state.id())
+        } else if self.rack_screen_button_state.intersects_point(x, y) {
+            Some(self.rack_screen_button_state.id())
+        } else {
+            None
+        }
+    }
+}
+
+impl Screen for ComponentScreen {
+    fn draw(
+        &mut self,
+        state: &State,
+        terminal: &mut crate::Term,
+    ) -> anyhow::Result<()> {
+        terminal.draw(|f| {
+            self.rack_screen_button_state.rect.x =
+                f.size().width - ScreenButtonState::width();
+            self.draw_background(f);
+            self.draw_status_bar(f, state);
+            self.draw_inventory(f, state);
+        })?;
+        Ok(())
+    }
+
+    fn on(&mut self, state: &mut State, event: ScreenEvent) -> Vec<Action> {
+        match event {
+            ScreenEvent::Term(TermEvent::Key(key_event)) => {
+                self.handle_key_event(state, key_event)
+            }
+            ScreenEvent::Term(TermEvent::Mouse(mouse_event)) => {
+                self.handle_mouse_event(state, mouse_event)
+            }
+            ScreenEvent::Tick => {
+                if !self.help_menu_state.is_closed() {
+                    self.help_menu_state.step();
+                    vec![Action::Redraw]
+                } else {
+                    vec![]
+                }
+            }
+            _ => vec![],
+        }
+    }
+}
