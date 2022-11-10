1e96ae95fca848da11630ab05c0d2aa7515ba4d3
Andrew J. Stone <andrew.j.stone.1@gmail.com>
2022-11-10 17:32:34 +0000

    [wicket] Start to pull real inventory from wicketd
    This commit hooks wicket up to wicketd via wicketd-client. Wicket no
    longer publishes fake inventory, and no longer plans to interact with
    MGS. Instead it only talks to wicketd which acts as a caching proxy for
    MGS inventory.
    
    The data retrieved from wicketd is converted into types already used
    by the display screen infrastructure in wicket. This is what's happening
    in `inventory.rs`. When the inventory is updated the screen is redrawn.
    
    This is just a start, and quite a few things are missing:
     * Real power state - this is currently faked to A2 for everything
     * ComponentInfo (actual SP inventory)
     * Version info - This still needs to be implemented on the SPs and
       plumbed all the way up
     * A display for the last time wicketd was talked to and if MGS is
       actually alive. We want to show how stale our data is. Right now
       if wicketd goes down we'll log it but not notice in the UI. If MGS
       goes down we don't have any idea.
     * ETAG support and diff support so we don't redraw the same inventory
       over and over.
    
    This was tested via running MGS, wicketd and sp simulators on my local
    helios box. If you run wicket standalone it still works fine, but the
    component screen won't show any inventory.

---
+++ wicket/src/wicketd.rs
@@ -0,0 +1,130 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+//! Code for talking to wicketd
+
+use crate::Event;
+use slog::{o, warn, Logger};
+use std::net::SocketAddrV6;
+use std::sync::mpsc::Sender;
+use tokio::sync::mpsc;
+use tokio::time::{interval, Duration, MissedTickBehavior};
+use wicketd_client::types::RackV1Inventory;
+
+const WICKETD_POLL_INTERVAL: Duration = Duration::from_secs(5);
+const WICKETD_TIMEOUT_MS: u32 = 1000; // 3 sec
+
+// Assume that these requests are periodic on the order of seconds or the
+// result of human interaction. In either case, this buffer should be plenty
+// large.
+const CHANNEL_CAPACITY: usize = 1000;
+
+// Eventually this will be filled in with things like triggering updates from the UI
+pub enum Request {}
+
+#[allow(unused)]
+pub struct WicketdHandle {
+    tx: mpsc::Sender<Request>,
+}
+
+/// Wrapper around Wicketd clients used to poll inventory
+/// and perform updates.
+pub struct WicketdManager {
+    log: Logger,
+
+    //TODO: We'll use this onece we start implementing updates
+    #[allow(unused)]
+    rx: mpsc::Receiver<Request>,
+    wizard_tx: Sender<Event>,
+    inventory_client: wicketd_client::Client,
+    inventory: RackV1Inventory,
+}
+
+impl WicketdManager {
+    pub fn new(
+        log: &Logger,
+        wizard_tx: Sender<Event>,
+        wicketd_addr: SocketAddrV6,
+    ) -> (WicketdHandle, WicketdManager) {
+        let log = log.new(o!("component" => "WicketdManager"));
+        let (tx, rx) = tokio::sync::mpsc::channel(CHANNEL_CAPACITY);
+        let endpoint =
+            format!("http://[{}]:{}", wicketd_addr.ip(), wicketd_addr.port());
+
+        let timeout =
+            std::time::Duration::from_millis(WICKETD_TIMEOUT_MS.into());
+        let client = reqwest::ClientBuilder::new()
+            .connect_timeout(timeout)
+            .timeout(timeout)
+            .build()
+            .unwrap();
+
+        let inventory_client = wicketd_client::Client::new_with_client(
+            &endpoint,
+            client,
+            log.clone(),
+        );
+        let inventory = RackV1Inventory { sps: vec![] };
+        let handle = WicketdHandle { tx };
+        let manager =
+            WicketdManager { log, rx, wizard_tx, inventory_client, inventory };
+
+        (handle, manager)
+    }
+
+    /// Manage interactions with wicketd on the same scrimlet
+    ///
+    /// * Send requests to wicketd
+    /// * Receive responses / errors
+    /// * Translate any responses/errors into [`Event`]s
+    ///   that can be utilized by the UI.
+    pub async fn run(self) {
+        let mut inventory_rx =
+            poll_inventory(&self.log, self.inventory_client, self.inventory)
+                .await;
+
+        // TODO: Eventually there will be a tokio::select! here that also
+        // allows issuing updates.
+        while let Some(inventory) = inventory_rx.recv().await {
+            // XXX: Should we log an error and exit here? This means the wizard
+            // died and the process is exiting.
+            let _ = self.wizard_tx.send(Event::Inventory(inventory));
+        }
+    }
+}
+
+async fn poll_inventory(
+    log: &Logger,
+    client: wicketd_client::Client,
+    _inventory: RackV1Inventory,
+) -> mpsc::Receiver<RackV1Inventory> {
+    let log = log.clone();
+
+    // We only want one oustanding request at a time
+    let (tx, rx) = mpsc::channel(1);
+
+    tokio::spawn(async move {
+        let mut ticker = interval(WICKETD_POLL_INTERVAL);
+        ticker.set_missed_tick_behavior(MissedTickBehavior::Delay);
+        loop {
+            ticker.tick().await;
+            // TODO: We should really be using ETAGs here
+            match client.get_inventory().await {
+                Ok(val) => {
+                    let new_inventory = val.into_inner();
+                    // TODO: We need the derive enhancements for progenitor to implement PartialEq/Eq
+                    //if new_inventory != inventory {
+                    //  inventory = new_inventory;
+                    //}
+                    let _ = tx.send(new_inventory).await;
+                }
+                Err(e) => {
+                    warn!(log, "{e}");
+                }
+            }
+        }
+    });
+
+    rx
+}
