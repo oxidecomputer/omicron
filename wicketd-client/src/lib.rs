8678610f15881470240c15367ad3ba887f38329a
Andrew J. Stone <andrew.j.stone.1@gmail.com>
2022-11-15 19:00:11 -0500

    [wicket/wicketd] Retrieve SpComponentList (#1953)
    Add SpComponentInfo to RackV1Inventory by pulling it from MGS as
    necessary. We only pull from MGS when the state of the SP has changed
    or we do not yet have the component info. This is because the info is
    static except for when the SP is upgraded.
    
    We then expose this information via the wicketd-client with the
    appropriately derived traits and plumb it up through wicket.

--- wicketd-client/src/lib.rs
+++ wicketd-client/src/lib.rs
@@ -4,6 +4,27 @@
 
 //! Interface for making API requests to wicketd
 
-use omicron_common::generate_logging_api;
-
-generate_logging_api!("../openapi/wicketd.json");
+progenitor::generate_api!(
+    spec = "../openapi/wicketd.json",
+    inner_type = slog::Logger,
+    pre_hook = (|log: &slog::Logger, request: &reqwest::Request| {
+        slog::debug!(log, "client request";
+            "method" => %request.method(),
+            "uri" => %request.url(),
+            "body" => ?&request.body(),
+        );
+    }),
+    post_hook = (|log: &slog::Logger, result: &Result<_, _>| {
+        slog::debug!(log, "client response"; "result" => ?result);
+    }),
+    derives = [schemars::JsonSchema],
+    patch =
+        {
+        SpIdentifier = { derives = [Copy, PartialEq, Eq, PartialOrd, Ord] },
+        SpState = { derives = [ PartialEq, Eq, PartialOrd, Ord] },
+        SpComponentInfo= { derives = [ PartialEq, Eq, PartialOrd, Ord] },
+        SpIgnition= { derives = [ PartialEq, Eq, PartialOrd, Ord] },
+        SpInventory = { derives = [ PartialEq, Eq, PartialOrd, Ord] },
+        RackV1Inventory = { derives = [ PartialEq, Eq, PartialOrd, Ord] },
+    }
+);
