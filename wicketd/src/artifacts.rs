ccd353ccb958987840017936b01721dbbe62668e
Rain <rain@oxide.computer>
2022-11-30 14:44:02 -0800

    [𝘀𝗽𝗿] initial version
    Created using spr 1.3.4

---
+++ wicketd/src/artifacts.rs
@@ -0,0 +1,44 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+// Copyright 2022 Oxide Computer Company
+
+use hyper::body::Bytes;
+use schemars::JsonSchema;
+use serde::Deserialize;
+use slog::Logger;
+
+/// Path parameters for Silo requests
+#[derive(Clone, Debug, Deserialize, JsonSchema)]
+#[allow(dead_code)]
+pub(crate) struct ArtifactId {
+    /// The artifact's name.
+    pub(crate) name: String,
+
+    /// The version of the artifact.
+    pub(crate) version: String,
+
+    /// A hash of the artifact.
+    /// TODO: is there a hash type?
+    pub(crate) hash: String,
+}
+
+/// The artifact store, used to cache artifacts.
+pub struct ArtifactStore {
+    log: Logger,
+    // TODO: implement this
+}
+
+impl ArtifactStore {
+    pub(crate) fn new(log: &Logger) -> Self {
+        let log = log.new(slog::o!("component" => "wicketd artifact store"));
+        Self { log }
+    }
+
+    pub(crate) fn get_artifact(&self, id: &ArtifactId) -> Option<Bytes> {
+        slog::debug!(self.log, "Artifact requested (this is a stub implementation which always 404s): {:?}", id);
+        // TODO: implement this
+        None
+    }
+}
--- wicketd/src/http_entrypoints.rs
+++ wicketd/src/http_entrypoints.rs
@@ -4,12 +4,16 @@
 
 //! HTTP entrypoint functions for wicketd
 
+use crate::artifacts::ArtifactId;
 use crate::RackV1Inventory;
 use dropshot::endpoint;
 use dropshot::ApiDescription;
+use dropshot::FreeformBody;
 use dropshot::HttpError;
 use dropshot::HttpResponseOk;
+use dropshot::Path;
 use dropshot::RequestContext;
+use hyper::Body;
 use std::sync::Arc;
 
 use crate::ServerContext;
@@ -22,6 +26,7 @@ pub fn api() -> WicketdApiDescription {
         api: &mut WicketdApiDescription,
     ) -> Result<(), String> {
         api.register(get_inventory)?;
+        api.register(get_artifact)?;
         Ok(())
     }
 
@@ -50,6 +55,23 @@ async fn get_inventory(
         Ok(inventory) => Ok(HttpResponseOk(inventory)),
         Err(_) => {
             Err(HttpError::for_unavail(None, "Server is shutting down".into()))
+        }
+    }
+}
+
+/// Fetch an artifact from the in-memory cache.
+#[endpoint {
+    method = GET,
+    path = "/artifacts/{name}/{version}/{hash}"
+}]
+async fn get_artifact(
+    rqctx: Arc<RequestContext<ServerContext>>,
+    path: Path<ArtifactId>,
+) -> Result<HttpResponseOk<FreeformBody>, HttpError> {
+    match rqctx.context().artifact_store.get_artifact(&path.into_inner()) {
+        Some(bytes) => Ok(HttpResponseOk(Body::from(bytes).into())),
+        None => {
+            Err(HttpError::for_not_found(None, "Artifact not found".into()))
         }
     }
 }
