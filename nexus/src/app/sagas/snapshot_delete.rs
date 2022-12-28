6970a859cc20ef13406cbb7826ead3bb0243fee4
Sean Klein <sean@oxide.computer>
2022-12-28 15:48:16 -0500

    [nexus] Make snapshot deletion a saga (#2090)
    Part of https://github.com/oxidecomputer/omicron/issues/2059

---
+++ nexus/src/app/sagas/snapshot_delete.rs
@@ -0,0 +1,113 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+use super::{ActionRegistry, NexusActionContext, NexusSaga};
+use crate::app::sagas;
+use crate::app::sagas::declare_saga_actions;
+use crate::context::OpContext;
+use crate::{authn, authz, db};
+use serde::Deserialize;
+use serde::Serialize;
+use steno::ActionError;
+use steno::Node;
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct Params {
+    pub serialized_authn: authn::saga::Serialized,
+    pub authz_snapshot: authz::Snapshot,
+    pub snapshot: db::model::Snapshot,
+}
+
+declare_saga_actions! {
+    snapshot_delete;
+    DELETE_SNAPSHOT_RECORD -> "no_result" {
+        + ssd_delete_snapshot_record
+    }
+}
+
+#[derive(Debug)]
+pub struct SagaSnapshotDelete;
+impl NexusSaga for SagaSnapshotDelete {
+    const NAME: &'static str = "snapshot-delete";
+    type Params = Params;
+
+    fn register_actions(registry: &mut ActionRegistry) {
+        snapshot_delete_register_actions(registry);
+    }
+
+    fn make_saga_dag(
+        params: &Self::Params,
+        mut builder: steno::DagBuilder,
+    ) -> Result<steno::Dag, super::SagaInitError> {
+        builder.append(delete_snapshot_record_action());
+
+        const DELETE_VOLUME_PARAMS: &'static str = "delete_volume_params";
+        const DELETE_VOLUME_DESTINATION_PARAMS: &'static str =
+            "delete_volume_destination_params";
+
+        let volume_delete_params = sagas::volume_delete::Params {
+            volume_id: params.snapshot.volume_id,
+        };
+        builder.append(Node::constant(
+            DELETE_VOLUME_PARAMS,
+            serde_json::to_value(&volume_delete_params).map_err(|e| {
+                super::SagaInitError::SerializeError(
+                    String::from("volume_id"),
+                    e,
+                )
+            })?,
+        ));
+
+        let volume_delete_params = sagas::volume_delete::Params {
+            volume_id: params.snapshot.destination_volume_id,
+        };
+        builder.append(Node::constant(
+            DELETE_VOLUME_DESTINATION_PARAMS,
+            serde_json::to_value(&volume_delete_params).map_err(|e| {
+                super::SagaInitError::SerializeError(
+                    String::from("volume_id"),
+                    e,
+                )
+            })?,
+        ));
+
+        let make_volume_delete_dag = || {
+            let subsaga_builder = steno::DagBuilder::new(steno::SagaName::new(
+                sagas::volume_delete::SagaVolumeDelete::NAME,
+            ));
+            sagas::volume_delete::create_dag(subsaga_builder)
+        };
+        builder.append(steno::Node::subsaga(
+            "delete_volume",
+            make_volume_delete_dag()?,
+            DELETE_VOLUME_PARAMS,
+        ));
+        builder.append(steno::Node::subsaga(
+            "delete_destination_volume",
+            make_volume_delete_dag()?,
+            DELETE_VOLUME_DESTINATION_PARAMS,
+        ));
+
+        Ok(builder.build()?)
+    }
+}
+
+async fn ssd_delete_snapshot_record(
+    sagactx: NexusActionContext,
+) -> Result<(), ActionError> {
+    let osagactx = sagactx.user_data();
+    let params = sagactx.saga_params::<Params>()?;
+    let opctx = OpContext::for_saga_action(&sagactx, &params.serialized_authn);
+
+    osagactx
+        .datastore()
+        .project_delete_snapshot(
+            &opctx,
+            &params.authz_snapshot,
+            &params.snapshot,
+        )
+        .await
+        .map_err(ActionError::action_failed)?;
+    Ok(())
+}
