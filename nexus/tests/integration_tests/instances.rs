oxidecomputer/omicron

commit f47aefcb7600486edef0451847098f7a11a557c0

[nexus] Remove project_id, rack_id from IP pools (#2056)
## Before this PR

- IP Pools could exist in at most one project. IP allocation during
instance creation occurred by [either by requesting an IP pool belonging
to a project, or by "just looking for any unreserved IP
Pool"](https://github.com/oxidecomputer/omicron/blob/79765a4e3b39a29bc9940c0e4a49c4364fbcc9e3/nexus/src/db/queries/external_ip.rs#L186-L212).
As discussed in https://github.com/oxidecomputer/omicron/issues/2055 ,
our intention is for IP pools to be used across multiple projects, and
for projects to be able to use multiple IP pools.
- "Service" IP pools were indexed by rack ID, though (as documented in
https://github.com/oxidecomputer/omicron/issues/1276 ), they should
probably be accessed by AZ instead.

## This PR

- Adds a default IP pool named `default`, which is used for address
allocation unless a more specific IP pool is provided
- Removes "project ID" from IP pools (and external IP addresses)
- Removes "rack ID" from IP pool API and DB representation

## In the future

- This PR doesn't provide the many-to-many connection between projects
and IP pools that we eventually want, where projects can be configured
to use different IP pools for different purposes. However, by removing
the not-quite-accurate relationship that an IP pool must belong to a
*single* project, the API moves closer towards this direction.
- We probably should access the `service_ip_pool` API with the AZ UUID
used for the query, but since AZs don't exist in the API yet, this has
been omitted.

Part of #2055

nexus/tests/integration_tests/instances.rs
@@ -13,6 +13,7 @@ use nexus_test_utils::resource_helpers::create_disk;
 use nexus_test_utils::resource_helpers::create_ip_pool;
 use nexus_test_utils::resource_helpers::object_create;
 use nexus_test_utils::resource_helpers::objects_list_page_authz;
+use nexus_test_utils::resource_helpers::populate_ip_pool;
 use nexus_test_utils::resource_helpers::DiskTest;
 use omicron_common::api::external::ByteCount;
 use omicron_common::api::external::Disk;
@@ -48,7 +49,6 @@ use nexus_test_utils_macros::nexus_test;
 type ControlPlaneTestContext =
     nexus_test_utils::ControlPlaneTestContext<omicron_nexus::Server>;
 
-static POOL_NAME: &str = "p0";
 static ORGANIZATION_NAME: &str = "test-org";
 static PROJECT_NAME: &str = "springfield-squidport";
 
@@ -61,7 +61,7 @@ fn get_instances_url() -> String {
 }
 
 async fn create_org_and_project(client: &ClientTestContext) -> Uuid {
-    create_ip_pool(&client, "p0", None, None).await;
+    populate_ip_pool(&client, "default", None).await;
     create_organization(&client, ORGANIZATION_NAME).await;
     let project = create_project(client, ORGANIZATION_NAME, PROJECT_NAME).await;
     project.identity.id
@@ -127,7 +127,7 @@ async fn test_instances_access_before_create_returns_not_found(
 async fn test_v1_instance_access(cptestctx: &ControlPlaneTestContext) {
     let client = &cptestctx.external_client;
 
-    create_ip_pool(&client, "p0", None, None).await;
+    populate_ip_pool(&client, "default", None).await;
     let org = create_organization(&client, ORGANIZATION_NAME).await;
     let project = create_project(client, ORGANIZATION_NAME, PROJECT_NAME).await;
 
@@ -190,14 +190,11 @@ async fn test_instances_create_reboot_halt(
     let apictx = &cptestctx.server.apictx;
     let nexus = &apictx.nexus;
 
-    // Create an IP pool and  project that we'll use for testing.
-    create_ip_pool(&client, POOL_NAME, None, None).await;
-    create_organization(&client, ORGANIZATION_NAME).await;
+    create_org_and_project(&client).await;
     let url_instances = format!(
         "/organizations/{}/projects/{}/instances",
         ORGANIZATION_NAME, PROJECT_NAME
     );
-    let _ = create_project(&client, ORGANIZATION_NAME, PROJECT_NAME).await;
 
     // Create an instance.
     let instance_url = format!("{}/just-rainsticks", url_instances);
@@ -516,14 +513,11 @@ async fn test_instances_create_stopped_start(
     let apictx = &cptestctx.server.apictx;
     let nexus = &apictx.nexus;
 
-    // Create an IP pool and  project that we'll use for testing.
-    create_ip_pool(&client, POOL_NAME, None, None).await;
-    create_organization(&client, ORGANIZATION_NAME).await;
+    create_org_and_project(&client).await;
     let url_instances = format!(
         "/organizations/{}/projects/{}/instances",
         ORGANIZATION_NAME, PROJECT_NAME
     );
-    let _ = create_project(&client, ORGANIZATION_NAME, PROJECT_NAME).await;
 
     // Create an instance in a stopped state.
     let instance: Instance = object_create(
@@ -572,14 +566,11 @@ async fn test_instances_delete_fails_when_running_succeeds_when_stopped(
     let apictx = &cptestctx.server.apictx;
     let nexus = &apictx.nexus;
 
-    // Create an IP pool and project that we'll use for testing.
-    create_ip_pool(&client, POOL_NAME, None, None).await;
-    create_organization(&client, ORGANIZATION_NAME).await;
+    create_org_and_project(&client).await;
     let url_instances = format!(
         "/organizations/{}/projects/{}/instances",
         ORGANIZATION_NAME, PROJECT_NAME
     );
-    let _ = create_project(&client, ORGANIZATION_NAME, PROJECT_NAME).await;
 
     // Create an instance.
     let instance_url = format!("{}/just-rainsticks", url_instances);
@@ -698,13 +689,11 @@ async fn test_instance_create_saga_removes_instance_database_record(
     let client = &cptestctx.external_client;
 
     // Create test IP pool, organization and project
-    create_ip_pool(&client, POOL_NAME, None, None).await;
-    create_organization(&client, ORGANIZATION_NAME).await;
+    create_org_and_project(&client).await;
     let url_instances = format!(
         "/organizations/{}/projects/{}/instances",
         ORGANIZATION_NAME, PROJECT_NAME
     );
-    let _ = create_project(&client, ORGANIZATION_NAME, PROJECT_NAME).await;
 
     // The network interface parameters.
     let default_name = "default".parse::<Name>().unwrap();
@@ -810,14 +799,11 @@ async fn test_instance_with_single_explicit_ip_address(
 ) {
     let client = &cptestctx.external_client;
 
-    // Create test IP pool, organization and project
-    create_ip_pool(&client, POOL_NAME, None, None).await;
-    create_organization(&client, ORGANIZATION_NAME).await;
+    create_org_and_project(&client).await;
     let url_instances = format!(
         "/organizations/{}/projects/{}/instances",
         ORGANIZATION_NAME, PROJECT_NAME
     );
-    let _ = create_project(&client, ORGANIZATION_NAME, PROJECT_NAME).await;
 
     // Create the parameters for the interface.
     let default_name = "default".parse::<Name>().unwrap();
@@ -886,14 +872,11 @@ async fn test_instance_with_new_custom_network_interfaces(
 ) {
     let client = &cptestctx.external_client;
 
-    // Create test IP pool, organization and project
-    create_ip_pool(&client, POOL_NAME, None, None).await;
-    create_organization(&client, ORGANIZATION_NAME).await;
+    create_org_and_project(&client).await;
     let url_instances = format!(
         "/organizations/{}/projects/{}/instances",
         ORGANIZATION_NAME, PROJECT_NAME
     );
-    let _ = create_project(&client, ORGANIZATION_NAME, PROJECT_NAME).await;
 
     // Create a VPC Subnet other than the default.
     //
@@ -1044,14 +1027,11 @@ async fn test_instance_create_delete_network_interface(
     let client = &cptestctx.external_client;
     let nexus = &cptestctx.server.apictx.nexus;
 
-    // Create test IP pool, organization and project
-    create_ip_pool(&client, POOL_NAME, None, None).await;
-    create_organization(&client, ORGANIZATION_NAME).await;
+    create_org_and_project(&client).await;
     let url_instances = format!(
         "/organizations/{}/projects/{}/instances",
         ORGANIZATION_NAME, PROJECT_NAME
     );
-    let _ = create_project(&client, ORGANIZATION_NAME, PROJECT_NAME).await;
 
     // Create the VPC Subnet for the secondary interface
     let secondary_subnet = params::VpcSubnetCreate {
@@ -1295,14 +1275,11 @@ async fn test_instance_update_network_interfaces(
     let client = &cptestctx.external_client;
     let nexus = &cptestctx.server.apictx.nexus;
 
-    // Create test IP pool, organization and project
-    create_ip_pool(&client, POOL_NAME, None, None).await;
-    create_organization(&client, ORGANIZATION_NAME).await;
+    create_org_and_project(&client).await;
     let url_instances = format!(
         "/organizations/{}/projects/{}/instances",
         ORGANIZATION_NAME, PROJECT_NAME
     );
-    let _ = create_project(&client, ORGANIZATION_NAME, PROJECT_NAME).await;
 
     // Create the VPC Subnet for the secondary interface
     let secondary_subnet = params::VpcSubnetCreate {
@@ -1778,15 +1755,9 @@ async fn test_instance_with_multiple_nics_unwinds_completely(
 async fn test_attach_one_disk_to_instance(cptestctx: &ControlPlaneTestContext) {
     let client = &cptestctx.external_client;
 
-    const POOL_NAME: &str = "p0";
-    const ORGANIZATION_NAME: &str = "bobs-barrel-of-bytes";
-    const PROJECT_NAME: &str = "bit-barrel";
-
     // Test pre-reqs
     DiskTest::new(&cptestctx).await;
-    create_ip_pool(&client, POOL_NAME, None, None).await;
-    create_organization(&client, ORGANIZATION_NAME).await;
-    create_project(client, ORGANIZATION_NAME, PROJECT_NAME).await;
+    create_org_and_project(&client).await;
 
     // Create the "probablydata" disk
     create_disk(&client, ORGANIZATION_NAME, PROJECT_NAME, "probablydata").await;
@@ -1872,15 +1843,9 @@ async fn test_instance_fails_to_boot_with_disk(
     // see: https://github.com/oxidecomputer/omicron/issues/1713
     let client = &cptestctx.external_client;
 
-    const POOL_NAME: &str = "p0";
-    const ORGANIZATION_NAME: &str = "bobs-barrel-of-bytes";
-    const PROJECT_NAME: &str = "bit-barrel";
-
     // Test pre-reqs
     DiskTest::new(&cptestctx).await;
-    create_ip_pool(&client, POOL_NAME, None, None).await;
-    create_organization(&client, ORGANIZATION_NAME).await;
-    create_project(client, ORGANIZATION_NAME, PROJECT_NAME).await;
+    create_org_and_project(&client).await;
 
     // Create the "probablydata" disk
     create_disk(&client, ORGANIZATION_NAME, PROJECT_NAME, "probablydata").await;
@@ -2175,15 +2140,9 @@ async fn test_attach_eight_disks_to_instance(
 ) {
     let client = &cptestctx.external_client;
 
-    const POOL_NAME: &str = "p0";
-    const ORGANIZATION_NAME: &str = "bobs-barrel-of-bytes";
-    const PROJECT_NAME: &str = "bit-barrel";
-
     // Test pre-reqs
     DiskTest::new(&cptestctx).await;
-    create_ip_pool(&client, POOL_NAME, None, None).await;
-    create_organization(&client, ORGANIZATION_NAME).await;
-    create_project(client, ORGANIZATION_NAME, PROJECT_NAME).await;
+    create_org_and_project(&client).await;
 
     // Make 8 disks
     for i in 0..8 {
@@ -2388,17 +2347,9 @@ async fn test_cannot_attach_nine_disks_to_instance(
 #[nexus_test]
 async fn test_cannot_attach_faulted_disks(cptestctx: &ControlPlaneTestContext) {
     let client = &cptestctx.external_client;
-
-    // Test pre-reqs
-    const POOL_NAME: &str = "p0";
-    const ORGANIZATION_NAME: &str = "bobs-barrel-of-bytes";
-    const PROJECT_NAME: &str = "bit-barrel";
-
     // Test pre-reqs
     DiskTest::new(&cptestctx).await;
-    create_ip_pool(&client, POOL_NAME, None, None).await;
-    create_organization(&client, ORGANIZATION_NAME).await;
-    create_project(client, ORGANIZATION_NAME, PROJECT_NAME).await;
+    create_org_and_project(&client).await;
 
     // Make 8 disks
     for i in 0..8 {
@@ -2527,15 +2478,9 @@ async fn test_disks_detached_when_instance_destroyed(
 ) {
     let client = &cptestctx.external_client;
 
-    const POOL_NAME: &str = "p0";
-    const ORGANIZATION_NAME: &str = "bobs-barrel-of-bytes";
-    const PROJECT_NAME: &str = "bit-barrel";
-
     // Test pre-reqs
     DiskTest::new(&cptestctx).await;
-    create_ip_pool(&client, POOL_NAME, None, None).await;
-    create_organization(&client, ORGANIZATION_NAME).await;
-    create_project(client, ORGANIZATION_NAME, PROJECT_NAME).await;
+    create_org_and_project(&client).await;
 
     // Make 8 disks
     for i in 0..8 {
@@ -2778,14 +2723,11 @@ async fn test_instance_serial(cptestctx: &ControlPlaneTestContext) {
     let apictx = &cptestctx.server.apictx;
     let nexus = &apictx.nexus;
 
-    // Create a project that we'll use for testing.
-    create_ip_pool(client, POOL_NAME, None, None).await;
-    create_organization(client, ORGANIZATION_NAME).await;
+    create_org_and_project(&client).await;
     let url_instances = format!(
         "/organizations/{}/projects/{}/instances",
         ORGANIZATION_NAME, PROJECT_NAME
     );
-    let _ = create_project(client, ORGANIZATION_NAME, PROJECT_NAME).await;
 
     // Make sure we get a 404 if we try to access the serial console before creation.
     let instance_serial_url =
@@ -2863,27 +2805,23 @@ async fn test_instance_serial(cptestctx: &ControlPlaneTestContext) {
 }
 
 #[nexus_test]
-async fn test_instance_ephemeral_ip_from_correct_project(
+async fn test_instance_ephemeral_ip_from_correct_pool(
     cptestctx: &ControlPlaneTestContext,
 ) {
     let client = &cptestctx.external_client;
 
-    // Create test organization and two projects.
+    // Create test organization and projects.
     create_organization(&client, ORGANIZATION_NAME).await;
     let url_instances = format!(
         "/organizations/{}/projects/{}/instances",
         ORGANIZATION_NAME, PROJECT_NAME
     );
     let _ = create_project(&client, ORGANIZATION_NAME, PROJECT_NAME).await;
-    let _ = create_project(&client, ORGANIZATION_NAME, "restricted").await;
 
     // Create two IP pools.
     //
-    // The first is restricted to the "restricted" project, the second unrestricted.
-    let project_path = params::ProjectPath {
-        organization: Name::try_from(ORGANIZATION_NAME.to_string()).unwrap(),
-        project: Name::try_from("restricted".to_string()).unwrap(),
-    };
+    // The first is given to the "default" pool, the provided to a distinct
+    // explicit pool.
     let first_range = IpRange::V4(
         Ipv4Range::new(
             std::net::Ipv4Addr::new(10, 0, 0, 1),
@@ -2898,17 +2836,10 @@ async fn test_instance_ephemeral_ip_from_correct_project(
         )
         .unwrap(),
     );
-    create_ip_pool(
-        &client,
-        "restricted-pool",
-        Some(first_range),
-        Some(project_path),
-    )
-    .await;
-    create_ip_pool(&client, "unrestricted-pool", Some(second_range), None)
-        .await;
+    populate_ip_pool(&client, "default", Some(first_range)).await;
+    create_ip_pool(&client, "other-pool", Some(second_range)).await;
 
-    // Create an instance in the default project, not the "dummy" project
+    // Create an instance explicitly using the "other-pool".
     let instance_params = params::InstanceCreate {
         identity: IdentityMetadataCreateParams {
             name: Name::try_from(String::from("ip-pool-test")).unwrap(),
@@ -2920,7 +2851,9 @@ async fn test_instance_ephemeral_ip_from_correct_project(
         user_data: vec![],
         network_interfaces: params::InstanceNetworkInterfaceAttachment::Default,
         external_ips: vec![params::ExternalIpCreate::Ephemeral {
-            pool_name: None,
+            pool_name: Some(
+                Name::try_from(String::from("other-pool")).unwrap(),
+            ),
         }],
         disks: vec![],
         start: true,
@@ -2951,8 +2884,8 @@ async fn test_instance_ephemeral_ip_from_correct_project(
         ips.items[0].ip >= second_range.first_address()
             && ips.items[0].ip <= second_range.last_address(),
         "Expected the Ephemeral IP to come from the second address \
-        range, since the first is reserved for a project different from \
-        the instance's project."
+        range, since the first is reserved for the default pool, not \
+        the requested pool."
     );
 }
 

common/src/sql/dbinit.sql
@@ -1112,16 +1112,9 @@ CREATE TABLE omicron.public.ip_pool (
     time_modified TIMESTAMPTZ NOT NULL,
     time_deleted TIMESTAMPTZ,
 
-    /* Optional ID of the project for which this pool is reserved. */
-    project_id UUID,
 
-    /*
-     * Optional rack ID, indicating this is a reserved pool for internal
-     * services on a specific rack.
-     * TODO(https://github.com/oxidecomputer/omicron/issues/1276): This
-     * should probably point to an AZ or fleet, not a rack.
-     */
-    rack_id UUID,
+    /* Identifies if the IP Pool is dedicated to Control Plane services */
+    internal BOOL NOT NULL,
 
     /* The collection's child-resource generation number */
     rcgen INT8 NOT NULL
@@ -1133,15 +1126,6 @@ CREATE TABLE omicron.public.ip_pool (
 CREATE UNIQUE INDEX ON omicron.public.ip_pool (
     name
 ) WHERE
-    time_deleted IS NULL;
-
-/*
- * Index ensuring uniqueness of IP pools by rack ID
- */
-CREATE UNIQUE INDEX ON omicron.public.ip_pool (
-    rack_id
-) WHERE
-    rack_id IS NOT NULL AND
     time_deleted IS NULL;
 
 /*
@@ -1158,20 +1142,11 @@ CREATE TABLE omicron.public.ip_pool_range (
     /* The range is inclusive of the last address. */
     last_address INET NOT NULL,
     ip_pool_id UUID NOT NULL,
-    /* Optional ID of the project for which this range is reserved.
-     *
-     * NOTE: This denormalizes the tables a bit, since the project_id is
-     * duplicated here and in the parent `ip_pool` table. We're allowing this
-     * for now, since it reduces the complexity of the already-bad IP allocation
-     * query, but we may want to revisit that, and JOIN with the parent table
-     * instead.
-     */
-    project_id UUID,
     /* Tracks child resources, IP addresses allocated out of this range. */
     rcgen INT8 NOT NULL
 );
 
-/* 
+/*
  * These help Nexus enforce that the ranges within an IP Pool do not overlap
  * with any other ranges. See `nexus/src/db/queries/ip_pool.rs` for the actual
  * query which does that.
@@ -1186,14 +1161,6 @@ CREATE UNIQUE INDEX ON omicron.public.ip_pool_range (
 )
 STORING (first_address)
 WHERE time_deleted IS NULL;
-
-/*
- * Index supporting allocation of IPs out of a Pool reserved for a project.
- */
-CREATE INDEX ON omicron.public.ip_pool_range (
-    project_id
-) WHERE
-    time_deleted IS NULL;
 
 
 /* The kind of external IP address. */
@@ -1245,9 +1212,6 @@ CREATE TABLE omicron.public.external_ip (
     /* FK to the `ip_pool_range` table. */
     ip_pool_range_id UUID NOT NULL,
 
-    /* FK to the `project` table. */
-    project_id UUID,
-
     /* FK to the `instance` table. See the constraints below. */
     instance_id UUID,
 
@@ -1262,15 +1226,6 @@ CREATE TABLE omicron.public.external_ip (
 
     /* The last port in the allowed range, also inclusive. */
     last_port INT4 NOT NULL,
-
-    /*
-     * The project can only be NULL for service IPs.
-     * Additionally, the project MUST be NULL for service IPs.
-     */
-    CONSTRAINT null_project CHECK(
-        (kind != 'service' AND project_id IS NOT NULL) OR
-        (kind = 'service' AND project_id IS NULL)
-    ),
 
     /* The name must be non-NULL iff this is a floating IP. */
     CONSTRAINT null_fip_name CHECK (

end-to-end-tests/src/bin/bootstrap.rs
@@ -3,7 +3,7 @@ use end_to_end_tests::helpers::ctx::{build_client, Context};
 use end_to_end_tests::helpers::{generate_name, get_system_ip_pool};
 use omicron_test_utils::dev::poll::{wait_for_condition, CondCheckError};
 use oxide_client::types::{
-    ByteCount, DiskCreate, DiskSource, IpPoolCreate, IpRange, Ipv4Range,
+    ByteCount, DiskCreate, DiskSource, IpRange, Ipv4Range,
 };
 use oxide_client::{ClientDisksExt, ClientOrganizationsExt, ClientSystemExt};
 use std::time::Duration;
@@ -30,21 +30,9 @@ async fn main() -> Result<()> {
     // ===== CREATE IP POOL ===== //
     eprintln!("creating IP pool...");
     let (first, last) = get_system_ip_pool()?;
-    let pool_name = client
-        .ip_pool_create()
-        .body(IpPoolCreate {
-            name: generate_name("ip-pool")?,
-            description: String::new(),
-            organization: None,
-            project: None,
-        })
-        .send()
-        .await?
-        .name
-        .clone();
     client
         .ip_pool_range_add()
-        .pool_name(pool_name)
+        .pool_name("default")
         .body(IpRange::V4(Ipv4Range { first, last }))
         .send()
         .await?;

nexus/db-model/src/external_ip.rs
@@ -58,7 +58,6 @@ pub struct ExternalIp {
     pub time_deleted: Option<DateTime<Utc>>,
     pub ip_pool_id: Uuid,
     pub ip_pool_range_id: Uuid,
-    pub project_id: Option<Uuid>,
     // This is Some(_) for:
     //  - all instance SNAT IPs
     //  - all ephemeral IPs
@@ -80,18 +79,6 @@ impl From<ExternalIp> for sled_agent_client::types::SourceNatConfig {
     }
 }
 
-/// Describes where the IP candidates for allocation come from: either
-/// from an IP pool, or from a project.
-///
-/// This ensures that a source is always specified, and a caller cannot
-/// request an external IP allocation without providing at least one of
-/// these options.
-#[derive(Debug, Clone, Copy)]
-pub enum IpSource {
-    Instance { project_id: Uuid, pool_id: Option<Uuid> },
-    Service { pool_id: Uuid },
-}
-
 /// An incomplete external IP, used to store state required for issuing the
 /// database query that selects an available IP and stores the resulting record.
 #[derive(Debug, Clone)]
@@ -102,15 +89,14 @@ pub struct IncompleteExternalIp {
     time_created: DateTime<Utc>,
     kind: IpKind,
     instance_id: Option<Uuid>,
-    source: IpSource,
+    pool_id: Uuid,
 }
 
 impl IncompleteExternalIp {
     pub fn for_instance_source_nat(
         id: Uuid,
-        project_id: Uuid,
         instance_id: Uuid,
-        pool_id: Option<Uuid>,
+        pool_id: Uuid,
     ) -> Self {
         Self {
             id,
@@ -119,16 +105,11 @@ impl IncompleteExternalIp {
             time_created: Utc::now(),
             kind: IpKind::SNat,
             instance_id: Some(instance_id),
-            source: IpSource::Instance { project_id, pool_id },
+            pool_id,
         }
     }
 
-    pub fn for_ephemeral(
-        id: Uuid,
-        project_id: Uuid,
-        instance_id: Uuid,
-        pool_id: Option<Uuid>,
-    ) -> Self {
+    pub fn for_ephemeral(id: Uuid, instance_id: Uuid, pool_id: Uuid) -> Self {
         Self {
             id,
             name: None,
@@ -136,7 +117,7 @@ impl IncompleteExternalIp {
             time_created: Utc::now(),
             kind: IpKind::Ephemeral,
             instance_id: Some(instance_id),
-            source: IpSource::Instance { project_id, pool_id },
+            pool_id,
         }
     }
 
@@ -144,8 +125,7 @@ impl IncompleteExternalIp {
         id: Uuid,
         name: &Name,
         description: &str,
-        project_id: Uuid,
-        pool_id: Option<Uuid>,
+        pool_id: Uuid,
     ) -> Self {
         Self {
             id,
@@ -154,7 +134,7 @@ impl IncompleteExternalIp {
             time_created: Utc::now(),
             kind: IpKind::Floating,
             instance_id: None,
-            source: IpSource::Instance { project_id, pool_id },
+            pool_id,
         }
     }
 
@@ -166,7 +146,7 @@ impl IncompleteExternalIp {
             time_created: Utc::now(),
             kind: IpKind::Service,
             instance_id: None,
-            source: IpSource::Service { pool_id },
+            pool_id,
         }
     }
 
@@ -194,8 +174,8 @@ impl IncompleteExternalIp {
         &self.instance_id
     }
 
-    pub fn source(&self) -> &IpSource {
-        &self.source
+    pub fn pool_id(&self) -> &Uuid {
+        &self.pool_id
     }
 }
 

commit d9b464aab369ec380e946a2f4d03bb6f1a481708

Fix ip pool policy for instance allocation (#2113)
Fixes a regression introduced by
https://github.com/oxidecomputer/omicron/pull/2056

Updates polar policy to allow instance creation from regular Silo users,
and adds a regression test

nexus/src/authz/omicron.polar
@@ -373,6 +373,11 @@ resource IpPoolList {
 has_relation(fleet: Fleet, "parent_fleet", ip_pool_list: IpPoolList)
 	if ip_pool_list.fleet = fleet;
 
+# Any authenticated user can create a child of a provided IP Pool.
+# This is necessary to use the pools when provisioning instances.
+has_permission(actor: AuthenticatedActor, "create_child", ip_pool: IpPool)
+	if silo in actor.silo and silo.fleet = ip_pool.fleet;
+
 # Describes the policy for accessing "/system/images" (in the API)
 resource GlobalImageList {
 	permissions = [

nexus/tests/integration_tests/instances.rs
@@ -11,6 +11,9 @@ use nexus_test_utils::http_testing::NexusRequest;
 use nexus_test_utils::http_testing::RequestBuilder;
 use nexus_test_utils::resource_helpers::create_disk;
 use nexus_test_utils::resource_helpers::create_ip_pool;
+use nexus_test_utils::resource_helpers::create_local_user;
+use nexus_test_utils::resource_helpers::create_silo;
+use nexus_test_utils::resource_helpers::grant_iam;
 use nexus_test_utils::resource_helpers::object_create;
 use nexus_test_utils::resource_helpers::objects_list_page_authz;
 use nexus_test_utils::resource_helpers::populate_ip_pool;
@@ -26,9 +29,11 @@ use omicron_common::api::external::InstanceState;
 use omicron_common::api::external::Ipv4Net;
 use omicron_common::api::external::Name;
 use omicron_common::api::external::NetworkInterface;
+use omicron_nexus::authz::SiloRole;
 use omicron_nexus::external_api::shared::IpKind;
 use omicron_nexus::external_api::shared::IpRange;
 use omicron_nexus::external_api::shared::Ipv4Range;
+use omicron_nexus::external_api::shared::SiloIdentityMode;
 use omicron_nexus::external_api::views;
 use omicron_nexus::TestInterfaces as _;
 use omicron_nexus::{external_api::params, Nexus};
@@ -2887,6 +2892,98 @@ async fn test_instance_ephemeral_ip_from_correct_pool(
         range, since the first is reserved for the default pool, not \
         the requested pool."
     );
+}
+
+#[nexus_test]
+async fn test_instance_create_in_silo(cptestctx: &ControlPlaneTestContext) {
+    let client = &cptestctx.external_client;
+
+    // Create a silo with a Collaborator User
+    let silo =
+        create_silo(&client, "authz", true, SiloIdentityMode::LocalOnly).await;
+    let user_id = create_local_user(
+        client,
+        &silo,
+        &"unpriv".parse().unwrap(),
+        params::UserPassword::InvalidPassword,
+    )
+    .await
+    .id;
+    grant_iam(
+        client,
+        "/system/silos/authz",
+        SiloRole::Collaborator,
+        user_id,
+        AuthnMode::PrivilegedUser,
+    )
+    .await;
+
+    // Populate IP Pool
+    populate_ip_pool(&client, "default", None).await;
+
+    // Create test organization and projects.
+    NexusRequest::objects_post(
+        client,
+        "/organizations",
+        &params::OrganizationCreate {
+            identity: IdentityMetadataCreateParams {
+                name: ORGANIZATION_NAME.parse().unwrap(),
+                description: String::new(),
+            },
+        },
+    )
+    .authn_as(AuthnMode::SiloUser(user_id))
+    .execute()
+    .await
+    .expect("failed to create Organization")
+    .parsed_body::<views::Organization>()
+    .expect("failed to parse new Organization");
+    NexusRequest::objects_post(
+        client,
+        &format!("/organizations/{ORGANIZATION_NAME}/projects"),
+        &params::ProjectCreate {
+            identity: IdentityMetadataCreateParams {
+                name: PROJECT_NAME.parse().unwrap(),
+                description: String::new(),
+            },
+        },
+    )
+    .authn_as(AuthnMode::SiloUser(user_id))
+    .execute()
+    .await
+    .expect("failed to create Project")
+    .parsed_body::<views::Project>()
+    .expect("failed to parse new Project");
+
+    // Create an instance using the authorization granted to the collaborator
+    // Silo User.
+    let instance_params = params::InstanceCreate {
+        identity: IdentityMetadataCreateParams {
+            name: Name::try_from(String::from("ip-pool-test")).unwrap(),
+            description: String::from("instance to test IP Pool authz"),
+        },
+        ncpus: InstanceCpuCount::try_from(2).unwrap(),
+        memory: ByteCount::from_gibibytes_u32(4),
+        hostname: String::from("inst"),
+        user_data: vec![],
+        network_interfaces: params::InstanceNetworkInterfaceAttachment::Default,
+        external_ips: vec![params::ExternalIpCreate::Ephemeral {
+            pool_name: Some(Name::try_from(String::from("default")).unwrap()),
+        }],
+        disks: vec![],
+        start: true,
+    };
+    let url_instances = format!(
+        "/organizations/{}/projects/{}/instances",
+        ORGANIZATION_NAME, PROJECT_NAME
+    );
+    NexusRequest::objects_post(client, &url_instances, &instance_params)
+        .authn_as(AuthnMode::SiloUser(user_id))
+        .execute()
+        .await
+        .expect("Failed to create instance")
+        .parsed_body::<Instance>()
+        .expect("Failed to parse instance");
 }
 
 async fn instance_get(