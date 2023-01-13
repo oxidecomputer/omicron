48494f17a25eb34db18346a507505d6a4a024848
Alan Hanson <alan@oxide.computer>
2023-01-13 19:58:12 +0000

    fix cargo fmt

--- sled-agent/src/sim/server.rs
+++ sled-agent/src/sim/server.rs
@@ -218,7 +218,7 @@ impl Server {
                 dns_server,
                 dns_dropshot_server,
             },
-            rack_init_request
+            rack_init_request,
         ))
     }
 
