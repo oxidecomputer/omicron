8cc5726b5d5248456a4fb225226652709f36b989
David Crespo <david-crespo@users.noreply.github.com>
2023-01-30 19:43:25 -0600

    Require `summary` on non-hidden Nexus endpoints (#2263)
    - add missing summaries to instance disks v1 endpoints
    - add test to require non-hidden endpoints to have summaries
    
    It's entirely possible this is easy to do in openapi-lint
    https://github.com/oxidecomputer/openapi-lint/issues/13 but it's _very_
    easy to do here and has a smaller blast radius since we're only
    requiring it for the external API, so it may actually be preferable.

--- nexus/src/external_api/http_entrypoints.rs
+++ nexus/src/external_api/http_entrypoints.rs
@@ -3271,6 +3271,7 @@ async fn instance_serial_console_stream(
     Ok(())
 }
 
+/// List an instance's disks
 #[endpoint {
     method = GET,
     path = "/v1/instances/{instance}/disks",
@@ -3357,6 +3358,7 @@ async fn instance_disk_list(
     apictx.external_latencies.instrument_dropshot_handler(&rqctx, handler).await
 }
 
+/// Attach a disk to an instance
 #[endpoint {
     method = POST,
     path = "/v1/instances/{instance}/disks/attach",
@@ -3422,6 +3424,7 @@ async fn instance_disk_attach(
     apictx.external_latencies.instrument_dropshot_handler(&rqctx, handler).await
 }
 
+/// Detach a disk from an instance
 #[endpoint {
     method = POST,
     path = "/v1/instances/{instance}/disks/detach",
