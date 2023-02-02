786ba6c2e4d4f948db03a57acb585ed0f46c5f74
Rain <rain@oxide.computer>
2023-02-02 14:17:34 -0800

    rustfmt
    Created using spr 1.3.4

--- tufaceous-lib/src/lib.rs
+++ tufaceous-lib/src/lib.rs
@@ -2,14 +2,14 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-mod artifact;
 mod archive;
+mod artifact;
 mod key;
 mod repository;
 mod root;
 mod target;
 
-pub use artifact::*;
 pub use archive::*;
+pub use artifact::*;
 pub use key::*;
 pub use repository::*;
