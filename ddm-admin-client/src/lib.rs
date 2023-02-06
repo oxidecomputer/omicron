676ae1b72a8a70a575e790527c34001a354df199
Adam H. Leventhal <ahl@oxide.computer>
2023-02-06 14:41:29 -0800

    ignore more clippy

--- ddm-admin-client/src/lib.rs
+++ ddm-admin-client/src/lib.rs
@@ -2,11 +2,12 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-// Copyright 2022 Oxide Computer Company
+// Copyright 2023 Oxide Computer Company
 
 #![allow(clippy::redundant_closure_call)]
 #![allow(clippy::needless_lifetimes)]
 #![allow(clippy::match_single_binding)]
+#![allow(clippy::clone_on_copy)]
 
 include!(concat!(env!("OUT_DIR"), "/ddm-admin-client.rs"));
 
