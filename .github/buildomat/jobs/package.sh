77fb889362314eea5b95900f934d3e3165019262
Sean Klein <sean@oxide.computer>
2023-03-03 12:48:08 -0500

    Package all globzl zone tarballs into single tarball

--- .github/buildomat/jobs/package.sh
+++ .github/buildomat/jobs/package.sh
@@ -5,10 +5,14 @@
 #: target = "helios-latest"
 #: rust_toolchain = "1.66.1"
 #: output_rules = [
-#:	"=/work/package.tar.gz",
+#:	"=/work/global-zone-packages.tar.gz",
 #:	"=/work/zones/*.tar.gz",
 #: ]
 #:
+#: [[publish]]
+#: series = "image"
+#: name = "global-zone-packages"
+#: from_output = "/out/global-zone-packages.tar.gz"
 
 set -o errexit
 set -o pipefail
@@ -17,18 +21,33 @@ set -o xtrace
 cargo --version
 rustc --version
 
+# Build
 ptime -m ./tools/install_builder_prerequisites.sh -yp
-ptime -m ./tools/create_self_signed_cert.sh -yp
-
 ptime -m cargo run --locked --release --bin omicron-package -- package
 
-files=(
-	out/*.tar
-	package-manifest.toml
-	smf/sled-agent/config.toml
-	target/release/omicron-package
-	tools/create_virtual_hardware.sh
-)
-ptime -m tar cvzf /work/package.tar.gz "${files[@]}"
+# Assemble global zone files in a temporary directory.
+tmp=$(mktemp -d)
+mkdir -p "${tmp}/sled-agent"
+tar -xvzf out/omicron-sled-agent.tar -C "${tmp}/sled-agent"
+mkdir -p "${tmp}/maghemite"
+tar -xvzf out/maghemite.tar -C "${tmp}/maghemite"
+
+# Load those global zone files into a tarball that's ready to be exported.
+mkdir -p /work
+ptime -m tar cvzf /work/global-zone-packages.tar.gz -C "${tmp}" .
+
+# Assemble Zone Images into their respective output locations.
 mkdir -p /work/zones
-mv out/*.tar.gz /work/zones/
+zones=(
+	out/clickhouse.tar.gz
+	out/cockroachdb.tar.gz
+	out/crucible-pantry.tar.gz
+	out/crucible.tar.gz
+	out/external-dns.tar.gz
+	out/internal-dns.tar.gz
+	out/omicron-nexus.tar.gz
+	out/oximeter-collector.tar.gz
+	out/propolis-server.tar.gz
+	out/switch-asic.tar.gz
+)
+mv "${zones[@]}" /work/zones/
