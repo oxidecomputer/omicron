8ff7935bf9fe1f2a78397a0a92d7d6988982821a
Andrew J. Stone <andrew.j.stone.1@gmail.com>
2023-03-07 00:01:35 +0000

    Run update posts in their own task

--- wicket/src/wicketd.rs
+++ wicket/src/wicketd.rs
@@ -7,11 +7,9 @@
 use slog::{o, warn, Logger};
 use std::convert::From;
 use std::net::SocketAddrV6;
-use tokio::sync::mpsc::{self, Receiver, Sender, UnboundedSender};
+use tokio::sync::mpsc::{self, Sender, UnboundedSender};
 use tokio::time::{interval, Duration, Instant, MissedTickBehavior};
-use wicketd_client::types::{
-    RackV1Inventory, SpIdentifier, SpType, UpdateLogAll,
-};
+use wicketd_client::types::{RackV1Inventory, SpIdentifier, SpType};
 use wicketd_client::GetInventoryResponse;
 
 use crate::state::ComponentId;
@@ -41,7 +39,8 @@ const WICKETD_TIMEOUT: Duration = Duration::from_millis(1000);
 // large.
 const CHANNEL_CAPACITY: usize = 1000;
 
-// Eventually this will be filled in with things like triggering updates from the UI
+/// Requests driven by the UI and sent from [`crate::Runner`] to [`WicketdManager`]
+#[allow(unused)]
 #[derive(Debug)]
 pub enum Request {
     StartUpdate(ComponentId),
@@ -58,7 +57,6 @@ pub struct WicketdManager {
     rx: mpsc::Receiver<Request>,
     events_tx: UnboundedSender<Event>,
     wicketd_addr: SocketAddrV6,
-    update_client: wicketd_client::Client,
 }
 
 impl WicketdManager {
@@ -69,11 +67,8 @@ impl WicketdManager {
     ) -> (WicketdHandle, WicketdManager) {
         let log = log.new(o!("component" => "WicketdManager"));
         let (tx, rx) = tokio::sync::mpsc::channel(CHANNEL_CAPACITY);
-        let update_client =
-            create_wicketd_client(&log, wicketd_addr, WICKETD_TIMEOUT);
         let handle = WicketdHandle { tx };
-        let manager =
-            WicketdManager { log, rx, events_tx, wicketd_addr, update_client };
+        let manager = WicketdManager { log, rx, events_tx, wicketd_addr };
 
         (handle, manager)
     }
@@ -94,10 +89,7 @@ impl WicketdManager {
                 Some(request) = self.rx.recv() => {
                     slog::info!(self.log, "Got wicketd req: {:?}", request);
                     let Request::StartUpdate(component_id) = request;
-                    let sp: SpIdentifier = component_id.into();
-                    let res = self.update_client.post_start_update(sp.type_, sp.slot).await;
-                    // TODO: Better error handling
-                    slog::info!(self.log,  "Update response: {:?}", res);
+                    self.start_update(component_id).await;
                 }
                 else => {
                     slog::info!(self.log, "Request receiver closed. Process must be exiting.");
@@ -105,6 +97,24 @@ impl WicketdManager {
                 }
             }
         }
+    }
+
+    async fn start_update(&self, component_id: ComponentId) {
+        let log = self.log.clone();
+        let tx = self.events_tx.clone();
+        let addr = self.wicketd_addr;
+        tokio::spawn(async move {
+            let update_client =
+                create_wicketd_client(&log, addr, WICKETD_TIMEOUT);
+            let sp: SpIdentifier = component_id.into();
+            let res = update_client.post_start_update(sp.type_, sp.slot).await;
+            // We don't return errors or success values, as there's nobody to
+            // return them to. Instead, all updates are periodically polled
+            // and global state mutated. This allows the update pane to
+            // report current status to users in a more detailed and holistic
+            // fashion.
+            slog::info!(log, "Update response for {}: {:?}", component_id, res);
+        });
     }
 
     async fn poll_artifacts(&self) {
