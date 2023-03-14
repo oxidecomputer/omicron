6ebba04e60cb3a6e6c018a19ef9a46c740560c37
Justin Bennett <oxide@just-be.dev>
2023-03-14 13:27:07 -0400

    `/v1/` current user endpoints (#2528)
    Part of #2028
    
    I've taken some artistic privileges here. Previously we had
    `/session/me/*`. Nothing else is hanging under `/session/` nor have I
    seen a proposal for anything to go there. In my investigations of other
    APIs folks have handled retrieving the current user in different ways:
    
    - GitHub uses [`GET
    /user`](https://docs.github.com/en/rest/users/users?apiVersion=2022-11-28#get-the-authenticated-user).
    - AWS uses [`GET
    /api/v1/me`](https://docs.aws.amazon.com/workdocs/latest/APIReference/API_GetCurrentUser.html)
    - Stack Exchange uses [`GET /me`](https://api.stackexchange.com/docs/me)
    - Cloudflare uses [`GET /user` (sort
    of)](https://api.cloudflare.com/#user-properties)
    
    ~I've done `/v1/current-user` here which is admittedly a bit verbose.
    It's somewhat self documenting though. I'm happy to change it back to
    `/me` or whatever else. The one thing about `/me` is that the operation
    names are strange. `me_ssh_keys_list` vs `current_user_ssh_keys`~
    
    After polling some folks it seems like `/v1/me` is the favored route, so
    I just went with that.

--- nexus/src/app/silo.rs
+++ nexus/src/app/silo.rs
@@ -558,6 +558,31 @@ impl super::Nexus {
     }
 
     // SSH Keys
+    pub fn ssh_key_lookup<'a>(
+        &'a self,
+        opctx: &'a OpContext,
+        ssh_key_selector: &'a params::SshKeySelector,
+    ) -> LookupResult<lookup::SshKey<'a>> {
+        match ssh_key_selector {
+            params::SshKeySelector {
+                silo_user_id: _,
+                ssh_key: NameOrId::Id(id),
+            } => {
+                let ssh_key =
+                    LookupPath::new(opctx, &self.db_datastore).ssh_key_id(*id);
+                Ok(ssh_key)
+            }
+            params::SshKeySelector {
+                silo_user_id,
+                ssh_key: NameOrId::Name(name),
+            } => {
+                let ssh_key = LookupPath::new(opctx, &self.db_datastore)
+                    .silo_user_id(*silo_user_id)
+                    .ssh_key_name(Name::ref_cast(name));
+                Ok(ssh_key)
+            }
+        }
+    }
 
     pub async fn ssh_key_create(
         &self,
@@ -578,7 +603,7 @@ impl super::Nexus {
         &self,
         opctx: &OpContext,
         silo_user_id: Uuid,
-        page_params: &DataPageParams<'_, Name>,
+        page_params: &PaginatedBy<'_>,
     ) -> ListResultVec<SshKey> {
         let (.., authz_user) = LookupPath::new(opctx, &self.datastore())
             .silo_user_id(silo_user_id)
@@ -588,34 +613,15 @@ impl super::Nexus {
         self.db_datastore.ssh_keys_list(opctx, &authz_user, page_params).await
     }
 
-    pub async fn ssh_key_fetch(
-        &self,
-        opctx: &OpContext,
-        silo_user_id: Uuid,
-        ssh_key_name: &Name,
-    ) -> LookupResult<SshKey> {
-        let (.., ssh_key) = LookupPath::new(opctx, &self.datastore())
-            .silo_user_id(silo_user_id)
-            .ssh_key_name(ssh_key_name)
-            .fetch()
-            .await?;
-        assert_eq!(ssh_key.name(), &ssh_key_name.0);
-        Ok(ssh_key)
-    }
-
     pub async fn ssh_key_delete(
         &self,
         opctx: &OpContext,
         silo_user_id: Uuid,
-        ssh_key_name: &Name,
+        ssh_key_lookup: &lookup::SshKey<'_>,
     ) -> DeleteResult {
-        let (.., authz_user, authz_ssh_key) =
-            LookupPath::new(opctx, &self.datastore())
-                .silo_user_id(silo_user_id)
-                .ssh_key_name(ssh_key_name)
-                .lookup_for(authz::Action::Delete)
-                .await?;
-        assert_eq!(authz_user.id(), silo_user_id);
+        let (.., authz_silo_user, authz_ssh_key) =
+            ssh_key_lookup.lookup_for(authz::Action::Delete).await?;
+        assert_eq!(authz_silo_user.id(), silo_user_id);
         self.db_datastore.ssh_key_delete(opctx, &authz_ssh_key).await
     }
 
