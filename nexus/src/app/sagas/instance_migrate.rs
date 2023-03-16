9a731557e585691f5057b5b43aa36707d10d99b3
James MacMahon <james@oxide.computer>
2023-03-16 16:38:01 -0400

    perform create_instance_v2p_mappings before sim_instance_migrate

--- nexus/src/app/sagas/instance_migrate.rs
+++ nexus/src/app/sagas/instance_migrate.rs
@@ -44,6 +44,10 @@ declare_saga_actions! {
     MIGRATE_PREP -> "migrate_instance" {
         + sim_migrate_prep
     }
+    V2P_ENSURE -> "v2p_ensure" {
+        // TODO robustness: This needs an undo action
+        + sim_v2p_ensure
+    }
     INSTANCE_MIGRATE -> "instance_migrate" {
         // TODO robustness: This needs an undo action
         + sim_instance_migrate
@@ -85,6 +89,7 @@ impl NexusSaga for SagaInstanceMigrate {
 
         builder.append(allocate_propolis_ip_action());
         builder.append(migrate_prep_action());
+        builder.append(v2p_ensure_action());
         builder.append(instance_migrate_action());
         builder.append(cleanup_source_action());
 
@@ -134,6 +139,40 @@ async fn sim_allocate_propolis_ip(
         &params.serialized_authn,
     );
     allocate_sled_ipv6(&opctx, sagactx, "dst_sled_uuid").await
+}
+
+/// Add V2P mappings for the destination instance
+async fn sim_v2p_ensure(
+    sagactx: NexusActionContext,
+) -> Result<(), ActionError> {
+    let osagactx = sagactx.user_data();
+    let params = sagactx.saga_params::<Params>()?;
+    let opctx = crate::context::op_context_for_saga_action(
+        &sagactx,
+        &params.serialized_authn,
+    );
+    let (instance_id, _) =
+        sagactx.lookup::<(Uuid, InstanceRuntimeState)>("migrate_instance")?;
+
+    // TODO-performance the instance_put in sim_instance_migrate will *start* a
+    // migration, but the source and destination propolis servers will perform
+    // it asynchronously. Updating the mappings here will briefly "disconnect"
+    // the source instance in the sense that it will be able to send packets out
+    // (as other instance's V2P mappings will be untouched) but will not be able
+    // to receive any packets (other instances will send packets to the
+    // destination propolis' sled but the vCPUs may not have started yet). Until
+    // the destination propolis takes over, there will be a small network outage
+    // for the instance.
+    //
+    // TODO-correctness if the migration fails, there's nothing that will unwind
+    // this and restore the original V2P mappings
+    osagactx
+        .nexus()
+        .create_instance_v2p_mappings(&opctx, instance_id)
+        .await
+        .map_err(ActionError::action_failed)?;
+
+    Ok(())
 }
 
 async fn sim_instance_migrate(
@@ -258,26 +297,6 @@ async fn sim_instance_migrate(
     osagactx
         .datastore()
         .instance_update_runtime(&instance_id, &new_runtime_state.into())
-        .await
-        .map_err(ActionError::action_failed)?;
-
-    // Add V2P mappings for destination instance
-    //
-    // TODO-performance the instance_put above will *start* a migration, but the
-    // source and destination propolis servers will perform it asynchronously.
-    // Updating the mappings here will briefly "disconnect" the source instance
-    // in the sense that it will be able to send packets out (as other
-    // instance's V2P mappings will be untouched) but will not be able to
-    // receive any packet (other instances will send packets to the destination
-    // propolis' sled but the vCPUs may not have started yet). Until the
-    // destination propolis takes over, there will be a small network outage for
-    // the instance.
-    //
-    // TODO-correctness if the migration fails, there's nothing that will unwind
-    // this and restore the original V2P mappings
-    osagactx
-        .nexus()
-        .create_instance_v2p_mappings(&opctx, instance_id)
         .await
         .map_err(ActionError::action_failed)?;
 
