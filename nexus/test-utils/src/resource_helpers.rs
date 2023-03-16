d407220d0fd8b235d34c38310791709da17569a5
Justin Bennett <git@just-be.dev>
2023-03-16 13:42:47 -0400

    Update more orgs/instances/etc

--- nexus/test-utils/src/resource_helpers.rs
+++ nexus/test-utils/src/resource_helpers.rs
@@ -305,7 +305,7 @@ pub async fn create_disk(
     disk_name: &str,
 ) -> Disk {
     let url = format!(
-        "/organizations/{}/projects/{}/disks",
+        "/v1/disks?organization={}&project={}",
         organization_name, project_name
     );
     object_create(
@@ -332,8 +332,8 @@ pub async fn delete_disk(
     disk_name: &str,
 ) {
     let url = format!(
-        "/organizations/{}/projects/{}/disks/{}",
-        organization_name, project_name, disk_name
+        "/v1/disks/{}?organization={}&project={}",
+        disk_name, organization_name, project_name,
     );
     object_delete(client, &url).await
 }
@@ -404,7 +404,7 @@ pub async fn create_vpc(
     object_create(
         &client,
         format!(
-            "/organizations/{}/projects/{}/vpcs",
+            "/v1/vpcs?organization={}&project={}",
             &organization_name, &project_name
         )
         .as_str(),
@@ -434,7 +434,7 @@ pub async fn create_vpc_with_error(
             client,
             Method::POST,
             format!(
-                "/organizations/{}/projects/{}/vpcs",
+                "/v1/vpcs?organization={}&project={}",
                 &organization_name, &project_name
             )
             .as_str(),
@@ -467,7 +467,7 @@ pub async fn create_router(
     NexusRequest::objects_post(
         &client,
         format!(
-            "/organizations/{}/projects/{}/vpcs/{}/routers",
+            "/v1/vpc-routers?organization={}&project={}&vpc={}",
             &organization_name, &project_name, &vpc_name
         )
         .as_str(),
