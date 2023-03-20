3989ccc41bc73995a12c271704b34a1b80404079
Andrew J. Stone <andrew.j.stone.1@gmail.com>
2023-03-20 17:31:22 +0000

    wip

--- wicket-dbg/src/server.rs
+++ wicket-dbg/src/server.rs
@@ -3,7 +3,6 @@
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
 use crate::Cmd;
-use std::io::{Read, Write};
 use std::net::{TcpListener, TcpStream, ToSocketAddrs};
 use std::path::PathBuf;
 use wicket::Snapshot;
@@ -35,13 +34,7 @@ impl Server {
 
     fn handle_client(&mut self, mut stream: TcpStream) -> anyhow::Result<()> {
         loop {
-            // Read 4 byte le size header, followed by data
-            let mut size_buf = [0u8; 4];
-            stream.read_exact(&mut size_buf[..])?;
-            let size = u32::from_le_bytes(size_buf) as usize;
-            let mut buf = vec![0; size];
-            stream.read_exact(&mut buf)?;
-            let cmd: Cmd = bincode::deserialize(&mut buf)?;
+            let cmd: Cmd = bincode::deserialize_from(&mut stream)?;
             println!("{:#?}", cmd);
             self.handle(cmd, &mut stream)?;
         }
@@ -56,11 +49,7 @@ impl Server {
             Cmd::Load(file) => {
                 self.recording_path = Some(file);
                 let res: Result<(), String> = Ok(());
-                // write 4 byte le size header, followed by data
-                let size: u32 = bincode::serialized_size(&res)?.try_into()?;
-                stream.write_all(&size.to_le_bytes())?;
-                let buf = bincode::serialize(&res)?;
-                stream.write_all(&buf)?;
+                bincode::serialize_into(stream, &res)?;
             }
             _ => unreachable!(),
         }
