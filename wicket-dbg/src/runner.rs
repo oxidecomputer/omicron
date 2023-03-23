3c2737dedda281ea7710d57371a7084e28ec95e6
Andrew J. Stone <andrew.j.stone.1@gmail.com>
2023-03-23 18:15:18 +0000

    Fix doctests

--- wicket-dbg/src/runner.rs
+++ wicket-dbg/src/runner.rs
@@ -2,7 +2,7 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-//! An executor for [`wicket::Event`]s in the [`wicket_dbg::Server`]
+//! An executor for [`wicket::Event`]s in the [`crate::Server`]
 
 use anyhow::bail;
 use crossterm::execute;
