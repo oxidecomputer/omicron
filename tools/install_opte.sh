31f59aa638e573d2d4fe76e570917290695ac254
Luqman Aden <luqman@oxide.computer>
2023-03-28 00:39:31 -0700

    Use package version and helios-netdev pkg repo to install opte instead of grabbing p5p from CI.

--- tools/install_opte.sh
+++ tools/install_opte.sh
@@ -20,73 +20,27 @@ fi
 SOURCE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 cd "${SOURCE_DIR}/.."
 OMICRON_TOP="$PWD"
-OUT_DIR="$OMICRON_TOP/out"
-XDE_DIR="$OUT_DIR/xde"
-mkdir -p "$XDE_DIR"
 
-# Create a temporary directory in which to download artifacts, removing it on
-# exit
-DOWNLOAD_DIR="$(mktemp -t -d)"
-trap remove_download_dir EXIT ERR
-
-function remove_download_dir {
-    rm -rf $DOWNLOAD_DIR
-}
-
-# Compute the SHA256 of the path in $1, returning just the sum
-function file_sha {
-    sha256sum "$1" | cut -d ' ' -f 1
-}
-
-# Download a file from $1 and compare its sha256 to the value provided in $2
-function download_and_check_sha {
-    local URL="$1"
-    local SHA="$2"
-    local FILENAME="$(basename "$URL")"
-    local DOWNLOAD_PATH="$(mktemp -p $DOWNLOAD_DIR)"
-    local OUT_PATH="$XDE_DIR/$FILENAME"
-
-    # Check if the file already exists, with the expected SHA
-    if ! [[ -f "$OUT_PATH" ]] || [[ "$SHA" != "$(file_sha "$OUT_PATH")" ]]; then
-        curl -L -o "$DOWNLOAD_PATH" "$URL" 2> /dev/null
-        local ACTUAL_SHA="$(sha256sum "$DOWNLOAD_PATH" | cut -d ' ' -f 1)"
-        if [[ "$ACTUAL_SHA" != "$SHA" ]]; then
-            echo "SHA mismatch downloding file $FILENAME"
-            exit 1
-        fi
-        mv -f "$DOWNLOAD_PATH" "$OUT_PATH"
-        echo "\"$OUT_PATH\" downloaded and has verified SHA"
-    else
-        echo "\"$OUT_PATH\" already exists with correct SHA"
-    fi
-}
-
-# Download a SHA-256 sum output from $1 and return just the SHA
-function sha_from_url {
-    local SHA_URL="$1"
-    curl -L "$SHA_URL" 2> /dev/null | cut -d ' ' -f 1
-}
-
-# Add the publisher specified by the provided path. If that publisher already
-# exists, set the origin instead. If more than one publisher with that name
-# exists, abort with an error.
-function add_publisher {
-    local ARCHIVE_PATH="$1"
-    local PUBLISHER_NAME="$(pkgrepo info -H -s "$ARCHIVE_PATH" | cut -d ' ' -f 1)"
+# Add the `helios-netdev` publisher, which provides the xde kernel driver
+# and the `opteadm` userland tool for interacting with it. If that publisher
+# already exists (e.g. from a previous path based setup), update it.
+function add_netdev_publisher {
+    local PUBLISHER_NAME="helios-netdev"
+    local PUBLISHER_ORIGIN="https://pkg.oxide.computer/helios-netdev/"
     local N_PUBLISHERS="$(pkg publisher | grep -c "$PUBLISHER_NAME")"
+
     if [[ "$N_PUBLISHERS" -gt 1 ]]; then
         echo "More than one publisher named \"$PUBLISHER_NAME\" found"
         echo "Removing all publishers and installing from scratch"
         pfexec pkg unset-publisher "$PUBLISHER_NAME"
-        pfexec pkg set-publisher -p "$ARCHIVE_PATH" --search-first
     elif [[ "$N_PUBLISHERS" -eq 1 ]]; then
         echo "Publisher \"$PUBLISHER_NAME\" already exists, setting"
-        echo "the origin to "$ARCHIVE_PATH""
-        pfexec pkg set-publisher --origin-uri "$ARCHIVE_PATH" --search-first "$PUBLISHER_NAME"
+        echo "the origin to \"$PUBLISHER_ORIGIN\""
     else
         echo "Publisher \"$PUBLISHER_NAME\" does not exist, adding"
-        pfexec pkg set-publisher -p "$ARCHIVE_PATH" --search-first
     fi
+
+    pfexec pkg set-publisher -O "$PUBLISHER_ORIGIN" $PUBLISHER_NAME
 }
 
 # The xde driver no longer requires separate kernel bits as of API version 21
@@ -116,25 +70,21 @@ if [[ "$RC" -ne 0 ]]; then
     exit 1
 fi
 
-# `helios-netdev` provides the xde kernel driver and the `opteadm` userland tool
-# for interacting with it.
-HELIOS_NETDEV_BASE_URL="https://buildomat.eng.oxide.computer/public/file/oxidecomputer/opte/repo"
-HELIOS_NETDEV_COMMIT="f36a283e91f228a542149ee9230126e9c4bb5e8b"
-HELIOS_NETDEV_REPO_URL="$HELIOS_NETDEV_BASE_URL/$HELIOS_NETDEV_COMMIT/opte.p5p"
-HELIOS_NETDEV_REPO_SHA_URL="$HELIOS_NETDEV_BASE_URL/$HELIOS_NETDEV_COMMIT/opte.p5p.sha256"
-HELIOS_NETDEV_REPO_PATH="$XDE_DIR/$(basename "$HELIOS_NETDEV_REPO_URL")"
+# Add the netdev publisher (if it doesn't already exist)
+add_netdev_publisher
 
-# Download and verify the package repositories
-download_and_check_sha "$HELIOS_NETDEV_REPO_URL" "$(sha_from_url "$HELIOS_NETDEV_REPO_SHA_URL")"
-
-# Add the OPTE and xde repositories and update packages.
-add_publisher "$HELIOS_NETDEV_REPO_PATH"
+# Grab the version of the opte package to install
+OPTE_VERSION="$(cat "$OMICRON_TOP/tools/opte_version")"
 
 # Actually install the xde kernel module and opteadm tool
 RC=0
-pfexec pkg install -v pkg://helios-netdev/driver/network/opte || RC=$?
-if [[ "$RC" -ne 0 ]] && [[ "$RC" -ne 4 ]]; then
-    echo "Installing xde kernel driver and opteadm tool failed"
+pfexec pkg install -v pkg://helios-netdev/driver/network/opte@$OPTE_VERSION || RC=$?
+if [[ "$RC" -eq 0 ]]; then
+    echo "xde driver installed successfully"
+elif [[ "$RC" -eq 4 ]]; then
+    echo "Correct xde driver already installed"
+else
+    echo "Installing xde driver failed"
     exit "$RC"
 fi
 
