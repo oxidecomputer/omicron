d5983f3aa0567b61eb3b217d7a18ee439d04359e
John Gallagher <john@oxidecomputer.com>
2023-04-07 16:24:42 -0400

    fix test

--- wicketd/tests/integration_tests/inventory.rs
+++ wicketd/tests/integration_tests/inventory.rs
@@ -9,6 +9,7 @@ use std::time::Duration;
 use super::setup::WicketdTestContext;
 use gateway_messages::SpPort;
 use gateway_test_utils::setup as gateway_setup;
+use wicketd_client::types::GetInventoryParams;
 use wicketd_client::GetInventoryResponse;
 
 #[tokio::test]
@@ -16,13 +17,13 @@ async fn test_inventory() {
     let gateway =
         gateway_setup::test_setup("test_inventory", SpPort::One).await;
     let wicketd_testctx = WicketdTestContext::setup(gateway).await;
-    let force_refresh = false;
+    let params = GetInventoryParams { force_refresh: Vec::new() };
 
     let inventory_fut = async {
         loop {
             let response = wicketd_testctx
                 .wicketd_client
-                .get_inventory(force_refresh)
+                .get_inventory(&params)
                 .await
                 .expect("get_inventory succeeded")
                 .into_inner();
