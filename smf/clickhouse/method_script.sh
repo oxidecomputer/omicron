b5a9ec9629a189aae6bcdb4375e60291360164d2
Sean Klein <sean@oxide.computer>
2023-04-20 00:50:04 -0400

    More method script cleanup

--- smf/clickhouse/method_script.sh
+++ smf/clickhouse/method_script.sh
@@ -4,11 +4,18 @@ set -x
 set -o errexit
 set -o pipefail
 
+. /lib/svc/share/smf_include.sh
+
 LISTEN_ADDR="$(svcprop -c -p config/listen_addr "${SMF_FMRI}")"
 LISTEN_PORT="$(svcprop -c -p config/listen_port "${SMF_FMRI}")"
 DATASTORE="$(svcprop -c -p config/store "${SMF_FMRI}")"
 DATALINK="$(svcprop -c -p config/datalink "${SMF_FMRI}")"
 GATEWAY="$(svcprop -c -p config/gateway "${SMF_FMRI}")"
+
+if [[ $DATALINK == unknown ]] || [[ $GATEWAY == unknown ]]; then
+    printf 'ERROR: missing datalink or gateway\n' >&2
+    exit "$SMF_EXIT_ERR_CONFIG"
+fi
 
 ipadm show-addr "$DATALINK/ll" || ipadm create-addr -t -T addrconf "$DATALINK/ll"
 ipadm show-addr "$DATALINK/omicron6"  || ipadm create-addr -t -T static -a "$LISTEN_ADDR" "$DATALINK/omicron6"
@@ -23,4 +30,4 @@ args=(
   "--http_port" "$LISTEN_PORT"
 )
 
-exec /opt/oxide/clickhouse/clickhouse server "${args[@]}"
+exec /opt/oxide/clickhouse/clickhouse server "${args[@]}" &
