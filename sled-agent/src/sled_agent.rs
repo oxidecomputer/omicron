19403d985bf8c3c965e392d52439d5cb3da00af5
Andrew J. Stone <andrew.j.stone.1@gmail.com>
2023-04-28 06:34:14 +0000

    wip

--- sled-agent/src/sled_agent.rs
+++ sled-agent/src/sled_agent.rs
@@ -157,7 +157,7 @@ impl KeyFile {
         // If we truncate we may leave dirty pages around
         // containing secrets.
         let mut file = tokio::fs::OpenOptions::new()
-            .create(true)
+            .write(true)
             .truncate(true)
             .open(&path.0)
             .await?;
