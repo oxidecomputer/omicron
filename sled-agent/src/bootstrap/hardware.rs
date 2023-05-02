9d261a538212ee046c4fdc771620d18d1864304f
John Gallagher <john@oxidecomputer.com>
2023-05-02 14:11:40 -0400

    [wicketd] Refuse to update the sled where wicketd is running (#2940)
    wicketd now refuses to update the sled on which it is running, by
    comparing the baseboard info it retries from the SP of the to-be-updated
    sled against the baseboard info of its own host. To do this, we plumb that
    baseboard info through sled-agent down into wicketd. A nontrivial
    byproduct of this is the addition of `#[serde(skip)]` on several
    `ServiceType` variants that should not be passed in from external
    clients (i.e., services that sled agent itself is responsible for
    starting autonomously, either in the gz (maghemite) or in the switch
    zone (the rest)).

--- sled-agent/src/bootstrap/hardware.rs
+++ sled-agent/src/bootstrap/hardware.rs
@@ -68,8 +68,12 @@ impl HardwareMonitorWorker {
                     match update {
                         Ok(update) => match update {
                             sled_hardware::HardwareUpdate::TofinoLoaded => {
+                                let baseboard = self.hardware.baseboard();
                                 let switch_zone_ip = None;
-                                if let Err(e) = self.services.activate_switch(switch_zone_ip).await {
+                                if let Err(e) = self.services.activate_switch(
+                                    switch_zone_ip,
+                                    baseboard,
+                                ).await {
                                     warn!(self.log, "Failed to activate switch: {e}");
                                 }
                             }
@@ -107,8 +111,10 @@ impl HardwareMonitorWorker {
     async fn full_hardware_scan(&self) {
         info!(self.log, "Performing full hardware scan");
         if self.hardware.is_scrimlet_driver_loaded() {
+            let baseboard = self.hardware.baseboard();
             let switch_zone_ip = None;
-            if let Err(e) = self.services.activate_switch(switch_zone_ip).await
+            if let Err(e) =
+                self.services.activate_switch(switch_zone_ip, baseboard).await
             {
                 warn!(self.log, "Failed to activate switch: {e}");
             }
--- sled-agent/src/sled_agent.rs
+++ sled-agent/src/sled_agent.rs
@@ -328,9 +328,13 @@ impl SledAgent {
         let scrimlet = self.inner.hardware.is_scrimlet_driver_loaded();
 
         if scrimlet {
+            let baseboard = self.inner.hardware.baseboard();
             let switch_zone_ip = Some(self.inner.switch_zone_ip());
-            if let Err(e) =
-                self.inner.services.activate_switch(switch_zone_ip).await
+            if let Err(e) = self
+                .inner
+                .services
+                .activate_switch(switch_zone_ip, baseboard)
+                .await
             {
                 warn!(log, "Failed to activate switch: {e}");
             }
@@ -368,11 +372,12 @@ impl SledAgent {
                         self.notify_nexus_about_self(&log);
                     }
                     HardwareUpdate::TofinoLoaded => {
+                        let baseboard = self.inner.hardware.baseboard();
                         let switch_zone_ip = Some(self.inner.switch_zone_ip());
                         if let Err(e) = self
                             .inner
                             .services
-                            .activate_switch(switch_zone_ip)
+                            .activate_switch(switch_zone_ip, baseboard)
                             .await
                         {
                             warn!(log, "Failed to activate switch: {e}");
