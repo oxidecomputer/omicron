60f64faf81853061e46b9e82d777bcc78b82bae4
Andrew J. Stone <andrew.j.stone.1@gmail.com>
2023-05-02 18:41:24 +0000

    wip

--- key-manager/src/lib.rs
+++ key-manager/src/lib.rs
@@ -4,9 +4,11 @@
 
 //! A crate used to derive keys useful for the Oxide control plane
 
+use std::collections::BTreeMap;
+
 use async_trait::async_trait;
 use hkdf::Hkdf;
-use secrecy::Secret;
+use secrecy::{ExposeSecret, Secret};
 use sha3::Sha3_256;
 use sled_hardware::DiskIdentity;
 use zeroize::{Zeroize, ZeroizeOnDrop};
@@ -16,21 +18,29 @@ use zeroize::{Zeroize, ZeroizeOnDrop};
 /// This should never be used directly, and always wrapped in a `Secret<Ikm>`
 /// upon construction.
 #[derive(Zeroize, ZeroizeOnDrop)]
-pub struct Ikm(Box<[u8; 32]>);
+pub struct Ikm(pub Box<[u8; 32]>);
 
 /// Secret Input Key Material for a given rack reconfiguration epoch
 pub struct VersionedIkm {
-    epoch: u64,
-    ikm: Secret<Ikm>,
+    pub epoch: u64,
+    pub salt: [u8; 32],
+    pub ikm: Secret<Ikm>,
+}
+
+impl VersionedIkm {
+    pub fn new(epoch: u64, salt: [u8; 32], data: &[u8; 32]) -> VersionedIkm {
+        let ikm = Secret::new(Ikm(Box::new(*data)));
+        VersionedIkm { epoch, salt, ikm }
+    }
 }
 
 /// A derived PRK for a given rack reconfiguration epoch
-pub struct VersionedPrk {
-    epoch: u64,
+struct VersionedPrk {
+    pub epoch: u64,
 
     /// The wrapper around a pseudo-random key(PRK) created via HKDF-Extract.
     /// This key is used to derive application level keys via HKDF-Expand
-    prk: Hkdf<Sha3_256>,
+    pub prk: Hkdf<Sha3_256>,
 }
 
 /// An error returned by the [`KeyManager`]
@@ -40,11 +50,11 @@ pub enum KeyManagerError {
 
 /// Derived Disk Encryption key
 #[derive(Zeroize, ZeroizeOnDrop)]
-pub struct Aes256GcmDiskEncryptionKey(Box<[u8; 32]>);
+pub struct Aes256GcmDiskEncryptionKey(pub Box<[u8; 32]>);
 
 pub struct VersionedAes256GcmDiskEncryptionKey {
-    epoch: u64,
-    key: Secret<Aes256GcmDiskEncryptionKey>,
+    pub epoch: u64,
+    pub key: Secret<Aes256GcmDiskEncryptionKey>,
 }
 
 /// The main mechanism used to derive keys from a shared secret for the oxide
@@ -57,29 +67,44 @@ pub struct KeyManager<S: SecretRetriever> {
     // A mechanism for retrieving input key material
     secret_retriever: S,
 
-    /// In the common case, we will only have a single `prk1` for everything.
+    /// In the common case, we will only have a single PRK for everything.
     ///
-    /// If there is an ongoing reconfiguration, we will have at least 2 prks.
+    /// If there is an ongoing reconfiguration, we will have at least 2 PRKs.
     /// In some cases of failure while multiple reconfigurations have taken
-    /// place, we may have multiple Prks. Policy should limit the number of
+    /// place, we may have multiple PRKs. Policy should limit the number of
     /// allowed reconfigurations before a node is no longer part of the trust
     /// quorum. This should most likely be a small number like `3`.
-    prks: Vec<VersionedPrk>,
+    prks: BTreeMap<u64, Hkdf<Sha3_256>>,
 }
 
 impl<S: SecretRetriever> KeyManager<S> {
-    fn new(secret_retriever: S) -> KeyManager<S> {
-        KeyManager { secret_retriever, prks: vec![] }
+    pub fn new(secret_retriever: S) -> KeyManager<S> {
+        KeyManager { secret_retriever, prks: BTreeMap::new() }
     }
 
     /// Load latest version of the input key material into the key manager.
     async fn load_latest_secret(&mut self) -> Result<(), S::Error> {
-        unimplemented!()
+        let ikm = self.secret_retriever.get_latest().await?;
+        self.insert_prk(ikm);
+        Ok(())
     }
 
     /// Load input key material for the given epoch into the key manager.
     async fn load_secret(&mut self, epoch: u64) -> Result<(), S::Error> {
-        unimplemented!()
+        match self.secret_retriever.get(epoch).await? {
+            SecretState::Current(ikm) => self.insert_prk(ikm),
+            SecretState::Reconfiguration { old, new } => {
+                self.insert_prk(old);
+                self.insert_prk(new);
+            }
+        }
+        Ok(())
+    }
+
+    fn insert_prk(&mut self, ikm: VersionedIkm) {
+        let prk =
+            Hkdf::new(Some(&ikm.salt), ikm.ikm.expose_secret().0.as_ref());
+        self.prks.insert(ikm.epoch, prk);
     }
 
     /// Derive an encryption key for the given [`sled_hardware::DiskIdentity`]
@@ -94,7 +119,7 @@ impl<S: SecretRetriever> KeyManager<S> {
     /// Clear the PRKs
     fn clear(&mut self) {
         // PRKs should be zeroized on drop
-        self.prks = vec![];
+        self.prks = BTreeMap::new();
     }
 }
 
@@ -137,4 +162,47 @@ pub trait SecretRetriever {
     /// keys for rotation and as needed.
 
     async fn get(&self, epoch: u64) -> Result<SecretState, Self::Error>;
+}
+
+//#[cfg(tests)]
+mod tests {
+    use super::*;
+    use std::collections::BTreeMap;
+
+    pub struct TestSecretRetriever {
+        ikms: BTreeMap<u64, [u8; 32]>,
+    }
+
+    impl TestSecretRetriever {
+        pub fn new() -> TestSecretRetriever {
+            TestSecretRetriever { ikms: BTreeMap::from([(0, [0u8; 32])]) }
+        }
+
+        pub fn insert(&mut self, epoch: u64, bytes: [u8; 32]) {
+            self.ikms.insert(epoch, bytes);
+        }
+    }
+
+    #[async_trait]
+    impl SecretRetriever for TestSecretRetriever {
+        type Error = ();
+
+        async fn get_latest(&self) -> Result<VersionedIkm, Self::Error> {
+            let salt = [0u8; 32];
+            let (epoch, bytes) = self.ikms.last_key_value().unwrap();
+            Ok(VersionedIkm::new(*epoch, salt, bytes))
+        }
+
+        async fn get(&self, epoch: u64) -> Result<SecretState, Self::Error> {
+            let salt = [0u8; 32];
+            let bytes = self.ikms.get(&epoch).ok_or(())?;
+            let ikm = VersionedIkm::new(epoch, salt, bytes);
+            let latest = self.get_latest().await.unwrap();
+            if ikm.epoch != latest.epoch {
+                Ok(SecretState::Reconfiguration { old: ikm, new: latest })
+            } else {
+                Ok(SecretState::Current(ikm))
+            }
+        }
+    }
 }
