69bc18e03919735e516412066c319deccb980cee
David Pacheco <dap@oxidecomputer.com>
2023-05-03 17:20:03 -0700

    more first-class environment setup

---
+++ env.sh
@@ -0,0 +1,12 @@
+# omicron environment file: source this file to set up your PATH to use the
+# various tools in the Omicron workspace where this file lives.  The test suite
+# and other commands expect these copies of these tools to be on your PATH.
+#
+# See also: ./.envrc
+
+set -o xtrace
+OMICRON_WS="$(dirname "${BASH_SOURCE[0]}")"
+export PATH="$OMICRON_WS/out/cockroachdb/bin:$PATH"
+export PATH="$OMICRON_WS/out/clickhouse:$PATH"
+export PATH="$OMICRON_WS/out/dendrite-stub/bin:$PATH"
+set +o xtrace
