bb81fe969768346ebdf68047ca685ebf0411b84e
David Crespo <david.crespo@oxidecomputer.com>
2023-05-09 13:34:50 -0500

    assert about cache header on assets too

--- nexus/tests/integration_tests/console_api.rs
+++ nexus/tests/integration_tests/console_api.rs
@@ -240,6 +240,10 @@ async fn test_assets(cptestctx: &ControlPlaneTestContext) {
     // existing file is returned
     let resp = RequestBuilder::new(&testctx, Method::GET, "/assets/hello.txt")
         .expect_status(Some(StatusCode::OK))
+        .expect_response_header(
+            http::header::CACHE_CONTROL,
+            "max-age=31536000, immutable",
+        )
         .execute()
         .await
         .expect("failed to get existing file");
@@ -255,6 +259,10 @@ async fn test_assets(cptestctx: &ControlPlaneTestContext) {
         "/assets/a_directory/another_file.txt",
     )
     .expect_status(Some(StatusCode::OK))
+    .expect_response_header(
+        http::header::CACHE_CONTROL,
+        "max-age=31536000, immutable",
+    )
     .execute()
     .await
     .expect("failed to get existing file");
@@ -288,6 +296,10 @@ async fn test_assets(cptestctx: &ControlPlaneTestContext) {
             .header(http::header::ACCEPT_ENCODING, "gzip")
             .expect_status(Some(StatusCode::OK))
             .expect_response_header(http::header::CONTENT_ENCODING, "gzip")
+            .expect_response_header(
+                http::header::CACHE_CONTROL,
+                "max-age=31536000, immutable",
+            )
             .execute()
             .await
             .expect("failed to get existing file");
