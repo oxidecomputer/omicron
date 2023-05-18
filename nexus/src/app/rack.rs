7ecfa20bfa0986e02fa56835496f7e956a1762ea
Adam H. Leventhal <ahl@oxide.computer>
2023-05-18 15:15:25 -0700

    clipping

--- nexus/src/app/rack.rs
+++ nexus/src/app/rack.rs
@@ -8,7 +8,6 @@ use crate::authz;
 use crate::db;
 use crate::db::lookup::LookupPath;
 use crate::external_api::params::CertificateCreate;
-use crate::external_api::shared::ServiceUsingCertificate;
 use crate::internal_api::params::RackInitializationRequest;
 use nexus_db_model::DnsGroup;
 use nexus_db_model::InitialDnsGroup;
