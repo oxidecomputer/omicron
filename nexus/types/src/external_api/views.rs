e49740e263d2d389a6e1ee7e0eefb8c1735404f4
David Crespo <david.crespo@oxidecomputer.com>
2023-05-18 21:05:52 -0500

    add group_attribute_name to SAML IdP view response

--- nexus/types/src/external_api/views.rs
+++ nexus/types/src/external_api/views.rs
@@ -62,23 +62,27 @@ pub struct SamlIdentityProvider {
     #[serde(flatten)]
     pub identity: IdentityMetadata,
 
-    /// idp's entity id
+    /// IdP's entity id
     pub idp_entity_id: String,
 
-    /// sp's client id
+    /// SP's client id
     pub sp_client_id: String,
 
-    /// service provider endpoint where the response will be sent
+    /// Service provider endpoint where the response will be sent
     pub acs_url: String,
 
-    /// service provider endpoint where the idp should send log out requests
+    /// Service provider endpoint where the idp should send log out requests
     pub slo_url: String,
 
-    /// customer's technical contact for saml configuration
+    /// Customer's technical contact for saml configuration
     pub technical_contact_email: String,
 
-    /// optional request signing public certificate (base64 encoded der file)
+    /// Optional request signing public certificate (base64 encoded der file)
     pub public_cert: Option<String>,
+
+    /// If set, attributes with this name will be considered to denote a user's
+    /// group membership, where the values will be the group names.
+    pub group_attribute_name: Option<String>,
 }
 
 // PROJECTS
