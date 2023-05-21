a5bccc3afe37f381d06f5a56754dfcbeb3d97b7f
Andrew J. Stone <andrew@oxidecomputer.com>
2023-05-21 17:05:22 -0400

    wip

--- bootstore/src/lib.rs
+++ bootstore/src/lib.rs
@@ -22,30 +22,33 @@ pub struct Sha3_256Digest([u8; 32]);
 
 // We keep these in a module to prevent naming conflicts
 pub mod scheme_params {
-    #[derive(Default, Debug, Clone)]
+    #[derive(Default, Debug, Clone, Copy)]
     pub struct ChaCha20Poly1305;
 
-    #[derive(Default, Debug, Clone)]
+    #[derive(Default, Debug, Clone, Copy)]
     pub struct Sha3_256;
 
-    #[derive(Default, Debug, Clone)]
+    #[derive(Default, Debug, Clone, Copy)]
     pub struct Hkdf;
 
-    #[derive(Default, Debug, Clone)]
+    #[derive(Default, Debug, Clone, Copy)]
     pub struct Tcp;
 
-    #[derive(Default, Debug, Clone)]
+    #[derive(Default, Debug, Clone, Copy)]
     pub struct No;
 
-    #[derive(Default, Debug, Clone)]
+    #[derive(Default, Debug, Clone, Copy)]
     pub struct Curve25519;
+
+    #[derive(Default, Debug, Clone, Copy)]
+    pub struct Cbor;
 }
 
 /// A static description of the V0 scheme for trust quorum
 ///
 /// This is primarily for informational purposes.
 use scheme_params::*;
-#[derive(Default, Debug, Clone)]
+#[derive(Default, Debug, Clone, Copy)]
 pub struct V0Scheme {
     encryption_algorithm: ChaCha20Poly1305,
     hash_algorithm: Sha3_256,
@@ -53,4 +56,5 @@ pub struct V0Scheme {
     trust_quorum_transport: Tcp,
     trusted_group_membership: No,
     shamir_curve: Curve25519,
+    serialization: Cbor,
 }
--- bootstore/src/trust_quorum/share_pkg.rs
+++ bootstore/src/trust_quorum/share_pkg.rs
@@ -2,11 +2,44 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
+use super::RackSecret;
+use super::TrustQuorumError;
 use crate::Sha3_256Digest;
+use chacha20poly1305::{aead::Aead, ChaCha20Poly1305, Key, KeyInit};
+use hkdf::Hkdf;
+use rand::{rngs::OsRng, RngCore};
+use secrecy::Secret;
 use serde::{Deserialize, Serialize};
+use sha3::{Digest, Sha3_256};
+use std::collections::BTreeMap;
 use std::fmt;
 use uuid::Uuid;
+use zeroize::Zeroizing;
 use zeroize::{Zeroize, ZeroizeOnDrop};
+
+/// Details about a `SharePkg` or analog, that can be read first to allow
+/// us to know what version and structure we are dealing with.
+#[derive(Clone, PartialEq, Serialize, Deserialize)]
+pub struct SharePkgHeader {
+    version: u32,
+    metadata: BTreeMap<String, String>,
+}
+
+impl SharePkgHeader {
+    fn new_v0() -> SharePkgHeader {
+        SharePkgHeader {
+            version: 0,
+            metadata: BTreeMap::from([("learned".into(), "false".into())]),
+        }
+    }
+
+    fn new_v0_learned() -> SharePkgHeader {
+        SharePkgHeader {
+            version: 0,
+            metadata: BTreeMap::from([("learned".into(), "true".into())]),
+        }
+    }
+}
 
 /// A container used distributed among trust quorum participants for
 /// trust quorum version 0 Scheme: [`crate::SchemeV0`].
@@ -23,33 +56,144 @@ pub struct SharePkgV0 {
     pub threshold: u8,
     pub share: Vec<u8>,
 
+    /// Digests of all 256 shares so that each sled can verify the integrity
+    /// of received shares.
+    ///
+    /// No need for expensive zeroizing
+    #[zeroize(skip)]
+    pub share_digests: Vec<Sha3_256Digest>,
+
+    /// Salt used for key derivation
+    /// Since we use the same key for all pkgs, we share the same salt
+    pub salt: [u8; 32],
+
+    /// Nonce used for encryption.
+    ///
+    /// We generate all pkgs during rack init and ensure uniqueness of all nonces
+    pub nonce: [u8; 12],
+
+    /// We include a distinct subset of unused shares for each sled in the
+    /// initial  group. This allows the sled to hand out unique shares when a
+    /// new sled joins the cluster. We keep these encrypted so that a single
+    /// sled does not have enough unencrrypted shares to unlock the rack
+    /// without participating in trust quorum.
+    ///
+    /// Each sled should keep local track of which shares it has already handed
+    /// out.
+    ///
+    /// Note, each sled derives the same encryption key, but uses the unique
+    /// nonce for the pkg.
+    ///
+    /// No need for expensive zeroizing
+    #[zeroize(skip)]
+    pub encrypted_shares: Vec<u8>,
+}
+
+/// An analog to [`SharePkgV0`] for nodes that were added after rack
+/// initialization. There is no encrypted_shares or nonces because of this.
+#[derive(Clone, PartialEq, Serialize, Deserialize, Zeroize, ZeroizeOnDrop)]
+pub struct LearnedSharePkgV0 {
+    #[zeroize(skip)]
+    pub rack_uuid: Uuid,
+    // We aren't planning on doing any reconfigurations with this version of
+    // the protocol
+    pub epoch: u32,
+    pub threshold: u8,
+    pub share: Vec<u8>,
+
     // Digests of all 256 shares so that each sled can verify the integrity
     // of received shares.
     //
     // No need for expensive nonsense
     #[zeroize(skip)]
     pub share_digests: Vec<Sha3_256Digest>,
+}
 
-    // Nonce used for encryption. We aren't worried about nonce-reuse
-    // because we only encrypt one message, namely the shares in the field below.
-    pub nonce: [u8; 12],
+/// Create a package for each sled
+pub fn create_pkgs(
+    rack_uuid: Uuid,
+    n: usize,
+) -> Result<Secret<Vec<SharePkgV0>>, TrustQuorumError> {
+    let rack_secret = RackSecret::new();
+    let threshold = n / 2 + 1;
+    let epoch = 0;
+    // We always generate 256 shares to allow new sleds to come online
+    let total_shares = 256;
+    let shares_per_sled = 256 / n;
+    let shares = rack_secret.split(threshold, total_shares)?;
+    let share_digests = share_digests(&shares);
+    let (cipher, salt) = derive_encryption_key(&rack_uuid, &rack_secret);
+    let mut pkgs = Vec::with_capacity(n);
+    for i in 0..n {
+        // Each pkg gets a distinct subset of shares
+        let mut iter =
+            shares.iter().skip(i * shares_per_sled).take(shares_per_sled);
+        let share = iter.next().unwrap();
+        let plaintext: Vec<u8> = iter.fold(Vec::new(), |mut acc, x| {
+            acc.extend_from_slice(x);
+            acc
+        });
+        let nonce = new_nonce(i);
+        let encrypted_shares = cipher
+            .encrypt((&nonce).into(), plaintext.as_ref())
+            .map_err(|_| TrustQuorumError::FailedToEncrypt)?;
 
-    // We include a distinct subset of unused shares for each sled in the
-    // initial  group. This allows the sled to hand out unique shares when a
-    // new sled joins the cluster. We keep these encrypted so that a single
-    // sled does not have enough unencrrypted shares to unlock the rack without
-    // participating in trust quorum.
-    //
-    // Each sled should keep local track of which shares it has already
-    // handed out.
-    #[zeroize(skip)]
-    pub encrypted_shares: Vec<u8>,
+        let pkg = SharePkgV0 {
+            rack_uuid: rack_uuid.clone(),
+            epoch,
+            threshold: threshold.try_into().unwrap(),
+            share: share.clone(),
+            share_digests: share_digests.clone(),
+            salt,
+            nonce,
+            encrypted_shares,
+        };
+        pkgs.push(pkg);
+    }
+    Ok(Secret::new(pkgs))
+}
 
-    // Sha3_256 Digest of all prior fields in order
-    // We digest the fields individually, in order so that we
-    // do not have to worry about canonical serialization.
-    #[zeroize(skip)]
-    pub pkg_digest: Sha3_256Digest,
+// This is a fairly standard nonce construction consisting of a random part and
+// a counter.
+//
+// We know we only have up to 32 packages (1 for each sled), so we fill the
+// first 11 bytes of the nonce with random byes, and the last byte with a
+// counter.
+fn new_nonce(i: usize) -> [u8; 12] {
+    let mut nonce = [0u8; 12];
+    OsRng.fill_bytes(&mut nonce[..11]);
+    nonce[11] = u8::try_from(i).unwrap();
+    nonce
+}
+
+fn share_digests(shares: &Vec<Vec<u8>>) -> Vec<Sha3_256Digest> {
+    shares
+        .iter()
+        .map(|s| {
+            Sha3_256Digest(Sha3_256::digest(&s).as_slice().try_into().unwrap())
+        })
+        .collect()
+}
+
+// Return a (cipher, salt) pair
+fn derive_encryption_key(
+    rack_uuid: &Uuid,
+    rack_secret: &RackSecret,
+) -> (ChaCha20Poly1305, [u8; 32]) {
+    let mut salt = [0u8; 32];
+    OsRng.fill_bytes(&mut salt);
+    let prk =
+        Hkdf::<Sha3_256>::new(Some(&salt[..]), rack_secret.as_ref().as_bytes());
+
+    // The "info" string is context to bind the key to its purpose
+    let mut key = Zeroizing::new([0u8; 32]);
+    prk.expand_multi_info(
+        &[b"trust-quorum-v0-key-shares-", &rack_uuid.as_ref()],
+        key.as_mut(),
+    )
+    .unwrap();
+    let cipher = ChaCha20Poly1305::new(Key::from_slice(key.as_ref()));
+    (cipher, salt)
 }
 
 // We don't want to risk debug-logging the actual share contents, so implement
@@ -62,9 +206,9 @@ impl fmt::Debug for SharePkgV0 {
             .field("threshold", &self.threshold)
             .field("share", &"Share")
             .field("share_digests", &self.share_digests)
+            .field("salt", &self.salt)
             .field("nonce", &self.nonce)
             .field("encrypted_shares", &"Encrypted")
-            .field("pkg_digest", &self.pkg_digest)
             .finish()
     }
 }
