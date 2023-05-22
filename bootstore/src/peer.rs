8a2840cb6e2a8500bb39a20bdc2b9b9b5b063b78
Andrew J. Stone <andrew@oxidecomputer.com>
2023-05-22 18:46:55 -0400

    wip

---
+++ bootstore/src/peer.rs
@@ -0,0 +1,9 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+//! Peers that communicate with each other to exchange key shares
+
+mod v0;
+
+pub use v0::*;
