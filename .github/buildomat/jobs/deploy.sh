57dfd913e22e3319c19f92abb076157382c697a7
Ryan Goodfellow <ryan.goodfellow@oxide.computer>
2023-05-27 09:41:47 -0700

    snapshot logs for buildomat artifacts

--- .github/buildomat/jobs/deploy.sh
+++ .github/buildomat/jobs/deploy.sh
@@ -4,9 +4,7 @@
 #: variety = "basic"
 #: target = "lab-opte-0.23"
 #: output_rules = [
-#:	"%/var/svc/log/oxide-sled-agent:default.log",
-#:	"%/zone/oxz_*/root/var/svc/log/oxide-*.log",
-#:	"%/zone/oxz_*/root/var/svc/log/system-illumos-*.log",
+#:  "%/snapshot/*.log",
 #: ]
 #: skip_clone = true
 #:
@@ -24,6 +22,15 @@ set -o xtrace
 # If we fail, try to collect some debugging information
 #
 _exit_trap() {
+    # Take a snapshot of logs. If these disappear or change while buildomat is
+    # collecting them, the CI job will fail.
+    pfexec mkdir /snapshot
+    pfexec cp /var/svc/log/oxide-sled-agent:default.log /snapshot/
+    pfexec find /zone/oxz_* -name proc -prune -o -name oxide-*.log -exec cp "{}" /snapshot/ \;
+    pfexec find /zone/oxz_* -name proc -prune -o -name system-illumos-*.log -exec cp "{}" /snapshot/ \;
+    pfexec zlogin softnpu cat /softnpu.log > /tmp/softnpu.log
+    pfexec cp /tmp/softnpu.log /snapshot/
+
 	local status=$?
 	[[ $status -eq 0 ]] && exit 0
 
