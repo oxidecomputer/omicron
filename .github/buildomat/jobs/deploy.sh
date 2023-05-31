241c673a0e3ccebe7c10b03e31f1dcc172dd2452
Ryan Goodfellow <ryan.goodfellow@oxide.computer>
2023-05-31 09:02:14 -0700

    Revert deploy log snapshot, don't collect propolis logs as CI artifacts. (#3263)

--- .github/buildomat/jobs/deploy.sh
+++ .github/buildomat/jobs/deploy.sh
@@ -4,7 +4,10 @@
 #: variety = "basic"
 #: target = "lab-opte-0.23"
 #: output_rules = [
-#:  "%/snapshot/*.log",
+#:  "%/var/svc/log/oxide-sled-agent:default.log",
+#:  "%/zone/oxz_*/root/var/svc/log/oxide-*.log",
+#:  "%/zone/oxz_*/root/var/svc/log/system-illumos-*.log",
+#:  "!/zone/oxz_propolis-server_*/root/var/svc/log/*.log"
 #: ]
 #: skip_clone = true
 #:
@@ -22,15 +25,6 @@ set -o xtrace
 # If we fail, try to collect some debugging information
 #
 _exit_trap() {
-    # Take a snapshot of logs. If these disappear or change while buildomat is
-    # collecting them, the CI job will fail.
-    pfexec mkdir /snapshot
-    pfexec cp /var/svc/log/oxide-sled-agent:default.log /snapshot/
-    pfexec find /zone/oxz_* -name proc -prune -o -name oxide-*.log -exec cp "{}" /snapshot/ \;
-    pfexec find /zone/oxz_* -name proc -prune -o -name system-illumos-*.log -exec cp "{}" /snapshot/ \;
-    pfexec zlogin softnpu cat /softnpu.log > /tmp/softnpu.log
-    pfexec cp /tmp/softnpu.log /snapshot/
-
 	local status=$?
 	[[ $status -eq 0 ]] && exit 0
 
@@ -51,10 +45,10 @@ _exit_trap() {
 		standalone \
 		dump-state
 	pfexec /opt/oxide/opte/bin/opteadm list-ports
-    z_swadm link ls
-    z_swadm addr list
-    z_swadm route list
-    z_swadm arp list
+	z_swadm link ls
+	z_swadm addr list
+	z_swadm route list
+	z_swadm arp list
 
 	PORTS=$(pfexec /opt/oxide/opte/bin/opteadm list-ports | tail +2 | awk '{ print $1; }')
 	for p in $PORTS; do
@@ -79,14 +73,14 @@ _exit_trap() {
 		pfexec zlogin "$z" arp -an
 	done
 
-    pfexec zlogin softnpu cat /softnpu.log
+	pfexec zlogin softnpu cat /softnpu.log
 
 	exit $status
 }
 trap _exit_trap EXIT
 
 z_swadm () {
-    pfexec zlogin oxz_switch /opt/oxide/dendrite/bin/swadm $@
+	pfexec zlogin oxz_switch /opt/oxide/dendrite/bin/swadm $@
 }
 
 #
