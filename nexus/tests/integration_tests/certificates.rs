a29df38667c3eaafc4b480cf7b848197c5064c7b
James MacMahon <james@oxide.computer>
2023-06-08 16:13:11 -0400

    match against "self-signed certificate" or "invalid peer certificate"

--- nexus/tests/integration_tests/certificates.rs
+++ nexus/tests/integration_tests/certificates.rs
@@ -621,7 +621,10 @@ async fn test_silo_certificates() {
         );
     if let oxide_client::Error::CommunicationError(error) = error {
         assert!(error.is_connect());
-        assert!(error.to_string().contains("invalid peer certificate"));
+        assert!(
+            error.to_string().contains("invalid peer certificate")
+                || error.to_string().contains("self-signed certificate")
+        );
     } else {
         panic!(
             "unexpected error connecting with wrong certificate: {:#}",
@@ -640,7 +643,10 @@ async fn test_silo_certificates() {
         );
     if let oxide_client::Error::CommunicationError(error) = error {
         assert!(error.is_connect());
-        assert!(error.to_string().contains("invalid peer certificate"));
+        assert!(
+            error.to_string().contains("invalid peer certificate")
+                || error.to_string().contains("self-signed certificate")
+        );
     } else {
         panic!(
             "unexpected error connecting with wrong certificate: {:#}",
