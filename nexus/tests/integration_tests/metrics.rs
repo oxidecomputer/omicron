295765614f3e1453b67bc098b29855f4ba39f639
David Crespo <david.crespo@oxidecomputer.com>
2023-06-22 23:52:48 -0500

    first pass at silo metrics endpoint test

--- nexus/tests/integration_tests/metrics.rs
+++ nexus/tests/integration_tests/metrics.rs
@@ -5,8 +5,14 @@
 use chrono::Utc;
 use dropshot::test_util::ClientTestContext;
 use dropshot::ResultsPage;
-use nexus_test_utils::resource_helpers::objects_list_page_authz;
+use http::{Method, StatusCode};
+use nexus_test_utils::http_testing::{AuthnMode, NexusRequest, RequestBuilder};
+use nexus_test_utils::resource_helpers::{
+    create_project, objects_list_page_authz,
+};
 use nexus_test_utils::ControlPlaneTestContext;
+use nexus_test_utils_macros::nexus_test;
+use omicron_nexus::db::fixed_data::silo::SILO_ID;
 use oximeter::types::Datum;
 use oximeter::types::Measurement;
 use uuid::Uuid;
@@ -37,8 +43,8 @@ pub async fn get_latest_silo_metric(
         Utc::now(),
         id_param,
     );
-    let measurements: ResultsPage<Measurement> =
-        objects_list_page_authz(client, &url).await;
+    let measurements =
+        objects_list_page_authz::<Measurement>(client, &url).await;
 
     // prevent more confusing error on next line
     assert!(measurements.items.len() == 1, "Expected exactly one measurement");
@@ -49,4 +55,64 @@ pub async fn get_latest_silo_metric(
         _ => panic!("Unexpected datum type {:?}", item.datum()),
     };
     return *datum;
+}
+
+#[nexus_test]
+async fn test_silo_metrics(
+    cptestctx: &ControlPlaneTestContext<omicron_nexus::Server>,
+) {
+    // Normally, Nexus is not registered as a producer for tests.
+    // Turn this bit on so we can also test some metrics from Nexus itself.
+    cptestctx.server.register_as_producer().await;
+
+    let client = &cptestctx.external_client;
+    // let oximeter = &cptestctx.oximeter;
+    // let apictx = &cptestctx.server.apictx();
+    // let nexus = &apictx.nexus;
+    // let datastore = nexus.datastore();
+
+    let get_url = |metric_name: &str, project_id: Option<Uuid>| -> String {
+        let id_param = match project_id {
+            Some(id) => format!("&project_id={}", id),
+            None => "".to_string(),
+        };
+        format!(
+            "/v1/metrics/{metric_name}?start_time={:?}&end_time={:?}&order=descending&limit=1{}", 
+            cptestctx.start_time,
+            Utc::now(),
+            id_param,
+        )
+    };
+
+    let project_id = create_project(&client, "my-proj").await.identity.id;
+
+    // silo metrics restricted to current silo — does not allow access to any
+    // other silo. to some extent this is guaranteed by the design of the query
+    // param, which only takes a project ID. so really we just test that only
+    // project IDs work.
+
+    // 404 if given project_id that is actually some other type of resource
+    let error = NexusRequest::new(
+        RequestBuilder::new(
+            client,
+            Method::GET,
+            &get_url("cpus_provisioned", Some(*SILO_ID)),
+        )
+        .expect_status(Some(StatusCode::NOT_FOUND)),
+    )
+    .authn_as(AuthnMode::PrivilegedUser)
+    .execute()
+    .await
+    .expect("unexpected success")
+    .parsed_body::<dropshot::HttpErrorResponseBody>()
+    .unwrap();
+
+    dbg!(error);
+
+    // using an actual project ID works though
+    objects_list_page_authz::<Measurement>(
+        client,
+        &get_url("cpus_provisioned", Some(project_id)),
+    )
+    .await;
 }
