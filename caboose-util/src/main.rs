5ef61493a8ae8f549786fb500e86d0aae1dd840f
iliana etaoin <iliana@oxide.computer>
2023-06-28 17:43:39 +0000

    use `Caboose::version`

--- caboose-util/src/main.rs
+++ caboose-util/src/main.rs
@@ -4,9 +4,8 @@
 
 // Copyright 2023 Oxide Computer Company
 
-use anyhow::{anyhow, bail, Context, Result};
+use anyhow::{bail, Context, Result};
 use hubtools::RawHubrisArchive;
-use tlvc_text::{Piece, Tag};
 
 fn main() -> Result<()> {
     let mut args = std::env::args().skip(1);
@@ -16,22 +15,8 @@ fn main() -> Result<()> {
                 &args.next().context("path to hubris archive required")?,
             )?;
             let caboose = archive.read_caboose()?;
-            let reader = tlvc::TlvcReader::begin(caboose.as_slice())
-                .map_err(|e| anyhow!("tlvc error: {e:?}"))?;
-            let t = tlvc_text::dump(reader);
-            for piece in t {
-                if let Piece::Chunk(tag, data) = piece {
-                    if tag == Tag::new(*b"VERS") {
-                        for datum in data {
-                            if let Piece::String(version) = datum {
-                                println!("{}", version);
-                                return Ok(());
-                            }
-                        }
-                    }
-                }
-            }
-            bail!("no VERS string found");
+            println!("{}", std::str::from_utf8(caboose.version()?)?);
+            Ok(())
         }
         unknown => bail!("unknown command {}", unknown),
     }
