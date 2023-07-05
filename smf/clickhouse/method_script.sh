3281cbd16756326b9b361a3580bd12864666cec2
karencfv <karencfv@users.noreply.github.com>
2023-07-05 18:21:46 +1200

    Create config templates and pseudocode for updated init config

--- smf/clickhouse/method_script.sh
+++ smf/clickhouse/method_script.sh
@@ -21,7 +21,18 @@ ipadm show-addr "$DATALINK/ll" || ipadm create-addr -t -T addrconf "$DATALINK/ll
 ipadm show-addr "$DATALINK/omicron6"  || ipadm create-addr -t -T static -a "$LISTEN_ADDR" "$DATALINK/omicron6"
 route get -inet6 default -inet6 "$GATEWAY" || route add -inet6 default -inet6 "$GATEWAY"
 
+# Retrieve addresses of the other clickhouse nodes, order them and assign them to be a replica or keeper node.
+# In a follow up PR, keepers will be their own service.
+#
+# Populate corresponding template and copy to /opt/oxide/clickhouse/clickhouse/config.d/
+# or /opt/oxide/clickhouse/clickhouse/keeper_config.xml
+# Clickhouse recommends to have these files in `/etc/clickhouse-keeper/` and `/etc/clickhouse-server/config.d/`,
+# but I'm not sure this makes sense to us since we're building the entire clickhouse binary instead of separate
+# `clickhouse-server`, 'clickhouse-keeper' and 'clickhouse-client' binaries.
+#
+# If this is a clickhouse repica node then run:
 args=(
+  # TODO: These arguments won't be necessary anymore with the config templates
   "--log-file" "/var/tmp/clickhouse-server.log"
   "--errorlog-file" "/var/tmp/clickhouse-server.errlog"
   "--"
@@ -31,3 +42,7 @@ args=(
 )
 
 exec /opt/oxide/clickhouse/clickhouse server "${args[@]}" &
+#
+# If this is a clickhouse keeper node then run:
+#
+# exec /opt/oxide/clickhouse/clickhouse keeper enable &
