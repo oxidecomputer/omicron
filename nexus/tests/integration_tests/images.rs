15e441f9ac24f4b3d8221eca07e25fe261297b1a
iliana etaoin <iliana@oxide.computer>
2023-07-07 17:00:44 -0700

    Disallow creating instances/disks with snapshots/images from another project (#3513)
    This adds these checks:
    - in `project_create_disk`: ensure the snapshot belongs to the same
    project, or ensure the image belongs to the silo or the same project
    - in `image_create`: ensure the snapshot belongs to the same project
    - in `project_create_instance`: for created disks, implement the same
    checks as `project_create_disk`
    
    This is a simple check to close off this kind of cross-project resource
    usage; we may want to enable this kind of thing in the future (perhaps
    with authz), but I think we will want to be more careful about the
    design, and it's easier to restrict it now and add functionality to
    permit this later.

--- nexus/tests/integration_tests/images.rs
+++ nexus/tests/integration_tests/images.rs
@@ -13,6 +13,7 @@ use nexus_test_utils::http_testing::RequestBuilder;
 use nexus_test_utils::resource_helpers::create_project;
 use nexus_test_utils::resource_helpers::DiskTest;
 use nexus_test_utils_macros::nexus_test;
+use omicron_common::api::external::Disk;
 
 use omicron_common::api::external::{ByteCount, IdentityMetadataCreateParams};
 use omicron_nexus::external_api::{params, views};
@@ -368,6 +369,65 @@ async fn test_make_disk_from_image(cptestctx: &ControlPlaneTestContext) {
 }
 
 #[nexus_test]
+async fn test_make_disk_from_other_project_image_fails(
+    cptestctx: &ControlPlaneTestContext,
+) {
+    let client = &cptestctx.external_client;
+    DiskTest::new(&cptestctx).await;
+
+    create_project(client, PROJECT_NAME).await;
+    let another_project = create_project(client, "another-proj").await;
+
+    let server = ServerBuilder::new().run().unwrap();
+    server.expect(
+        Expectation::matching(request::method_path("HEAD", "/image.raw"))
+            .times(1..)
+            .respond_with(
+                status_code(200).append_header(
+                    "Content-Length",
+                    format!("{}", 4096 * 1000),
+                ),
+            ),
+    );
+
+    let images_url = get_project_images_url(PROJECT_NAME);
+    let image_create_params = get_image_create(params::ImageSource::Url {
+        url: server.url("/image.raw").to_string(),
+        block_size: BLOCK_SIZE,
+    });
+    let image =
+        NexusRequest::objects_post(client, &images_url, &image_create_params)
+            .authn_as(AuthnMode::PrivilegedUser)
+            .execute_and_parse_unwrap::<views::Image>()
+            .await;
+
+    let new_disk = params::DiskCreate {
+        identity: IdentityMetadataCreateParams {
+            name: "stolen-disk".parse().unwrap(),
+            description: String::from("yoink"),
+        },
+        disk_source: params::DiskSource::Image { image_id: image.identity.id },
+        size: ByteCount::from_gibibytes_u32(1),
+    };
+    let disks_url =
+        format!("/v1/disks?project={}", another_project.identity.name);
+    let error = NexusRequest::expect_failure_with_body(
+        client,
+        StatusCode::BAD_REQUEST,
+        Method::POST,
+        &disks_url,
+        &new_disk,
+    )
+    .authn_as(AuthnMode::PrivilegedUser)
+    .execute()
+    .await
+    .expect("expected 400")
+    .parsed_body::<dropshot::HttpErrorResponseBody>()
+    .unwrap();
+    assert_eq!(error.message, "image does not belong to this project");
+}
+
+#[nexus_test]
 async fn test_make_disk_from_image_too_small(
     cptestctx: &ControlPlaneTestContext,
 ) {
@@ -556,4 +616,96 @@ async fn test_image_promotion(cptestctx: &ControlPlaneTestContext) {
     .expect("unexpected success")
     .parsed_body::<dropshot::HttpErrorResponseBody>()
     .unwrap();
+}
+
+#[nexus_test]
+async fn test_image_from_other_project_snapshot_fails(
+    cptestctx: &ControlPlaneTestContext,
+) {
+    let client = &cptestctx.external_client;
+    DiskTest::new(&cptestctx).await;
+
+    let server = ServerBuilder::new().run().unwrap();
+    server.expect(
+        Expectation::matching(request::method_path("HEAD", "/image.raw"))
+            .times(1..)
+            .respond_with(
+                status_code(200).append_header(
+                    "Content-Length",
+                    format!("{}", 4096 * 1000),
+                ),
+            ),
+    );
+
+    create_project(client, PROJECT_NAME).await;
+    let images_url = get_project_images_url(PROJECT_NAME);
+    let disks_url = format!("/v1/disks?project={}", PROJECT_NAME);
+    let snapshots_url = format!("/v1/snapshots?project={}", PROJECT_NAME);
+
+    // Create an image
+    let image_create_params = get_image_create(params::ImageSource::Url {
+        url: server.url("/image.raw").to_string(),
+        block_size: BLOCK_SIZE,
+    });
+    let image: views::Image =
+        NexusRequest::objects_post(client, &images_url, &image_create_params)
+            .authn_as(AuthnMode::PrivilegedUser)
+            .execute_and_parse_unwrap()
+            .await;
+    // Create a disk from this image
+    let disk_create_params = params::DiskCreate {
+        identity: IdentityMetadataCreateParams {
+            name: "disk".parse().unwrap(),
+            description: "meow".into(),
+        },
+        disk_source: params::DiskSource::Image { image_id: image.identity.id },
+        size: ByteCount::from_gibibytes_u32(1),
+    };
+    let disk: Disk =
+        NexusRequest::objects_post(client, &disks_url, &disk_create_params)
+            .authn_as(AuthnMode::PrivilegedUser)
+            .execute()
+            .await
+            .unwrap()
+            .parsed_body()
+            .unwrap();
+    // Create a snapshot from this disk
+    let snapshot_create_params = params::SnapshotCreate {
+        identity: IdentityMetadataCreateParams {
+            name: "snapshot".parse().unwrap(),
+            description: "meow".into(),
+        },
+        disk: disk.identity.id.into(),
+    };
+    let snapshot: views::Snapshot = NexusRequest::objects_post(
+        client,
+        &snapshots_url,
+        &snapshot_create_params,
+    )
+    .authn_as(AuthnMode::PrivilegedUser)
+    .execute()
+    .await
+    .unwrap()
+    .parsed_body()
+    .unwrap();
+
+    // Create a second project and ensure image creation fails
+    let another_project = create_project(client, "another-proj").await;
+    let images_url =
+        get_project_images_url(another_project.identity.name.as_str());
+    let image_create_params = get_image_create(params::ImageSource::Snapshot {
+        id: snapshot.identity.id,
+    });
+    let error = NexusRequest::new(
+        RequestBuilder::new(client, Method::POST, &images_url)
+            .body(Some(&image_create_params))
+            .expect_status(Some(StatusCode::BAD_REQUEST)),
+    )
+    .authn_as(AuthnMode::PrivilegedUser)
+    .execute()
+    .await
+    .expect("unexpected success")
+    .parsed_body::<dropshot::HttpErrorResponseBody>()
+    .unwrap();
+    assert_eq!(error.message, "snapshot does not belong to this project");
 }
