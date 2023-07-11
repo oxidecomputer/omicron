c9d8e9dd277a7b1b1038ca861addfa65e45f216d
David Pacheco <dap@cs.brown.edu>
2023-07-11 22:40:01 +0000

    try 4

--- sled-agent/src/rack_setup/plan/service.rs
+++ sled-agent/src/rack_setup/plan/service.rs
@@ -44,7 +44,7 @@ use uuid::Uuid;
 const BOUNDARY_NTP_COUNT: usize = 2;
 
 // The number of Nexus instances to create from RSS.
-const NEXUS_COUNT: usize = 1;
+const NEXUS_COUNT: usize = 3;
 
 // The number of CRDB instances to create from RSS.
 const CRDB_COUNT: usize = 5;
