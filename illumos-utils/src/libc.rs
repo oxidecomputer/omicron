43c4ba7cc2aa0b81b285c261e9d045cc5dd12043
Jordan Paige Hendricks <jordan@oxide.computer>
2023-07-12 15:40:15 +0000

    Add license to newly added file

--- illumos-utils/src/libc.rs
+++ illumos-utils/src/libc.rs
@@ -1,4 +1,8 @@
-/// Miscellaneous FFI wrapper functions for libc
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+//! Miscellaneous FFI wrapper functions for libc
 
 /// sysconf(3c)
 pub fn sysconf(arg: i32) -> std::io::Result<i64> {
