65de196e229ed2e98bfa85b59d4dd61b974f5431
John Gallagher <john@oxidecomputer.com>
2023-07-13 20:31:30 -0400

    [wicket] Replace SP inventory debug print with formatted lines (#3606)
    Replaces our debug print screen:
    
    
    ![inventory-old](https://github.com/oxidecomputer/omicron/assets/1435635/e885e5df-9ef2-46c1-9544-9beb46f93f32)
    
    with formatted output:
    
    
    ![inventory-new](https://github.com/oxidecomputer/omicron/assets/1435635/d332693e-d506-449e-bf4b-7568bda65978)
    
    This will be more tedious to update with future changes to the
    information available here, and is already a little tedious in places
    where related data is split across different elements (e.g., the caboose
    data is separate from the state, and the rot state is contained within
    the SP state), but is _so much_ easier to read I think it's definitely
    worth it.
    
    ---------
    
    Co-authored-by: Rain <rain@oxide.computer>

--- wicket/src/state/inventory.rs
+++ wicket/src/state/inventory.rs
@@ -119,6 +119,25 @@ impl Sp {
     pub fn state(&self) -> Option<&SpState> {
         self.state.as_ref()
     }
+
+    pub fn caboose_active(&self) -> Option<&SpComponentCaboose> {
+        self.caboose_active.as_ref()
+    }
+
+    pub fn caboose_inactive(&self) -> Option<&SpComponentCaboose> {
+        self.caboose_inactive.as_ref()
+    }
+
+    pub fn rot(&self) -> Option<&RotInventory> {
+        self.rot.as_ref()
+    }
+
+    pub fn components(&self) -> &[SpComponentInfo] {
+        match self.components.as_ref() {
+            Some(components) => components,
+            None => &[],
+        }
+    }
 }
 
 // XXX: Eventually a Sled will have a host component.
