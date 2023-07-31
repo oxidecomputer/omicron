d8050f644b77916b9d603d2281c23255541836d9
John Gallagher <john@oxidecomputer.com>
2023-07-31 11:27:55 -0400

    One egrep instead of four greps
    Co-authored-by: Andy Fiddaman <andy@oxidecomputer.com>

--- .github/buildomat/jobs/tuf-repo.sh
+++ .github/buildomat/jobs/tuf-repo.sh
@@ -106,10 +106,7 @@ EOF
 # for development workflows using `omicron-package install` that don't build a
 # full omicron OS ramdisk, so we filter them out here.
 for zone in $(find /work/package/install -maxdepth 1 -type f -name '*.tar.gz' \
-    | grep -v propolis-server.tar.gz \
-    | grep -v mgs.tar.gz \
-    | grep -v overlay.tar.gz \
-    | grep -v switch.tar.gz); do
+    | egrep -v '(propolis-server|mgs|overlay|switch)\.tar\.gz'); do
     cat >>/work/manifest.toml <<EOF
 [[artifact.control_plane.source.zones]]
 kind = "file"
