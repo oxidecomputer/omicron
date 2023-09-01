de1fa54088e9f2fe399ed0d86d4b5c652498f2c9
Luqman Aden <luqman@oxide.computer>
2023-09-01 15:49:30 -0700

    nexus: allow IpPoolList access via silo conferred roles as well (#3999)
    We were missing loading silo conferred roles for authz checks against `IpPoolList`.
    
    Also rename and make load_roles_for_resource private to try to limit same issue in the future.

--- nexus/db-queries/src/authz/api_resources.rs
+++ nexus/db-queries/src/authz/api_resources.rs
@@ -29,9 +29,7 @@
 use super::actor::AnyActor;
 use super::context::AuthorizedResource;
 use super::oso_generic::Init;
-use super::roles::{
-    load_roles_for_resource, load_roles_for_resource_tree, RoleSet,
-};
+use super::roles::{load_roles_for_resource_tree, RoleSet};
 use super::Action;
 use super::{actor::AuthenticatedActor, Authz};
 use crate::authn;
@@ -290,15 +288,8 @@ impl AuthorizedResource for ConsoleSessionList {
         'd: 'f,
         'e: 'f,
     {
-        load_roles_for_resource(
-            opctx,
-            datastore,
-            authn,
-            ResourceType::Fleet,
-            *FLEET_ID,
-            roleset,
-        )
-        .boxed()
+        load_roles_for_resource_tree(&FLEET, opctx, datastore, authn, roleset)
+            .boxed()
     }
 
     fn on_unauthorized(
@@ -353,15 +344,8 @@ impl AuthorizedResource for DnsConfig {
         'd: 'f,
         'e: 'f,
     {
-        load_roles_for_resource(
-            opctx,
-            datastore,
-            authn,
-            ResourceType::Fleet,
-            *FLEET_ID,
-            roleset,
-        )
-        .boxed()
+        load_roles_for_resource_tree(&FLEET, opctx, datastore, authn, roleset)
+            .boxed()
     }
 
     fn on_unauthorized(
@@ -418,16 +402,9 @@ impl AuthorizedResource for IpPoolList {
     {
         // There are no roles on the IpPoolList, only permissions. But we still
         // need to load the Fleet-related roles to verify that the actor has the
-        // "admin" role on the Fleet.
-        load_roles_for_resource(
-            opctx,
-            datastore,
-            authn,
-            ResourceType::Fleet,
-            *FLEET_ID,
-            roleset,
-        )
-        .boxed()
+        // "admin" role on the Fleet (possibly conferred from a Silo role).
+        load_roles_for_resource_tree(&FLEET, opctx, datastore, authn, roleset)
+            .boxed()
     }
 
     fn on_unauthorized(
@@ -477,15 +454,8 @@ impl AuthorizedResource for DeviceAuthRequestList {
         // There are no roles on the DeviceAuthRequestList, only permissions. But we
         // still need to load the Fleet-related roles to verify that the actor has the
         // "admin" role on the Fleet.
-        load_roles_for_resource(
-            opctx,
-            datastore,
-            authn,
-            ResourceType::Fleet,
-            *FLEET_ID,
-            roleset,
-        )
-        .boxed()
+        load_roles_for_resource_tree(&FLEET, opctx, datastore, authn, roleset)
+            .boxed()
     }
 
     fn on_unauthorized(
