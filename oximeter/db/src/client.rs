b7cb2e7c612779f52d6211e64b4b78df00c86e65
karencfv <karencfv@users.noreply.github.com>
2023-09-06 15:48:06 -0600

    Trigger another CI run to make sure the flake is gone

--- oximeter/db/src/client.rs
+++ oximeter/db/src/client.rs
@@ -787,8 +787,8 @@ mod tests {
         assert!(result.contains("hiya"));
 
         // TODO(https://github.com/oxidecomputer/omicron/issues/4001): With distributed
-        // engine, it can take a long time to sync the data. So it's tricky to test that
-        // the data exists on both nodes.
+        // engine, it can take a long time to sync the data. This means it's tricky to 
+        // test that the data exists on both nodes.
 
         k1.cleanup().await.expect("Failed to cleanup ClickHouse keeper 1");
         k2.cleanup().await.expect("Failed to cleanup ClickHouse keeper 2");
