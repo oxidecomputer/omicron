2a0556888069d6733b9296d17de26da493e0857c
Sean Klein <sean@oxide.computer>
2023-09-20 09:35:33 -0700

    [omdb] Add a couple simple Sled Agent queries

---
+++ omdb/src/bin/omdb/sled.rs
@@ -0,0 +1,109 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+//! omdb commands that query or update specific Sleds
+
+use anyhow::bail;
+use anyhow::Context;
+use clap::Args;
+use clap::Subcommand;
+
+/// Arguments to the "omdb sled" subcommand
+#[derive(Debug, Args)]
+pub struct SledArgs {
+    /// URL of the Sled internal API
+    #[clap(long, env("OMDB_SLED_URL"))]
+    sled_url: Option<String>,
+
+    #[command(subcommand)]
+    command: SledCommands,
+}
+
+/// Subcommands for the "omdb sled" subcommand
+#[derive(Debug, Subcommand)]
+enum SledCommands {
+    /// print information about zones
+    #[clap(subcommand)]
+    Zones(ZoneCommands),
+
+    /// print information about zpools
+    #[clap(subcommand)]
+    Zpools(ZpoolCommands),
+}
+
+#[derive(Debug, Subcommand)]
+enum ZoneCommands {
+    /// Print list of all zones
+    List,
+}
+
+#[derive(Debug, Subcommand)]
+enum ZpoolCommands {
+    /// Print list of all zpools
+    List,
+}
+
+impl SledArgs {
+    /// Run a `omdb sled` subcommand.
+    pub async fn run_cmd(
+        &self,
+        log: &slog::Logger,
+    ) -> Result<(), anyhow::Error> {
+        // This is a little goofy. The sled URL is required, but can come
+        // from the environment, in which case it won't be on the command line.
+        let Some(sled_url) = &self.sled_url else {
+            bail!(
+                "sled URL must be specified with --sled-url or \
+                OMDB_SLED_URL"
+            );
+        };
+        let client = sled_agent_client::Client::new(sled_url, log.clone());
+
+        match &self.command {
+            SledCommands::Zones(ZoneCommands::List) => {
+                cmd_zones_list(&client).await
+            }
+            SledCommands::Zpools(ZpoolCommands::List) => {
+                cmd_zpools_list(&client).await
+            }
+        }
+    }
+}
+
+/// Runs `omdb sled zones list`
+async fn cmd_zones_list(
+    client: &sled_agent_client::Client,
+) -> Result<(), anyhow::Error> {
+    let response = client.zones_list().await.context("listing zones")?;
+    let zones = response.into_inner();
+    let zones: Vec<_> = zones.into_iter().collect();
+
+    println!("zones:");
+    if zones.is_empty() {
+        println!("    <none>");
+    }
+    for zone in &zones {
+        println!("    {:?}", zone);
+    }
+
+    Ok(())
+}
+
+/// Runs `omdb sled zpools list`
+async fn cmd_zpools_list(
+    client: &sled_agent_client::Client,
+) -> Result<(), anyhow::Error> {
+    let response = client.zpools_get().await.context("listing zpools")?;
+    let zpools = response.into_inner();
+
+    println!("zpools:");
+    if zpools.is_empty() {
+        println!("    <none>");
+    }
+    for zpool in &zpools {
+        println!("    {:?}", zpool);
+    }
+
+    Ok(())
+}
