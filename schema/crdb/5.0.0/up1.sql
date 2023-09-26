9b56e405ec4639132f243cedfb733b7e05943d9b
Alan Hanson <alan@oxide.computer>
2023-09-26 23:30:19 +0000

    Fix schema update files

--- schema/crdb/5.0.0/up1.sql
+++ schema/crdb/5.0.0/up1.sql
@@ -1,1 +1,1 @@
-CREATE INDEX IF NOT EXISTS lookup_zpool_by_disk ON omicron.public.zpool (physical_disk_id, id) WHERE time_deleted IS NULL;
+CREATE INDEX IF NOT EXISTS lookup_zpool_by_disk ON omicron.public.zpool (physical_disk_id, id) WHERE physical_disk_id IS NOT NULL AND time_deleted IS NULL;
--- schema/crdb/5.0.0/up2.sql
+++ schema/crdb/5.0.0/up2.sql
@@ -1,1 +1,1 @@
-CREATE INDEX IF NOT EXISTS lookup_dataset_by_zpool ON omicron.public.dataset (pool_id, id) WHERE time_deleted IS NULL;
+CREATE INDEX IF NOT EXISTS lookup_dataset_by_zpool ON omicron.public.dataset (pool_id, id) WHERE pool_id IS NOT NULL AND time_deleted IS NULL;
