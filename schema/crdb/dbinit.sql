202387e1d99579c64ad3f602d1746999d47990e4
James MacMahon <james@oxide.computer>
2023-10-04 11:21:30 -0400

    create new schema 6.0.0, 5.0.0 was taken by main

--- schema/crdb/dbinit.sql
+++ schema/crdb/dbinit.sql
@@ -2577,7 +2577,7 @@ INSERT INTO omicron.public.db_metadata (
     version,
     target_version
 ) VALUES
-    ( TRUE, NOW(), NOW(), '5.0.0', NULL)
+    ( TRUE, NOW(), NOW(), '6.0.0', NULL)
 ON CONFLICT DO NOTHING;
 
 COMMIT;
