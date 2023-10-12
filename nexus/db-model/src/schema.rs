153e5897a07fdb9c1ab632887f63703f4ada3b6e
Greg Colombo <greg@oxidecomputer.com>
2023-10-12 15:59:13 +0000

    Move instance/VMM table schema upgrade to version 7.0.0
    The instance/VMM table schema change was slated to be 6.0.0 in the original
    version of its pull request. That version was then added by a separate PR, but
    this didn't cause a merge conflict because the instance/VMM upgrade used an
    extra "0" in its schema upgrade files (for fear that there might be more than
    nine of them and that the leading 0 would be necessary to ensure they had the
    correct lexographical ordering).
    
    The schema changes don't conflict with each other, so everything actually works
    fine, but having two logically separate updates in one version is at the very
    least aesthetically displeasing.
    
    Move the instance schema upgrade to version 7.0.0. Rename the files to remove
    the leading 0 in their numbers, since that turned out not to be needed.
    
    Tested via cargo tests (there are no other functional or schema changes beyond
    renaming and updating version constants).

--- nexus/db-model/src/schema.rs
+++ nexus/db-model/src/schema.rs
@@ -1142,7 +1142,7 @@ table! {
 ///
 /// This should be updated whenever the schema is changed. For more details,
 /// refer to: schema/crdb/README.adoc
-pub const SCHEMA_VERSION: SemverVersion = SemverVersion::new(6, 0, 0);
+pub const SCHEMA_VERSION: SemverVersion = SemverVersion::new(7, 0, 0);
 
 allow_tables_to_appear_in_same_query!(
     system_update,
