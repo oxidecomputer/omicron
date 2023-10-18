4b99e9a48363436c284b28e382265d40e81e388a
Ryan Goodfellow <ryan.goodfellow@oxide.computer>
2023-10-18 09:53:05 -0700

    break up db update statements into individual files

--- schema/crdb/8.0.0/up03.sql
+++ schema/crdb/8.0.0/up03.sql
@@ -1,2 +1,1 @@
-ALTER TABLE omicron.public.bgp_config ADD COLUMN IF NOT EXISTS bgp_announce_set_id UUID NOT NULL;
 ALTER TABLE omicron.public.switch_port_settings_bgp_peer_config DROP COLUMN IF EXISTS bgp_announce_set_id;
