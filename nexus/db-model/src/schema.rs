eff1828a3aab6f320227c2f822231217790733c5
Levon Tarver <levon@oxide.computer>
2023-11-18 00:15:18 +0000

    bump dendrite and schema

--- nexus/db-model/src/schema.rs
+++ nexus/db-model/src/schema.rs
@@ -1269,7 +1269,7 @@ table! {
 ///
 /// This should be updated whenever the schema is changed. For more details,
 /// refer to: schema/crdb/README.adoc
-pub const SCHEMA_VERSION: SemverVersion = SemverVersion::new(10, 0, 0);
+pub const SCHEMA_VERSION: SemverVersion = SemverVersion::new(11, 0, 0);
 
 allow_tables_to_appear_in_same_query!(
     system_update,
--- schema/crdb/dbinit.sql
+++ schema/crdb/dbinit.sql
@@ -2906,7 +2906,7 @@ INSERT INTO omicron.public.db_metadata (
     version,
     target_version
 ) VALUES
-    ( TRUE, NOW(), NOW(), '10.0.0', NULL)
+    ( TRUE, NOW(), NOW(), '11.0.0', NULL)
 ON CONFLICT DO NOTHING;
 
 COMMIT;
