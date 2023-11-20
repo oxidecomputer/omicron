c7f8382a886de2852c564e11d0aa1b08c5cc31da
Benjamin Naecker <ben@oxide.computer>
2023-11-20 22:02:32 +0000

    Bump schema version

--- nexus/db-model/src/schema.rs
+++ nexus/db-model/src/schema.rs
@@ -1270,7 +1270,7 @@ table! {
 ///
 /// This should be updated whenever the schema is changed. For more details,
 /// refer to: schema/crdb/README.adoc
-pub const SCHEMA_VERSION: SemverVersion = SemverVersion::new(11, 0, 0);
+pub const SCHEMA_VERSION: SemverVersion = SemverVersion::new(12, 0, 0);
 
 allow_tables_to_appear_in_same_query!(
     system_update,
