b32d21bae51a7b3df965cc45179b65685f31bfaa
Mike Zeller <mike@mikezeller.net>
2023-11-21 03:14:01 +0000

    Add MPL

--- ipcc/src/ffi.rs
+++ ipcc/src/ffi.rs
@@ -1,3 +1,9 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+// Copyright 2023 Oxide Computer Company
+
 #![allow(non_upper_case_globals)]
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
