4b6029edb526718198f6b87286f6f82acf829de6
Kyle Simpson <kyle@oxide.computer>
2023-11-27 22:16:38 +0000

    Move new changes to Schema v15.0

--- nexus/db-model/src/schema.rs
+++ nexus/db-model/src/schema.rs
@@ -1320,7 +1320,7 @@ table! {
 ///
 /// This should be updated whenever the schema is changed. For more details,
 /// refer to: schema/crdb/README.adoc
-pub const SCHEMA_VERSION: SemverVersion = SemverVersion::new(14, 0, 0);
+pub const SCHEMA_VERSION: SemverVersion = SemverVersion::new(15, 0, 0);
 
 allow_tables_to_appear_in_same_query!(
     system_update,
--- schema/crdb/dbinit.sql
+++ schema/crdb/dbinit.sql
@@ -3045,7 +3045,7 @@ INSERT INTO omicron.public.db_metadata (
     version,
     target_version
 ) VALUES
-    ( TRUE, NOW(), NOW(), '14.0.0', NULL)
+    ( TRUE, NOW(), NOW(), '15.0.0', NULL)
 ON CONFLICT DO NOTHING;
 
 COMMIT;
