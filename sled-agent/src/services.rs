e6ea02b04896f97897675fb98a76b8ec34925fc0
David Pacheco <dap@oxidecomputer.com>
2023-11-30 12:43:24 -0800

    post-4466 cleanup

--- sled-agent/src/services.rs
+++ sled-agent/src/services.rs
@@ -850,8 +850,6 @@ impl ServiceManager {
                     matches!(
                         z.zone_type,
                         OmicronZoneType::InternalDns { .. }
-                            | OmicronZoneType::BoundaryNtp { .. }
-                            | OmicronZoneType::InternalNtp { .. }
                     )
                 },
             )
@@ -861,8 +859,6 @@ impl ServiceManager {
         // synchronization, which is a pre-requisite for the other services. We
         // keep `OmicronZoneType::InternalDns` because
         // `ensure_all_omicron_zones` is additive.
-        // TODO This looks like a duplicate of the block above -- why do we do
-        // this?
         let all_zones_request = self
             .ensure_all_omicron_zones(
                 &mut existing_zones,
