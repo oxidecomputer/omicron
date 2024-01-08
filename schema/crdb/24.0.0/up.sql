d9e7ccc879b70abf9d34a78495b12f72563c13ac
Andrew J. Stone <andrew.j.stone.1@gmail.com>
2024-01-08 22:35:54 +0000

    fix typo

--- schema/crdb/24.0.0/up.sql
+++ schema/crdb/24.0.0/up.sql
@@ -1,3 +1,3 @@
 UPDATE omicron.public.sled
-    SET last_used_address = (netmask(setmasklen(ip, 64)) & ip) + 0xFFFF
+    SET last_used_address = (netmask(set_masklen(ip, 64)) & ip) + 0xFFFF
     WHERE time_deleted is null;
