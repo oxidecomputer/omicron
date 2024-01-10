a300c81d90939387c4296ced8025632e4e639daf
Justin Bennett <git@just-be.dev>
2024-01-10 18:55:21 -0500

    Drop ssh keys reference from model

--- nexus/db-model/src/instance.rs
+++ nexus/db-model/src/instance.rs
@@ -34,8 +34,6 @@ pub struct Instance {
     /// user data for instance initialization systems (e.g. cloud-init)
     pub user_data: Vec<u8>,
 
-    pub ssh_keys: Option<Vec<String>>,
-
     /// The number of vCPUs (i.e., virtual logical processors) to allocate for
     /// this instance.
     #[diesel(column_name = ncpus)]
@@ -79,13 +77,6 @@ impl Instance {
             identity,
             project_id,
             user_data: params.user_data.clone(),
-            // Comes in as a Name or ID for a consistent API but is stored
-            // as a Vec<String> in the database
-            ssh_keys: params.ssh_keys.clone().map(|vec| {
-                vec.into_iter()
-                    .map(|name_or_id| name_or_id.to_string())
-                    .collect()
-            }),
             ncpus: params.ncpus.into(),
             memory: params.memory.into(),
             hostname: params.hostname.clone(),
