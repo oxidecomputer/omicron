ce76d0f3c64bc328adf2e0be7eb60026dfbf594a
David Pacheco <dap@oxidecomputer.com>
2024-01-16 11:48:17 -0800

    add test for ip_allocator

--- nexus/deployment/src/ip_allocator.rs
+++ nexus/deployment/src/ip_allocator.rs
@@ -55,3 +55,66 @@ impl IpAllocator {
         Some(next)
     }
 }
+
+#[cfg(test)]
+mod test {
+    use super::IpAllocator;
+    use std::collections::BTreeSet;
+    use std::net::Ipv6Addr;
+
+    #[test]
+    fn test_basic() {
+        let range_start: Ipv6Addr = "fd00::d0".parse().unwrap();
+        let range_end: Ipv6Addr = "fd00::e8".parse().unwrap();
+        let reserved: BTreeSet<Ipv6Addr> = [
+            // These first two are deliberately out of range.
+            "fd00::ff".parse().unwrap(),
+            "fd00::c0".parse().unwrap(),
+            "fd00::d3".parse().unwrap(),
+            "fd00::d7".parse().unwrap(),
+        ]
+        .iter()
+        .copied()
+        .collect();
+
+        let mut allocator = IpAllocator::new(range_start, range_end);
+        for r in &reserved {
+            allocator.reserve(*r);
+        }
+
+        let mut allocated = BTreeSet::new();
+        while let Some(addr) = allocator.alloc() {
+            println!("allocated: {}", addr);
+            assert!(!reserved.contains(&addr));
+            assert!(!allocated.contains(&addr));
+            allocated.insert(addr);
+        }
+
+        assert_eq!(
+            allocated,
+            [
+                // Because d7 was reserved, everything up to it is also skipped.
+                // It doesn't have to work that way, but it currently does.
+                "fd00::d8".parse().unwrap(),
+                "fd00::d9".parse().unwrap(),
+                "fd00::da".parse().unwrap(),
+                "fd00::db".parse().unwrap(),
+                "fd00::dc".parse().unwrap(),
+                "fd00::dd".parse().unwrap(),
+                "fd00::de".parse().unwrap(),
+                "fd00::df".parse().unwrap(),
+                "fd00::e0".parse().unwrap(),
+                "fd00::e1".parse().unwrap(),
+                "fd00::e2".parse().unwrap(),
+                "fd00::e3".parse().unwrap(),
+                "fd00::e4".parse().unwrap(),
+                "fd00::e5".parse().unwrap(),
+                "fd00::e6".parse().unwrap(),
+                "fd00::e7".parse().unwrap(),
+            ]
+            .iter()
+            .copied()
+            .collect()
+        );
+    }
+}
