6cd49f70a9ddb593da501e07c841669620a2ea36
Levon Tarver <levon@oxide.computer>
2024-01-26 22:01:59 +0000

    bump schema

--- schema/crdb/dbinit.sql
+++ schema/crdb/dbinit.sql
@@ -3405,7 +3405,7 @@ INSERT INTO omicron.public.db_metadata (
     version,
     target_version
 ) VALUES
-    ( TRUE, NOW(), NOW(), '28.0.0', NULL)
+    ( TRUE, NOW(), NOW(), '29.0.0', NULL)
 ON CONFLICT DO NOTHING;
 
 COMMIT;
