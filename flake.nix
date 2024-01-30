484323c032cbbbb7a6b9055c692110f4bf553645
Eliza Weisman <eliza@elizas.website>
2024-01-30 11:58:32 -0800

    Add `flake.nix` for Nix users
    In order to build Omicron, including running `cargo check` or
    `rust-analyzer`, a number of non-Rust dependencies must be present. This
    commit adds a [Nix flake] file which users of the Nix package manager
    can [use to enter a dev shell][nix-develop] that contains those
    dependencies, making it easy for Nix and NixOS users to develop Omicron
    on NixOS.
    
    The Nix flake configured here creates a build environment with `clang`
    as the C compiler toolchain, and ensures that the following libraries
    are available:
    
    - `libclang` (of a version matching the `clang` C toolchain)
    - `libpq`
    - `libxml2`
    - `libxmlsec`
    - `openssl`
    - `sqlite`
    
    In addition, it provides `pkg-config`, `cmake`, and `rustup`, and
    ensures that the various environment variables that the Rust bindings
    for these libraries use to find the libraries are set correctly.
    
    This makes it possible to run `cargo check`, `cargo build`, and
    `rust-analyzer` on NixOS.
    
    I've also added to the `.envrc` file so that `direnv` will automatically
    use the Nix flake's dev environment, if a `nix` binary is present on the
    user's system and that `nix` binary supports flakes.
    
    [Nix flake]: https://nixos.wiki/wiki/Flakes
    [nix-develop]: https://nixos.wiki/wiki/Development_environment_with_nix-shell#nix_develop

---
+++ flake.nix
@@ -0,0 +1,63 @@
+{
+  description = "Development environment for Omicron";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+      };
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          overlays = [ (import rust-overlay) ];
+          pkgs = import nixpkgs {
+            inherit system overlays;
+          };
+          # use the Rust toolchain defined in the `rust-toolchain.toml` file.
+          rustToolchain = pkgs.pkgsBuildHost.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+          nativeBuildInputs = with pkgs; [
+            rustToolchain
+            cmake
+            stdenv
+            pkg-config
+          ];
+          buildInputs = with pkgs; [
+            # libs
+            openssl
+            postgresql
+            xmlsec
+            sqlite
+            libclang
+            libxml2
+          ];
+        in
+        with pkgs;
+        {
+          devShells.default = mkShell.override
+            {
+              # use Clang as the C compiler for all C libraries
+              stdenv = clangStdenv;
+            }
+            {
+              inherit buildInputs nativeBuildInputs;
+
+              name = "omicron";
+              DEP_PQ_LIBDIRS = " ${postgresql.lib}/lib";
+              LIBCLANG_PATH = "${libclang.lib}/lib";
+              LOCALE_ARCHIVE = "${glibcLocales}/lib/locale/locale-archive";
+              LC_ALL = "en_US.UTF-8";
+              OPENSSL_DIR = "${openssl.dev}";
+              OPENSSL_LIB_DIR = "${openssl.out}/lib";
+              CARGO_TERM_COLOR = "always";
+            };
+        }
+      );
+}
