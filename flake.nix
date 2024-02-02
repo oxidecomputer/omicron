b3ac3990773889b17a8c2a5135395ef02398f9ef
Eliza Weisman <eliza@elizas.website>
2024-02-02 11:17:27 -0800

    nix flake: download OpenAPI JSON in flake

--- flake.nix
+++ flake.nix
@@ -33,6 +33,44 @@
             libclang
             libxml2
           ];
+
+          downloadOpenAPI = { name, apiFile, versionPath }: with pkgs.lib; let
+            extractHash = { line, prefix }: strings.removeSuffix "\"" (strings.removePrefix "${prefix}=\"" line);
+            versionFile = strings.fileContents versionPath;
+            parts = strings.splitString "\n" versionFile;
+            commit = extractHash { line = elemAt parts 0; prefix = "COMMIT"; };
+            sha = extractHash { line = elemAt parts 1; prefix = "SHA2"; };
+          in
+          {
+            file = builtins.fetchurl {
+              url = "https://buildomat.eng.oxide.computer/public/file/oxidecomputer/${name}/openapi/${commit}/${apiFile}.json";
+              sha256 = sha;
+            };
+            filename =
+              let
+                f = "${apiFile}-${commit}.json";
+              in
+              debug.traceValFn (v: " downloaded ${name} OpenAPI: ${v}") f;
+          };
+
+          dendriteOpenAPI = downloadOpenAPI {
+            name = "dendrite";
+            apiFile = "dpd";
+            versionPath = ./tools/dendrite_openapi_version;
+          };
+
+          ddmOpenAPI = downloadOpenAPI {
+            name = "maghemite";
+            apiFile = "ddm-admin";
+            versionPath = ./tools/maghemite_ddm_openapi_version;
+          };
+
+          mgOpenAPI = downloadOpenAPI {
+            name = "maghemite";
+            apiFile = "mg-admin";
+            versionPath = ./tools/maghemite_mg_openapi_version;
+          };
+
         in
         with pkgs;
         {
@@ -53,7 +91,7 @@
                 # on NixOS due to dynamically loading a bunch of libraries in a
                 # way that `nix-ld` doesn't seem to help with. Therefore, depend
                 # on the pre-built patched clickhouse package from nixpkgs,
-                # instead. 
+                # instead.
                 clickhouse
               ];
 
@@ -66,7 +104,16 @@
               # Needed by rustfmt-wrapper, see:
               # https://github.com/oxidecomputer/rustfmt-wrapper/blob/main/src/lib.rs
               RUSTFMT = "${rustToolchain}/bin/rustfmt";
+
+              shellHook = ''
+                rm -r out/downloads
+                mkdir -p out/downloads
+                ln -s ${dendriteOpenAPI.file} out/downloads/${dendriteOpenAPI.filename}
+                ln -s ${mgOpenAPI.file} out/downloads/${mgOpenAPI.filename}
+                ln -s ${ddmOpenAPI.file} out/downloads/${ddmOpenAPI.filename}
+              '';
             };
         }
       );
 }
+
