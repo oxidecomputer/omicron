25820cc26882ce18d3ae92605e1ec5465d117d8f
Sean Klein <sean@oxide.computer>
2024-02-14 00:04:31 -0800

    Fix tests

--- nexus/db-queries/src/db/queries/region_allocation.rs
+++ nexus/db-queries/src/db/queries/region_allocation.rs
@@ -85,7 +85,11 @@ impl<'a, Query> SqlQueryBinds
 // assists with counting bind parameters and recommends avoiding the usage of
 // any non-static strings in query construction.
 struct QueryBuilder {
-    query: diesel::query_builder::BoxedSqlQuery<'static, Pg, diesel::query_builder::SqlQuery>,
+    query: diesel::query_builder::BoxedSqlQuery<
+        'static,
+        Pg,
+        diesel::query_builder::SqlQuery,
+    >,
     bind_counter: BindParamCounter,
 }
 
@@ -101,7 +105,10 @@ impl QueryBuilder {
     // the SQL string.
     fn param(self) -> Self {
         Self {
-            query: self.query.sql("$").sql(self.bind_counter.next().to_string()),
+            query: self
+                .query
+                .sql("$")
+                .sql(self.bind_counter.next().to_string()),
             bind_counter: self.bind_counter,
         }
     }
@@ -110,22 +117,16 @@ impl QueryBuilder {
     // Only permits "&'static str" intentionally to limit susceptibility to
     // SQL injection.
     fn sql(self, s: &'static str) -> Self {
-        Self {
-            query: self.query.sql(s),
-            bind_counter: self.bind_counter,
-        }
+        Self { query: self.query.sql(s), bind_counter: self.bind_counter }
     }
 
     fn bind<BindSt, Value>(self, b: Value) -> Self
     where
         Pg: sql_types::HasSqlType<BindSt>,
         Value: diesel::serialize::ToSql<BindSt, Pg> + Send + 'static,
-        BindSt: Send + 'static
+        BindSt: Send + 'static,
     {
-        Self {
-            query: self.query.bind(b),
-            bind_counter: self.bind_counter,
-        }
+        Self { query: self.query.bind(b), bind_counter: self.bind_counter }
     }
 }
 
@@ -166,12 +167,11 @@ pub fn allocation_query(
     let size_delta = block_size * blocks_per_extent * extent_count;
     let redunancy: i64 = i64::try_from(REGION_REDUNDANCY_THRESHOLD).unwrap();
 
-
     let builder = QueryBuilder::new().sql(
     // Find all old regions associated with a particular volume
 "WITH
-  old_regions AS
-    (SELECT
+  old_regions AS (
+    SELECT
       region.id,
       region.time_created,
       region.time_modified,
@@ -186,8 +186,8 @@ pub fn allocation_query(
     // Calculates the old size being used by zpools under consideration as targets for region
     // allocation.
     .sql("
-  old_zpool_usage AS
-    (SELECT
+  old_zpool_usage AS (
+    SELECT
       dataset.pool_id,
       sum(dataset.size_used) AS size_used
     FROM dataset WHERE ((dataset.size_used IS NOT NULL) AND (dataset.time_deleted IS NULL)) GROUP BY dataset.pool_id),");
@@ -198,22 +198,40 @@ pub fn allocation_query(
     // distinct bind parameters depending on the conditional branch.
     if distinct_sleds {
         builder.sql("
-  candidate_zpools AS
-    (SELECT
+  candidate_zpools AS (
+    SELECT DISTINCT ON (zpool.sled_id)
       old_zpool_usage.pool_id
-    FROM (old_zpool_usage INNER JOIN (zpool INNER JOIN sled ON (zpool.sled_id = sled.id)) ON (zpool.id = old_zpool_usage.pool_id))
-    WHERE (((old_zpool_usage.size_used + ").param().sql(" ) <= total_size) AND (sled.provision_state = 'provisionable'))),")
-        .bind::<sql_types::BigInt, _>(size_delta as i64)
-    } else {
-        builder.sql("
-  candidate_zpools AS
-    (SELECT DISTINCT ON (zpool.sled_id)
-      old_zpool_usage.pool_id
-    FROM (old_zpool_usage INNER JOIN (zpool INNER JOIN sled ON (zpool.sled_id = sled.id)) ON (zpool.id = old_zpool_usage.pool_id))
-    WHERE (((old_zpool_usage.size_used + ").param().sql(" ) <= total_size) AND (sled.provision_state = 'provisionable'))
+    FROM (
+        old_zpool_usage
+          INNER JOIN
+        (zpool INNER JOIN sled ON (zpool.sled_id = sled.id)) ON (zpool.id = old_zpool_usage.pool_id)
+    )
+    WHERE (
+      ((old_zpool_usage.size_used + ").param().sql(" ) <= total_size)
+      AND
+      (sled.provision_state = 'provisionable')
+    )
     ORDER BY zpool.sled_id, md5((CAST(zpool.id as BYTEA) || ").param().sql("))),")
         .bind::<sql_types::BigInt, _>(size_delta as i64)
         .bind::<sql_types::Bytea, _>(seed.clone())
+    } else {
+        builder.sql("
+  candidate_zpools AS (
+    SELECT
+      old_zpool_usage.pool_id
+    FROM (
+      old_zpool_usage
+      INNER JOIN
+      (zpool INNER JOIN sled ON (zpool.sled_id = sled.id))
+      ON (zpool.id = old_zpool_usage.pool_id)
+    )
+    WHERE (
+      ((old_zpool_usage.size_used + ").param().sql(" ) <= total_size)
+      AND
+      (sled.provision_state = 'provisionable')
+    )
+  ),")
+        .bind::<sql_types::BigInt, _>(size_delta as i64)
     }
     // Find datasets which could be used for provisioning regions.
     //
@@ -225,20 +243,24 @@ pub fn allocation_query(
     // We select only one dataset from each zpool.
     .sql(
 "
-  candidate_datasets AS
-    (SELECT DISTINCT ON (dataset.pool_id)
+  candidate_datasets AS (
+    SELECT DISTINCT ON (dataset.pool_id)
       dataset.id,
       dataset.pool_id
     FROM (dataset INNER JOIN candidate_zpools ON (dataset.pool_id = candidate_zpools.pool_id))
-    WHERE (((dataset.time_deleted IS NULL) AND (dataset.size_used IS NOT NULL)) AND (dataset.kind = 'crucible'))
+    WHERE (
+      ((dataset.time_deleted IS NULL) AND
+      (dataset.size_used IS NOT NULL)) AND
+      (dataset.kind = 'crucible')
+    )
     ORDER BY dataset.pool_id, md5((CAST(dataset.id as BYTEA) || ").param().sql("))),")
     .bind::<sql_types::Bytea, _>(seed.clone())
     // We order by md5 to shuffle the ordering of the datasets.
     // md5 has a uniform output distribution so it does the job.
     .sql(
 "
-  shuffled_candidate_datasets AS
-    (SELECT
+  shuffled_candidate_datasets AS (
+    SELECT
       candidate_datasets.id,
       candidate_datasets.pool_id
     FROM candidate_datasets
@@ -248,8 +270,8 @@ pub fn allocation_query(
     // Create the regions-to-be-inserted for the volume.
     .sql(
 "
-  candidate_regions AS
-    (SELECT
+  candidate_regions AS (
+    SELECT
       gen_random_uuid() AS id,
       now() AS time_created,
       now() AS time_modified,
@@ -268,10 +290,9 @@ pub fn allocation_query(
     // 1. Which datasets will have size adjustments
     // 2. Which pools those datasets belong to
     // 3. The delta in size-used
-    .sql(
-"
-  proposed_dataset_changes AS
-    (SELECT
+    .sql("
+  proposed_dataset_changes AS (
+    SELECT
       candidate_regions.dataset_id AS id,
       dataset.pool_id AS pool_id,
       ((candidate_regions.block_size * candidate_regions.blocks_per_extent) * candidate_regions.extent_count) AS size_used_delta
@@ -300,17 +321,21 @@ pub fn allocation_query(
     // before we improve the allocation strategy, this check will make sure
     // we don't violate drive redundancy, and generate an error instead.
     .sql("
-  do_insert AS
-    (SELECT
-      (((((SELECT COUNT(*) FROM old_regions LIMIT 1) < ").param().sql(") AND CAST(IF(((SELECT COUNT(*) FROM candidate_zpools LIMIT 1) >= ").param().sql(concatcp!("), 'TRUE', '", NOT_ENOUGH_ZPOOL_SPACE_SENTINEL, "') AS BOOL)) AND CAST(IF(((SELECT COUNT(*) FROM candidate_regions LIMIT 1) >= ")).param().sql(concatcp!("), 'TRUE', '", NOT_ENOUGH_DATASETS_SENTINEL, "') AS BOOL)) AND CAST(IF(((SELECT COUNT(DISTINCT dataset.pool_id) FROM (candidate_regions INNER JOIN dataset ON (candidate_regions.dataset_id = dataset.id)) LIMIT 1) >= ")).param().sql(concatcp!("), 'TRUE', '", NOT_ENOUGH_UNIQUE_ZPOOLS_SENTINEL, "') AS BOOL)) AS insert),"))
+  do_insert AS (
+    SELECT (((
+        ((SELECT COUNT(*) FROM old_regions LIMIT 1) < ").param().sql(") AND
+        CAST(IF(((SELECT COUNT(*) FROM candidate_zpools LIMIT 1) >= ").param().sql(concatcp!("), 'TRUE', '", NOT_ENOUGH_ZPOOL_SPACE_SENTINEL, "') AS BOOL)) AND
+        CAST(IF(((SELECT COUNT(*) FROM candidate_regions LIMIT 1) >= ")).param().sql(concatcp!("), 'TRUE', '", NOT_ENOUGH_DATASETS_SENTINEL, "') AS BOOL)) AND
+        CAST(IF(((SELECT COUNT(DISTINCT dataset.pool_id) FROM (candidate_regions INNER JOIN dataset ON (candidate_regions.dataset_id = dataset.id)) LIMIT 1) >= ")).param().sql(concatcp!("), 'TRUE', '", NOT_ENOUGH_UNIQUE_ZPOOLS_SENTINEL, "') AS BOOL)
+    ) AS insert),"))
     .bind::<sql_types::BigInt, _>(redunancy)
     .bind::<sql_types::BigInt, _>(redunancy)
     .bind::<sql_types::BigInt, _>(redunancy)
     .bind::<sql_types::BigInt, _>(redunancy)
     .sql(
 "
-  inserted_regions AS
-    (INSERT INTO region
+  inserted_regions AS (
+    INSERT INTO region
       (id, time_created, time_modified, dataset_id, volume_id, block_size, blocks_per_extent, extent_count)
     SELECT
       candidate_regions.id,
@@ -334,8 +359,8 @@ pub fn allocation_query(
       region.blocks_per_extent,
       region.extent_count
     ),
-  updated_datasets AS
-    (UPDATE dataset SET
+  updated_datasets AS (
+    UPDATE dataset SET
       size_used = (dataset.size_used + (SELECT proposed_dataset_changes.size_used_delta FROM proposed_dataset_changes WHERE (proposed_dataset_changes.id = dataset.id) LIMIT 1))
     WHERE (
       (dataset.id = ANY(SELECT proposed_dataset_changes.id FROM proposed_dataset_changes)) AND
