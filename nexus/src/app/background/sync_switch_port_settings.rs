ab0ed147590bbf1949783f5913d9a511324dc400
Levon Tarver <levon@oxide.computer>
2024-02-20 23:52:01 +0000

    more WIP: continue port of saga to RPW

--- nexus/src/app/background/sync_switch_port_settings.rs
+++ nexus/src/app/background/sync_switch_port_settings.rs
@@ -12,20 +12,22 @@ use crate::app::{
 
 use internal_dns::resolver::Resolver;
 use internal_dns::ServiceName;
+use nexus_db_model::BgpConfig;
+use uuid::Uuid;
 
 use super::common::BackgroundTask;
 use dpd_client::types::PortId;
 use futures::future::BoxFuture;
 use futures::FutureExt;
 use mg_admin_client::types::{
-    AddStaticRoute4Request, BfdPeerInfo, BgpPeerConfig,
+    AddStaticRoute4Request, ApplyRequest, BgpPeerConfig,
     DeleteStaticRoute4Request, Prefix4, StaticRoute4, StaticRoute4List,
 };
 use nexus_db_queries::{
     context::OpContext,
     db::{datastore::SwitchPortSettingsCombinedResult, DataStore},
 };
-use nexus_types::identity::Resource;
+use nexus_types::{external_api::params, identity::Resource};
 use omicron_common::{address::DENDRITE_PORT, OMICRON_DPD_TAG};
 use omicron_common::{
     address::{get_sled_address, Ipv6Subnet, MGD_PORT},
@@ -258,32 +260,104 @@ impl BackgroundTask for SwitchPortSettingsManager {
             // calculate and apply BGP changes
             //
 
-            let mut downstream_bgp_peers: HashMap<
+            // build a list of desired settings for each switch
+            let mut desired_bgp_configs: HashMap<
                 SwitchLocation,
-                Vec<BgpPeerConfig>,
+                Vec<ApplyRequest>,
             > = HashMap::new();
 
-            for (location, client) in &mgd_clients {
-                let Ok(response) = client.inner.get_routers().await else {
-                    todo!("handle error")
-                };
-                todo!("update downstream_bgp_peers")
-            }
+            // we currently only support one bgp config per switch
+            let mut switch_bgp_config: HashMap<SwitchLocation, (Uuid, BgpConfig)> = HashMap::new();
 
-            let mut upstream_bgp_peers: HashMap<
-                SwitchLocation,
-                Vec<BgpPeerConfig>,
-            > = HashMap::new();
+            // Prefixes are associated to BgpConfig via the config id
+            let mut bgp_announce_set: HashMap<Uuid, Vec<Prefix4>> = HashMap::new();
 
-            for (location, _port, change) in &changes {
+            for (location, port, change) in &changes {
                 let PortSettingsChange::Apply(settings) = change else {
                     continue;
                 };
 
-                let mut bgp_peers: Vec<BgpPeerConfig> = settings
-                    .bgp_peers
-                    .iter()
-                    .map(|peer| BgpPeerConfig {
+                // desired peer configurations for a given switch port
+                let mut peers: HashMap<String, Vec<BgpPeerConfig>> = HashMap::new();
+
+                for peer in &settings.bgp_peers {
+                    let bgp_config_id = peer.bgp_config_id;
+
+                    // since we only have one bgp config per switch, we only need to fetch it once
+                    let bgp_config = match switch_bgp_config.entry(*location) {
+                        Entry::Occupied(occupied_entry) => {
+                            let (existing_id, existing_config) = occupied_entry.get().clone();
+                            // verify peers don't have differing configs
+                            if existing_id != bgp_config_id {
+                                // should we flag the switch and not do *any* updates to it?
+                                // with the logic as-is, it will skip the config for this port and move on
+                                 error!(
+                                    log,
+                                    "peers do not have matching asn (only one asn allowed per switch)";
+                                    "switch" => ?location,
+                                    "first_config_id" => ?existing_id,
+                                    "second_config_id" => ?bgp_config_id,
+                                );
+                                break;
+                            }
+                            existing_config
+                        },
+                        Entry::Vacant(vacant_entry) => {
+                            // get the bgp config for this peer
+                            let config = match self
+                                .datastore
+                                .bgp_config_get(opctx, &bgp_config_id.into())
+                                .await
+                            {
+                                Ok(config) => config,
+                                Err(_) => todo!(),
+                            };
+                            vacant_entry.insert((bgp_config_id, config.clone()));
+                            config
+                        },
+                    };
+
+                    //
+                    // build a list of prefixes from the announcements in the bgp config
+                    //
+
+                    // Same thing as above, check to see if we've already built the announce set,
+                    // if so we'll skip this step
+                    if bgp_announce_set.get(&bgp_config.bgp_announce_set_id).is_none() {
+                        let announcements = match self
+                            .datastore
+                            .bgp_announce_list(
+                                opctx,
+                                &params::BgpAnnounceSetSelector {
+                                    name_or_id: bgp_config
+                                        .bgp_announce_set_id
+                                        .into(),
+                                },
+                            )
+                            .await
+                        {
+                            Ok(a) => a,
+                            Err(_) => todo!(),
+                        };
+
+                        let mut prefixes: Vec<Prefix4> = vec![];
+
+                        for announcement in &announcements {
+                            let value = match announcement.network.ip() {
+                                IpAddr::V4(value) => value,
+                                IpAddr::V6(a) => {
+                                    error!(log, "bad request, only ipv4 supported at this time"; "requested_address" => ?a);
+                                    continue;
+                                },
+                            };
+                            prefixes.push(Prefix4 { value, length: announcement.network.prefix() });
+                        }
+                        bgp_announce_set.insert(bgp_config.bgp_announce_set_id, prefixes);
+                    }
+
+
+                    // now that the peer passes the above validations, add it to the list for configuration
+                    let peer_config = BgpPeerConfig {
                         name: format!("{}", peer.addr.ip()),
                         host: format!("{}:179", peer.addr.ip()),
                         hold_time: peer.hold_time.0.into(),
@@ -293,42 +367,73 @@ impl BackgroundTask for SwitchPortSettingsManager {
                         keepalive: peer.keepalive.0.into(),
                         resolution: BGP_SESSION_RESOLUTION,
                         passive: false,
-                    })
-                    .collect();
+                    };
 
-                match upstream_bgp_peers.entry(*location) {
+                    // update the stored vec if it exists, create a new on if it doesn't exist
+                    match peers.entry(port.port_name.clone()) {
+                        Entry::Occupied(mut occupied_entry) => {
+                            occupied_entry.get_mut().push(peer_config);
+                        },
+                        Entry::Vacant(vacant_entry) => {
+                            vacant_entry.insert(vec![peer_config]);
+                        },
+                    }
+                }
+
+                let (config_id, request_bgp_config) = match switch_bgp_config.get(location) {
+                    Some(config) => config,
+                    None => {
+                        info!(log, "no bgp config found for switch, skipping."; "switch" => ?location);
+                        continue;
+                    },
+                };
+
+                let request_prefixes = match bgp_announce_set.get(&request_bgp_config.bgp_announce_set_id) {
+                    Some(prefixes) => prefixes,
+                    None => {
+                        error!(
+                            log,
+                            "no prefixes to announce found for bgp config";
+                            "switch" => ?location,
+                            "announce_set_id" => ?request_bgp_config.bgp_announce_set_id,
+                            "bgp_config_id" => ?config_id,
+                        );
+                        continue;
+                    },
+                };
+
+                let request = ApplyRequest {
+                    asn: *request_bgp_config.asn,
+                    peers,
+                    originate: request_prefixes.clone(),
+                };
+
+                match desired_bgp_configs.entry(*location) {
                     Entry::Occupied(mut occupied_entry) => {
-                        occupied_entry.get_mut().append(&mut bgp_peers);
+                        occupied_entry.get_mut().push(request);
                     }
                     Entry::Vacant(vacant_entry) => {
-                        vacant_entry.insert(bgp_peers);
+                        vacant_entry.insert(vec![request]);
                     }
                 }
             }
 
-            let mut bgp_peers_to_add: HashMap<
-                SwitchLocation,
-                Vec<BgpPeerConfig>,
-            > = HashMap::new();
-
-            for (location, peers) in &upstream_bgp_peers {
-                // if upstream peer is not in downstream, add it
-                let Some(peers_on_switch) = downstream_bgp_peers.get(location)
-                else {
-                    todo!("handle missing location")
+            // verify that each switch's peer group are all using the same asn
+            // if they are not, reject the set of peers
+            for (location, configs) in &desired_bgp_configs {
+                let client = match mgd_clients.get(location) {
+                    Some(client) => client,
+                    None => {
+                        error!(log, "no mgd client found for switch"; "switch_location" => ?location);
+                        continue;
+                    },
                 };
-
-                for peer in peers {}
+                for config in configs {
+                    if let Err(e) = client.inner.bgp_apply(config).await {
+                        error!(log, "error while applying bgp configuration"; "error" => ?e);
+                    }
+                }
             }
-
-            let mut bgp_peers_to_del: HashMap<
-                SwitchLocation,
-                Vec<BgpPeerConfig>,
-            > = HashMap::new();
-
-            // delete old peers
-
-            // add new peers
 
             //
             // calculated and apply bootstore changes
