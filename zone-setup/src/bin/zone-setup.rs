746f36aa0eb7ae37b61553f34342dd1995a4e238
karencfv <karencfv@users.noreply.github.com>
2024-03-14 16:54:47 -0700

    Configure zone-setup cli for ntp zone

--- zone-setup/src/bin/zone-setup.rs
+++ zone-setup/src/bin/zone-setup.rs
@@ -5,7 +5,7 @@
 //! CLI to set up zone networking
 
 use anyhow::anyhow;
-use clap::{arg, command, ArgMatches, Command};
+use clap::{arg, command, Arg, ArgMatches, Command};
 use illumos_utils::ipadm::Ipadm;
 use illumos_utils::route::{Gateway, Route};
 use omicron_common::cmd::fatal;
@@ -49,6 +49,29 @@ fn parse_opte_iface(s: &str) -> anyhow::Result<String> {
         return Err(anyhow!("ERROR: Missing OPTE interface"));
     };
     s.parse().map_err(|_| anyhow!("ERROR: Invalid OPTE interface"))
+}
+
+fn parse_chrony_conf(s: &str) -> anyhow::Result<String> {
+    if s == "unknown" {
+        return Err(anyhow!("ERROR: Missing chrony configuration file"));
+    };
+
+    // TODO: actually check the format of the string mends with "chrony.conf"
+    s.parse().map_err(|_| anyhow!("ERROR: Invalid chrony configuration file"))
+}
+
+fn parse_chrony_tpl(s: &str) -> anyhow::Result<String> {
+    if s == "unknown" {
+        return Err(anyhow!("ERROR: Missing chrony template"));
+    };
+
+    // TODO: actually check the format of the string matches one of internal
+    // or boundary NTP
+    s.parse().map_err(|_| anyhow!("ERROR: Invalid chrony template"))
+}
+
+fn parse_boundary(s: &str) -> anyhow::Result<bool> {
+    s.parse().map_err(|_| anyhow!("ERROR: Invalid boundary input"))
 }
 
 #[tokio::main]
@@ -120,6 +143,48 @@ async fn do_run() -> Result<(), CmdError> {
                     .value_parser(parse_ip),
                 ),
         )
+        .subcommand(
+            Command::new("ntp")
+                .about("Start (set up), refresh and stop methods for NTP zone")
+                .subcommand(
+                    Command::new("start")
+                        .about("NTP zone setup and start chronyd daemon") 
+                        // TODO: Maybe I don't even need --file nor --template
+                        .arg(
+                            arg!(-f --file <String> "Chrony configuration file")
+                            .required(true)
+                    .value_parser(parse_chrony_conf)
+                        )
+                    .arg(
+                        arg!(-t --template <String> "Boundary or internal NTP configuration template")
+                        .required(true)
+                .value_parser(parse_chrony_tpl),
+                    )
+                    .arg(
+                        arg!(-b --boundary <bool> "Whether this is a boundary or internal NTP zone")
+                        .required(true)
+                .value_parser(parse_boundary),
+                    )
+                    .arg(
+                        Arg::new("servers")
+                        .short('s')
+                        .long("servers")
+                        .value_delimiter(' ')
+                        .help("List of NTP servers separated by a space")
+                        .required(true)
+                        // TODO: Add some parsing to this?
+                    )
+                    .arg(
+                        Arg::new("allow")
+                        .short('a')
+                        .long("allow")
+                        .value_delimiter(' ')
+                        .help("List of allowed IPv6 addresses separated by a space")
+                        // Not required as this only appears in boundary NTP config
+                        // TODO: Add some parsing to this?
+                    ),
+                ),
+        )
         .get_matches();
 
     if let Some(matches) = matches.subcommand_matches("common-networking") {
@@ -133,7 +198,10 @@ async fn do_run() -> Result<(), CmdError> {
     Ok(())
 }
 
-async fn common_nw_set_up(matches: &ArgMatches, log: Logger) -> Result<(), CmdError> {
+async fn common_nw_set_up(
+    matches: &ArgMatches,
+    log: Logger,
+) -> Result<(), CmdError> {
     let datalink: &String = matches.get_one("datalink").unwrap();
     let static_addr: &Ipv6Addr = matches.get_one("static_addr").unwrap();
     let gateway: Ipv6Addr = *matches.get_one("gateway").unwrap();
