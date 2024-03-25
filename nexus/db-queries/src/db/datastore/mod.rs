f508831c21c604353bf61b81c61f5506a83191e1
Eliza Weisman <eliza@elizas.website>
2024-03-25 11:17:52 -0700

    start sketching saga

--- nexus/db-queries/src/db/datastore/mod.rs
+++ nexus/db-queries/src/db/datastore/mod.rs
@@ -111,7 +111,7 @@ mod zpool;
 pub use address_lot::AddressLotCreateResult;
 pub use dns::DataStoreDnsTest;
 pub use dns::DnsVersionUpdateBuilder;
-pub use instance::InstanceAndActiveVmm;
+pub use instance::{InstanceAndActiveVmm, InstanceSnapshot};
 pub use inventory::DataStoreInventoryTest;
 use nexus_db_model::AllSchemaVersions;
 pub use rack::RackInit;
