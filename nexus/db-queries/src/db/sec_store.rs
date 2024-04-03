6822514b8394e6f3bd43d130daef4b03229080e4
Rain <rain@oxide.computer>
2024-04-02 23:33:58 -0700

    [𝘀𝗽𝗿] initial version
    Created using spr 1.3.6-beta.1

--- nexus/db-queries/src/db/sec_store.rs
+++ nexus/db-queries/src/db/sec_store.rs
@@ -7,9 +7,12 @@
 use crate::db::{self, model::Generation};
 use anyhow::Context;
 use async_trait::async_trait;
+use futures::TryFutureExt;
+use omicron_common::backoff;
 use slog::Logger;
 use std::fmt;
 use std::sync::Arc;
+use std::time::Duration;
 use steno::SagaId;
 
 /// Implementation of [`steno::SecStore`] backed by the Omicron CockroachDB
@@ -53,16 +56,46 @@ impl steno::SecStore for CockroachDbSecStore {
     }
 
     async fn record_event(&self, event: steno::SagaNodeEvent) {
-        debug!(&self.log, "recording saga event";
+        let log = self.log.new(o!(
             "saga_id" => event.saga_id.to_string(),
-            "node_id" => ?event.node_id,
-            "event_type" => ?event.event_type,
-        );
+            "node_id" => event.node_id.to_string(),
+            "event_type" => format!("{:?}", event.event_type),
+        ));
+
+        debug!(&log, "recording saga event");
         let our_event = db::saga_types::SagaNodeEvent::new(event, self.sec_id);
 
-        // TODO-robustness This should be wrapped with a retry loop rather than
-        // unwrapping the result.  See omicron#2416.
-        self.datastore.saga_create_event(&our_event).await.unwrap();
+        backoff::retry_notify_ext(
+            // This is an internal service query to CockroachDB.
+            backoff::retry_policy_internal_service(),
+            || {
+                // XXX: do we need to filter the kinds of errors we retry on?
+                // Are there particular database errors which are unrecoverable
+                // and where we should just panic.
+                self.datastore
+                    .saga_create_event(&our_event)
+                    .map_err(backoff::BackoffError::transient)
+            },
+            move |error, call_count, total_duration| {
+                if call_count == 0 {
+                    info!(
+                        &log,
+                        "failed to record saga event, retrying";
+                        "error" => ?error,
+                    );
+                } else if total_duration > Duration::from_secs(20) {
+                    warn!(
+                        &log,
+                        "failed to record saga event, retrying";
+                        "error" => ?error,
+                        "call_count" => call_count,
+                        "total_duration" => ?total_duration,
+                    );
+                }
+            },
+        )
+        .await
+        .expect("the above backoff retries forever")
     }
 
     async fn saga_update(&self, id: SagaId, update: steno::SagaCachedState) {
