347c91e085263cfeae6b61e86f811d1ecb89883a
Rain <rain@oxide.computer>
2024-04-25 01:46:26 -0700

    [𝘀𝗽𝗿] initial version
    Created using spr 1.3.6-beta.1

--- dev-tools/omdb/src/bin/omdb/nexus.rs
+++ dev-tools/omdb/src/bin/omdb/nexus.rs
@@ -20,6 +20,7 @@ use futures::future::try_join;
 use futures::TryStreamExt;
 use nexus_client::types::ActivationReason;
 use nexus_client::types::BackgroundTask;
+use nexus_client::types::BackgroundTasksActivateRequest;
 use nexus_client::types::CurrentStatus;
 use nexus_client::types::LastResult;
 use nexus_client::types::SledSelector;
@@ -77,6 +78,15 @@ enum BackgroundTasksCommands {
     List,
     /// Print human-readable summary of the status of each background task
     Show,
+    /// Activate one or more background tasks
+    Activate(BackgroundTasksActivateArgs),
+}
+
+#[derive(Debug, Args)]
+struct BackgroundTasksActivateArgs {
+    /// Name of the background tasks to activate
+    #[clap(value_name = "TASK_NAME", required = true)]
+    tasks: Vec<String>,
 }
 
 #[derive(Debug, Args)]
@@ -298,6 +308,9 @@ impl NexusArgs {
             NexusCommands::BackgroundTasks(BackgroundTasksArgs {
                 command: BackgroundTasksCommands::Show,
             }) => cmd_nexus_background_tasks_show(&client).await,
+            NexusCommands::BackgroundTasks(BackgroundTasksArgs {
+                command: BackgroundTasksCommands::Activate(args),
+            }) => cmd_nexus_background_tasks_activate(&client, args).await,
 
             NexusCommands::Blueprints(BlueprintsArgs {
                 command: BlueprintsCommands::List,
@@ -462,6 +475,23 @@ async fn cmd_nexus_background_tasks_show(
         print_task(bgtask);
     }
 
+    Ok(())
+}
+
+/// Runs `omdb nexus background-tasks activate`
+async fn cmd_nexus_background_tasks_activate(
+    client: &nexus_client::Client,
+    args: &BackgroundTasksActivateArgs,
+) -> Result<(), anyhow::Error> {
+    let body = BackgroundTasksActivateRequest {
+        bgtask_names: args.tasks.iter().cloned().collect(),
+    };
+    client
+        .bgtask_activate(&body)
+        .await
+        .context("error activating background tasks")?;
+
+    eprintln!("activated background tasks: {}", args.tasks.join(", "));
     Ok(())
 }
 
