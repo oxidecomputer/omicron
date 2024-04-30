3f6264b81304ca4c8f59c170ad3b04838dcad80d
David Crespo <david-crespo@users.noreply.github.com>
2024-04-29 20:03:56 -0500

    [nexus] Fix overzealous authz check on floating IP create (#5660)
    Fixes #5508 
    
    I think and hope that this is all that's required!

--- nexus/src/app/external_ip.rs
+++ nexus/src/app/external_ip.rs
@@ -114,7 +114,11 @@ impl super::Nexus {
         let pool = match pool {
             Some(pool) => Some(
                 self.ip_pool_lookup(opctx, &pool)?
-                    .lookup_for(authz::Action::Read)
+                    // every authenticated user has CreateChild on IP pools
+                    // because they need to be able to allocate IPs from them.
+                    // The check that the pool is linked to the current silo
+                    // happens inside allocate_floating_ip
+                    .lookup_for(authz::Action::CreateChild)
                     .await?
                     .0,
             ),
