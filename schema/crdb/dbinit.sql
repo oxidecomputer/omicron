90ca30b5bc5c731fb23e8596ef6088420ae73c1c
Benjamin Naecker <ben@oxide.computer>
2024-05-05 20:12:28 +0000

    Insert a default allowlist during schema migrations
    On new racks, an allowlist for user-facing services is populated during
    RSS. Nexus waits for the allowlist to exist before starting its external
    server, to ensure that the rules exist from the first request. On
    existing racks, however, RSS has already run and the allowlist is never
    inserted, causing Nexus to wait forever.
    
    This commit adds a one-time insertion of the default allowlist (allowing
    any external traffic) as a data-only DB migration. An existing allowlist
    is not modified to preserve existing allowlists during further
    migrations.

--- schema/crdb/dbinit.sql
+++ schema/crdb/dbinit.sql
@@ -3829,7 +3829,7 @@ INSERT INTO omicron.public.db_metadata (
     version,
     target_version
 ) VALUES
-    (TRUE, NOW(), NOW(), '57.0.0', NULL)
+    (TRUE, NOW(), NOW(), '58.0.0', NULL)
 ON CONFLICT DO NOTHING;
 
 COMMIT;
