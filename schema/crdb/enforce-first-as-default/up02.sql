77769b19df64e095a3517ea2e0a5e3e1e385dc7c
John Gallagher <john@oxidecomputer.com>
2024-05-06 09:06:59 -0400

    add intermediate migration step to update NULL columns

--- schema/crdb/enforce-first-as-default/up02.sql
+++ schema/crdb/enforce-first-as-default/up02.sql
@@ -1,1 +1,5 @@
-ALTER TABLE omicron.public.switch_port_settings_bgp_peer_config ALTER COLUMN enforce_first_as SET NOT NULL;
+set local disallow_full_table_scans = off;
+
+UPDATE omicron.public.switch_port_settings_bgp_peer_config
+    SET enforce_first_as = false
+    WHERE enforce_first_as IS NULL;
