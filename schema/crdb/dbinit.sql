10bc9374eddafe710ebf28a4e3378f4772724d7e
Eliza Weisman <eliza@elizas.website>
2024-05-08 09:45:53 -0700

    oh, so *that's* how you add to the schema

--- schema/crdb/dbinit.sql
+++ schema/crdb/dbinit.sql
@@ -3846,7 +3846,7 @@ INSERT INTO omicron.public.db_metadata (
     version,
     target_version
 ) VALUES
-    (TRUE, NOW(), NOW(), '59.0.0', NULL)
+    (TRUE, NOW(), NOW(), '60.0.0', NULL)
 ON CONFLICT DO NOTHING;
 
 COMMIT;
