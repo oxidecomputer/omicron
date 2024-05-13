006fb41fb18fbe271adb11babadc884093710a5b
Eliza Weisman <eliza@elizas.website>
2024-05-13 09:42:05 -0700

    wip

--- nexus/src/app/sagas/instance_update/mod.rs
+++ nexus/src/app/sagas/instance_update/mod.rs
@@ -18,6 +18,8 @@ use omicron_common::api::external::{Error, InstanceState};
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 
+pub mod destroyed;
+
 /// Parameters to the instance update saga.
 #[derive(Debug, Deserialize, Serialize)]
 pub(crate) struct Params {
@@ -26,36 +28,25 @@ pub(crate) struct Params {
     /// Authentication context to use to fetch the instance's current state from
     /// the database.
     pub serialized_authn: authn::saga::Serialized,
-}
 
-declare_saga_actions! {
-    instance_update;
-
-    // Read the target Instance from CRDB and join with its active VMM and
-    // migration target VMM records if they exist, and then acquire the
-    // "instance updater" lock with this saga's ID if no other saga is currently
-    // updating the instance.
-    LOOKUP_AND_LOCK_INSTANCE -> "instance_and_vmms" {
-        + siu_lookup_and_lock_instance
-        - siu_lookup_and_lock_instance_undo
-    }
-
-    //
+    pub start_state: InstanceSnapshot,
 }
 
 const SAGA_INSTANCE_LOCK_ID: &str = "saga_instance_lock_id";
 
-async fn siu_lookup_and_lock_instance(
+async fn siu_lock_instance(
     sagactx: NexusActionContext,
 ) -> Result<(), anyhow::Error> {
     let osagactx = sagactx.user_data();
-    let Params { ref authz_instance, ref serialized_authn, .. } =
-        sagactx.saga_params::<Params>()?;
+    let Params {
+        ref authz_instance,
+        ref serialized_authn,
+        ref start_state,
+        ..
+    } = sagactx.saga_params::<Params>()?;
 
     let opctx =
         crate::context::op_context_for_saga_action(&sagactx, serialized_authn);
-    let snapshot =
-        osagactx.datastore().instance_fetch_all(&opctx, authz_instance).await?;
 
     // try to lock
 
@@ -65,15 +56,15 @@ async fn siu_lookup_and_lock_instance(
         .instance_updater_try_lock(
             &opctx,
             &authz_instance,
-            &snapshot.instance.runtime_state.updater_gen,
+            start_state.instance.runtime_state.updater_gen,
             &lock_id,
         )
         .await?;
 
-    Ok(snapshot)
+    Ok(())
 }
 
-async fn siu_lookup_and_lock_instance_undo(
+async fn siu_unlock_instance(
     sagactx: NexusActionContext,
 ) -> Result<(), anyhow::Error> {
     let osagactx = sagactx.user_data();
@@ -83,10 +74,9 @@ async fn siu_lookup_and_lock_instance_undo(
 
     let opctx =
         crate::context::op_context_for_saga_action(&sagactx, serialized_authn);
-    osagactx.datastore().instance_updater_unlock(
-        &opctx,
-        &authz_instance,
-        &lock_id,
-    )?;
+    osagactx
+        .datastore()
+        .instance_updater_unlock(&opctx, &authz_instance, &lock_id)
+        .await?;
     Ok(())
 }
