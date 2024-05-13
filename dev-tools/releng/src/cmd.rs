712aef140a025188c2bd54caa8d42a5bd50e63ab
iliana etaoin <iliana@oxide.computer>
2024-05-13 05:32:48 +0000

    [shakes fist at tokio]

--- dev-tools/releng/src/cmd.rs
+++ dev-tools/releng/src/cmd.rs
@@ -14,6 +14,7 @@ use anyhow::Context;
 use anyhow::Result;
 use slog::debug;
 use slog::Logger;
+use tokio::io::AsyncWriteExt;
 use tokio::process::Command;
 
 pub(crate) trait CommandExt {
@@ -64,52 +65,66 @@ impl CommandExt for Command {
     }
 
     async fn is_success(&mut self, logger: &Logger) -> Result<bool> {
-        let output = run(
-            self.stdin(Stdio::null())
-                .stdout(Stdio::inherit())
-                .stderr(Stdio::inherit()),
-            logger,
-        )
-        .await?;
-        Ok(output.status.success())
+        Ok(xtrace(self, logger, Command::status).await?.success())
     }
 
     async fn ensure_success(&mut self, logger: &Logger) -> Result<()> {
-        let output = run(
-            self.stdin(Stdio::null())
-                .stdout(Stdio::inherit())
-                .stderr(Stdio::inherit()),
-            logger,
-        )
-        .await?;
-        self.check_status(output.status)
+        let status = xtrace(self, logger, Command::status).await?;
+        self.check_status(status)
     }
 
     async fn ensure_stdout(&mut self, logger: &Logger) -> Result<String> {
-        let output = run(
-            self.stdin(Stdio::null())
-                .stdout(Stdio::piped())
-                .stderr(Stdio::inherit()),
-            logger,
-        )
-        .await?;
+        let output = xtrace(self, logger, Command::output).await?;
+
+        // Obnoxiously, `tokio::process::Command::output` overrides
+        // your stdout and stderr settings (because it doesn't use
+        // std::process::Command::output).
+        //
+        // Compensate by dumping whatever is in `output.stderr` to stderr.
+        tokio::io::stderr().write_all(&output.stderr).await?;
+
         self.check_status(output.status)?;
         String::from_utf8(output.stdout).context("command stdout was not UTF-8")
     }
 }
 
-async fn run(command: &mut Command, logger: &Logger) -> Result<Output> {
+trait AsStatus {
+    fn as_status(&self) -> &ExitStatus;
+}
+
+impl AsStatus for ExitStatus {
+    fn as_status(&self) -> &ExitStatus {
+        &self
+    }
+}
+
+impl AsStatus for Output {
+    fn as_status(&self) -> &ExitStatus {
+        &self.status
+    }
+}
+
+async fn xtrace<F, Fut, T>(
+    command: &mut Command,
+    logger: &Logger,
+    f: F,
+) -> Result<T>
+where
+    F: FnOnce(&mut Command) -> Fut,
+    Fut: std::future::Future<Output = std::io::Result<T>>,
+    T: AsStatus,
+{
+    command.stdin(Stdio::null()).kill_on_drop(true);
     debug!(logger, "running: {}", command.to_string());
     let start = Instant::now();
-    let output =
-        command.kill_on_drop(true).output().await.with_context(|| {
-            format!("failed to exec `{}`", command.to_string())
-        })?;
+    let result = f(command)
+        .await
+        .with_context(|| format!("failed to exec `{}`", command.to_string()))?;
     debug!(
         logger,
         "process exited with {} ({:?})",
-        output.status,
+        result.as_status(),
         Instant::now().saturating_duration_since(start)
     );
-    Ok(output)
+    Ok(result)
 }
