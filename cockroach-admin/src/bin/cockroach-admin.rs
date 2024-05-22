66ba1b33a6120085a81c24ddc5f33d63602db0d5
John Gallagher <john@oxidecomputer.com>
2024-05-22 10:16:46 -0400

    add initial cockroach-admin server

---
+++ cockroach-admin/src/bin/cockroach-admin.rs
@@ -0,0 +1,79 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+//! Executable program to run the Omicron CockroachDb admin interface (not to be
+//! confused with CockroachDb's built-in HTTP API)
+
+use anyhow::anyhow;
+use camino::Utf8PathBuf;
+use clap::Parser;
+use omicron_cockroach_admin::CockroachCli;
+use omicron_cockroach_admin::Config;
+use omicron_common::cmd::fatal;
+use omicron_common::cmd::CmdError;
+use std::net::SocketAddr;
+use std::net::SocketAddrV6;
+
+#[derive(Debug, Parser)]
+#[clap(name = "cockroach-admin", about = "Omicron CRDB cluster admin server")]
+enum Args {
+    /// Print the OpenAPI Spec document and exit
+    Openapi,
+
+    /// Start the CRDB admin server
+    Run {
+        /// Path to the `cockroach` CLI
+        #[clap(long, action)]
+        path_to_cockroach_binary: Utf8PathBuf,
+
+        /// Socket address for a running cockroach server instance
+        #[clap(long, action)]
+        cockroach_address: SocketAddrV6,
+
+        /// Address on which this server should bin
+        #[clap(long, action)]
+        http_address: SocketAddrV6,
+
+        /// Path to the server config file
+        #[clap(long, action)]
+        config_file_path: Utf8PathBuf,
+    },
+}
+
+#[tokio::main]
+async fn main() {
+    if let Err(err) = main_impl().await {
+        fatal(err);
+    }
+}
+
+async fn main_impl() -> Result<(), CmdError> {
+    let args = Args::parse();
+
+    match args {
+        Args::Openapi => omicron_cockroach_admin::run_openapi()
+            .map_err(|e| CmdError::Failure(anyhow!(e))),
+        Args::Run {
+            path_to_cockroach_binary,
+            cockroach_address,
+            http_address,
+            config_file_path,
+        } => {
+            let cockroach_cli =
+                CockroachCli::new(path_to_cockroach_binary, cockroach_address);
+            let mut config = Config::from_file(&config_file_path)
+                .map_err(|err| CmdError::Failure(anyhow!(err)))?;
+            config.dropshot.bind_address = SocketAddr::V6(http_address);
+            let server =
+                omicron_cockroach_admin::start_server(cockroach_cli, config)
+                    .await
+                    .map_err(|err| CmdError::Failure(anyhow!(err)))?;
+            server.await.map_err(|err| {
+                CmdError::Failure(anyhow!(
+                    "server failed after starting: {err}"
+                ))
+            })
+        }
+    }
+}
