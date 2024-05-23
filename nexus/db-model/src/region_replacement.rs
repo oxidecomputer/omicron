e4bb5a43798df832bfe6809dac8e622a60cfa835
James MacMahon <james@oxide.computer>
2024-05-23 00:20:48 +0000

    add PR comment to RegionReplacement struct

--- nexus/db-model/src/region_replacement.rs
+++ nexus/db-model/src/region_replacement.rs
@@ -47,6 +47,69 @@ impl std::str::FromStr for RegionReplacementState {
 }
 
 /// Database representation of a Region replacement request.
+///
+/// This record stores the data related to the operations required for Nexus to
+/// orchestrate replacing a region in a volume. It transitions through the
+/// following states:
+///
+///     Requested   <--              ---
+///                   |              |
+///         |         |              |
+///         v         |              |  responsibility of region
+///                   |              |  replacement start saga
+///     Allocating  --               |
+///                                  |
+///         |                        |
+///         v                        ---
+///                                  ---
+///      Running    <--              |
+///                   |              |
+///         |         |              |
+///         v         |              | responsibility of region
+///                   |              | replacement drive saga
+///      Driving    --               |
+///                                  |
+///         |                        |
+///         v                        ---
+///                                  ---
+///  ReplacementDone  <--            |
+///                     |            |
+///         |           |            |
+///         v           |            |
+///                     |            | responsibility of region
+///     Completing    --             | replacement finish saga
+///                                  |
+///         |                        |
+///         v                        |
+///                                  |
+///     Completed                    ---
+///
+/// which are captured in the RegionReplacementState enum. Annotated on the
+/// right are which sagas are responsible for which state transitions. The state
+/// transitions themselves are performed by these sagas and all involve a query
+/// that:
+///
+///  - checks that the starting state (and other values as required) make sense
+///  - updates the state while setting a unique operating_saga_id id (and any
+///    other fields as appropriate)
+///
+/// As multiple background tasks will be waking up, checking to see what sagas
+/// need to be triggered, and requesting that these region replacement sagas
+/// run, this is meant to block multiple sagas from running at the same time in
+/// an effort to cut down on interference - most will unwind at the first step
+/// of performing this state transition instead of somewhere in the middle.
+///
+/// The correctness of a region replacement relies on certain operations
+/// happening only when the record is in a certain state. For example: Nexus
+/// should not undo a volume modification _after_ an upstairs has been sent a
+/// replacement request, so volume modification happens at the Allocating state
+/// (in the start saga), and replacement requests are only sent in the Driving
+/// state (in the drive saga) - this ensures that replacement requests are only
+/// sent if the start saga completed successfully, meaning the volume
+/// modification was committed to the database and will not change or be
+/// unwound.
+///
+/// See also: RegionReplacementStep records
 #[derive(
     Queryable,
     Insertable,
