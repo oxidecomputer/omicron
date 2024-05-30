8aca0a29a4bcf52ca54ad6089bc9b3fbcdaa23af
John Gallagher <john@oxidecomputer.com>
2024-05-30 15:48:16 -0400

    Add cockroach-admin OpenAPI and client crate

---
+++ clients/cockroach-admin-client/src/lib.rs
@@ -0,0 +1,21 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+//! Interface for making API requests to a Bootstrap Agent
+
+progenitor::generate_api!(
+    spec = "../../openapi/cockroach-admin.json",
+    inner_type = slog::Logger,
+    pre_hook = (|log: &slog::Logger, request: &reqwest::Request| {
+        slog::debug!(log, "client request";
+            "method" => %request.method(),
+            "uri" => %request.url(),
+            "body" => ?&request.body(),
+        );
+    }),
+    post_hook = (|log: &slog::Logger, result: &Result<_, _>| {
+        slog::debug!(log, "client response"; "result" => ?result);
+    }),
+    derives = [schemars::JsonSchema],
+);
