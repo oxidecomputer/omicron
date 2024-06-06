eddc2827fa8080f5f48516ff7ee6e4e0b48c1165
Sean Klein <sean@oxide.computer>
2024-06-06 10:23:56 -0700

    Add comments

--- schema/crdb/fix-provisioning-counters/up.sql
+++ schema/crdb/fix-provisioning-counters/up.sql
@@ -1,5 +1,10 @@
+-- This change fixes provisioning counters, alongside the
+-- underflow fix provided in https://github.com/oxidecomputer/omicron/pull/5830.
+-- Although this underflow has been fixed, it could have resulted
+-- in invalid accounting, which is mitigated by this schema change.
+--
 -- This update is currently occurring offline, so we exploit
--- that fact to identify that all instances should be offline.
+-- that fact to identify that all instances should be off.
 
 SET LOCAL disallow_full_table_scans = OFF;
 
