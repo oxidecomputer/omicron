6d45b97b17105a60e91bcbb2988a5619a4460acb
Sean Klein <sean@oxide.computer>
2024-07-15 16:22:34 -0700

    Add decommissioning test to reconfigurator for disks

--- nexus/reconfigurator/execution/src/omicron_physical_disks.rs
+++ nexus/reconfigurator/execution/src/omicron_physical_disks.rs
@@ -180,23 +180,43 @@ pub(crate) async fn clean_up_decommissioned_disks(
 #[cfg(test)]
 mod test {
     use super::deploy_disks;
+
+    use crate::DataStore;
     use crate::Sled;
+    use async_bb8_diesel::AsyncRunQueryDsl;
+    use diesel::ExpressionMethods;
+    use diesel::QueryDsl;
     use httptest::matchers::{all_of, json_decoded, request};
     use httptest::responders::json_encoded;
     use httptest::responders::status_code;
     use httptest::Expectation;
+    use nexus_db_model::Dataset;
+    use nexus_db_model::DatasetKind;
+    use nexus_db_model::PhysicalDisk;
+    use nexus_db_model::PhysicalDiskKind;
+    use nexus_db_model::PhysicalDiskPolicy;
+    use nexus_db_model::PhysicalDiskState;
+    use nexus_db_model::Region;
+    use nexus_db_model::Zpool;
     use nexus_db_queries::context::OpContext;
+    use nexus_db_queries::db;
+    use nexus_test_utils::SLED_AGENT_UUID;
     use nexus_test_utils_macros::nexus_test;
     use nexus_types::deployment::{
         Blueprint, BlueprintPhysicalDiskConfig, BlueprintPhysicalDisksConfig,
-        BlueprintTarget, CockroachDbPreserveDowngrade,
+        BlueprintTarget, CockroachDbPreserveDowngrade, DiskFilter,
     };
+    use nexus_types::identity::Asset;
+    use omicron_common::api::external::DataPageParams;
     use omicron_common::api::external::Generation;
     use omicron_common::disk::DiskIdentity;
+    use omicron_uuid_kinds::GenericUuid;
+    use omicron_uuid_kinds::PhysicalDiskUuid;
     use omicron_uuid_kinds::SledUuid;
     use omicron_uuid_kinds::ZpoolUuid;
     use std::collections::BTreeMap;
     use std::net::SocketAddr;
+    use std::str::FromStr;
     use uuid::Uuid;
 
     type ControlPlaneTestContext =
@@ -429,5 +449,235 @@ mod test {
         );
         s1.verify_and_clear();
         s2.verify_and_clear();
+    }
+
+    async fn make_disk_in_db(
+        datastore: &DataStore,
+        opctx: &OpContext,
+        i: usize,
+        sled_id: SledUuid,
+    ) -> PhysicalDiskUuid {
+        let id = PhysicalDiskUuid::from_untyped_uuid(Uuid::new_v4());
+        let physical_disk = PhysicalDisk::new(
+            id.into_untyped_uuid(),
+            "v".into(),
+            format!("s-{i})"),
+            "m".into(),
+            PhysicalDiskKind::U2,
+            sled_id.into_untyped_uuid(),
+        );
+        datastore
+            .physical_disk_insert(&opctx, physical_disk.clone())
+            .await
+            .unwrap();
+        id
+    }
+
+    async fn add_zpool_dataset_and_region(
+        datastore: &DataStore,
+        opctx: &OpContext,
+        id: PhysicalDiskUuid,
+        sled_id: SledUuid,
+    ) {
+        let zpool = datastore
+            .zpool_insert(
+                opctx,
+                Zpool::new(
+                    Uuid::new_v4(),
+                    sled_id.into_untyped_uuid(),
+                    id.into_untyped_uuid(),
+                ),
+            )
+            .await
+            .unwrap();
+
+        let dataset = datastore
+            .dataset_upsert(Dataset::new(
+                Uuid::new_v4(),
+                zpool.id(),
+                std::net::SocketAddrV6::new(
+                    std::net::Ipv6Addr::LOCALHOST,
+                    0,
+                    0,
+                    0,
+                ),
+                DatasetKind::Crucible,
+            ))
+            .await
+            .unwrap();
+
+        // There isn't a great API to insert regions (we normally allocate!)
+        // so insert the record manually here.
+        let region = {
+            let volume_id = Uuid::new_v4();
+            Region::new(
+                dataset.id(),
+                volume_id,
+                512_i64.try_into().unwrap(),
+                10,
+                10,
+                1,
+            )
+        };
+        let conn = datastore.pool_connection_for_tests().await.unwrap();
+        use nexus_db_model::schema::region::dsl;
+        diesel::insert_into(dsl::region)
+            .values(region)
+            .execute_async(&*conn)
+            .await
+            .unwrap();
+    }
+
+    async fn get_pools(
+        datastore: &DataStore,
+        id: PhysicalDiskUuid,
+    ) -> Vec<ZpoolUuid> {
+        let conn = datastore.pool_connection_for_tests().await.unwrap();
+
+        use db::schema::zpool::dsl;
+        dsl::zpool
+            .filter(dsl::time_deleted.is_null())
+            .filter(dsl::physical_disk_id.eq(id.into_untyped_uuid()))
+            .select(dsl::id)
+            .load_async::<Uuid>(&*conn)
+            .await
+            .map(|ids| {
+                ids.into_iter()
+                    .map(|id| ZpoolUuid::from_untyped_uuid(id))
+                    .collect()
+            })
+            .unwrap()
+    }
+
+    async fn get_datasets(datastore: &DataStore, id: ZpoolUuid) -> Vec<Uuid> {
+        let conn = datastore.pool_connection_for_tests().await.unwrap();
+
+        use db::schema::dataset::dsl;
+        dsl::dataset
+            .filter(dsl::time_deleted.is_null())
+            .filter(dsl::pool_id.eq(id.into_untyped_uuid()))
+            .select(dsl::id)
+            .load_async(&*conn)
+            .await
+            .unwrap()
+    }
+
+    async fn get_regions(datastore: &DataStore, id: Uuid) -> Vec<Uuid> {
+        let conn = datastore.pool_connection_for_tests().await.unwrap();
+
+        use db::schema::region::dsl;
+        dsl::region
+            .filter(dsl::dataset_id.eq(id.into_untyped_uuid()))
+            .select(dsl::id)
+            .load_async(&*conn)
+            .await
+            .unwrap()
+    }
+
+    #[nexus_test]
+    async fn test_decommission_expunged_disks(
+        cptestctx: &ControlPlaneTestContext,
+    ) {
+        let nexus = &cptestctx.server.server_context().nexus;
+        let datastore = nexus.datastore();
+        let opctx = OpContext::for_tests(
+            cptestctx.logctx.log.clone(),
+            datastore.clone(),
+        );
+
+        let sled_id = SledUuid::from_untyped_uuid(
+            Uuid::from_str(&SLED_AGENT_UUID).unwrap(),
+        );
+
+        // Create a couple disks in the database
+        let disks = [
+            make_disk_in_db(&datastore, &opctx, 0, sled_id).await,
+            make_disk_in_db(&datastore, &opctx, 1, sled_id).await,
+        ];
+
+        // Add a zpool, dataset, and region to each disk.
+        for disk_id in disks {
+            add_zpool_dataset_and_region(&datastore, &opctx, disk_id, sled_id)
+                .await;
+        }
+
+        let disk_to_expunge = disks[0];
+        let other_disk = disks[1];
+
+        // Expunge one of the disks
+        datastore
+            .physical_disk_update_policy(
+                &opctx,
+                disk_to_expunge.into_untyped_uuid(),
+                PhysicalDiskPolicy::Expunged,
+            )
+            .await
+            .unwrap();
+
+        // Verify that the state of both disks is "active"
+        let all_disks = datastore
+            .physical_disk_list(
+                &opctx,
+                &DataPageParams::max_page(),
+                DiskFilter::All,
+            )
+            .await
+            .unwrap()
+            .into_iter()
+            .map(|disk| (disk.id(), disk))
+            .collect::<BTreeMap<_, _>>();
+        let d = &all_disks[&disk_to_expunge.into_untyped_uuid()];
+        assert_eq!(d.disk_state, PhysicalDiskState::Active);
+        assert_eq!(d.disk_policy, PhysicalDiskPolicy::Expunged);
+        let d = &all_disks[&other_disk.into_untyped_uuid()];
+        assert_eq!(d.disk_state, PhysicalDiskState::Active);
+        assert_eq!(d.disk_policy, PhysicalDiskPolicy::InService);
+
+        super::decommission_expunged_disks(&opctx, &datastore).await.unwrap();
+
+        // After decommissioning, we see the expunged disk become
+        // decommissioned. The other disk remains in-service.
+        let all_disks = datastore
+            .physical_disk_list(
+                &opctx,
+                &DataPageParams::max_page(),
+                DiskFilter::All,
+            )
+            .await
+            .unwrap()
+            .into_iter()
+            .map(|disk| (disk.id(), disk))
+            .collect::<BTreeMap<_, _>>();
+        let d = &all_disks[&disk_to_expunge.into_untyped_uuid()];
+        assert_eq!(d.disk_state, PhysicalDiskState::Decommissioned);
+        assert_eq!(d.disk_policy, PhysicalDiskPolicy::Expunged);
+        let d = &all_disks[&other_disk.into_untyped_uuid()];
+        assert_eq!(d.disk_state, PhysicalDiskState::Active);
+        assert_eq!(d.disk_policy, PhysicalDiskPolicy::InService);
+
+        // Before we decommission the disk, we should also be able to see all
+        // pools, datasets, and regions.
+        let pools = get_pools(&datastore, disk_to_expunge).await;
+        assert_eq!(pools.len(), 1);
+        let datasets = get_datasets(&datastore, pools[0]).await;
+        assert_eq!(datasets.len(), 1);
+        let regions = get_regions(&datastore, datasets[0]).await;
+        assert_eq!(regions.len(), 1);
+
+        // After the cleanup, we should not be able to observe any zpools
+        // nor datasets for the decommissioned disk.
+        super::clean_up_decommissioned_disks(&opctx, &datastore).await.unwrap();
+
+        assert!(get_pools(&datastore, disk_to_expunge).await.is_empty());
+        assert!(get_datasets(&datastore, pools[0]).await.is_empty());
+        assert!(get_regions(&datastore, datasets[0]).await.is_empty());
+
+        // However, the "other disk" should still exist.
+        let pools = get_pools(&datastore, other_disk).await;
+        assert_eq!(pools.len(), 1);
+        let datasets = get_datasets(&datastore, pools[0]).await;
+        assert_eq!(datasets.len(), 1);
+        let regions = get_regions(&datastore, datasets[0]).await;
+        assert_eq!(regions.len(), 1);
     }
 }
