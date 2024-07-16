4cde53b60102917aaf54cf5ac5156f73e2c90bb9
Rain <rain@oxide.computer>
2024-07-16 14:23:45 -0700

    [wicketd] convert wicket -> wicketd API to a trait (#6078)
    Move the wicketd API to a new `wicketd-api` crate, and use that
    everywhere.
    
    I had to do a few more changes along the way:
    
    1. In the interest of keeping the API crate itself small, I moved a lot of the
    underlying types to wicket-common. An example is the update-engine spec
    for preflight checks -- that shouldn't really live in the API crate.
    2. As a followup I also used those types in the API rather than relying
    on generic specs. This made the JSON a bit bigger but resulted in a nice
    cleanup on the wicket end, since the types can be shared.
    3. With wicketd-api in its own crate, two copies of `SpIdentifier` and all of the
    other types around that got way out of hand. Luckily, addressing that
    was quite simple -- just using the gateway-client copy of `SpIdentifier`
    everywhere. This means a few more `replace`s, but I think the value of
    never having to see an `SpIdentifier2` is quite high.
    
    The wicketd API being difficult to update was a key motivation behind me
    starting work on API traits, so it's nice to see that come to fruition.

--- wicket/src/ui/panes/overview.rs
+++ wicket/src/ui/panes/overview.rs
@@ -22,12 +22,12 @@ use ratatui::style::Style;
 use ratatui::text::{Line, Span, Text};
 use ratatui::widgets::{Block, BorderType, Borders, Paragraph};
 use ratatui::Frame;
-use wicketd_client::types::RotState;
-use wicketd_client::types::SpComponentCaboose;
-use wicketd_client::types::SpComponentInfo;
-use wicketd_client::types::SpComponentPresence;
-use wicketd_client::types::SpIgnition;
-use wicketd_client::types::SpState;
+use wicket_common::inventory::RotState;
+use wicket_common::inventory::SpComponentCaboose;
+use wicket_common::inventory::SpComponentInfo;
+use wicket_common::inventory::SpComponentPresence;
+use wicket_common::inventory::SpIgnition;
+use wicket_common::inventory::SpState;
 
 enum PopupKind {
     Ignition,
@@ -844,9 +844,9 @@ fn inventory_description(component: &Component) -> Text {
                     ]
                     .into(),
                 );
-                if let Some(caboose) =
-                    sp.rot().and_then(|r| r.caboose_stage0.as_ref())
-                {
+                if let Some(caboose) = sp.rot().and_then(|r| {
+                    r.caboose_stage0.as_ref().map_or(None, |x| x.as_ref())
+                }) {
                     append_caboose(&mut spans, nest_bullet(), caboose);
                 } else {
                     spans.push(
@@ -889,9 +889,9 @@ fn inventory_description(component: &Component) -> Text {
                     ]
                     .into(),
                 );
-                if let Some(caboose) =
-                    sp.rot().and_then(|r| r.caboose_stage0next.as_ref())
-                {
+                if let Some(caboose) = sp.rot().and_then(|r| {
+                    r.caboose_stage0next.as_ref().map_or(None, |x| x.as_ref())
+                }) {
                     append_caboose(&mut spans, nest_bullet(), caboose);
                 } else {
                     spans.push(
