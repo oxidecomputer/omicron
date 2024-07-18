4a42f13532f91b963e672f25ce8d38b2c71f235b
Andrew J. Stone <andrew@oxidecomputer.com>
2024-07-18 20:44:27 +0000

    wip

--- oximeter/db/tests/integration_test.rs
+++ oximeter/db/tests/integration_test.rs
@@ -3,27 +3,25 @@
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
 use anyhow::Context;
-use camino_tempfile::Builder;
+use clickward::{Deployment, KeeperClient, KeeperError};
 use dropshot::test_util::log_prefix_for_test;
 use omicron_test_utils::dev::poll;
 use omicron_test_utils::dev::test_setup_log;
 use oximeter_db::{Client, DbWrite};
 use std::time::Duration;
-use tokio::net::TcpStream;
 
 #[tokio::test]
 async fn test_cluster() -> anyhow::Result<()> {
+    tokio::time::sleep(Duration::from_secs(10)).await;
     let logctx = test_setup_log("test_cluster");
     let log = &logctx.log;
 
     let (parent_dir, prefix) = log_prefix_for_test(logctx.test_name());
-    let dir = Builder::new()
-        .prefix(&format!("{prefix}-clickward-deployment"))
-        .tempdir_in(parent_dir)
-        .context("failed to create tempdir for clickward deployment")?;
+    let path = parent_dir.join(format!("{prefix}-oximeter-clickward-test"));
+    std::fs::create_dir(&path)?;
 
-    let deployment = clickward::Deployment::new_with_default_port_config(
-        dir.path().into(),
+    let mut deployment = Deployment::new_with_default_port_config(
+        path.clone(),
         "oximeter_cluster".to_string(),
     );
 
@@ -38,40 +36,51 @@ async fn test_cluster() -> anyhow::Result<()> {
     let client2 = Client::new(deployment.http_addr(2)?, log);
     wait_for_ping(&client1).await?;
     wait_for_ping(&client2).await?;
-    wait_for_keepers(&deployment, 1..3).await?;
+    wait_for_keepers(&deployment, (1..=3).collect()).await?;
 
     client1.init_replicated_db().await.context("Failed to initialize db")?;
 
-    // Ensure our database tables show up on both clients
+    // Ensure our database tables show up on both servers
     let output1 = client1.list_replicated_tables().await?;
     let output2 = client2.list_replicated_tables().await?;
     assert_eq!(output1, output2);
 
     deployment.teardown()?;
+    std::fs::remove_dir_all(path)?;
+    logctx.cleanup_successful();
 
     Ok(())
 }
 
-/// Wait for all keeper servers to accept connections
+/// Wait for all keeper servers to be capable of handling commands
 async fn wait_for_keepers(
-    deployment: &clickward::Deployment,
-    range: std::ops::Range<u64>,
+    deployment: &Deployment,
+    ids: Vec<u64>,
 ) -> anyhow::Result<()> {
-    let mut keeper_addrs = Vec::new();
-    for id in range {
-        keeper_addrs.push(deployment.keeper_addr(id)?);
+    let mut keepers = vec![];
+    for id in ids {
+        keepers.push(KeeperClient::new(deployment.keeper_addr(id)?));
     }
 
     poll::wait_for_condition(
         || async {
             let mut done = true;
-            for addr in keeper_addrs.clone() {
-                if TcpStream::connect(addr).await.is_err() {
-                    done = false;
+            for keeper in &keepers {
+                match keeper.config().await {
+                    Ok(config) => {
+                        // The node isn't really up yet
+                        if config.len() != keepers.len() {
+                            done = false;
+                            break;
+                        }
+                    }
+                    Err(_) => {
+                        done = false;
+                    }
                 }
             }
             if !done {
-                Err(poll::CondCheckError::<std::io::Error>::NotYet)
+                Err(poll::CondCheckError::<KeeperError>::NotYet)
             } else {
                 Ok(())
             }
