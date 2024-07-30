b999bf709050e76c3983ddbff33075a02d9c5689
Sean Klein <sean@oxide.computer>
2024-07-29 18:43:52 -0700

    make it a schema change

--- schema/crdb/dbinit.sql
+++ schema/crdb/dbinit.sql
@@ -4156,7 +4156,7 @@ INSERT INTO omicron.public.db_metadata (
     version,
     target_version
 ) VALUES
-    (TRUE, NOW(), NOW(), '83.0.0', NULL)
+    (TRUE, NOW(), NOW(), '84.0.0', NULL)
 ON CONFLICT DO NOTHING;
 
 COMMIT;
