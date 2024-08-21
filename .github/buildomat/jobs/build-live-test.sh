ec5fadbf14324bec65090b26281aa85ebbf17f1e
David Pacheco <dap@oxidecomputer.com>
2024-08-21 09:03:12 -0700

    add CI that builds it

---
+++ .github/buildomat/jobs/build-live-test.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#:
+#: name = "build-live-tests"
+#: variety = "basic"
+#: target = "helios-2.0"
+#: rust_toolchain = true
+#: output_rules = []
+
+# Builds the "live-tests" nextest archive
+
+set -o errexit
+set -o pipefail
+set -o xtrace
+
+cargo --version
+rustc --version
+
+#
+# Set up our PATH for use with this workspace.
+#
+source ./env.sh
+
+banner prerequisites
+ptime -m bash ./tools/install_builder_prerequisites.sh -y
+
+banner live-test
+ptime -m cargo xtask live-test
