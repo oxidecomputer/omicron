2cbe7d94efa55e05f9edbc112ca5fb1ec4828489
iliana etaoin <iliana@oxide.computer>
2024-08-30 17:45:44 +0000

    warn wicket users that rack reset doesn't work yet

--- wicket/src/ui/panes/rack_setup.rs
+++ wicket/src/ui/panes/rack_setup.rs
@@ -285,10 +285,29 @@ fn draw_rack_reset_popup(
                 "Rack Reset (DESTRUCTIVE!)",
                 style::header(true),
             )]);
-            let body = Text::from(vec![Line::from(vec![Span::styled(
+            let mut body = Text::from(vec![Line::from(vec![Span::styled(
                 "Would you like to reset the rack to an uninitialized state?",
                 style::plain_text(),
             )])]);
+            // One might see this warning and ask "why is this feature even
+            // here, then?" We do eventually want "rack reset" to work as a
+            // sort of factory reset, and the current implementation is a good
+            // starting point, so there's no sense in removing it (this is
+            // certainly not the only feature currently in this state).
+            //
+            // The warning is intended to remove the speed bump where someone
+            // has to find out the hard way that this doesn't work, without
+            // removing the speed bump where we're reminded of the feature that
+            // doesn't work yet.
+            body.lines.push(Line::from(""));
+            body.lines.push(Line::from(vec![
+                Span::styled("WARNING", style::warning()),
+                Span::styled(
+                    ": This does not work yet and will leave the rack \
+                     in an unknown state (see omicron#3820)",
+                    style::plain_text(),
+                ),
+            ]));
             let buttons =
                 vec![ButtonText::new("Yes", "Y"), ButtonText::new("No", "N")];
 
