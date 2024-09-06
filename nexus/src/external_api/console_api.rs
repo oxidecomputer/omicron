3f6235cfb83b94bcdc2917edb42d290d11d8d58e
Mike Zeller <mike@mikezeller.net>
2024-09-06 21:34:13 +0000

    Ignore relay state parse error

--- nexus/src/external_api/console_api.rs
+++ nexus/src/external_api/console_api.rs
@@ -317,14 +317,15 @@ pub(crate) async fn login_saml(
                 }
             };
 
-        let relay_state =
-            if let Some(value) = relay_state_string {
-                Some(RelayState::from_encoded(value).map_err(|e| {
-                    HttpError::for_internal_error(format!("{}", e))
-                })?)
-            } else {
-                None
-            };
+        let relay_state = if let Some(value) = relay_state_string {
+            // We used to return an internal server error if we failed to parse
+            // the relay state from the IDP. Since an IDP can technically put
+            // anything it wants here it's better to ignore the parse error and
+            // continue on with the login flow.
+            RelayState::from_encoded(value).ok()
+        } else {
+            None
+        };
 
         let user = nexus
             .silo_user_from_authenticated_subject(
