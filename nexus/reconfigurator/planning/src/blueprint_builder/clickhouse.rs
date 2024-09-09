cb9a9a9f775086f2b127bd97b5da4ec924f2fb80
Andrew J. Stone <andrew@oxidecomputer.com>
2024-09-09 23:57:45 +0000

    much better logic

--- nexus/reconfigurator/planning/src/blueprint_builder/clickhouse.rs
+++ nexus/reconfigurator/planning/src/blueprint_builder/clickhouse.rs
@@ -4,13 +4,22 @@
 
 //! A mechanism for allocating clickhouse keeper nodes for clustered clickhouse setups
 
+use clickhouse_admin_types::KeeperId;
 use nexus_types::deployment::{
     blueprint_zone_type, Blueprint, BlueprintZoneFilter, BlueprintZoneType,
-    ClickhouseClusterConfig, PlanningInput,
+    PlanningInput,
 };
 use nexus_types::inventory::Collection;
-use slog::Logger;
+use omicron_uuid_kinds::OmicronZoneUuid;
+use slog::{error, Logger};
+use std::collections::{BTreeMap, BTreeSet};
 use thiserror::Error;
+
+struct KeeperZones {
+    in_service:
+        BTreeMap<OmicronZoneUuid, blueprint_zone_type::ClickhouseKeeper>,
+    expunged: BTreeMap<OmicronZoneUuid, blueprint_zone_type::ClickhouseKeeper>,
+}
 
 /// Only a single keeper may be added or removed from the clickhouse keeper
 /// cluster at a time. If an addition or removal is in progress, we must wait to
@@ -29,15 +38,181 @@ pub enum KeeperAllocationError {
     NoConfig,
     #[error("failed to retrieve clickhouse keeper membership from inventory")]
     NoInventory,
+    #[error("cannot add more than one keeper at a time: {added_keepers:?}")]
+    BadMembershipChange { added_keepers: BTreeSet<KeeperId> },
 }
 
 impl<'a> KeeperAllocator<'a> {
-    /// Are any membership changes required for the keeper cluster desired state
+    /// Generate the next configuration of our keeper cluster membership
     ///
-    /// This method solely ensures that any zones that should have keepers in the blueprint do,
-    /// and any that shouldn't do not. It does not ensure that this desired state has been
-    /// reached. That is the goal of the executor, and is reflected in the current inventory.
-    fn desired_state_is_valid(&self) -> Result<bool, KeeperAllocationError> {
+    /// If there is a configuration change in progress or the configuration
+    /// matches our desired state then the membership will not change, and we'll
+    /// report `Ok(None)`. A configuration matches our desired state when all
+    /// in service keeper zones have keepers in the clickhouse keeper cluster
+    /// membership and all expunged zones do not have keepers in the clickhouse
+    /// keeper cluster membership.
+    pub fn plan(
+        &self,
+    ) -> Result<Option<BTreeSet<KeeperId>>, KeeperAllocationError> {
+        let parent_keeper_zones = self.all_keeper_zones_in_parent_blueprint();
+        let parent_keeper_membership = self
+            .desired_keeper_membership_in_parent_blueprint(
+                &parent_keeper_zones,
+            )?;
+
+        // Does inventory reflect our desired keeper membership? If not, the
+        // executor will keep trying until it does. Note that this may end up
+        // requiring support, but if so it's because of a bug in clickhouse and
+        // so trying to automate that away at this stage is probably not worth
+        // it.
+        let Some((_, inventory_membership)) =
+            self.inventory.latest_clickhouse_keeper_membership()
+        else {
+            // We can't get inventory so we assume that a reconfiguration must
+            // be in progress for safety purposes.
+            return Err(KeeperAllocationError::NoInventory);
+        };
+
+        let mut desired_membership: BTreeSet<_> =
+            parent_keeper_membership.keys().cloned().collect();
+
+        if desired_membership != inventory_membership.raft_config {
+            // We're still trying to reach our desired state We want to ensure
+            // however, that if we are currently trying to add a node, that we
+            // have not expunged the zone of the keeper that we are trying to
+            // add. This can happen for a number of reasons, and without this
+            // check we would not be able to make forward progress.
+            let added_keepers: BTreeSet<_> = desired_membership
+                .difference(&inventory_membership.raft_config)
+                .cloned()
+                .collect();
+
+            // We should only be adding or removing 1 keeper at a time
+            if added_keepers.len() > 1 {
+                error!(
+                    self.log,
+                    concat!(
+                        "Keeper membership error: attempting to add",
+                        "more than one keeper to cluster: {:?}"
+                    ),
+                    added_keepers
+                );
+                return Err(KeeperAllocationError::BadMembershipChange {
+                    added_keepers,
+                });
+            }
+
+            // If we are not adding a keeper than we are done.
+            // The executor is trying to remove one from the cluster.
+            //
+            // This should always succeed eventually, barring a bug in
+            // clickhouse.
+            //
+            // TODO: Should we ensure that we don't have a planner bug
+            // by checking to see if we have more than one keeper to remove?
+            if added_keepers.len() == 0 {
+                return Ok(None);
+            }
+
+            // We must be adding a keeper. Let's make sure that the zone
+            // for this keeper has not already been expunged.
+            //
+            // Unwraps are safe because we know there is one added keeper and we
+            // generated it from the keys in `parent_keeper_membership`.
+            let zone_id = parent_keeper_membership
+                .get(added_keepers.first().unwrap())
+                .unwrap();
+            if parent_keeper_zones.expunged.get(zone_id).is_some() {
+                // We need to change our desired state. A node we were trying
+                // to add was expunged. Our desired state is equivalent to
+                // the inventory so let's set it to that and go through one
+                // more round of planning. If the sled was added to the keeper
+                // cluster in the meantime this will allow it to get removed in
+                // the next execution.
+                return Ok(Some(inventory_membership.raft_config.clone()));
+            } else {
+                // We are still trying to add this keeper
+                return Ok(None);
+            }
+        }
+
+        // Our desired membership from the parent blueprint matches the
+        // inventory.
+        //
+        // Do we need to add or remove any nodes? We expunge first, because
+        // those nodes are already gone and don't help our quorum.
+        for keeper_zone in parent_keeper_zones.expunged.values() {
+            if desired_membership.contains(&keeper_zone.keeper_id) {
+                // Let's expunge the first match we see
+                desired_membership.remove(&keeper_zone.keeper_id);
+                return Ok(Some(desired_membership));
+            }
+        }
+
+        for keeper_zone in parent_keeper_zones.in_service.values() {
+            if !desired_membership.contains(&keeper_zone.keeper_id) {
+                // Let's add the first match we see
+                desired_membership.insert(keeper_zone.keeper_id);
+                return Ok(Some(desired_membership));
+            }
+        }
+
+        // We have reached our desired state and there are no actions
+        // to take.
+        Ok(None)
+    }
+
+    /// Is there an ongoing membership change
+
+    /// Get all `ClickhouseKeeper` zones from the parent blueprint
+    fn all_keeper_zones_in_parent_blueprint(&self) -> KeeperZones {
+        let in_service: BTreeMap<_, _> = self
+            .parent_blueprint
+            .all_omicron_zones(BlueprintZoneFilter::ShouldBeRunning)
+            .filter_map(|(_, bp_zone_config)| {
+                if let BlueprintZoneType::ClickhouseKeeper(keeper_zone_type) =
+                    &bp_zone_config.zone_type
+                {
+                    Some((bp_zone_config.id, keeper_zone_type.clone()))
+                } else {
+                    None
+                }
+            })
+            .collect();
+        let expunged: BTreeMap<_, _> = self
+            .parent_blueprint
+            .all_omicron_zones(BlueprintZoneFilter::ShouldBeRunning)
+            .filter_map(|(_, bp_zone_config)| {
+                if let BlueprintZoneType::ClickhouseKeeper(keeper_zone_type) =
+                    &bp_zone_config.zone_type
+                {
+                    Some((bp_zone_config.id, keeper_zone_type.clone()))
+                } else {
+                    None
+                }
+            })
+            .collect();
+        KeeperZones { in_service, expunged }
+    }
+
+    /// Get all `KeeperId`s that we wanted to be part of the clickhouse keeper
+    /// cluster membership in the parent blueprint.
+    ///
+    /// This is our desired state, reflected in the parent blueprint, not what
+    /// the actual membership is as reflected by the inventory.
+    ///
+    /// Also note that a zone may already be expunged, and yet we may still be
+    /// trying to add it as a keeper member in the executor. We'll handle this in the
+    /// `plan` method above.
+    ///
+    /// Panics if there is a clickhouse keeper zone id in
+    /// `self.clickhouse_cluster_config.zones_with_keepers`, but not a
+    /// corresponding omicron zone in `all_keeper_zones`.
+    fn desired_keeper_membership_in_parent_blueprint(
+        &self,
+        all_keeper_zones: &KeeperZones,
+    ) -> Result<BTreeMap<KeeperId, OmicronZoneUuid>, KeeperAllocationError>
+    {
         // Get our current clickhouse cluster configuration if there is one
         let Some(clickhouse_cluster_config) =
             &self.parent_blueprint.clickhouse_cluster_config
@@ -45,107 +220,33 @@ impl<'a> KeeperAllocator<'a> {
             return Err(KeeperAllocationError::NoConfig);
         };
 
-        // Have we allocated a keeper process to each of our in-service keeper zones?
-        let all_in_service_zones_have_keepers = self
-            .parent_blueprint
-            .all_omicron_zones(BlueprintZoneFilter::ShouldBeRunning)
-            .all(|(_, bp_zone_config)| {
-                clickhouse_cluster_config
-                    .zones_with_keepers
-                    .contains(&bp_zone_config.id)
-            });
-
-        // Do we have any allocated keeper processes for expunged zones?
-        let all_expunged_zones_do_not_have_keepers = self
-            .parent_blueprint
-            .all_omicron_zones(BlueprintZoneFilter::Expunged)
-            .all(|(_, bp_zone_config)| {
-                !clickhouse_cluster_config
-                    .zones_with_keepers
-                    .contains(&bp_zone_config.id)
-            });
-
-        Ok(all_in_service_zones_have_keepers
-            && all_expunged_zones_do_not_have_keepers)
-    }
-
-    ///  Is there currently a reconfiguration of the clickhouse keeper cluster
-    /// membership in progress?
-    ///
-    // TODO: THIS IS GOING AWAY -mostly
-    pub fn is_membership_change_in_progress(&self) -> bool {
-        let Some(clickhouse_cluster_config) =
-            &self.parent_blueprint.clickhouse_cluster_config
-        else {
-            // There's no membership change in progress because we haven't
-            // deployed a clickhouse cluster at all yet.
-            return false;
-        };
-
-        // Get the latest clickhouse cluster membership
-        let Some((_, membership)) =
-            self.inventory.latest_clickhouse_keeper_membership()
-        else {
-            // We can't retrieve the latest membership and so we assume
-            // that a reconfiguration is underway for safety reasons. In
-            // any case, if we can't talk to any `clickhouse-admin` servers
-            // in the `ClickhouseKeeper` zones then we cannot attempt a
-            // reconfiguration.
-            return true;
-        };
-
-        // Ensure all in service keeper zones are part of the raft configuration
-        if !self
-            .parent_blueprint
-            .all_omicron_zones(BlueprintZoneFilter::ShouldBeRunning)
-            .all(|(_, bp_zone_config)| {
-                if let BlueprintZoneType::ClickhouseKeeper(
-                    blueprint_zone_type::ClickhouseKeeper { keeper_id, .. },
-                ) = bp_zone_config.zone_type
-                {
-                    // Check the inventory to see if a keeper that should be
-                    // running is not part of the raft configuration.
-                    membership.raft_config.contains(&keeper_id)
+        Ok(clickhouse_cluster_config
+            .zones_with_keepers
+            .iter()
+            .map(|zone_id| {
+                if let Some(keeper) = all_keeper_zones.in_service.get(zone_id) {
+                    (keeper.keeper_id, *zone_id)
                 } else {
-                    // We ignore other types of zones, so we don't want to fail
-                    // our check
-                    true
+                    // It is an invariant violation/programmer error if there is
+                    // not a keeper zone for each keeper id.
+                    //
+                    // We panic here as planning is broken at this point.
+                    if let Some(keeper) = all_keeper_zones.expunged.get(zone_id)
+                    {
+                        (keeper.keeper_id, *zone_id)
+                    } else {
+                        let msg = format!(
+                            concat!(
+                                "Failed to find zone id {}",
+                                "when looking up keeper id"
+                            ),
+                            zone_id
+                        );
+                        error!(self.log, "{msg}");
+                        panic!("{msg}");
+                    }
                 }
             })
-        {
-            // We are still attempting to add this node
-            //
-            // TODO: We need a way to detect if the attempted addition actually
-            // failed here. This will involve querying the keeper cluster via
-            // inventory in case the configuration doesn't change for a while.
-            // I don't know how to get that information out of the keeper yet,
-            // except by reading the log files.
-            return true;
-        }
-
-        // Ensure all expunged zones are no longer part of the configuration
-        if !self
-            .parent_blueprint
-            .all_omicron_zones(BlueprintZoneFilter::Expunged)
-            .all(|(_, bp_zone_config)| {
-                if let BlueprintZoneType::ClickhouseKeeper(
-                    blueprint_zone_type::ClickhouseKeeper { keeper_id, .. },
-                ) = bp_zone_config.zone_type
-                {
-                    !membership.raft_config.contains(&keeper_id)
-                } else {
-                    // We ignore other types of zones, so we don't want to fail
-                    // our check
-                    true
-                }
-            })
-        {
-            // We are still attempting to remove keepers for expunged zones from
-            // the configuration.
-            return true;
-        }
-
-        // There is no ongoing reconfiguration of the keeper raft cluster
-        false
+            .collect())
     }
 }
