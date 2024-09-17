55c2fe28ba70659a7d5fb893473d046cc1a7bc3f
iximeow <iximeow@oxide.computer>
2024-09-17 07:37:29 +0000

    actually set up crdb properly with new column

---
+++ schema/crdb/add-instance-boot-device/up.sql
@@ -0,0 +1,1 @@
+ALTER TABLE omicron.public.instance ADD COLUMN IF NOT EXISTS boot_device STRING(63);
--- schema/crdb/dbinit.sql
+++ schema/crdb/dbinit.sql
@@ -4297,7 +4297,7 @@ INSERT INTO omicron.public.db_metadata (
     version,
     target_version
 ) VALUES
-    (TRUE, NOW(), NOW(), '97.0.0', NULL)
+    (TRUE, NOW(), NOW(), '98.0.0', NULL)
 ON CONFLICT DO NOTHING;
 
 COMMIT;
