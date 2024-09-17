a4afff97bc1b3cc620d9ecf040c807ba9a63a32b
Eliza Weisman <eliza@elizas.website>
2024-09-17 11:27:05 -0700

    HAHAHA I just totally forgot to add the migration

--- schema/crdb/dbinit.sql
+++ schema/crdb/dbinit.sql
@@ -4307,7 +4307,7 @@ INSERT INTO omicron.public.db_metadata (
     version,
     target_version
 ) VALUES
-    (TRUE, NOW(), NOW(), '100.0.0', NULL)
+    (TRUE, NOW(), NOW(), '101.0.0', NULL)
 ON CONFLICT DO NOTHING;
 
 COMMIT;
