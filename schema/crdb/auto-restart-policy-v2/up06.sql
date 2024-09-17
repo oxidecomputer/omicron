de17f14bf75520c2c580a647339509188e80571e
Eliza Weisman <eliza@elizas.website>
2024-09-17 09:21:14 -0700

    fix typo in migration (oops)

--- schema/crdb/auto-restart-policy-v2/up06.sql
+++ schema/crdb/auto-restart-policy-v2/up06.sql
@@ -1,7 +1,7 @@
 SET LOCAL disallow_full_table_scans = off;
 UPDATE omicron.public.instance SET auto_restart_policy = CASE
     WHEN old_auto_restart_policy = 'all_failures' THEN 'best_effort'
-    WHEN old_auto_restart_policy = 'sled_failrues_only' THEN 'best_effort'
+    WHEN old_auto_restart_policy = 'sled_failures_only' THEN 'best_effort'
     WHEN old_auto_restart_policy = 'never' THEN 'never'
     ELSE NULL
 END;
