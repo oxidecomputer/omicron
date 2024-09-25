ce475769e82a20aa80cdee731f75a8c7f80a1f00
Andrew J. Stone <andrew@oxidecomputer.com>
2024-09-25 23:13:52 +0000

    fix clippy fix

--- nexus/reconfigurator/planning/src/planner.rs
+++ nexus/reconfigurator/planning/src/planner.rs
@@ -2549,7 +2549,7 @@ mod test {
         let old_config = blueprint3.clickhouse_cluster_config.as_ref().unwrap();
         let config = blueprint4.clickhouse_cluster_config.as_ref().unwrap();
         assert_eq!(config.generation, old_config.generation.next());
-        assert!(config.keepers.contains_key(&bp_zone_config.id));
+        assert!(!config.keepers.contains_key(&bp_zone_config.id));
         // We've only removed one keeper from our desired state
         assert_eq!(config.keepers.len() + 1, old_config.keepers.len());
         // We haven't allocated any new keepers
@@ -2606,7 +2606,7 @@ mod test {
 
         // Our generation has changed to reflect the added keeper
         assert_eq!(config.generation, old_config.generation.next());
-        assert!(config.keepers.contains_key(&bp_zone_config.id));
+        assert!(!config.keepers.contains_key(&bp_zone_config.id));
         // We've only added one keeper from our desired state
         // This brings us back up to our target count
         assert_eq!(config.keepers.len(), old_config.keepers.len() + 1);
