278ca2e40b1e643367f9086097500770325319ef
iximeow <iximeow@oxide.computer>
2024-09-27 02:47:03 +0000

    explain why we do the weird thing

--- .github/buildomat/jobs/omicron-common.sh
+++ .github/buildomat/jobs/omicron-common.sh
@@ -21,6 +21,16 @@ cd /tmp
 cargo new --lib test-project
 cd test-project
 cargo add omicron-common --path /work/oxidecomputer/omicron/common
+# Bootstrap `test-project`'s dependencies from the checked-in Cargo.lock.
+# This means that `test-project` builds with the same commits as the main repo
+# for any dependencies referenced as `{ git = "...", ref = "<branch>" }`. If we
+# do not prepopulate `Cargo.lock` like this, an update in a dependency might get
+# picked up here and be incompatible with `omicron-common`, causing it to fail
+# to build (see Omicron issue #6691).
+#
+# The extra dependencies in `omicron` will get pruned by Cargo when it
+# recalculates dependencies, but any dependencies that match will stay at the
+# commit/version/etc already indicated in the lockfile.
 cp /work/oxidecomputer/omicron/Cargo.lock Cargo.lock
 cargo check
 cargo build --release
