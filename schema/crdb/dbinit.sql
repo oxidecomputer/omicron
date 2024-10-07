ea75a1ab3b18896862733944bef383618e25c384
John Gallagher <john@oxidecomputer.com>
2024-10-07 13:11:39 -0400

    fix dbinit.sql version

--- schema/crdb/dbinit.sql
+++ schema/crdb/dbinit.sql
@@ -4485,7 +4485,7 @@ INSERT INTO omicron.public.db_metadata (
     version,
     target_version
 ) VALUES
-    (TRUE, NOW(), NOW(), '108.0.0', NULL)
+    (TRUE, NOW(), NOW(), '109.0.0', NULL)
 ON CONFLICT DO NOTHING;
 
 COMMIT;
