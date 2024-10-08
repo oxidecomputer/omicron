36d5b03667d9902f53f1e0fff269cdff42727117
Artemis Everfree <artemis@oxidecomputer.com>
2024-10-07 18:02:23 -0700

    dedupe cargo bay works i think, and live tests are almost there

--- dev-tools/xtask/src/a4x2_package.rs
+++ dev-tools/xtask/src/a4x2_package.rs
@@ -49,8 +49,8 @@ pub fn run_cmd(args: A4x2PackageArgs) -> Result<()> {
     std::env::var("CARGO").unwrap_or_else(|_| String::from("cargo"));
 
     // XXX does this make sense as a working directory?
-    let work_dir =
-        Utf8PathBuf::from(format!("{}/.cache/a4x2-package", &std::env::var("HOME")?));
+    let home_dir = Utf8PathBuf::from(std::env::var("HOME")?);
+    let work_dir = home_dir.join(".cache/a4x2-package");
 
     // Delete any results from previous runs. We don't mind if there's errors.
     fs::remove_dir_all(&work_dir).ok();
@@ -155,8 +155,9 @@ pub fn run_cmd(args: A4x2PackageArgs) -> Result<()> {
 
             if can_dedupe {
                 // yay, extract out to the common dir.
-
-                sh.copy_file(&g0_path, &common_dir.join(base_path))?;
+                let dest = common_dir.join(base_path);
+                sh.create_dir(&dest.parent().unwrap())?;
+                sh.copy_file(&g0_path, &dest)?;
                 sh.remove_path(&g0_path)?;
                 for prefix in &prefixes {
                     sh.remove_path(&prefix.join(base_path))?;
@@ -164,9 +165,8 @@ pub fn run_cmd(args: A4x2PackageArgs) -> Result<()> {
             }
         }
 
-
         // At this point, `cargo-bay` is ready for us
-        cmd!(sh, "tar -czf {out_dir}/cargo-bay.tar.gz cargo-bay/").run()?;
+        cmd!(sh, "tar -czf {out_dir}/cargo-bay.tgz cargo-bay/").run()?;
     }
 
     if args.end_to_end_tests {
@@ -180,7 +180,8 @@ pub fn run_cmd(args: A4x2PackageArgs) -> Result<()> {
 
         // We need nextest available, both to generate the test bundle and to
         // include in the output tarball to execute the bundle on a4x2.
-        if args.ci {
+        // XXX is it like, fine, to hardcode this? does this work outside of CI?
+        let nextest_path = if args.ci {
             // In CI, we need to download nextest
             let config_str = sh.read_file(".config/nextest.toml")?;
             let nextest_config: NextestConfig = toml::from_str(&config_str)?;
@@ -193,15 +194,32 @@ pub fn run_cmd(args: A4x2PackageArgs) -> Result<()> {
             cmd!(sh, "bash -c")
                 .arg(&format!("curl -sSfL --retry 10 https://get.nexte.st/'{}'/illumos | gunzip | tar -xvf - -C ~/.cargo/bin", nextest_config.nextest_version.recommended))
                 .run()?;
+            home_dir.join(".cargo/bin/cargo-nextest")
         } else {
-            // Outside of CI, we let error messages take their course, and it's
-            // up to the user to figure it out.
-        }
+            // XXX test nextest exists & is the right version? need to see if
+            // the incidental errors from stuff below already take care of
+            // telling someone outside of CI what they need to be doing.
+            Utf8PathBuf::from(cmd!(sh, "/usr/bin/which cargo-nextest").read()?)
+        };
 
         // Build and generate the live tests bundle
         // generates
         // - target/live-tests-archive.tgz
         cmd!(sh, "{cargo} xtask live-tests").run()?;
+
+        let live_test_bundle_dir = work_dir.join("live-tests-bundle");
+        sh.create_dir(&live_test_bundle_dir)?;
+
+        // a tar within a tar, a door behind a door
+        sh.copy_file("target/live-tests-archive.tgz", &live_test_bundle_dir)?;
+
+        // and we need textest to execute it
+        sh.copy_file(&nextest_path, &live_test_bundle_dir)?;
+
+        // output tar will have live-tests and nextest. this will get uploaded
+        // into one of the a4x2 sleds.
+        // TODO this needs to be relative paths
+        cmd!(sh, "tar -czf {out_dir}/live-tests-bundle.tgz {live_test_bundle_dir}").run()?;
     }
 
     println!("hi :)");
