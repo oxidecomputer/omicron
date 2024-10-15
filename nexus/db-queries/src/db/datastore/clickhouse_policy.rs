5938c79ad0268e22c16575a35e1ab9be2c933faf
Andrew J. Stone <andrew@oxidecomputer.com>
2024-10-15 00:40:04 +0000

    wip

--- nexus/db-queries/src/db/datastore/clickhouse_policy.rs
+++ nexus/db-queries/src/db/datastore/clickhouse_policy.rs
@@ -18,6 +18,7 @@ use nexus_db_model::ClickhousePolicy as DbClickhousePolicy;
 use nexus_db_model::SqlU32;
 use nexus_types::deployment::ClickhousePolicy;
 use omicron_common::api::external::DataPageParams;
+use omicron_common::api::external::Error;
 use omicron_common::api::external::ListResultVec;
 
 impl DataStore {
@@ -43,5 +44,54 @@ impl DataStore {
         .map_err(|e| public_error_from_diesel(e, ErrorHandler::Server))?;
 
         Ok(policies.into_iter().map(ClickhousePolicy::from).collect())
+    }
+
+    pub async fn clickhouse_policy_insert_latest_version(
+        &self,
+        opctx: &OpContext,
+        policy: ClickhousePolicy,
+    ) -> Result<(), Error> {
+        use db::schema::clickhouse_policy::dsl;
+
+        opctx
+            .authorize(authz::Action::Modify, &authz::BLUEPRINT_CONFIG)
+            .await?;
+
+        if policy.version == 1 {
+            return self
+                .clickhouse_policy_insert_first_policy(opctx, policy)
+                .await;
+        }
+
+        // Example query:
+        // INSERT INTO clickhouse_policy SELECT 2, 'both', 3, 5, now() \
+        // from clickhouse_policy \
+        //   where version in  \
+        //     (select version from clickhouse_policy WHERE version in \
+        //        \\ (select version from clickhouse_policy \
+        //              ORDER BY version DESC LIMIT 1) AND version = 1);
+
+        todo!()
+    }
+
+    /// Insert the first clickhouse policy in the database at version 1.
+    ///
+    /// Only insert this policy if no other policy exists yet.
+    async fn clickhouse_policy_insert_first_policy(
+        &self,
+        opctx: &OpContext,
+        policy: ClickhousePolicy,
+    ) -> Result<(), Error> {
+        use db::schema::clickhouse_policy::dsl;
+
+        // An example query:
+        //
+        // INSERT INTO clickhouse_policy \
+        //    (version, clickhouse_mode, clickhouse_cluster_target_servers,  \
+        //       clickhouse_cluster_target_keepers, time_created) \
+        //    SELECT 1, 'single_node_only', 0, 0, now() \
+        //    WHERE NOT EXISTS (SELECT * FROM clickhouse_policy)
+
+        todo!()
     }
 }
