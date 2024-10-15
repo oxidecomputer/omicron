90dc30ba2217e8dd82ae4d2e5cd45741bf29968e
Andrew J. Stone <andrew@oxidecomputer.com>
2024-10-15 22:56:47 +0000

    wip

--- nexus/db-queries/src/db/datastore/clickhouse_policy.rs
+++ nexus/db-queries/src/db/datastore/clickhouse_policy.rs
@@ -81,7 +81,7 @@ impl DataStore {
     pub async fn clickhouse_policy_insert_latest_version(
         &self,
         opctx: &OpContext,
-        policy: ClickhousePolicy,
+        policy: &ClickhousePolicy,
     ) -> Result<(), Error> {
         if policy.version < 1 {
             return Err(Error::invalid_request(
@@ -98,12 +98,15 @@ impl DataStore {
             let prev_version = policy.version - 1;
 
             sql_query(
-                r"INSERT INTO clickhouse_policy SELECT ?, ?, ?, ?, ? \
-              from clickhouse_policy where version in \
-                (select version from clickhouse_policy WHERE version in \
-                  (select version from clikchouse_policy \
-                    ORDER BY version DESC LIMIT 1) \
-                AND version = ?)",
+                r"INSERT INTO clickhouse_policy 
+                 (version, clickhouse_mode, clickhouse_cluster_target_servers, 
+                  clickhouse_cluster_target_keepers, time_created) 
+                 SELECT $1, $2, $3, $4, $5 
+                  FROM clickhouse_policy where version IN 
+                    (SELECT version FROM clickhouse_policy WHERE version IN 
+                      (SELECT version FROM clikchouse_policy 
+                        ORDER BY version DESC LIMIT 1) 
+                     AND version = $6)",
             )
             .bind::<sql_types::BigInt, SqlU32>(policy.version.into())
             .bind::<ClickhouseModeEnum, DbClickhouseMode>((&policy.mode).into())
@@ -143,8 +146,11 @@ impl DataStore {
         policy: &ClickhousePolicy,
     ) -> Result<usize, Error> {
         sql_query(
-            r"INSERT INTO clickhouse_policy SELECT ?, ?, ?, ?, ? \
-                   WHERE NOT EXISTS (SELECT * FROM clickhouse_policy)",
+            r"INSERT INTO clickhouse_policy 
+              (version, clickhouse_mode, clickhouse_cluster_target_servers, 
+               clickhouse_cluster_target_keepers, time_created) 
+             SELECT $1, $2, $3, $4, $5 
+             WHERE NOT EXISTS (SELECT * FROM clickhouse_policy)",
         )
         .bind::<sql_types::BigInt, SqlU32>(policy.version.into())
         .bind::<ClickhouseModeEnum, DbClickhouseMode>((&policy.mode).into())
@@ -154,5 +160,98 @@ impl DataStore {
         .execute_async(&*self.pool_connection_authorized(opctx).await?)
         .await
         .map_err(|e| public_error_from_diesel(e, ErrorHandler::Server))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::db::datastore::test_utils::datastore_test;
+    use nexus_inventory::now_db_precision;
+    use nexus_test_utils::db::test_setup_database;
+    use nexus_types::deployment::ClickhouseMode;
+    use omicron_test_utils::dev;
+
+    #[tokio::test]
+    async fn test_clickhouse_policy_basic() {
+        // Setup
+        let logctx = dev::test_setup_log("test_clickhouse_policy_basic");
+        let mut db = test_setup_database(&logctx.log).await;
+        let (opctx, datastore) = datastore_test(&logctx, &db).await;
+
+        // Listing an empty table should return an empty vec
+
+        assert!(datastore
+            .clickhouse_policy_list(&opctx, &DataPageParams::max_page())
+            .await
+            .unwrap()
+            .is_empty());
+
+        // Fail to insert a policy with version 0
+        let mut policy = ClickhousePolicy {
+            version: 0,
+            mode: ClickhouseMode::SingleNodeOnly,
+            time_created: now_db_precision(),
+        };
+
+        assert!(datastore
+            .clickhouse_policy_insert_latest_version(&opctx, &policy)
+            .await
+            .is_err());
+
+        // Inserting version 2 before version 1 should not work
+        policy.version = 2;
+        assert!(datastore
+            .clickhouse_policy_insert_latest_version(&opctx, &policy)
+            .await
+            .is_err());
+
+        // Inserting version 1 should work
+        policy.version = 1;
+        assert!(datastore
+            .clickhouse_policy_insert_latest_version(&opctx, &policy)
+            .await
+            .is_ok());
+
+        // Inserting version 2 should work
+        policy.version = 2;
+        assert!(datastore
+            .clickhouse_policy_insert_latest_version(&opctx, &policy)
+            .await
+            .is_ok());
+
+        // Inserting version 4 should not work, since the prior version is 2
+        policy.version = 4;
+        assert!(datastore
+            .clickhouse_policy_insert_latest_version(&opctx, &policy)
+            .await
+            .is_err());
+
+        // Inserting version 3 should work
+        policy.version = 3;
+        assert!(datastore
+            .clickhouse_policy_insert_latest_version(&opctx, &policy)
+            .await
+            .is_ok());
+
+        // Inserting version 4 should work
+        policy.version = 4;
+        policy.mode =
+            ClickhouseMode::Both { target_servers: 3, target_keepers: 5 };
+        assert!(datastore
+            .clickhouse_policy_insert_latest_version(&opctx, &policy)
+            .await
+            .is_ok());
+
+        let history = datastore
+            .clickhouse_policy_list(&opctx, &DataPageParams::max_page())
+            .await
+            .unwrap();
+
+        println!("{:#?}", history);
+
+        // Clean up.
+        db.cleanup().await.unwrap();
+        logctx.cleanup_successful();
     }
 }
