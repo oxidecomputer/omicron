c17475367cba51b2d68d19494635a8af563e8119
Eliza Weisman <eliza@elizas.website>
2024-10-16 14:03:01 -0700

    [ereport] start sketching proxy

---
+++ ereporter/src/proxy.rs
@@ -0,0 +1,56 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+use ereporter_api::Entry;
+use omicron_common::api::external::Error;
+use omicron_common::api::external::Generation;
+use std::future::Future;
+use std::sync::Arc;
+use tokio::sync::{mpsc, watch};
+use uuid::Uuid;
+
+pub trait ReporterProxy {
+    fn list(
+        &self,
+        starting_seq: Option<Generation>,
+        limit: Option<usize>,
+    ) -> impl Future<Output = Result<Vec<Entry>, Error>> + Send;
+
+    fn acknowledge(
+        &self,
+        seq: Generation,
+    ) -> impl Future<Output = Result<(), Error>> + Send;
+
+    fn recover_seq(
+        &self,
+        seq: Generation,
+    ) -> impl Future<Output = Result<(), Error>> + Send;
+}
+
+pub(crate) struct ProxyWorker<P> {
+    proxy: P,
+    log: slog::Logger,
+    id: Uuid,
+    server: watch::Receiver<Option<crate::server::State>>,
+    server_reqs: mpsc::Receiver<crate::buffer::ServerReq>,
+    seqs: mpsc::Receiver<Generation>,
+}
+
+impl<P: ReporterProxy> ProxyWorker<P> {
+    async fn run(mut self) {
+        loop {
+            tokio::select! {
+                biased;
+                seq = self.seqs.recv() => {
+                    if let Some(seq) = seq {
+                        if let Err(_e) = self.proxy.recover_seq(seq).await {
+                            // TODO(eliza): waht do about error
+                        }
+                    }
+
+                },
+            }
+        }
+    }
+}
