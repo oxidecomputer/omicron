5cafe15b184326c66c6622f63ef6573db44ab202
Eliza Weisman <eliza@elizas.website>
2024-10-25 16:01:16 -0700

    [omdb] add VMMs list command
    There's a full-table scan that needs to be fixed

--- dev-tools/omdb/src/bin/omdb/db.rs
+++ dev-tools/omdb/src/bin/omdb/db.rs
@@ -16,6 +16,7 @@
 #![allow(clippy::useless_vec)]
 
 use crate::check_allow_destructive::DestructiveOperationToken;
+use crate::helpers::const_max_len;
 use crate::helpers::CONNECTION_OPTIONS_HEADING;
 use crate::helpers::DATABASE_OPTIONS_HEADING;
 use crate::Omdb;
@@ -138,7 +139,6 @@ use std::fmt::Display;
 use std::num::NonZeroU32;
 use std::sync::Arc;
 use strum::IntoEnumIterator;
-use strum::VariantArray;
 use tabled::Tabled;
 use uuid::Uuid;
 
@@ -326,6 +326,11 @@ enum DbCommands {
     Validate(ValidateArgs),
     /// Print information about volumes
     Volumes(VolumeArgs),
+    /// Print information about Propolis virtual machine manager (VMM)
+    /// processes.
+    Vmm(VmmArgs),
+    /// Alias to `omdb db vmm list`.
+    Vmms(VmmListArgs),
 }
 
 #[derive(Debug, Args)]
@@ -438,7 +443,7 @@ struct InstanceListArgs {
         long = "state",
         conflicts_with = "running",
         value_parser = PossibleValuesParser::new(
-            db::model::InstanceState::VARIANTS
+            db::model::InstanceState::ALL_STATES
                 .iter()
                 .map(|v| PossibleValue::new(v.label()))
         ),
@@ -798,6 +803,51 @@ struct VolumeInfoArgs {
     uuid: Uuid,
 }
 
+#[derive(Debug, Args)]
+struct VmmArgs {
+    #[command(subcommand)]
+    command: VmmCommands,
+}
+
+#[derive(Debug, Subcommand)]
+enum VmmCommands {
+    /// Get info for a specific VMM process
+    #[clap(alias = "show")]
+    Info(VmmInfoArgs),
+    /// List VMM processes
+    #[clap(alias = "ls")]
+    List(VmmListArgs),
+}
+
+#[derive(Debug, Args)]
+struct VmmInfoArgs {
+    /// The UUID of the VMM process.
+    uuid: Uuid,
+}
+
+#[derive(Debug, Args)]
+struct VmmListArgs {
+    /// Enable verbose output.
+    ///
+    /// You may need a really wide monitor for this!
+    #[arg(long, short)]
+    verbose: bool,
+
+    /// Only show VMMs in the provided state(s).
+    ///
+    /// By default, all VMM states are selected.
+    #[arg(
+        short,
+        long = "state",
+        value_parser = PossibleValuesParser::new(
+            db::model::VmmState::ALL_STATES
+                .iter()
+                .map(|v| PossibleValue::new(v.label()))
+        ),
+    )]
+    states: Vec<db::model::VmmState>,
+}
+
 impl DbArgs {
     /// Run a `omdb db` subcommand.
     pub(crate) async fn run_cmd(
@@ -1030,6 +1080,15 @@ impl DbArgs {
             DbCommands::Volumes(VolumeArgs {
                 command: VolumeCommands::List,
             }) => cmd_db_volume_list(&datastore, &self.fetch_opts).await,
+
+            DbCommands::Vmm(VmmArgs { command: VmmCommands::Info(args) }) => {
+                cmd_db_vmm_info(&opctx, &datastore, &self.fetch_opts, &args)
+                    .await
+            }
+            DbCommands::Vmm(VmmArgs { command: VmmCommands::List(args) })
+            | DbCommands::Vmms(args) => {
+                cmd_db_vmm_list(&datastore, &self.fetch_opts, args).await
+            }
         };
         datastore.terminate().await;
         res
@@ -2925,7 +2984,7 @@ async fn cmd_db_instance_info(
     };
     use nexus_db_model::{
         Instance, InstanceKarmicStatus, InstanceRuntimeState, Migration,
-        Reincarnatability, Vmm,
+        Reincarnatability,
     };
     let &InstanceInfoArgs { ref id, history } = args;
 
@@ -3102,6 +3161,7 @@ async fn cmd_db_instance_info(
 
     println!("    {ACTIVE_VMM:>WIDTH$}: {propolis_id:?}");
     println!("    {TARGET_VMM:>WIDTH$}: {dst_propolis_id:?}");
+
     println!(
         "{}{MIGRATION_ID:>WIDTH$}: {migration_id:?}",
         if migration_id.is_some() { "(i) " } else { "    " },
@@ -3113,16 +3173,14 @@ async fn cmd_db_instance_info(
     }
     println!(" at generation: {}", instance.updater_gen.0);
 
-    fn print_vmm(slug: &str, kind: &str, id: Uuid, vmm: Option<&Vmm>) {
+    fn print_vmm(kind: &str, id: Uuid, vmm: Option<&Vmm>) {
         match vmm {
             Some(vmm) => {
                 println!(
-                    "\n    {slug:>WIDTH$}:\n{}",
-                    textwrap::indent(
-                        &format!("{vmm:#?}"),
-                        &" ".repeat(WIDTH - slug.len() + 8)
-                    )
+                    "\n{:=<80}",
+                    format!("== {} VMM ", kind.to_ascii_uppercase())
                 );
+                prettyprint_vmm("    ", vmm, Some(WIDTH), None, true);
                 if vmm.time_deleted.is_some() {
                     eprintln!(
                         "\n/!\\ BAD: dangling foreign key to deleted {kind} \
@@ -3140,7 +3198,7 @@ async fn cmd_db_instance_info(
     }
 
     if let Some(id) = propolis_id {
-        print_vmm(ACTIVE_VMM_RECORD, "active", id, active_vmm.as_ref());
+        print_vmm("active", id, active_vmm.as_ref());
     }
 
     if let Some(id) = dst_propolis_id {
@@ -3153,7 +3211,7 @@ async fn cmd_db_instance_info(
         match fetch_result {
             Ok(rs) => {
                 let vmm = rs.into_iter().next();
-                print_vmm(TARGET_VMM_RECORD, "target", id, vmm.as_ref());
+                print_vmm("target", id, vmm.as_ref());
             }
             Err(e) => {
                 eprintln!("error looking up target VMM record {id}: {e}");
@@ -3216,7 +3274,7 @@ async fn cmd_db_instance_info(
     #[derive(Tabled)]
     #[tabled(rename_all = "SCREAMING_SNAKE_CASE")]
     struct DiskRow {
-        #[tabled(display_with = "display_option_blank")]
+        #[tabled(rename = "#", display_with = "display_option_blank")]
         slot: Option<u8>,
         #[tabled(inline)]
         identity: DiskIdentity,
@@ -3244,7 +3302,7 @@ async fn cmd_db_instance_info(
     }
 
     if !disks.is_empty() {
-        println!("\n{:=<80}\n", "== ATTACHED DISKS");
+        println!("\n{:=<80}\n", "== ATTACHED DISKS ");
 
         check_limit(&disks, fetch_opts.fetch_limit, ctx);
         let table = if fetch_opts.include_deleted {
@@ -3298,6 +3356,17 @@ async fn cmd_db_instance_info(
         }
 
         let ctx = || "listing past VMMs";
+        #[derive(Tabled)]
+        #[tabled(rename_all = "SCREAMING_SNAKE_CASE")]
+        struct VmmRow {
+            #[tabled(inline)]
+            state: VmmStateRow,
+            sled_id: Uuid,
+            #[tabled(display_with = "datetime_rfc3339_concise")]
+            time_created: chrono::DateTime<Utc>,
+            #[tabled(display_with = "datetime_opt_rfc3339_concise")]
+            time_deleted: Option<chrono::DateTime<Utc>>,
+        }
         let vmms = vmm_dsl::vmm
             .filter(vmm_dsl::instance_id.eq(id.into_untyped_uuid()))
             .limit(i64::from(u32::from(fetch_opts.fetch_limit)))
@@ -3312,10 +3381,36 @@ async fn cmd_db_instance_info(
 
             check_limit(&vmms, fetch_opts.fetch_limit, ctx);
 
-            let table = tabled::Table::new(vmms.iter().map(VmmStateRow::from))
-                .with(tabled::settings::Style::empty())
-                .with(tabled::settings::Padding::new(0, 1, 0, 0))
-                .to_string();
+            let table = tabled::Table::new(vmms.iter().map(|vmm| {
+                let &Vmm {
+                    id,
+                    sled_id,
+                    propolis_ip: _,
+                    propolis_port: _,
+                    instance_id: _,
+                    time_created,
+                    time_deleted,
+                    runtime:
+                        db::model::VmmRuntimeState {
+                            time_state_updated: _,
+                            r#gen,
+                            state,
+                        },
+                } = vmm;
+                VmmRow {
+                    state: VmmStateRow {
+                        id,
+                        state,
+                        generation: r#gen.0.into(),
+                    },
+                    sled_id,
+                    time_created,
+                    time_deleted,
+                }
+            }))
+            .with(tabled::settings::Style::empty())
+            .with(tabled::settings::Padding::new(0, 1, 0, 0))
+            .to_string();
             println!("{table}");
         }
     }
@@ -3330,36 +3425,8 @@ struct VmmStateRow {
     state: db::model::VmmState,
     #[tabled(rename = "GEN")]
     generation: u64,
-    sled_id: Uuid,
-    #[tabled(display_with = "datetime_rfc3339_concise")]
-    time_created: chrono::DateTime<Utc>,
-    #[tabled(display_with = "datetime_opt_rfc3339_concise")]
-    time_deleted: Option<chrono::DateTime<Utc>>,
 }
 
-impl From<&'_ Vmm> for VmmStateRow {
-    fn from(vmm: &Vmm) -> Self {
-        let &Vmm {
-            id,
-            time_created,
-            time_deleted,
-            sled_id,
-            propolis_ip: _,
-            propolis_port: _,
-            instance_id: _,
-            runtime:
-                db::model::VmmRuntimeState { time_state_updated: _, r#gen, state },
-        } = vmm;
-        Self {
-            id,
-            state,
-            time_created,
-            time_deleted,
-            generation: r#gen.0.into(),
-            sled_id,
-        }
-    }
-}
 #[derive(Tabled)]
 #[tabled(rename_all = "SCREAMING_SNAKE_CASE")]
 struct CustomerInstanceRow {
@@ -5612,6 +5679,266 @@ impl From<&'_ Migration> for MigrationVmms {
             tgt_vmm: target_propolis_id,
         }
     }
+}
+
+// VMMs
+
+async fn cmd_db_vmm_info(
+    opctx: &OpContext,
+    datastore: &DataStore,
+    _: &DbFetchOptions,
+    &VmmInfoArgs { uuid }: &VmmInfoArgs,
+) -> Result<(), anyhow::Error> {
+    use nexus_db_model::schema::vmm::dsl as vmm_dsl;
+    let vmm = vmm_dsl::vmm
+        .filter(vmm_dsl::id.eq(uuid))
+        .select(Vmm::as_select())
+        .limit(1)
+        .load_async(&*datastore.pool_connection_for_tests().await?)
+        .await
+        .with_context(|| format!("failed to fetch VMM record for {uuid}"))?
+        .into_iter()
+        .next()
+        .ok_or_else(|| anyhow::anyhow!("no VMM found with ID {uuid}"))?;
+    let sled_result =
+        LookupPath::new(opctx, datastore).sled_id(vmm.sled_id).fetch().await;
+    let sled = match sled_result {
+        Ok((_, sled)) => Some(sled),
+        Err(err) => {
+            eprintln!(
+                "WARN: failed to fetch sled with ID {}: {err}",
+                vmm.sled_id
+            );
+            None
+        }
+    };
+
+    println!("\n{:=<80}", "== VMM ");
+    prettyprint_vmm(
+        "    ",
+        &vmm,
+        None,
+        sled.as_ref().map(|sled| sled.serial_number()),
+        true,
+    );
+    Ok(())
+}
+
+fn prettyprint_vmm(
+    indent: &str,
+    vmm: &Vmm,
+    width: Option<usize>,
+    sled_serial: Option<&str>,
+    inst_id: bool,
+) {
+    const ID: &'static str = "ID";
+    const CREATED: &'static str = "created at";
+    const DELETED: &'static str = "deleted at";
+    const UPDATED: &'static str = "updated at";
+    const INSTANCE_ID: &'static str = "instance ID";
+    const SLED_ID: &'static str = "sled ID";
+    const SLED_SERIAL: &'static str = "sled serial";
+    const ADDRESS: &'static str = "propolis address";
+    const STATE: &'static str = "state";
+    const WIDTH: usize = const_max_len(&[
+        ID,
+        CREATED,
+        DELETED,
+        UPDATED,
+        INSTANCE_ID,
+        SLED_ID,
+        SLED_SERIAL,
+        STATE,
+        ADDRESS,
+    ]);
+
+    let width = std::cmp::max(width, Some(WIDTH)).unwrap_or(WIDTH);
+    let Vmm {
+        id,
+        time_created,
+        time_deleted,
+        instance_id,
+        sled_id,
+        propolis_ip,
+        propolis_port,
+        runtime: db::model::VmmRuntimeState { state, r#gen, time_state_updated },
+    } = vmm;
+
+    println!("{indent}{ID:>width$}: {id}");
+    if inst_id {
+        println!("{indent}{INSTANCE_ID:>width$}: {instance_id}");
+    }
+    println!("{indent}{CREATED:>width$}: {time_created}");
+    if let Some(deleted) = time_deleted {
+        println!("{indent}{DELETED:width$}: {deleted}");
+    }
+    println!("{indent}{STATE:>width$}: {state}");
+    let g = u64::from(r#gen.0);
+    println!(
+        "{indent}{UPDATED:>width$}: {time_state_updated:?} (generation {g})"
+    );
+
+    println!(
+        "{indent}{ADDRESS:>width$}: {}:{}",
+        propolis_ip.ip(),
+        propolis_port.0
+    );
+    println!("{indent}{SLED_ID:>width$}: {sled_id}");
+    if let Some(serial) = sled_serial {
+        println!("{indent}{SLED_SERIAL:>width$}: {serial}");
+    }
+}
+
+async fn cmd_db_vmm_list(
+    datastore: &DataStore,
+    fetch_opts: &DbFetchOptions,
+    &VmmListArgs { ref states, verbose }: &VmmListArgs,
+) -> Result<(), anyhow::Error> {
+    use db::schema::{sled::dsl as sled_dsl, vmm::dsl};
+
+    let ctx = || "loading VMMs";
+    let mut query = dsl::vmm.into_boxed();
+
+    if !fetch_opts.include_deleted {
+        query = query.filter(dsl::time_deleted.is_null());
+    }
+
+    if !states.is_empty() {
+        query = query.filter(dsl::state.eq_any(states.clone()));
+    }
+
+    let vmms = query
+        .left_join(sled_dsl::sled.on(sled_dsl::id.eq(dsl::sled_id)))
+        .order_by(dsl::time_state_updated.desc())
+        .limit(i64::from(u32::from(fetch_opts.fetch_limit)))
+        .select((Vmm::as_select(), Option::<Sled>::as_select()))
+        .load_async::<(Vmm, Option<Sled>)>(
+            &*datastore.pool_connection_for_tests().await?,
+        )
+        .await
+        .with_context(ctx)?;
+
+    check_limit(&vmms, fetch_opts.fetch_limit, ctx);
+
+    #[derive(Tabled)]
+    #[tabled(rename_all = "SCREAMING_SNAKE_CASE")]
+    struct VmmRow {
+        instance_id: Uuid,
+        #[tabled(inline)]
+        state: VmmStateRow,
+        #[tabled(display_with = "display_option_blank")]
+        sled: Option<String>,
+    }
+
+    impl From<&'_ (Vmm, Option<Sled>)> for VmmRow {
+        fn from((ref vmm, ref sled): &(Vmm, Option<Sled>)) -> Self {
+            let &Vmm {
+                id,
+                time_created: _,
+                time_deleted: _,
+                instance_id,
+                sled_id: _,
+                propolis_ip: _,
+                propolis_port: _,
+                runtime:
+                    db::model::VmmRuntimeState {
+                        state,
+                        r#gen,
+                        time_state_updated: _,
+                    },
+            } = vmm;
+            VmmRow {
+                instance_id,
+                state: VmmStateRow { id, state, generation: r#gen.0.into() },
+                sled: sled
+                    .as_ref()
+                    .map(|sled| sled.serial_number().to_string()),
+            }
+        }
+    }
+
+    #[derive(Tabled)]
+    #[tabled(rename_all = "SCREAMING_SNAKE_CASE")]
+    struct VerboseVmmRow {
+        #[tabled(inline)]
+        inner: VmmRow,
+        sled_id: Uuid,
+        address: std::net::SocketAddr,
+        #[tabled(display_with = "datetime_rfc3339_concise")]
+        time_created: DateTime<Utc>,
+        #[tabled(display_with = "datetime_rfc3339_concise")]
+        time_updated: DateTime<Utc>,
+    }
+
+    impl From<&'_ (Vmm, Option<Sled>)> for VerboseVmmRow {
+        fn from(it: &(Vmm, Option<Sled>)) -> Self {
+            let Vmm {
+                time_created,
+                time_deleted: _,
+                sled_id,
+                propolis_ip,
+                propolis_port,
+                ref runtime,
+                ..
+            } = it.0;
+            VerboseVmmRow {
+                sled_id,
+                inner: VmmRow::from(it),
+                address: std::net::SocketAddr::new(
+                    propolis_ip.ip(),
+                    propolis_port.into(),
+                ),
+                time_created,
+                time_updated: runtime.time_state_updated,
+            }
+        }
+    }
+
+    #[derive(Tabled)]
+    #[tabled(rename_all = "SCREAMING_SNAKE_CASE")]
+    struct WithDeleted<T: Tabled> {
+        #[tabled(inline)]
+        inner: T,
+        #[tabled(display_with = "datetime_opt_rfc3339_concise")]
+        time_deleted: Option<DateTime<Utc>>,
+    }
+
+    impl<'a, T> From<&'a (Vmm, Option<Sled>)> for WithDeleted<T>
+    where
+        T: Tabled + From<&'a (Vmm, Option<Sled>)>,
+    {
+        fn from(it: &'a (Vmm, Option<Sled>)) -> Self {
+            Self { inner: T::from(it), time_deleted: it.0.time_deleted }
+        }
+    }
+
+    let table = match (verbose, fetch_opts.include_deleted) {
+        (true, true) => tabled::Table::new(
+            vmms.iter().map(WithDeleted::<VerboseVmmRow>::from),
+        )
+        .with(tabled::settings::Style::empty())
+        .with(tabled::settings::Padding::new(0, 1, 0, 0))
+        .to_string(),
+        (true, false) => {
+            tabled::Table::new(vmms.iter().map(VerboseVmmRow::from))
+                .with(tabled::settings::Style::empty())
+                .with(tabled::settings::Padding::new(0, 1, 0, 0))
+                .to_string()
+        }
+        (false, true) => {
+            tabled::Table::new(vmms.iter().map(WithDeleted::<VmmRow>::from))
+                .with(tabled::settings::Style::empty())
+                .with(tabled::settings::Padding::new(0, 1, 0, 0))
+                .to_string()
+        }
+        (false, false) => tabled::Table::new(vmms.iter().map(VmmRow::from))
+            .with(tabled::settings::Style::empty())
+            .with(tabled::settings::Padding::new(0, 1, 0, 0))
+            .to_string(),
+    };
+    println!("{table}");
+
+    Ok(())
 }
 
 // Display an empty cell for an Option<T> if it's None.
