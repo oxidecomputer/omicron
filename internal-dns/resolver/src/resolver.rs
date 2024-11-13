c7c0f3e32b69febe6a712983b3aa9b91387dbabd
David Pacheco <dap@oxidecomputer.com>
2024-11-12 16:51:42 -0800

    test build for dropshot + API versioning

--- internal-dns/resolver/src/resolver.rs
+++ internal-dns/resolver/src/resolver.rs
@@ -799,7 +799,7 @@ mod test {
     // Progenitor client.
     fn expect_openapi_json_valid_for_test_server() {
         let api = api();
-        let openapi = api.openapi("Test Server", "v0.1.0");
+        let openapi = api.openapi("Test Server", semver::Version::new(0,1,0));
         let mut output = std::io::Cursor::new(Vec::new());
         openapi.write(&mut output).unwrap();
         expectorate::assert_contents(
