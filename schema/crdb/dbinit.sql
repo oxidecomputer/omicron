491000094ee5a07a916efba366cd2f8a742a7aaa
Andrew J. Stone <andrew@oxidecomputer.com>
2024-11-22 23:24:34 +0000

    schema migration

--- schema/crdb/dbinit.sql
+++ schema/crdb/dbinit.sql
@@ -4687,7 +4687,7 @@ INSERT INTO omicron.public.db_metadata (
     version,
     target_version
 ) VALUES
-    (TRUE, NOW(), NOW(), '114.0.0', NULL)
+    (TRUE, NOW(), NOW(), '115.0.0', NULL)
 ON CONFLICT DO NOTHING;
 
 COMMIT;
---
+++ schema/crdb/inv-omicron-physical-disks-generation/up1.sql
@@ -0,0 +1,3 @@
+ALTER TABLE omicron.public.inv_sled_agent 
+    ADD COLUMN IF NOT EXISTS omicrion_physical_disks_generation INT8
+        NOT NULL DEFAULT 0;
