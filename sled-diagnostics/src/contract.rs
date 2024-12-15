3db8b04172ce00bfd3b218eca364a48b381ba856
Mike Zeller <mike@mikezeller.net>
2024-12-15 18:16:28 +0000

    license
    Created using spr 1.3.6-beta.1

--- sled-diagnostics/src/contract.rs
+++ sled-diagnostics/src/contract.rs
@@ -1,3 +1,9 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+// ! Bindings to libcontract(3lib).
+
 use fs_err as fs;
 use libc::{c_char, c_int, c_void, pid_t};
 use slog::{warn, Logger};
