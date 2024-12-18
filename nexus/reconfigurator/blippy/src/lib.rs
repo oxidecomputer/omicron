e6af5cf74ee7b23a06da92cc04e3af341739d550
John Gallagher <john@oxidecomputer.com>
2024-12-18 15:22:43 -0500

    basic doc comment

--- nexus/reconfigurator/blippy/src/lib.rs
+++ nexus/reconfigurator/blippy/src/lib.rs
@@ -3,6 +3,13 @@
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
 //! Blippy: the blueprint checker
+//!
+//! [`Blippy`] performs a variety of checks on blueprints to ensure they are
+//! internally-consistent (e.g., "every in-service zone that should have one or
+//! more datasets do", or "any given external IP address is used by at most one
+//! in-service zone"). It emits [`BlippyReport`]s in the form of a list of
+//! [`BlippyNote`]s, each of which has an associated severity and parent
+//! component (typically a sled).
 
 mod blippy;
 mod checks;
