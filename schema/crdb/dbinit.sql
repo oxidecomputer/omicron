d80c4c9ce6a851fad9e7dd502d4ff838b3985e6d
Eliza Weisman <eliza@elizas.website>
2024-12-18 14:24:39 -0800

    more diesel plumbing

--- schema/crdb/dbinit.sql
+++ schema/crdb/dbinit.sql
@@ -4856,7 +4856,7 @@ INSERT INTO omicron.public.db_metadata (
     version,
     target_version
 ) VALUES
-    (TRUE, NOW(), NOW(), '116.0.0', NULL)
+    (TRUE, NOW(), NOW(), '117.0.0', NULL)
 ON CONFLICT DO NOTHING;
 
 COMMIT;
