7ce92986442ca1be37d342aed6d1c4d7941f21ec
Eliza Weisman <eliza@elizas.website>
2025-01-09 12:19:08 -0800

    evaluate subscription globs on creation
    this way, rather than transforming the globs into patterns that we can
    match in the database every time an event is dispatched, we instead
    use globs to generate "exact" subscriptions to specific named event
    classes. this avoids doing a full scan over the subscription table every
    time an event is dispatched, since we can look up subscribed receivers
    by event class name instead.
    
    we'll have to do this processing on receiver creation or when a
    subscription is added, and when new event classes are added (e.g. on
    software updates).
    
    thanks @andrewjstone for pointing me in this direction!

--- nexus/db-model/src/webhook_rx.rs
+++ nexus/db-model/src/webhook_rx.rs
@@ -3,7 +3,10 @@
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
 use crate::collection::DatastoreCollectionConfig;
-use crate::schema::{webhook_rx, webhook_rx_secret, webhook_rx_subscription};
+use crate::schema::{
+    webhook_rx, webhook_rx_event_glob, webhook_rx_secret,
+    webhook_rx_subscription,
+};
 use crate::typed_uuid::DbTypedUuid;
 use crate::Generation;
 use chrono::{DateTime, Utc};
@@ -50,6 +53,13 @@ impl DatastoreCollectionConfig<WebhookRxSubscription> for WebhookReceiver {
     type CollectionIdColumn = webhook_rx_subscription::dsl::rx_id;
 }
 
+impl DatastoreCollectionConfig<WebhookRxEventGlob> for WebhookReceiver {
+    type CollectionId = Uuid;
+    type GenerationNumberColumn = webhook_rx::dsl::rcgen;
+    type CollectionTimeDeletedColumn = webhook_rx::dsl::time_deleted;
+    type CollectionIdColumn = webhook_rx_event_glob::dsl::rx_id;
+}
+
 // TODO(eliza): should deliveries/delivery attempts also be treated as children
 // of a webhook receiver?
 
@@ -71,56 +81,101 @@ pub struct WebhookRxSecret {
 #[diesel(table_name = webhook_rx_subscription)]
 pub struct WebhookRxSubscription {
     pub rx_id: DbTypedUuid<WebhookReceiverKind>,
-    #[diesel(embed)]
-    pub glob: WebhookGlob,
+    pub event_class: String,
+    pub glob: Option<String>,
     pub time_created: DateTime<Utc>,
 }
 
 #[derive(
     Clone, Debug, Queryable, Selectable, Insertable, Serialize, Deserialize,
 )]
-#[diesel(table_name = webhook_rx_subscription)]
+#[diesel(table_name = webhook_rx_event_glob)]
+pub struct WebhookRxEventGlob {
+    pub rx_id: DbTypedUuid<WebhookReceiverKind>,
+    #[diesel(embed)]
+    pub glob: WebhookGlob,
+    pub time_created: DateTime<Utc>,
+}
+
+impl WebhookRxEventGlob {
+    pub fn new(rx_id: WebhookReceiverUuid, glob: WebhookGlob) -> Self {
+        Self { rx_id: DbTypedUuid(rx_id), glob, time_created: Utc::now() }
+    }
+}
+#[derive(Clone, Debug)]
+pub enum WebhookSubscriptionKind {
+    Glob(WebhookGlob),
+    Exact(String),
+}
+
+impl WebhookSubscriptionKind {
+    pub fn new(value: String) -> Result<Self, Error> {
+        if value.is_empty() {
+            return Err(Error::invalid_value(
+                "event_class",
+                "must not be empty",
+            ));
+        }
+        if value.contains('*') {
+            let regex = WebhookGlob::regex_from_glob(&value)?;
+            Ok(Self::Glob(WebhookGlob { regex, glob: value }))
+        } else {
+            Ok(Self::Exact(value))
+        }
+    }
+}
+
+#[derive(
+    Clone, Debug, Queryable, Selectable, Insertable, Serialize, Deserialize,
+)]
+#[diesel(table_name = webhook_rx_event_glob)]
 pub struct WebhookGlob {
-    pub event_class: String,
-    pub similar_to: String,
+    pub glob: String,
+    pub regex: String,
 }
 
 impl FromStr for WebhookGlob {
     type Err = Error;
-    fn from_str(event_class: &str) -> Result<Self, Self::Err> {
-        fn seg2regex(segment: &str, similar_to: &mut String) {
+    fn from_str(glob: &str) -> Result<Self, Self::Err> {
+        let regex = Self::regex_from_glob(glob)?;
+        Ok(Self { glob: glob.to_string(), regex })
+    }
+}
+
+impl WebhookGlob {
+    fn regex_from_glob(glob: &str) -> Result<String, Error> {
+        let seg2regex = |segment: &str,
+                         regex: &mut String|
+         -> Result<(), Error> {
             match segment {
                 // Match one segment (i.e. any number of segment characters)
-                "*" => similar_to.push_str("[a-zA-Z0-9\\_\\-]+"),
+                "*" => regex.push_str("[^\\.]+"),
                 // Match any number of segments
-                "**" => similar_to.push('%'),
-                // Match the literal segment.
-                // Because `_` his a metacharacter in Postgres' SIMILAR TO
-                // regexes, we've gotta go through and escape them.
-                s => {
-                    // TODO(eliza): validate what characters are in the segment...
-                    for s in s.split_inclusive('_') {
-                        // Handle the fact that there might not be a `_` in the
-                        // string at all
-                        if let Some(s) = s.strip_suffix('_') {
-                            similar_to.push_str(s);
-                            similar_to.push_str("\\_");
-                        } else {
-                            similar_to.push_str(s);
-                        }
-                    }
+                "**" => regex.push_str(".+"),
+                s if s.contains('*') => {
+                    return Err(Error::invalid_value(
+                        "event_class",
+                        "invalid event class {glob:?}: all segments must be \
+                        either '*', '**', or any sequence of non-'*' characters",
+                    ))
                 }
+                // Match the literal segment.
+                s =>  regex.push_str(s),
             }
-        }
+            Ok(())
+        };
 
-        // The subscription's regex will always be at least as long as the event class.
-        let mut similar_to = String::with_capacity(event_class.len());
-        let mut segments = event_class.split('.');
+        // The subscription's regex will always be at least as long as the event
+        // class glob, plus start and end anchors.
+        let mut regex = String::with_capacity(glob.len());
+
+        regex.push('^'); // Start anchor
+        let mut segments = glob.split('.');
         if let Some(segment) = segments.next() {
-            seg2regex(segment, &mut similar_to);
+            seg2regex(segment, &mut regex)?;
             for segment in segments {
-                similar_to.push('.'); // segment separator
-                seg2regex(segment, &mut similar_to);
+                regex.push_str("\\."); // segment separator
+                seg2regex(segment, &mut regex)?;
             }
         } else {
             return Err(Error::invalid_value(
@@ -128,14 +183,29 @@ impl FromStr for WebhookGlob {
                 "must not be empty",
             ));
         };
+        regex.push('$'); // End anchor
 
-        Ok(Self { event_class: event_class.to_string(), similar_to })
+        Ok(regex)
     }
 }
 
 impl WebhookRxSubscription {
-    pub fn new(rx_id: WebhookReceiverUuid, glob: WebhookGlob) -> Self {
-        Self { rx_id: DbTypedUuid(rx_id), glob, time_created: Utc::now() }
+    pub fn exact(rx_id: WebhookReceiverUuid, event_class: String) -> Self {
+        Self {
+            rx_id: DbTypedUuid(rx_id),
+            event_class,
+            glob: None,
+            time_created: Utc::now(),
+        }
+    }
+
+    pub fn for_glob(glob: &WebhookRxEventGlob, event_class: String) -> Self {
+        Self {
+            rx_id: glob.rx_id,
+            glob: Some(glob.glob.glob.clone()),
+            event_class,
+            time_created: Utc::now(),
+        }
     }
 }
 
@@ -146,26 +216,26 @@ mod test {
     #[test]
     fn test_event_class_glob_to_regex() {
         const CASES: &[(&str, &str)] = &[
-            ("foo.bar", "foo.bar"),
-            ("foo.*.bar", "foo.[a-zA-Z0-9\\_\\-]+.bar"),
-            ("foo.*", "foo.[a-zA-Z0-9\\_\\-]+"),
-            ("*.foo", "[a-zA-Z0-9\\_\\-]+.foo"),
-            ("foo.**.bar", "foo.%.bar"),
-            ("foo.**", "foo.%"),
-            ("foo_bar.baz", "foo\\_bar.baz"),
-            ("foo_bar.*.baz", "foo\\_bar.[a-zA-Z0-9\\_\\-]+.baz"),
+            ("foo.bar", "^foo.bar$"),
+            ("foo.*.bar", "^foo\\.[^\\.]*\\.bar$"),
+            ("foo.*", "^foo\\.[^\\.]*$"),
+            ("*.foo", "^[^\\.]*\\.foo$"),
+            ("foo.**.bar", "^foo\\..+\\.bar$"),
+            ("foo.**", "^foo\\..+$"),
+            ("foo_bar.baz", "^foo_bar\\.baz$"),
+            ("foo_bar.*.baz", "^foo_bar\\.[^\\.]+\\.baz$"),
         ];
         for (class, regex) in CASES {
             let glob = match WebhookGlob::from_str(dbg!(class)) {
                 Ok(glob) => glob,
                 Err(error) => panic!(
-                    "event class glob {class:?} should produce the regex 
+                    "event class glob {class:?} should produce the regex
                      {regex:?}, but instead failed to parse: {error}"
                 ),
             };
             assert_eq!(
                 dbg!(regex),
-                dbg!(&glob.similar_to),
+                dbg!(&glob.regex),
                 "event class {class:?} should produce the regex {regex:?}"
             );
         }
