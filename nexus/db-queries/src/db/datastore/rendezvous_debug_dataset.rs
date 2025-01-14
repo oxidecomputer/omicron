541a22e5d49a2cec94fe0b5caf8154905affdb44
John Gallagher <john@oxidecomputer.com>
2025-01-14 14:27:53 -0500

    rustdoc fix

--- nexus/db-queries/src/db/datastore/rendezvous_debug_dataset.rs
+++ nexus/db-queries/src/db/datastore/rendezvous_debug_dataset.rs
@@ -2,7 +2,7 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-//! [`DataStore`] methods on [`DebugDataset`]s.
+//! [`DataStore`] methods on [`RendezvousDebugDataset`]s.
 
 use super::DataStore;
 use super::SQL_BATCH_SIZE;
