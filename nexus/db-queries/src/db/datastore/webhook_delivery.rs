1b609a1392e29d5cac17c5d062e3b015a78a1726
Eliza Weisman <eliza@elizas.website>
2025-01-15 10:28:47 -0800

    okay there we go

--- nexus/db-queries/src/db/datastore/webhook_delivery.rs
+++ nexus/db-queries/src/db/datastore/webhook_delivery.rs
@@ -6,23 +6,28 @@
 
 use super::DataStore;
 use crate::context::OpContext;
+use crate::db::error::public_error_from_diesel;
+use crate::db::error::ErrorHandler;
 use crate::db::model::WebhookDelivery;
 use crate::db::model::WebhookDeliveryAttempt;
+use crate::db::model::WebhookEvent;
 use crate::db::schema::webhook_delivery::dsl;
+use crate::db::schema::webhook_event::dsl as event_dsl;
+use crate::db::update_and_check::UpdateAndCheck;
+use crate::db::update_and_check::UpdateStatus;
+use async_bb8_diesel::AsyncRunQueryDsl;
 use chrono::TimeDelta;
 use chrono::{DateTime, Utc};
 use diesel::prelude::*;
 use omicron_common::api::external::Error;
-use omicron_uuid_kinds::{
-    GenericUuid, WebhookDeliveryUuid, WebhookReceiverUuid,
-};
-use uuid::Uuid;
+use omicron_common::api::external::ListResultVec;
+use omicron_uuid_kinds::{GenericUuid, OmicronZoneUuid, WebhookReceiverUuid};
 
 #[derive(Debug, Clone, Eq, PartialEq)]
 pub enum DeliveryAttemptState {
     Started,
     AlreadyCompleted(DateTime<Utc>),
-    InProgress { nexus_id: Uuid, started: DateTime<Utc> },
+    InProgress { nexus_id: OmicronZoneUuid, started: DateTime<Utc> },
 }
 
 impl DataStore {
@@ -31,7 +36,7 @@ impl DataStore {
         opctx: &OpContext,
         rx_id: &WebhookReceiverUuid,
         lease_timeout: TimeDelta,
-    ) -> ListResultVec<WebhookDelivery> {
+    ) -> ListResultVec<(WebhookDelivery, WebhookEvent)> {
         let conn = self.pool_connection_authorized(opctx).await?;
         let now =
             diesel::dsl::now.into_sql::<diesel::pg::sql_types::Timestamptz>();
@@ -48,15 +53,22 @@ impl DataStore {
                 )),
             )
             .order_by(dsl::time_created.asc())
-            .load_async(&conn)
+            // Join with the `webhook_event` table to get the event class, which
+            // is necessary to construct delivery requests.
+            .inner_join(
+                event_dsl::webhook_event.on(event_dsl::id.eq(dsl::event_id)),
+            )
+            .select((WebhookDelivery::as_select(), WebhookEvent::as_select()))
+            .load_async(&*conn)
             .await
+            .map_err(|e| public_error_from_diesel(e, ErrorHandler::Server))
     }
 
     pub async fn webhook_delivery_start_attempt(
         &self,
         opctx: &OpContext,
         delivery: &WebhookDelivery,
-        nexus_id: &Uuid,
+        nexus_id: &OmicronZoneUuid,
         lease_timeout: TimeDelta,
     ) -> Result<DeliveryAttemptState, Error> {
         let conn = self.pool_connection_authorized(opctx).await?;
@@ -77,31 +89,32 @@ impl DataStore {
             )
             .set((
                 dsl::time_delivery_started.eq(now.nullable()),
-                dsl::deliverator_id.eq(nexus_id),
+                dsl::deliverator_id.eq(nexus_id.into_untyped_uuid()),
             ))
             .check_if_exists::<WebhookDelivery>(id)
             .execute_and_check(&conn)
             .await
             .map_err(|e| public_error_from_diesel(e, ErrorHandler::Server))?;
         match updated.status {
-            UpdateStatus::Updated => Ok(true),
-            UpdateStatus::NotUpdatedButExists { found, .. } => {
-                if let Some(completed) = found.time_completed {
+            UpdateStatus::Updated => Ok(DeliveryAttemptState::Started),
+            UpdateStatus::NotUpdatedButExists => {
+                if let Some(completed) = updated.found.time_completed {
                     return Ok(DeliveryAttemptState::AlreadyCompleted(
                         completed,
                     ));
                 }
 
-                if let Some(started) = found.time_delivery_started {
-                    let nexus_id = found.deliverator_id.ok_or_else(
-                        Error::internal_error(
-                            "if a delivery attempt has a last started \
+                if let Some(started) = updated.found.time_delivery_started {
+                    let nexus_id =
+                        updated.found.deliverator_id.ok_or_else(|| {
+                            Error::internal_error(
+                                "if a delivery attempt has a last started \
                                  timestamp, the database should ensure that \
                                  it also has a Nexus ID",
-                        ),
-                    )?;
+                            )
+                        })?;
                     return Ok(DeliveryAttemptState::InProgress {
-                        nexus_id,
+                        nexus_id: nexus_id.into(),
                         started,
                     });
                 }
@@ -115,7 +128,7 @@ impl DataStore {
         &self,
         opctx: &OpContext,
         delivery: &WebhookDelivery,
-        nexus_id: &Uuid,
+        nexus_id: &OmicronZoneUuid,
         result: &WebhookDeliveryAttempt,
     ) -> Result<(), Error> {
         Err(Error::internal_error("TODO ELIZA DO THIS PART"))
