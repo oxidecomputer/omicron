e7d9467f367fc9fed7fa5ea9b9c7dff5fb745d96
David Pacheco <dap@oxidecomputer.com>
2025-01-29 09:47:51 -0800

    WIP (compiles): clap arg structs

--- dev-tools/openapi-manager/src/cmd/dispatch.rs
+++ dev-tools/openapi-manager/src/cmd/dispatch.rs
@@ -13,6 +13,8 @@ use crate::{
         check::check_impl, dump::dump_impl, generate::generate_impl,
         list::list_impl, new_check::new_check_impl, output::OutputOpts,
     },
+    environment::{BlessedSource, GeneratedSource, LocalSource},
+    git::GitRevision,
     spec::Environment,
 };
 
@@ -59,6 +61,68 @@ pub enum Command {
 
     /// Check that APIs are up-to-date.
     NewCheck(NewCheckArgs),
+}
+
+// XXX-dap TODO-doc, examples
+#[derive(Debug, Args)]
+struct BlessedSourceArgs {
+    #[clap(long, conflicts_with("blessed_from_git"))]
+    blessed_from_dir: Option<Utf8PathBuf>,
+
+    #[clap(long)]
+    // XXX-dap check no shell injection here
+    blessed_from_git: Option<String>,
+}
+
+impl TryFrom<BlessedSourceArgs> for BlessedSource {
+    type Error = anyhow::Error;
+
+    fn try_from(b: BlessedSourceArgs) -> Result<Self, Self::Error> {
+        // XXX-dap test this
+        // clap should not allow both of these to be set.
+        assert!(b.blessed_from_dir.is_none() || b.blessed_from_git.is_none());
+
+        if let Some(local_directory) = b.blessed_from_dir {
+            return Ok(BlessedSource::Directory { local_directory });
+        }
+
+        let (revision_str, maybe_directory) = match &b.blessed_from_git {
+            None => ("main", None),
+            Some(arg) => match arg.split_once(":") {
+                Some((r, d)) => (r, Some(d)),
+                None => (arg.as_str(), None),
+            },
+        };
+        let revision = GitRevision::from(String::from(revision_str));
+        let directory = Utf8PathBuf::from(maybe_directory.unwrap_or("openapi"));
+        Ok(BlessedSource::GitRevisionMergeBase { revision, directory })
+    }
+}
+
+#[derive(Debug, Args)]
+pub struct GeneratedSourceArgs {
+    /// if specified, use files in this directory instead of generating OpenAPI
+    /// documents from the API implementation itself (for testing)
+    #[clap(long)]
+    generated_from_dir: Option<Utf8PathBuf>,
+}
+
+impl From<GeneratedSourceArgs> for GeneratedSource {
+    fn from(value: GeneratedSourceArgs) -> Self {
+        match value.generated_from_dir {
+            Some(local_directory) => {
+                GeneratedSource::Directory { local_directory }
+            }
+            None => GeneratedSource::Generated,
+        }
+    }
+}
+
+#[derive(Debug, Args)]
+pub struct LocalSourceArgs {
+    /// local directory where this workspace's OpenAPI documents are stored
+    #[clap(long)]
+    dir: Option<Utf8PathBuf>,
 }
 
 #[derive(Debug, Args)]
