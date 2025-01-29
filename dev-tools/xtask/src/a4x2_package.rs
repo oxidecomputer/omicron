dfa7e18af3e4088ac52ea039f0f422df85e46c81
Artemis Everfree <artemis@oxidecomputer.com>
2025-01-28 20:52:38 -0800

    Add a4x2-package/a4x2-deploy xtasks
    This PR adds two new xtasks:
    
    - a4x2-package
    - a4x2-deploy
    
    ```
    > cargo xtask a4x2-package -h
    Generate a tarball with omicron packaged for deployment onto a4x2
    
    Usage: cargo xtask a4x2-package [OPTIONS]
    
    Options:
          --live-tests        Bundle omicron live tests into the output tarball
          --end-to-end-tests  Bundle omicron end-to-end-tests package into the output tarball
      -h, --help              Print help
    ```
    
    This command will:
    
    - download & build the latest a4x2 from oxidecomputer/testbed
    - clone the local working tree to a temporary path & build + package it with a4x2
    - if requested, build live-tests
    - if requested, build end-to-end tests (but there is no way to run these yet)
    - package these all into a tar bundle, at `out/a4x2-package-out.tgz`
    
    ```
    > cargo xtask a4x2-deploy -h
    Run a4x2 and deploy omicron onto it, and optionally run live-tests and
    end to end tests
    
    Usage: cargo xtask a4x2-deploy <COMMAND>
    
    Commands:
      start      Start a4x2, deploy a control plane to it, and then leave it running
      stop       Stop a4x2 that was previously launched with the `start` subcommand
      run-tests  Start a4x2, run tests, and then stop a4x2 unless you request otherwise
      help       Print this message or the help of the given subcommand(s)
    
    Options:
      -h, --help  Print help
    ```
    
    This command will:
    
    - tear down any existing a4x2 created by a previous invocation of a4x2-deploy
    - unpack a bundle generated by a4x2-package
    - download latest OVMF code & propolis server
    - launch a4x2
    - ping the control plane until it becomes alive (or a timeout is reached,
      in which case it considers the launch failed)
    - if requested, transfer live tests to the system and execute them
    - print information on how to access the server
    
    By default, a4x2-deploy will use a bundle from `out/a4x2-package.tgz`,
    but an alternative can be specified with `--package <path>`, so you can
    build a package and send it to someone else for them to run on their
    machine.
    
    - a4x2 is still a bit flaky. Sometimes I will reboot my machine for good luck.
    - a4x2-deploy could and should automatically collect logs from the
      system when launches fail, but does not yet.
    - no support for incremental builds of omicron. a4x2 makes some changes to
      tomls in the current working directory, which is why we clone the source
      directory to another path prior to running the a4x2 package step.
    
    Additionally, because of cloning the source to a temporary working path,
    any changes you have not `git add`ed will not be seen by `a4x2-package`.
    I consider this either mildly useful or mildly confusing depending on
    what you're doing in the moment. The jj crew can of course just run `jj`
    before using `a4x2-package`.
    
    We could opt instead of doing a `cp -a` of the working directory instead
    of a git clone, but this could be problematic due to copying potentially
    massive `target/` directories. We could use `rsync` wtith excludes, but
    I'm not sure we can rely on it always being present. The `git clone`
    seemed like the most reliable to me, and it has precedent in the
    `releng` xtask. But I do find this to be somewhat annoying when
    iterating on changes.

---
+++ dev-tools/xtask/src/a4x2_package.rs
@@ -0,0 +1,382 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+//! Subcommand: cargo xtask a4x2-package
+
+use anyhow::Result;
+use camino::{Utf8Path, Utf8PathBuf};
+use clap::Parser;
+use serde::Deserialize;
+use sha2::Digest;
+use std::env;
+use std::fs;
+use std::io::{Read, Write};
+use walkdir::WalkDir;
+use xshell::{cmd, Shell};
+
+#[derive(Parser)]
+pub struct A4x2PackageArgs {
+    /// Bundle omicron live tests into the output tarball
+    #[clap(long)]
+    live_tests: bool,
+
+    /// Bundle omicron end-to-end-tests package into the output tarball
+    #[clap(long)]
+    end_to_end_tests: bool,
+}
+
+#[derive(Deserialize, Debug)]
+#[serde(rename_all = "kebab-case")]
+struct NextestConfig {
+    nextest_version: NextestConfigVersion,
+}
+
+#[derive(Deserialize, Debug)]
+struct NextestConfigVersion {
+    // required: String,
+    recommended: String,
+}
+
+struct Environment {
+    cargo: String,
+    home_dir: Utf8PathBuf,
+    work_dir: Utf8PathBuf,
+    src_dir: Utf8PathBuf,
+    out_dir: Utf8PathBuf,
+    omicron_dir: Utf8PathBuf,
+    in_ci: bool,
+}
+
+pub fn run_cmd(args: A4x2PackageArgs) -> Result<()> {
+    let sh = Shell::new()?;
+
+    let env = {
+        // Buildomat sets the CI environment variable. We'll work a bit
+        // differently in that case (no cleanup, use /work)
+        let in_ci = env::var("CI").is_ok();
+
+        let cargo =
+            std::env::var("CARGO").unwrap_or_else(|_| String::from("cargo"));
+
+        let home_dir = Utf8PathBuf::from(std::env::var("HOME")?);
+        let work_dir = if in_ci {
+            Utf8PathBuf::from("/work/a4x2-package")
+        } else {
+            home_dir.join(".cache/a4x2-package")
+        };
+
+        let src_dir = Utf8PathBuf::try_from(env::current_dir()?)?;
+
+        // Delete any results from previous runs. We don't mind if there's errors.
+        fs::remove_dir_all(&work_dir).ok();
+        // Create work dir
+        fs::create_dir_all(&work_dir)?;
+        sh.change_dir(&work_dir);
+
+        // Output. Maybe in CI we want this to be /out
+        let out_dir = work_dir.join("a4x2-package-out");
+        fs::create_dir_all(&out_dir)?;
+
+        // Clone of omicron that we can modify without causing trouble for anyone
+        let omicron_dir = work_dir.join("omicron");
+        fs::create_dir_all(&omicron_dir)?;
+
+        Environment {
+            cargo,
+            home_dir,
+            work_dir,
+            src_dir,
+            out_dir,
+            omicron_dir,
+            in_ci,
+        }
+    };
+
+    prepare_source(&sh, &env)?;
+
+    if args.end_to_end_tests {
+        build_end_to_end_tests(&sh, &env)?;
+    }
+
+    if args.live_tests {
+        build_live_tests(&sh, &env)?;
+    }
+
+    // This needs to happen last because it messes with the working tree in a
+    // way that end-to-end tests doesnt like when building
+    build_a4x2(&sh, &env)?;
+
+    create_output_artifact(&sh, &env)?;
+
+    Ok(())
+}
+
+/// Clone local working tree into work dir. This is nabbed from
+/// buildomat-at-home
+fn prepare_source(sh: &Shell, env: &Environment) -> Result<()> {
+    cmd!(sh, "banner 'prepare'").run()?;
+
+    // Get a ref we can checkout from the local working tree
+    let treeish = {
+        let _popdir = sh.push_dir(&env.src_dir);
+        let mut treeish = cmd!(sh, "git stash create").read()?;
+        if treeish.is_empty() {
+            // nothing to stash
+            eprintln!("Nothing to stash, using most recent commit for clone");
+            treeish = cmd!(sh, "git rev-parse HEAD").read()?;
+        }
+        assert_eq!(
+            treeish.len(),
+            40,
+            "treeish should be a 40-character commit hash"
+        );
+        assert!(
+            treeish.chars().all(|c| c.is_ascii_hexdigit()),
+            "treeish should be a 40-character commit hash"
+        );
+        treeish
+    };
+
+    // Clone the local source tree into the packaging work dir
+    let _popdir = sh.push_dir(&env.omicron_dir);
+    cmd!(sh, "git init").run()?;
+
+    let src_dir = &env.src_dir;
+    cmd!(sh, "git remote add origin {src_dir}").run()?;
+
+    cmd!(sh, "git fetch origin {treeish}").run()?;
+    cmd!(sh, "git checkout {treeish}").run()?;
+
+    // XXX is this needed in CI?
+    cmd!(sh, "./tools/install_builder_prerequisites.sh -yp").run()?;
+
+    Ok(())
+}
+
+fn build_end_to_end_tests(sh: &Shell, env: &Environment) -> Result<()> {
+    // I'm not confident this does what it should, because I don't know much
+    // about the end to end tests. This is just going from what the shell script
+    // did.
+    cmd!(sh, "banner 'end to end'").run()?;
+    let _popdir = sh.push_dir(&env.omicron_dir);
+
+    let cargo = &env.cargo;
+    cmd!(sh, "{cargo} build -p end-to-end-tests --bin commtest --bin dhcp-server --release").run()?;
+
+    let end_to_end_dir = env.out_dir.join("end-to-end-tests");
+    sh.create_dir(&end_to_end_dir)?;
+    sh.copy_file("target/release/commtest", &end_to_end_dir)?;
+    sh.copy_file("target/release/dhcp-server", &end_to_end_dir)?;
+
+    Ok(())
+}
+
+/// Generate a bundle with the live tests included and ready to go.
+fn build_live_tests(sh: &Shell, env: &Environment) -> Result<()> {
+    cmd!(sh, "banner 'live tests'").run()?;
+    let _popdir = sh.push_dir(&env.omicron_dir);
+
+    // We need nextest available, both to generate the test bundle and to
+    // include in the output tarball to execute the bundle on a4x2.
+
+    // XXX right now we only download a new nextest in CI, and we use
+    // the user's locally installed nextest otherwise. I'm inclined to
+    // say we should always download a fresh one for consistency.
+    // thoughts?
+    let nextest_path = if env.in_ci {
+        // In CI, we need to download nextest
+        let config_str = sh.read_file(".config/nextest.toml")?;
+        let nextest_config: NextestConfig = toml::from_str(&config_str)?;
+
+        // This line of bash comes from build-and-test.sh
+        // If you want to rewrite this in rust, please! Make sure you
+        // - retry correctly
+        // - follow redirects correctly
+        // - handle http errors
+        cmd!(sh, "bash -c")
+            .arg(&format!("curl -sSfL --retry 10 https://get.nexte.st/'{}'/illumos | gunzip | tar -xvf - -C ~/.cargo/bin", nextest_config.nextest_version.recommended))
+            .run()?;
+        env.home_dir.join(".cargo/bin/cargo-nextest")
+    } else {
+        // XXX do we need to test nextest exists & is the right version? need to
+        // see if the incidental errors from stuff below already take care of
+        // telling someone outside of CI what they need to be doing.
+        Utf8PathBuf::from(cmd!(sh, "/usr/bin/which cargo-nextest").read()?)
+    };
+
+    // Build and generate the live tests bundle
+    // generates
+    // - target/live-tests-archive.tgz
+    let cargo = &env.cargo;
+    cmd!(sh, "{cargo} xtask live-tests").run()?;
+
+    let live_test_bundle_dir = env.work_dir.join("live-tests-bundle");
+    sh.create_dir(&live_test_bundle_dir)?;
+
+    // a tar within a tar, a door behind a door
+    cmd!(
+        sh,
+        "tar -xzf target/live-tests-archive.tgz -C {live_test_bundle_dir}"
+    )
+    .run()?;
+
+    // and we need nextest to execute it
+    sh.copy_file(&nextest_path, &live_test_bundle_dir)?;
+
+    // Finally, the script that will run nextest
+    // TODO: should we make this into a rust program that runs in the VM?
+    let switch_zone_script_path = live_test_bundle_dir.join("run-live-tests");
+    let switch_zone_script = r#"
+        set -euxo pipefail
+        /opt/oxide/omdb/bin/omdb -w nexus blueprints target enable current
+
+        TMPDIR=/var/tmp ./cargo-nextest nextest run \
+            --profile=live-tests \
+            --archive-file live-tests-archive/omicron-live-tests.tar.zst \
+            --workspace-remap live-tests-archive
+    "#;
+    sh.write_file(&switch_zone_script_path, switch_zone_script)?;
+    cmd!(sh, "chmod +x {switch_zone_script_path}").run()?;
+
+    // output tar will have live-tests and nextest. this will get uploaded
+    // into one of the a4x2 sleds, which is why it is a self contained tar
+    let out_dir = &env.out_dir;
+
+    // intentionally relative path so the tarball gets relative paths
+    let _popdir = sh.push_dir(&env.work_dir);
+    let bundle_dir_name = live_test_bundle_dir.file_name().unwrap();
+    cmd!(sh, "tar -czf {out_dir}/live-tests-bundle.tgz {bundle_dir_name}/")
+        .run()?;
+
+    Ok(())
+}
+
+fn build_a4x2(sh: &Shell, env: &Environment) -> Result<()> {
+    cmd!(sh, "banner testbed").run()?;
+    let testbed_dir = env.work_dir.join("testbed");
+    let a4x2_dir = testbed_dir.join("a4x2");
+
+    // TODO move this clone & build to prepare, so that if the omicron build
+    // takes awhile we wont have lost our git token when we get here. Relevant
+    // for CI but not for running locally.
+    cmd!(
+        sh,
+        "git clone https://github.com/oxidecomputer/testbed {testbed_dir}"
+    )
+    .run()?;
+
+    // XXX use main branch
+    // cmd!(sh, "git -C {testbed_dir} checkout artemis/init-in-smf").run()?;
+
+    let _popdir = sh.push_dir(&a4x2_dir);
+
+    // build a4x2
+    let cargo = &env.cargo;
+    cmd!(sh, "{cargo} build --release").run()?;
+
+    sh.copy_file("../target/release/a4x2", &env.out_dir)?;
+
+    // This will modify some omicron configs, build omicron, and package things
+    // up for the a4x2 cargo-bay
+    cmd!(sh, "./config/build-packages.sh")
+        .env("OMICRON", &env.omicron_dir)
+        .run()?;
+
+    // debatable whether this should happen in package or deploy
+    cmd!(sh, "./config/fetch-softnpu-artifacts.sh").run()?;
+
+    // Deduplicate cargo-bay output amongst g0-g3. They differ on a few
+    // files; deduping the rest is critical to keeping the final archive
+    // size manageable for CI. To do this we will
+    // - iterate files in g0
+    // - check equivalent paths for g1/g3
+    // - if hashes match, extract out to `sled-common/`
+    let cargo_bay_dir = a4x2_dir.join("cargo-bay");
+    let common_dir = cargo_bay_dir.join("sled-common");
+    let g0_dir = cargo_bay_dir.join("g0");
+    let prefixes = [
+        cargo_bay_dir.join("g1"),
+        cargo_bay_dir.join("g2"),
+        cargo_bay_dir.join("g3"),
+    ];
+    for ent in WalkDir::new(&g0_dir) {
+        let ent = ent?;
+        if !ent.file_type().is_file() {
+            continue;
+        }
+
+        let g0_path =
+            Utf8PathBuf::from_path_buf(ent.path().to_path_buf()).unwrap();
+        let g0_hash = sha256(&g0_path)?;
+
+        let base_path = g0_path.strip_prefix(&g0_dir)?;
+        let mut can_dedupe = true;
+        for prefix in &prefixes {
+            let path = prefix.join(base_path);
+            // if it doesn't exist in all bays, we can't dedupe
+            if !path.try_exists()? {
+                can_dedupe = false;
+                break;
+            }
+
+            let hash = sha256(&path)?;
+            if hash != g0_hash {
+                can_dedupe = false;
+                break;
+            }
+        }
+
+        if can_dedupe {
+            // yay, extract out to the common dir.
+            let dest = common_dir.join(base_path);
+            sh.create_dir(&dest.parent().unwrap())?;
+            sh.copy_file(&g0_path, &dest)?;
+            sh.remove_path(&g0_path)?;
+            for prefix in &prefixes {
+                sh.remove_path(&prefix.join(base_path))?;
+            }
+        }
+    }
+
+    // At this point, `cargo-bay` is ready for us
+    // We don't actually need all of config/ but it's a small dir
+    let out_dir = &env.out_dir;
+    cmd!(sh, "mv cargo-bay/ config/ {out_dir}/").run()?;
+
+    Ok(())
+}
+
+/// Create a tgz file with everything built during a4x2-package invocation
+fn create_output_artifact(sh: &Shell, env: &Environment) -> Result<()> {
+    cmd!(sh, "banner bundle").run()?;
+    let _popdir = sh.push_dir(&env.work_dir);
+    let pkg_dir_name = env.out_dir.file_name().unwrap();
+
+    // Place output in `out/` when running locally, or leave in work dir for CI
+    let artifact = Utf8PathBuf::from("a4x2-package-out.tgz");
+    let artifact = if env.in_ci {
+        env.work_dir.join(artifact)
+    } else {
+        env.src_dir.join("out").join(artifact)
+    };
+
+    cmd!(sh, "tar -czf {artifact} {pkg_dir_name}/").run()?;
+
+    Ok(())
+}
+
+fn sha256(path: &Utf8Path) -> Result<Vec<u8>> {
+    let mut buf = vec![0u8; 65536];
+    let mut file = fs::File::open(path)?;
+    let mut ctx = sha2::Sha256::new();
+    loop {
+        let n = file.read(&mut buf)?;
+        if n == 0 {
+            break;
+        }
+        ctx.write_all(&buf[0..n])?;
+    }
+
+    Ok(ctx.finalize().to_vec())
+}
