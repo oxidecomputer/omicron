525d374bfb42bb604b77923534a356e472b5b520
David Pacheco <dap@oxidecomputer.com>
2025-01-30 20:45:28 -0800

    add XXX-dap

--- dev-tools/openapi-manager/src/cmd/dispatch.rs
+++ dev-tools/openapi-manager/src/cmd/dispatch.rs
@@ -2,6 +2,9 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
+// XXX-dap do we now need a "diff" command to see what's changed between
+// versions?
+
 use std::process::ExitCode;
 
 use anyhow::Result;
