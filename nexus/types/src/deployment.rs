3ae42bf76cb9b55993705b817157e4f60935b6dd
Sean Klein <sean@oxide.computer>
2025-02-07 08:32:13 -0800

    [reconfigurator] Avoid deploying expunged datasets to sled agents (#7308)
    If a blueprint has marked a dataset as expunged, stop telling sled
    agents to deploy it.
    
    Fixes https://github.com/oxidecomputer/omicron/issues/7304

--- nexus/types/src/deployment.rs
+++ nexus/types/src/deployment.rs
@@ -949,14 +949,29 @@ pub struct BlueprintDatasetsConfig {
     pub datasets: IdMap<BlueprintDatasetConfig>,
 }
 
-impl From<BlueprintDatasetsConfig> for DatasetsConfig {
-    fn from(config: BlueprintDatasetsConfig) -> Self {
-        Self {
-            generation: config.generation,
-            datasets: config
+impl BlueprintDatasetsConfig {
+    /// Converts [Self] into [DatasetsConfig].
+    ///
+    /// [DatasetsConfig] is a format of the dataset configuration that can be
+    /// passed to Sled Agents for deployment.
+    ///
+    /// This function is effectively a [std::convert::From] implementation, but
+    /// is named slightly more explicitly, as it filters the blueprint
+    /// configuration to only consider in-service datasets.
+    pub fn into_in_service_datasets(self) -> DatasetsConfig {
+        DatasetsConfig {
+            generation: self.generation,
+            datasets: self
                 .datasets
                 .into_iter()
-                .map(|d| (d.id, d.into()))
+                .filter_map(|d| {
+                    if d.disposition.matches(BlueprintDatasetFilter::InService)
+                    {
+                        Some((d.id, d.into()))
+                    } else {
+                        None
+                    }
+                })
                 .collect(),
         }
     }
