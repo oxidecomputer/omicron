ddc648cac8d95aa59ad01609b3ad0d111278e0cf
Eliza Weisman <eliza@elizas.website>
2025-02-11 11:43:33 -0800

    test receivers with multiple secrets

--- nexus/tests/integration_tests/webhooks.rs
+++ nexus/tests/integration_tests/webhooks.rs
@@ -14,7 +14,9 @@ use nexus_test_utils_macros::nexus_test;
 use nexus_types::external_api::{params, views};
 use omicron_common::api::external::IdentityMetadataCreateParams;
 use omicron_uuid_kinds::WebhookEventUuid;
+use omicron_uuid_kinds::WebhookReceiverUuid;
 use sha2::Sha256;
+use uuid::Uuid;
 
 type ControlPlaneTestContext =
     nexus_test_utils::ControlPlaneTestContext<omicron_nexus::Server>;
@@ -26,6 +28,22 @@ async fn webhook_create(
     resource_helpers::object_create::<params::WebhookCreate, views::Webhook>(
         &ctx.external_client,
         "/experimental/v1/webhooks",
+        params,
+    )
+    .await
+}
+
+async fn secret_add(
+    ctx: &ControlPlaneTestContext,
+    webhook_id: WebhookReceiverUuid,
+    params: &params::WebhookSecretCreate,
+) -> views::WebhookSecretId {
+    resource_helpers::object_create::<
+        params::WebhookSecretCreate,
+        views::WebhookSecretId,
+    >(
+        &ctx.external_client,
+        &format!("/experimental/v1/webhooks/{webhook_id}/secrets"),
         params,
     )
     .await
@@ -46,9 +64,10 @@ fn is_valid_for_webhook(
 }
 
 fn signature_verifies(
-    secret_id: String,
+    secret_id: Uuid,
     secret: Vec<u8>,
 ) -> impl Fn(&HttpMockRequest) -> bool {
+    let secret_id = secret_id.to_string();
     move |req| {
         let hdrs = req.headers();
         let Some(sig_hdr) = hdrs.get_all("x-oxide-signature").iter().filter_map(|hdr| {
@@ -124,18 +143,123 @@ async fn test_event_delivery(cptestctx: &ControlPlaneTestContext) {
                 })
                 .to_string();
                 when.method(POST)
+                    .header("x-oxide-event-class", "test.foo")
+                    .header("x-oxide-event-id", id.to_string())
                     .and(is_valid_for_webhook(&webhook))
                     .is_true(signature_verifies(
-                        webhook.secrets[0].id.to_string(),
+                        webhook.secrets[0].id,
                         "my cool secret".as_bytes().to_vec(),
                     ))
-                    .json_body_includes(body)
-                    .header("x-oxide-event-class", "test.foo")
-                    .header("x-oxide-event-id", id.to_string());
+                    .json_body_includes(body);
                 then.status(200);
             })
             .await
     };
+
+    // Publish an event
+    let event = nexus
+        .webhook_event_publish(
+            &opctx,
+            id,
+            WebhookEventClass::TestFoo,
+            serde_json::json!({"hello_world": true}),
+        )
+        .await
+        .expect("event should be published successfully");
+    dbg!(event);
+
+    dbg!(activate_background_task(internal_client, "webhook_dispatcher").await);
+    dbg!(
+        activate_background_task(internal_client, "webhook_deliverator").await
+    );
+
+    mock.assert_async().await;
+}
+
+#[nexus_test]
+async fn test_multiple_secrets(cptestctx: &ControlPlaneTestContext) {
+    let nexus = cptestctx.server.server_context().nexus.clone();
+    let internal_client = &cptestctx.internal_client;
+
+    let datastore = nexus.datastore();
+    let opctx =
+        OpContext::for_tests(cptestctx.logctx.log.new(o!()), datastore.clone());
+
+    let server = httpmock::MockServer::start_async().await;
+
+    let id = WebhookEventUuid::new_v4();
+    let endpoint =
+        server.url("/webhooks").parse().expect("this should be a valid URL");
+
+    const SECRET1: &str = "it's an older code, sir, but it checks out";
+    const SECRET2: &str = "Joshua";
+    const SECRET3: &str = "Setec Astronomy";
+
+    // Create a webhook receiver.
+    let webhook = webhook_create(
+        &cptestctx,
+        &params::WebhookCreate {
+            identity: IdentityMetadataCreateParams {
+                name: "my-great-webhook".parse().unwrap(),
+                description: String::from("my great webhook"),
+            },
+            endpoint,
+            secrets: vec![SECRET1.to_string()],
+            events: vec!["test.foo".to_string()],
+            disable_probes: false,
+        },
+    )
+    .await;
+    dbg!(&webhook);
+
+    let secret1_id = webhook.secrets[0].id;
+
+    // Add a second secret to the webhook receiver.
+    let secret2_id = dbg!(
+        secret_add(
+            &cptestctx,
+            webhook.id,
+            &params::WebhookSecretCreate { secret: SECRET2.to_string() },
+        )
+        .await
+    )
+    .id;
+
+    // And a third one, just for kicks.
+    let secret3_id = dbg!(
+        secret_add(
+            &cptestctx,
+            webhook.id,
+            &params::WebhookSecretCreate { secret: SECRET3.to_string() },
+        )
+        .await
+    )
+    .id;
+
+    let mock = server
+        .mock_async(|when, then| {
+            when.method(POST)
+                .header("x-oxide-event-class", "test.foo")
+                .header("x-oxide-event-id", id.to_string())
+                .and(is_valid_for_webhook(&webhook))
+                // There should be a signature header present for all three
+                // secrets, and they should all verify the contents of the
+                // webhook request.
+                .is_true(signature_verifies(
+                    secret1_id,
+                    SECRET1.as_bytes().to_vec(),
+                ))
+                .is_true(signature_verifies(
+                    secret2_id,
+                    SECRET2.as_bytes().to_vec(),
+                ))
+                .is_true(signature_verifies(
+                    secret3_id,
+                    SECRET3.as_bytes().to_vec(),
+                ));
+            then.status(200);
+        })
+        .await;
 
     // Publish an event
     let event = nexus
