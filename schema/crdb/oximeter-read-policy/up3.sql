661dab2ccdde4c41016343ab862816662da80d61
karencfv <karencfv@users.noreply.github.com>
2025-02-14 13:35:06 +1300

    Add the files

---
+++ schema/crdb/oximeter-read-policy/up3.sql
@@ -0,0 +1,10 @@
+-- Oximeter read policy defaults to reading from a single node ClickHouse server.
+INSERT INTO omicron.public.oximeter_read_policy (
+    version,
+    oximeter_read_mode,
+    time_created
+) VALUES (
+    0,
+    'single_node',
+    NOW()
+);
