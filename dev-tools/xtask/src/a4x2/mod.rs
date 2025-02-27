a0034727b4f1766a0ea0c0d41b5c959d1e56ab8b
Artemis Everfree <artemis@oxidecomputer.com>
2025-02-26 17:30:12 -0800

    Lift some constants into a4x2 module; ssh str escape

--- dev-tools/xtask/src/a4x2/mod.rs
+++ dev-tools/xtask/src/a4x2/mod.rs
@@ -6,6 +6,15 @@ use clap::Subcommand;
 mod a4x2_deploy;
 mod a4x2_package;
 
+/// live-tests nextest bundle is named this.
+static LIVE_TEST_BUNDLE_NAME: &str = "live-tests-bundle.tgz";
+
+/// directory within the bundle tar
+static LIVE_TEST_BUNDLE_DIR: &str = "live-tests-bundle";
+
+/// script within the bundle directory
+static LIVE_TEST_BUNDLE_SCRIPT: &str = "run-live-tests";
+
 #[derive(Subcommand)]
 pub enum A4x2Cmds {
     /// Generate a tarball with omicron packaged for deployment onto a4x2
