b0fa00ae1a366f77b12739dfb51fe15b0816e1ee
John Gallagher <john@oxidecomputer.com>
2025-02-28 15:14:38 -0500

    correct rustdoc

--- nexus/tests/integration_tests/metrics_querier.rs
+++ nexus/tests/integration_tests/metrics_querier.rs
@@ -2,8 +2,8 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-/// This module does not perform any integration tests of its own; it provides
-/// [`MetricsQuerier`] for other integration tests to use.
+//! This module does not perform any integration tests of its own; it provides
+//! [`MetricsQuerier`] for other integration tests to use.
 
 use chrono::Utc;
 use dropshot::HttpErrorResponseBody;
