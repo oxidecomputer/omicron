944e07c6d46393579c0409677585649fcaf66d67
Rain <rain@oxide.computer>
2025-03-05 23:40:09 +0000

    Created using spr 1.3.6-beta.1

--- dev-tools/xtask/src/common.rs
+++ dev-tools/xtask/src/common.rs
@@ -5,7 +5,7 @@
 //! Common xtask command helpers
 
 use anyhow::{Context, Result, bail};
-use std::{process::Command, sync::LazyLock};
+use std::process::Command;
 
 /// Runs the given command, printing some basic debug information around it, and
 /// failing with an error message if the command does not exit successfully
@@ -53,21 +53,21 @@ pub(crate) fn cargo_command() -> Command {
 struct SanitizedEnvVars {
     // At the moment we only ban some prefixes, but we may also want to ban env
     // vars by exact name in the future.
-    prefixes: Vec<&'static str>,
+    prefixes: &'static [&'static str],
 }
 
 impl SanitizedEnvVars {
-    fn new() -> Self {
+    const fn new() -> Self {
         // Remove many of the environment variables set in
         // https://doc.rust-lang.org/cargo/reference/environment-variables.html#environment-variables-cargo-sets-for-build-scripts.
         // This is done to avoid recompilation with crates like ring between
         // `cargo clippy` and `cargo xtask clippy`. (This is really a bug in
-        // Cargo, link to the bug pending it being filed upstream.)
+        // both ring's build script and in Cargo.)
         //
         // The current list is informed by looking at ring's build script, so
         // it's not guaranteed to be exhaustive and it may need to grow over
         // time.
-        let prefixes = vec!["CARGO_PKG_", "CARGO_MANIFEST_", "CARGO_CFG_"];
+        let prefixes = &["CARGO_PKG_", "CARGO_MANIFEST_", "CARGO_CFG_"];
         Self { prefixes }
     }
 
@@ -76,5 +76,4 @@ impl SanitizedEnvVars {
     }
 }
 
-static SANITIZED_ENV_VARS: LazyLock<SanitizedEnvVars> =
-    LazyLock::new(SanitizedEnvVars::new);
+static SANITIZED_ENV_VARS: SanitizedEnvVars = SanitizedEnvVars::new();
