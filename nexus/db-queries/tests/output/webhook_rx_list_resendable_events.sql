2e7185ef7c689559bd8354a16f395222e1d79089
Eliza Weisman <eliza@elizas.website>
2025-03-14 12:59:06 -0700

    update expectorate query

--- nexus/db-queries/tests/output/webhook_rx_list_resendable_events.sql
+++ nexus/db-queries/tests/output/webhook_rx_list_resendable_events.sql
@@ -2,6 +2,7 @@ SELECT
   DISTINCT
   webhook_event.id,
   webhook_event.time_created,
+  webhook_event.time_modified,
   webhook_event.time_dispatched,
   webhook_event.event_class,
   webhook_event.event,
@@ -18,7 +19,7 @@ WHERE
           FROM
             webhook_delivery AS also_delivey
           WHERE
-            (also_delivey.event_id = webhook_event.id AND also_delivey.failed_permanently = $3)
+            (also_delivey.event_id = webhook_event.id AND also_delivey.state != $3)
             AND also_delivey.trigger != $4
         )
       )
