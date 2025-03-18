201bad5e4b82c4faa9d522f68835530a420a8417
David Pacheco <dap@oxidecomputer.com>
2025-03-18 09:03:43 -0700

    undo change to register_zone_external_networking_step()

--- nexus/reconfigurator/execution/src/lib.rs
+++ nexus/reconfigurator/execution/src/lib.rs
@@ -254,6 +254,8 @@ fn map_err_to_step_warning(
     }
 }
 
+/// We explicitly don't continue with execution after this step. It only talks
+/// to CRDB, and if nexus cannot talk to CRDB, we probably shouldn't continue.
 fn register_zone_external_networking_step<'a>(
     registrar: &ComponentRegistrar<'_, 'a>,
     opctx: &'a OpContext,
@@ -269,13 +271,14 @@ fn register_zone_external_networking_step<'a>(
             ExecutionStepId::Ensure,
             "Ensure external networking resources",
             move |_cx| async move {
-                let res = datastore
+                datastore
                     .blueprint_ensure_external_networking_resources(
                         opctx, blueprint,
                     )
                     .await
-                    .map_err(|err| anyhow!(err));
-                Ok(map_err_to_step_warning(res))
+                    .map_err(|err| anyhow!(err))?;
+
+                StepSuccess::new(()).into()
             },
         )
         .register();
