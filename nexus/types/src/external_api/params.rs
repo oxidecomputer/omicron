467e0aff77e73f093b9da8317e1abcaff7ee5dfc
David Crespo <david-crespo@users.noreply.github.com>
2025-03-24 18:24:58 -0500

    Remove alpine disk source from OpenAPI schema (#7863)
    Removing `YouCanBootAnythingAsLongAsItsAlpine` fully (#4222) requires
    changing the tests that rely on it. There aren't that many ([repo
    search](https://github.com/search?q=repo%3Aoxidecomputer%2Fomicron%20YouCanBootAnythingAsLongAsItsAlpine&type=code))
    but I'd have to puzzle it out. What I can do trivially, however, is get
    it out the OpenAPI schema so API and CLI users don't have to see it.

--- nexus/types/src/external_api/params.rs
+++ nexus/types/src/external_api/params.rs
@@ -2112,6 +2112,7 @@ pub enum ImageSource {
 
     /// Boot the Alpine ISO that ships with the Propolis zone. Intended for
     /// development purposes only.
+    #[schemars(skip)] // keep it out of the OpenAPI schema
     YouCanBootAnythingAsLongAsItsAlpine,
 }
 
