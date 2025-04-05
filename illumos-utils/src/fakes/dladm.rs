bad5ff2e206921ffbe918db7be6a9f74897d5d6b
Sean Klein <sean@oxide.computer>
2025-04-05 12:24:26 -0700

    Fully remove automock, USE_MOCKS (#7908)
    Fixes https://github.com/oxidecomputer/omicron/issues/4481

---
+++ illumos-utils/src/fakes/dladm.rs
@@ -0,0 +1,46 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+use crate::dladm::Api;
+use crate::dladm::CreateVnicError;
+use crate::dladm::DeleteVnicError;
+use crate::dladm::FindPhysicalLinkError;
+use crate::dladm::VnicSource;
+use crate::link::Link;
+use omicron_common::api::external::MacAddr;
+use omicron_common::vlan::VlanID;
+use std::sync::Arc;
+
+/// A fake implementation of [crate::dladm::Dladm].
+///
+/// This struct implements the [crate::dladm::Api] interface but avoids
+/// interacting with the host OS.
+pub struct Dladm {}
+
+impl Dladm {
+    pub fn new() -> Arc<Self> {
+        Arc::new(Self {})
+    }
+}
+
+impl Api for Dladm {
+    fn create_vnic(
+        &self,
+        _source: &(dyn VnicSource + 'static),
+        _vnic_name: &str,
+        _mac: Option<MacAddr>,
+        _vlan: Option<VlanID>,
+        _mtu: usize,
+    ) -> Result<(), CreateVnicError> {
+        Ok(())
+    }
+
+    fn verify_link(&self, link: &str) -> Result<Link, FindPhysicalLinkError> {
+        Ok(Link::wrap_physical(link))
+    }
+
+    fn delete_vnic(&self, _name: &str) -> Result<(), DeleteVnicError> {
+        Ok(())
+    }
+}
