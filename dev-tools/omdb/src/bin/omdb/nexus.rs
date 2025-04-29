b73b6ff5d772542d60e4337bd5646e3d39aac8d0
Sean Klein <seanmarionklein@gmail.com>
2025-04-28 17:02:45 -0700

    Better support for waiting for collection to finish

--- dev-tools/omdb/src/bin/omdb/nexus.rs
+++ dev-tools/omdb/src/bin/omdb/nexus.rs
@@ -43,6 +43,8 @@ use nexus_client::types::LastResult;
 use nexus_client::types::PhysicalDiskPath;
 use nexus_client::types::SagaState;
 use nexus_client::types::SledSelector;
+use nexus_client::types::SupportBundleInfo;
+use nexus_client::types::SupportBundleState;
 use nexus_client::types::UninitializedSledId;
 use nexus_db_lookup::LookupPath;
 use nexus_db_queries::db::DataStore;
@@ -97,6 +99,7 @@ use serde::Deserialize;
 use slog_error_chain::InlineErrorChain;
 use std::collections::BTreeMap;
 use std::collections::BTreeSet;
+use std::io::Write;
 use std::str::FromStr;
 use std::sync::Arc;
 use std::time::Duration;
@@ -3640,12 +3643,56 @@ enum InspectRunStep {
     PipeFile,
 }
 
+async fn wait_for_bundle_to_be_collected(
+    client: &nexus_client::Client,
+    id: SupportBundleUuid,
+) -> Result<SupportBundleInfo, anyhow::Error> {
+    let mut printed_wait_msg = false;
+    loop {
+        let sb = client
+            .support_bundle_view(id.as_untyped_uuid())
+            .await
+            .with_context(|| {
+                format!("failed to query for support bundle {}", id)
+            })?;
+
+        match sb.state {
+            SupportBundleState::Active => {
+                if printed_wait_msg {
+                    eprintln!("");
+                }
+                return Ok(sb.into_inner());
+            }
+            SupportBundleState::Collecting => {
+                if !printed_wait_msg {
+                    eprint!("Waiting for {} to finish collection...", id);
+                    printed_wait_msg = true;
+                }
+                tokio::time::sleep(Duration::from_secs(1)).await;
+                eprint!(".");
+                std::io::stderr()
+                    .flush()
+                    .with_context(|| format!("cannot flush stderr"))?;
+            }
+            other => bail!("Unexepcted state: {other}"),
+        }
+    }
+}
+
 async fn access_bundle_from_id(
     client: &nexus_client::Client,
     id: Option<SupportBundleUuid>,
 ) -> Result<support_bundle_reader_lib::InternalApiAccess<'_>, anyhow::Error> {
     let id = match id {
-        Some(id) => id,
+        Some(id) => {
+            // Ensure the bundle has been collected
+            let sb = wait_for_bundle_to_be_collected(
+                client,
+                SupportBundleUuid::from_untyped_uuid(*id.as_untyped_uuid()),
+            )
+            .await?;
+            SupportBundleUuid::from_untyped_uuid(sb.id.into_untyped_uuid())
+        }
         None => {
             // Grab the latest if one isn't supplied
             let support_bundle_stream =
@@ -3656,17 +3703,39 @@ async fn access_bundle_from_id(
                 .context("listing support bundles")?;
             support_bundles.sort_by_key(|k| k.time_created);
 
-            let sb = support_bundles
-                .into_iter()
-                .find(|sb| {
-                    matches!(
-                        sb.state,
-                        nexus_client::types::SupportBundleState::Active
-                    )
-                })
-                .ok_or(anyhow::anyhow!(
-                    "Cannot find active support bundle. Try creating one"
-                ))?;
+            let active_sb = support_bundles
+                .iter()
+                .find(|sb| matches!(sb.state, SupportBundleState::Active));
+
+            let sb = match active_sb {
+                Some(sb) => sb.clone(),
+                None => {
+                    // This is a special case, but not an uncommon one:
+                    //
+                    // - Someone just created a bundle...
+                    // - ... but collection is still happening.
+                    //
+                    // To smooth out this experience for users, we wait for the
+                    // collection to complete.
+                    let collecting_sb = support_bundles.iter().find(|sb| {
+                        matches!(sb.state, SupportBundleState::Collecting)
+                    });
+                    if let Some(collecting_sb) = collecting_sb {
+                        let id = &collecting_sb.id;
+                        wait_for_bundle_to_be_collected(
+                            client,
+                            SupportBundleUuid::from_untyped_uuid(
+                                *id.as_untyped_uuid(),
+                            ),
+                        )
+                        .await?
+                    } else {
+                        bail!(
+                            "Cannot find active support bundle. Try creating one"
+                        )
+                    }
+                }
+            };
 
             eprintln!("Inspecting bundle {} from {}", sb.id, sb.time_created);
 
@@ -3757,11 +3826,11 @@ async fn run_support_bundle_dashboard<B: Backend>(
             let shifted = key.modifiers.contains(event::KeyModifiers::SHIFT);
             match key.code {
                 KeyCode::Char('q') => return Ok(InspectRunStep::Exit),
-                KeyCode::Up | KeyCode::Char('k') => {
+                KeyCode::Up | KeyCode::Char('k') | KeyCode::Char('K') => {
                     let count = if shifted { 5 } else { 1 };
                     dashboard.select_up(count).await?;
                 }
-                KeyCode::Down | KeyCode::Char('j') => {
+                KeyCode::Down | KeyCode::Char('j') | KeyCode::Char('J') => {
                     let count = if shifted { 5 } else { 1 };
                     dashboard.select_down(count).await?;
                 }
