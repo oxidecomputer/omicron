16a4c5675d86bcda0a448e8ffdda0b327cdeba13
John Gallagher <john@oxidecomputer.com>
2025-04-30 16:06:45 -0400

    make trait futures Send

--- sled-agent/config-reconciler/src/sled_agent_facilities.rs
+++ sled-agent/config-reconciler/src/sled_agent_facilities.rs
@@ -12,40 +12,36 @@ use omicron_common::address::Ipv6Subnet;
 use omicron_common::address::SLED_PREFIX;
 use sled_agent_types::zone_bundle::ZoneBundleCause;
 use sled_storage::config::MountConfig;
+use std::future::Future;
 use tufaceous_artifact::ArtifactHash;
 
-#[allow(async_fn_in_trait)] // TODO confirm this makes sense and explain
 pub trait SledAgentFacilities: Send + 'static {
-    type StartZoneError;
-    type MetricsUntrackZoneLinksError;
-    type ZoneBundleCreateError;
-
     /// Called by the reconciler task to inform sled-agent that time is
     /// sychronized. May be called multiple times.
     // TODO-cleanup should we do this work ourselves instead? This is
     // currently implemented by `ServiceManager` and does a couple one-time
     // setup things (like rewrite the OS boot time). We could probably absorb
     // that work and remove this callback.
-    async fn on_time_sync(&self);
+    fn on_time_sync(&self) -> impl Future<Output = ()> + Send;
 
     /// Method to start a zone.
     // TODO-cleanup This is implemented by
     // `ServiceManager::start_omicron_zone()`, which does too much; we should
     // absorb some of its functionality and shrink this interface. We definitely
     // should not need to pass the full list of U2 zpools.
-    async fn start_omicron_zone(
+    fn start_omicron_zone(
         &self,
         zone_config: &OmicronZoneConfig,
         mount_config: &MountConfig,
         is_time_synchronized: bool,
         all_u2_pools: &[ZpoolName],
-    ) -> Result<RunningZone, Self::StartZoneError>;
+    ) -> impl Future<Output = anyhow::Result<RunningZone>> + Send;
 
     /// Stop tracking metrics for a zone's datalinks.
     fn metrics_untrack_zone_links(
         &self,
         zone: &RunningZone,
-    ) -> Result<(), Self::MetricsUntrackZoneLinksError>;
+    ) -> anyhow::Result<()>;
 
     /// Instruct DDM to start advertising a prefix.
     fn ddm_add_internal_dns_prefix(&self, prefix: Ipv6Subnet<SLED_PREFIX>);
@@ -54,20 +50,17 @@ pub trait SledAgentFacilities: Send + 'static {
     fn ddm_remove_internal_dns_prefix(&self, prefix: Ipv6Subnet<SLED_PREFIX>);
 
     /// Create a zone bundle.
-    async fn zone_bundle_create(
+    fn zone_bundle_create(
         &self,
         zone: &RunningZone,
         cause: ZoneBundleCause,
-    ) -> Result<(), Self::ZoneBundleCreateError>;
+    ) -> impl Future<Output = anyhow::Result<()>> + Send;
 }
 
-#[allow(async_fn_in_trait)] // TODO confirm this makes sense and explain
 pub trait SledAgentArtifactStore: Send + 'static {
-    type ArtifactExistsValidationError;
-
     /// Check an artifact exists in the TUF Repo Depot storage.
-    async fn validate_artifact_exists_in_storage(
+    fn validate_artifact_exists_in_storage(
         &self,
         artifact: ArtifactHash,
-    ) -> Result<(), Self::ArtifactExistsValidationError>;
+    ) -> impl Future<Output = anyhow::Result<()>> + Send;
 }
