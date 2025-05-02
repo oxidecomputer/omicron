f3fb7195511bc7f77079804fd0a43f4646013d66
David Pacheco <dap@oxidecomputer.com>
2025-05-02 14:16:58 -0700

    use match for future-proofing

--- nexus/mgs-updates/src/test_util/mod.rs
+++ nexus/mgs-updates/src/test_util/mod.rs
@@ -13,21 +13,32 @@ pub mod updates;
 /// Returns whether caboose `c1` (reported by an SP via MGS) matches caboose
 /// `c2` (constructed or read via `hubtools`)
 pub fn cabooses_equal(c1: &SpComponentCaboose, c2: &hubtools::Caboose) -> bool {
-    let (Ok(name), Ok(board), Ok(version), Ok(git_commit)) =
+    let SpComponentCaboose {
+        board,
+        // epoch is currently ignored.  `hubtools::Caboose` does not expose it
+        // so we cannot check it.
+        epoch: _epoch,
+        git_commit,
+        name,
+        sign,
+        version,
+    } = c1;
+
+    let (Ok(c2name), Ok(c2board), Ok(c2version), Ok(c2git_commit)) =
         (c2.name(), c2.board(), c2.version(), c2.git_commit())
     else {
         return false;
     };
 
-    if c1.name.as_bytes() != name
-        || c1.board.as_bytes() != board
-        || c1.version.as_bytes() != version
-        || c1.git_commit.as_bytes() != git_commit
+    if name.as_bytes() != c2name
+        || board.as_bytes() != c2board
+        || version.as_bytes() != c2version
+        || git_commit.as_bytes() != c2git_commit
     {
         return false;
     }
 
-    match (&c1.sign, c2.sign()) {
+    match (&sign, c2.sign()) {
         (Some(c1sign), Ok(c2sign)) => c1sign.as_bytes() == c2sign,
         (None, Err(CabooseError::MissingTag { .. })) => true,
         (Some(_), Err(CabooseError::MissingTag { .. }))
