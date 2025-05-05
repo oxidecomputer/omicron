1a04e496f3d96289f868370ccaafe6e51ef41b42
Eliza Weisman <eliza@elizas.website>
2025-05-05 11:33:36 -0700

    okay, *finally* make the migration actually work

--- schema/crdb/instance-intended-state/up03.sql
+++ schema/crdb/instance-intended-state/up03.sql
@@ -1,12 +1,13 @@
--- backfill instance intended states based on the current state of the instance.
+-- backfill instance intended states based on the current state of the
+-- instance's active VMM record.
 SET LOCAL disallow_full_table_scans = off;
 UPDATE instance SET intended_state = CASE
     WHEN instance.state = 'destroyed' THEN 'destroyed'
+    WHEN instance.state = 'failed' THEN 'running'
     WHEN instance.state = 'vmm' AND vmm.state = 'stopped' THEN 'stopped'
     WHEN instance.state = 'vmm' AND vmm.state = 'stopping' THEN 'stopped'
     WHEN instance.state = 'vmm' THEN 'running'
     ELSE 'stopped'
 END
-FROM omicron.public.instance instance
-    LEFT JOIN omicron.public.vmm
-    ON instance.vmm.active_propolis_id = vmm.id;
+FROM omicron.public.vmm
+WHERE instance.active_propolis_id = vmm.id;
--- schema/crdb/instance-intended-state/up04.sql
+++ schema/crdb/instance-intended-state/up04.sql
@@ -1,4 +1,13 @@
--- now make the new column non-nullable
-ALTER TABLE omicron.public.instance
-ALTER COLUMN intended_state
-SET NOT NULL;
+-- backfill instances which don't have active VMM records.
+--
+-- this has to be done separately from `up04.sql` as the `FROM
+-- omicron.public.vmm` clause to join with the active VMM record will not update
+-- instances without active VMM records.
+SET LOCAL disallow_full_table_scans = off;
+UPDATE instance SET intended_state = CASE
+    WHEN instance.state = 'destroyed' THEN 'destroyed'
+    WHEN instance.state = 'failed' THEN 'running'
+    ELSE 'stopped'
+END
+-- only update instances whose states were not touched in the previous step.
+WHERE instance.intended_state IS NULL;
