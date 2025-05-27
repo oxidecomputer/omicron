37a2122e4be0d947c141833a695bf11a1c32f537
Sean Klein <sean@oxide.computer>
2025-05-27 09:57:25 -0700

    Update openapi doc comments to work around rustfmt bug (#8210)
    Workaround to help integrate
    https://github.com/oxidecomputer/oxide.rs/pull/1110

--- sled-agent/api/src/lib.rs
+++ sled-agent/api/src/lib.rs
@@ -820,13 +820,9 @@ pub struct SupportBundleMetadata {
 /// Range request headers
 #[derive(Debug, Deserialize, Serialize, JsonSchema)]
 pub struct RangeRequestHeaders {
-    /// A request to access a portion of the resource, such as:
+    /// A request to access a portion of the resource, such as `bytes=0-499`
     ///
-    /// ```text
-    /// bytes=0-499
-    /// ```
-    ///
-    /// <https://developer.mozilla.org/en-US/docs/Web/HTTP/Reference/Headers/Range>
+    /// See: <https://developer.mozilla.org/en-US/docs/Web/HTTP/Reference/Headers/Range>
     pub range: Option<String>,
 }
 
