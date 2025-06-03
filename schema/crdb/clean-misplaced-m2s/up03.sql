2943dd3d32674613f844dfa017d8a4c6edeb86b1
Sean Klein <seanmarionklein@gmail.com>
2025-06-03 15:10:05 -0700

    idempotency

--- schema/crdb/clean-misplaced-m2s/up03.sql
+++ schema/crdb/clean-misplaced-m2s/up03.sql
@@ -1,4 +1,4 @@
 -- Prevent the physical_disk table from seeing anything but
 -- U.2s until we decide to increase scope.
 ALTER TABLE omicron.public.physical_disk
-ADD CONSTRAINT physical_disk_variant_u2 CHECK (variant = 'u2');
+ADD CONSTRAINT IF NOT EXISTS physical_disk_variant_u2 CHECK (variant = 'u2');
