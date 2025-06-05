44af98189005f437cc1689fc29a902a1e643274e
John Gallagher <john@oxidecomputer.com>
2025-06-05 15:24:41 -0400

    schema migration

---
+++ schema/crdb/add-last-reconciliation-orphaned-datasets/up.sql
@@ -0,0 +1,13 @@
+CREATE TABLE IF NOT EXISTS omicron.public.inv_last_reconciliation_orphaned_dataset (
+    inv_collection_id UUID NOT NULL,
+    sled_id UUID NOT NULL,
+    pool_id UUID NOT NULL,
+    kind omicron.public.dataset_kind NOT NULL,
+    zone_name TEXT NOT NULL,
+    CONSTRAINT zone_name_for_zone_kind CHECK (
+      (kind != 'zone' AND zone_name = '') OR
+      (kind = 'zone' AND zone_name != '')
+    ),
+    reason TEXT NOT NULL,
+    PRIMARY KEY (inv_collection_id, sled_id, pool_id, kind, zone_name)
+);
