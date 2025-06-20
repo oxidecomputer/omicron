0f802e22704532b889d994ca23f6d3117545a697
John Gallagher <john@oxidecomputer.com>
2025-06-20 13:29:55 -0400

    cleanup

--- sled-agent/config-reconciler/src/host_phase_2.rs
+++ sled-agent/config-reconciler/src/host_phase_2.rs
@@ -15,6 +15,9 @@ use slog_error_chain::InlineErrorChain;
 use std::cmp;
 use std::fs::File;
 use std::io;
+use std::io::BufRead as _;
+use std::io::BufReader;
+use std::io::Read as _;
 use std::os::fd::AsRawFd as _;
 use tufaceous_artifact::ArtifactHash;
 
@@ -77,6 +80,8 @@ impl BootPartitionDetails {
     }
 
     fn read_blocking(path: Utf8PathBuf) -> Self {
+        const ONE_MIB: usize = 1024 * 1024;
+
         let f = match File::open(&path) {
             Ok(f) => f,
             Err(err) => {
@@ -88,7 +93,7 @@ impl BootPartitionDetails {
         };
 
         // Determine the disk's block size.
-        let block_size = match MediaInfoExtended::from_fd(f.as_raw_fd()) {
+        let mut block_size = match MediaInfoExtended::from_fd(f.as_raw_fd()) {
             Ok(media_info) => media_info.logical_block_size as usize,
             Err(err) => {
                 return Self::ErrorDeterminingBlockSize {
@@ -98,50 +103,35 @@ impl BootPartitionDetails {
             }
         };
 
-        Self::read_blocking_with_block_size(f, path, block_size)
+        // We expect a block_size of 512 or 4096 in practice, but that's a
+        // pretty small amount to read at once. If we have a block size that
+        // evenly divides 1 MiB, bump up to that. (Going larger doesn't seem to
+        // matter much on the disks we have at hand when this is written, and
+        // this isn't a performance-criticial path anyway.)
+        //
+        // Alternatively, guard against something going very wrong in
+        // `MediaInfoExtended`; we'll allocate a buffer of this size when we
+        // read, so if we get back something wild as the logical block size,
+        // we'll assume that's wrong and cap it at 128 MiB.
+        if block_size < ONE_MIB && ONE_MIB % block_size == 0 {
+            block_size = ONE_MIB;
+        } else if block_size > 128 * ONE_MIB {
+            block_size = 128 * ONE_MIB;
+        }
+
+        Self::read_blocking_with_buf_size(
+            &mut BufReaderExactSize::with_capacity(block_size, f),
+            path,
+        )
     }
 
     // This is separated from `read_blocking()` so we can write unit tests over
     // this function without needing real disks that respond to the
     // `MediaInfoExtended` ioctl.
-    fn read_blocking_with_block_size<R: io::Read>(
-        mut f: R,
+    fn read_blocking_with_buf_size<R: io::Read>(
+        f: &mut BufReaderExactSize<R>,
         path: Utf8PathBuf,
-        block_size: usize,
     ) -> Self {
-        const ONE_MIB: usize = 1024 * 1024;
-
-        // In practice we expect block sizes of 512 or 4096, but we can read
-        // bigger chunks; we'll choose 1 MiB (and fall back to the block size if
-        // we somehow have a disk with block sizes > 1 MiB)...
-        let buf_size = cmp::max(block_size, ONE_MIB);
-
-        // ...but guard against something really wild (e.g., `-1` being
-        // interpreted as a block size, or proptest passing us usize::MAX); cap
-        // the block size at something reasonable.
-        let buf_size = cmp::min(buf_size, 16 * ONE_MIB);
-
-        let mut buf = vec![0; buf_size];
-
-        if let Err(err) = f.read_exact(&mut buf) {
-            return Self::ErrorReadingDisk {
-                path,
-                err: InlineErrorChain::new(&err).to_string(),
-            };
-        }
-
-        let image_header = match BootImageHeader::parse(&buf) {
-            Ok(header) => header,
-            Err(err) => {
-                return Self::ErrorParsingImageHeader(
-                    InlineErrorChain::new(&err).to_string(),
-                );
-            }
-        };
-
-        let artifact_size =
-            image_header.data_size as usize + BootImageHeader::SIZE;
-
         // Compute two SHA256 hashes as we read the contents of this boot image.
         // The `image_header` contains a sha256 of the data _after_ the header
         // itself, but the artifact hash that lands in the TUF repo depo
@@ -150,30 +140,56 @@ impl BootPartitionDetails {
         let mut artifact_hasher = sha2::Sha256::new();
         let mut data_hasher = sha2::Sha256::new();
 
-        // This should probably never happen, but isn't fatal...
-        if artifact_size < buf.len() {
-            artifact_hasher.update(&buf[..artifact_size]);
-            data_hasher.update(&buf[BootImageHeader::SIZE..artifact_size]);
-        } else {
-            artifact_hasher.update(&buf[..]);
-            data_hasher.update(&buf[BootImageHeader::SIZE..]);
-            let mut nleft = artifact_size - buf.len();
-            while nleft > 0 {
-                // Always read a full buffer...
-                if let Err(err) = f.read_exact(&mut buf) {
+        // Read the image header and accumulate it into artifact_hasher but not
+        // data_hasher.
+        let image_header = {
+            let mut buf = [0; BootImageHeader::SIZE];
+            if let Err(err) = f.read_exact(&mut buf) {
+                return Self::ErrorReadingDisk {
+                    path,
+                    err: InlineErrorChain::new(&err).to_string(),
+                };
+            }
+            match BootImageHeader::parse(&buf) {
+                Ok(header) => {
+                    artifact_hasher.update(&buf);
+                    header
+                }
+                Err(err) => {
+                    return Self::ErrorParsingImageHeader(
+                        InlineErrorChain::new(&err).to_string(),
+                    );
+                }
+            }
+        };
+
+        let artifact_size =
+            image_header.data_size as usize + BootImageHeader::SIZE;
+
+        let mut nleft = artifact_size - BootImageHeader::SIZE;
+        while nleft > 0 {
+            // Read the rest of the image in block-sized chunks by filling the
+            // underlying `BufReader`'s buffer and consuming it.
+            let buf = match f.fill_buf() {
+                Ok(buf) => buf,
+                Err(err) => {
                     return Self::ErrorReadingDisk {
                         path,
                         err: InlineErrorChain::new(&err).to_string(),
                     };
                 }
+            };
 
-                // ...but the last such read might have extra data.
-                let nvalid = cmp::min(nleft, buf.len());
-                artifact_hasher.update(&buf[..nvalid]);
-                data_hasher.update(&buf[..nvalid]);
+            // Our last block may have too much data; only hash to the end of
+            // the image.
+            let nread = buf.len();
+            let nvalid = cmp::min(nleft, nread);
 
-                nleft -= nvalid;
-            }
+            artifact_hasher.update(&buf[..nvalid]);
+            data_hasher.update(&buf[..nvalid]);
+
+            nleft -= nvalid;
+            f.consume(nread);
         }
 
         let artifact_hash = artifact_hasher.finalize();
@@ -267,25 +283,82 @@ impl BootImageHeader {
     }
 }
 
+/// The std lib [`BufReader`]'s [`io::Read`] implementation will bypass the
+/// buffer and issue reads directly to the inner reader if the requested buffer
+/// size is large enough. When reading raw disks, we don't want to do that: we
+/// always want to issue reads that are a multiple of the disk's block size, so
+/// this wrapper limits reads to at most the configured block size (which is
+/// expected to be such a multiple).
+struct BufReaderExactSize<R> {
+    inner: BufReader<R>,
+    block_size: usize,
+}
+
+impl<R: io::Read> BufReaderExactSize<R> {
+    fn with_capacity(block_size: usize, inner: R) -> Self {
+        Self { inner: BufReader::with_capacity(block_size, inner), block_size }
+    }
+
+    fn into_inner(self) -> R {
+        self.inner.into_inner()
+    }
+}
+
+impl<R: io::Read> io::Read for BufReaderExactSize<R> {
+    fn read(&mut self, mut buf: &mut [u8]) -> io::Result<usize> {
+        // This is our extension of `BufReader`; if we're asked for a large
+        // read, restrict it to at most our block size to avoid `BufReader`
+        // bypassing its buffer. (If buf.len() is exactly block_size, it will
+        // still bypass the buffer, but that's fine - it'll issue an exactly
+        // block_size read.)
+        if buf.len() > self.block_size {
+            buf = &mut buf[..self.block_size];
+        }
+        self.inner.read(buf)
+    }
+}
+
+impl<R: io::Read> io::BufRead for BufReaderExactSize<R> {
+    fn fill_buf(&mut self) -> io::Result<&[u8]> {
+        self.inner.fill_buf()
+    }
+
+    fn consume(&mut self, amount: usize) {
+        self.inner.consume(amount)
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
     use bytes::BufMut as _;
+    use proptest::collection::vec;
+    use proptest::prelude::*;
+    use proptest::prop_oneof;
     use test_strategy::proptest;
 
+    // We're reading a raw disk that is (presumably) much larger than any phase
+    // 2 image written to them, which allows us to read past the end of the
+    // image. This is important because it allows us to always issue reads that
+    // are a multiple of the disk's block size, even if the OS image itself is
+    // not. We emulate the disk being larger than the image in tests by wrapping
+    // the fake OS image in this `NeverEndingReader`, which returns 0s after the
+    // end of the inner reader.
     struct NeverEndingReader<R> {
         inner: R,
         inner_done: bool,
+        read_sizes_requested: Vec<usize>,
     }
 
     impl<R: io::Read> NeverEndingReader<R> {
         fn new(inner: R) -> Self {
-            Self { inner, inner_done: false }
+            Self { inner, inner_done: false, read_sizes_requested: Vec::new() }
         }
     }
 
     impl<R: io::Read> io::Read for NeverEndingReader<R> {
         fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
+            self.read_sizes_requested.push(buf.len());
             loop {
                 if self.inner_done {
                     buf.fill(0);
@@ -317,18 +390,42 @@ mod tests {
         sha256.into()
     }
 
+    /// Custom strategy for our disk block size: 500, 4097, 1 MiB, or random
+    /// between 1 MiB and 2 MiB (25% each). These are chosen to exercise
+    /// different edge cases:
+    ///
+    /// * 500 is smaller than the image header and will require multiple reads
+    /// * 4097 is 1 byte larger than the image header
+    /// * 1 MiB is the common case we expect in practice; any real disk block
+    ///   size that divides 1 MiB will induce a 1 MiB read size
+    /// * Random large block sizes
+    fn block_size_strategy() -> impl Strategy<Value = usize> {
+        const ONE_MIB: usize = 1024 * 1024;
+        prop_oneof![
+            Just(500usize),
+            Just(4097usize),
+            Just(ONE_MIB),
+            (ONE_MIB..=2 * ONE_MIB),
+        ]
+    }
+
     #[proptest]
-    fn proptest_read_valid_host_phase2(mut data: Vec<u8>, block_size: usize) {
+    fn proptest_read_valid_host_phase2(
+        #[strategy(vec(any::<u8>(), 0..1024*1024))] mut data: Vec<u8>,
+        #[strategy(block_size_strategy())] block_size: usize,
+    ) {
         let expected_header_sha256 = prepend_valid_image_hader(&mut data);
         let expected_artifact_hash =
             ArtifactHash(sha2::Sha256::digest(&data).into());
         let expected_artifact_size = data.len();
-        eprintln!("{} {block_size}", data.len());
 
-        match BootPartitionDetails::read_blocking_with_block_size(
-            NeverEndingReader::new(&*data),
-            "/does-not-matter".into(),
+        let mut reader = BufReaderExactSize::with_capacity(
             block_size,
+            NeverEndingReader::new(&*data),
+        );
+        match BootPartitionDetails::read_blocking_with_buf_size(
+            &mut reader,
+            "/does-not-matter".into(),
         ) {
             BootPartitionDetails::Phase2Image {
                 artifact_hash,
@@ -351,5 +448,15 @@ mod tests {
                 panic!("unexpected result: {res:?}");
             }
         }
+
+        // All reads to the underlying disk should have been exactly our block
+        // size.
+        assert!(
+            reader
+                .into_inner()
+                .read_sizes_requested
+                .iter()
+                .all(|&sz| sz == block_size)
+        );
     }
 }
