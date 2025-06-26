63f811d1c0dcd7f53e4c26d50d7907a4815a3522
David Crespo <david.crespo@oxidecomputer.com>
2025-06-26 16:10:37 -0500

    don't be lazy, test utilization values in test-suite-silo

--- nexus/tests/integration_tests/utilization.rs
+++ nexus/tests/integration_tests/utilization.rs
@@ -7,16 +7,26 @@ use nexus_test_utils::http_testing::RequestBuilder;
 use nexus_test_utils::resource_helpers::DiskTest;
 use nexus_test_utils::resource_helpers::create_default_ip_pool;
 use nexus_test_utils::resource_helpers::create_instance;
+use nexus_test_utils::resource_helpers::create_local_user;
 use nexus_test_utils::resource_helpers::create_project;
+use nexus_test_utils::resource_helpers::grant_iam;
+use nexus_test_utils::resource_helpers::link_ip_pool;
+use nexus_test_utils::resource_helpers::object_get;
+use nexus_test_utils::resource_helpers::object_put;
 use nexus_test_utils::resource_helpers::objects_list_page_authz;
+use nexus_test_utils::resource_helpers::test_params;
 use nexus_test_utils_macros::nexus_test;
 use nexus_types::external_api::params;
 use nexus_types::external_api::params::SiloQuotasCreate;
+use nexus_types::external_api::views::Silo;
+use nexus_types::external_api::views::SiloQuotas;
 use nexus_types::external_api::views::SiloUtilization;
 use nexus_types::external_api::views::Utilization;
 use nexus_types::external_api::views::VirtualResourceCounts;
 use omicron_common::api::external::ByteCount;
 use omicron_common::api::external::IdentityMetadataCreateParams;
+use omicron_common::api::external::InstanceCpuCount;
+use oxide_client::types::SiloRole;
 
 static PROJECT_NAME: &str = "utilization-test-project";
 static INSTANCE_NAME: &str = "utilization-test-instance";
@@ -30,19 +40,19 @@ async fn test_utilization(cptestctx: &ControlPlaneTestContext) {
 
     create_default_ip_pool(&client).await;
 
-    // default-silo has quotas, but is explicitly filtered out by ID in the DB
-    // query to avoid user confusion. so list is empty
+    // default-silo has quotas, but is explicitly filtered out by ID in the
+    // DB query to avoid user confusion. test-suite-silo also exists, but is
+    // filtered out because it has no quotas, so list is empty
     let current_util = fetch_util(client).await;
     assert!(current_util.is_empty());
 
+    let quotas_url = "/v1/system/silos/test-suite-silo/quotas";
     // set high quota for test silo
-    let _ = NexusRequest::object_put(
+    let _: SiloQuotas = object_put(
         client,
-        "/v1/system/silos/test-suite-silo/quotas",
-        Some(&params::SiloQuotasCreate::arbitrarily_high_default()),
+        quotas_url,
+        &params::SiloQuotasCreate::arbitrarily_high_default(),
     )
-    .authn_as(AuthnMode::PrivilegedUser)
-    .execute()
     .await;
 
     // now test-suite-silo shows up
@@ -55,18 +65,98 @@ async fn test_utilization(cptestctx: &ControlPlaneTestContext) {
         SiloQuotasCreate::arbitrarily_high_default().into()
     );
 
-    // now we take the quota back off of test-suite-silo and end up empty again
-    let _ = NexusRequest::object_put(
+    // in order to create resources in test-suite-silo, we have to create a user
+    // with the right perms so we have a user ID on hand to use in the authn_as
+    let silo_url = "/v1/system/silos/test-suite-silo";
+    let test_suite_silo: Silo = object_get(client, silo_url).await;
+    link_ip_pool(client, "default", &test_suite_silo.identity.id, true).await;
+    let user1 = create_local_user(
         client,
-        "/v1/system/silos/test-suite-silo/quotas",
-        Some(&params::SiloQuotasCreate::empty()),
+        &test_suite_silo,
+        &"user1".parse().unwrap(),
+        test_params::UserPassword::LoginDisallowed,
     )
-    .authn_as(AuthnMode::PrivilegedUser)
-    .execute()
+    .await;
+    grant_iam(
+        client,
+        silo_url,
+        SiloRole::Collaborator,
+        user1.id,
+        AuthnMode::PrivilegedUser,
+    )
     .await;
 
-    let current_util = fetch_util(client).await;
+    let test_project_name = "test-suite-project";
 
+    // Create project in test-suite-silo as the test user
+    NexusRequest::objects_post(
+        client,
+        "/v1/projects",
+        &params::ProjectCreate {
+            identity: IdentityMetadataCreateParams {
+                name: test_project_name.parse().unwrap(),
+                description: String::new(),
+            },
+        },
+    )
+    .authn_as(AuthnMode::SiloUser(user1.id))
+    .execute()
+    .await
+    .expect("failed to create project in test-suite-silo");
+
+    // Create instance in test-suite-silo as the test user
+    let instance_params = params::InstanceCreate {
+        identity: IdentityMetadataCreateParams {
+            name: "test-inst".parse().unwrap(),
+            description: "test instance in test-suite-silo".to_string(),
+        },
+        ncpus: InstanceCpuCount::try_from(2).unwrap(),
+        memory: ByteCount::from_gibibytes_u32(4),
+        hostname: "test-inst".parse().unwrap(),
+        user_data: vec![],
+        ssh_public_keys: None,
+        network_interfaces: params::InstanceNetworkInterfaceAttachment::Default,
+        external_ips: vec![],
+        disks: vec![],
+        boot_disk: None,
+        start: true,
+        auto_restart_policy: Default::default(),
+        anti_affinity_groups: Vec::new(),
+    };
+
+    NexusRequest::objects_post(
+        client,
+        &format!("/v1/instances?project={}", test_project_name),
+        &instance_params,
+    )
+    .authn_as(AuthnMode::SiloUser(user1.id))
+    .execute()
+    .await
+    .expect("failed to create instance in test-suite-silo");
+
+    // Check that utilization now shows provisioned resources
+    let current_util = fetch_util(client).await;
+    assert_eq!(current_util.len(), 1);
+    assert_eq!(current_util[0].silo_name, "test-suite-silo");
+    assert_eq!(
+        current_util[0].provisioned,
+        VirtualResourceCounts {
+            cpus: 2,
+            memory: ByteCount::from_gibibytes_u32(4),
+            storage: ByteCount::from(0)
+        }
+    );
+    assert_eq!(
+        current_util[0].allocated,
+        SiloQuotasCreate::arbitrarily_high_default().into()
+    );
+
+    // now we take the quota back off of test-suite-silo and end up empty again
+    let _: SiloQuotas =
+        object_put(client, quotas_url, &params::SiloQuotasCreate::empty())
+            .await;
+
+    let current_util = fetch_util(client).await;
     assert!(current_util.is_empty());
 
     // you can still fetch utilization for the default silo by name if you want,
@@ -95,13 +185,8 @@ async fn test_utilization(cptestctx: &ControlPlaneTestContext) {
     .expect("failed to start instance");
 
     // get utilization for just the default silo
-    let default_silo_util = NexusRequest::object_get(
-        client,
-        "/v1/system/utilization/silos/default-silo",
-    )
-    .authn_as(AuthnMode::PrivilegedUser)
-    .execute_and_parse_unwrap::<SiloUtilization>()
-    .await;
+    let default_silo_util: SiloUtilization =
+        object_get(client, "/v1/system/utilization/silos/default-silo").await;
 
     assert_eq!(
         default_silo_util.provisioned,
@@ -140,13 +225,8 @@ async fn test_utilization(cptestctx: &ControlPlaneTestContext) {
     .expect("disk failed to create");
 
     // Get the silo but this time using the silo admin view
-    let default_silo_util = NexusRequest::object_get(client, "/v1/utilization")
-        .authn_as(AuthnMode::PrivilegedUser)
-        .execute()
-        .await
-        .expect("failed to fetch utilization for current (default) silo")
-        .parsed_body::<Utilization>()
-        .unwrap();
+    let default_silo_util: Utilization =
+        object_get(client, "/v1/utilization").await;
 
     assert_eq!(
         default_silo_util.provisioned,
