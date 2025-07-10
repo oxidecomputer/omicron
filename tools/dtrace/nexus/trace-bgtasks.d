10410439a97e454021806691e846c95c7ad65849
Benjamin Naecker <ben@oxide.computer>
2025-07-10 03:05:42 +0000

    Add USDT probes for Nexus background tasks
    Partially closes #8424

---
+++ tools/dtrace/nexus/trace-bgtasks.d
@@ -0,0 +1,27 @@
+#!/usr/sbin/dtrace -Zqs
+
+nexus*:::background-task-activate-start
+{
+        this->task_name = copyinstr(arg0);
+        iters[this->task_name] = arg1;
+        reasons[this->task_name] = copyinstr(arg2);
+        ts[this->task_name] = timestamp;
+}
+
+nexus*:::background-task-activate-done
+/iters[this->task_name]/
+{
+        this->task_Name = copyinstr(arg0);
+        this->duration = timestamp - ts[this->task_name];
+        printf(
+            "task_name=\"%s\" iteration=%d reason=%s details=%s duration=%dus\n",
+            this->task_name,
+            iters[this->task_name],
+            reasons[this->task_name],
+            copyinstr(arg2),
+            this->duration / 1000
+        );
+        iters[this->task_name] = 0;
+        reasons[this->task_name] = 0;
+        ts[this->task_name] = 0;
+}
