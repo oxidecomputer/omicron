26d6417bfd51627e079a668706a498d3305c7334
Matthew Sanabria <matthew.sanabria@oxide.computer>
2025-07-11 09:54:03 -0400

    nexus: update `switch_port_settings_route_config` schema
    I renamed the `local_pref` column to `rib_priority` to complete the
    rename in https://github.com/oxidecomputer/omicron/pull/6693.
    
    I also changed the type of the renamed column from `INT8` to
    `INT2`, clamping existing values to `0` or `255`. This was missed
    in https://github.com/oxidecomputer/omicron/pull/6693 and led to the
    following error when reading the value from the database.
    
    ```
    {"msg":"request completed","v":0,"name":"omicron-dev","level":30,"time":"2025-07-11T13:57:48.670343242Z","hostname":"ms-ox01","pid":3113,"uri":"/v1/system/networking/switch-port-settings","method":"POST","req_id":"12b35f5a-2255-4244-a5aa-f9c84032fb81","remote_addr":"127.0.0.1:41766","local_addr":"127.0.0.1:12220","component":"dropshot_external","name":"e6bff1ff-24fb-49dc-a54e-c6a350cd4d6c","error_message_external":"Internal Server Error","error_message_internal":"Unknown diesel error creating SwitchPortSettings called \"example\": Error deserializing field 'local_pref': Received more than 2 bytes while decoding an i16. Was an Integer expression accidentally marked as SmallInt?","latency_us":133766,"response_code":500}
    ```
    
    The error occurred because the Rust type was `SqlU8` and the database
    type was `INT8`. Reads would fail because `INT8` columns could not be
    read into `SqlU8` types without loss of precision.
    
    This was caught in
    https://github.com/oxidecomputer/terraform-provider-oxide/pull/426
    when implementing a Terraform provider resource for switch
    port settings. It's likely that this has been broken since
    https://github.com/oxidecomputer/omicron/pull/6693 and any user that
    attempted to set `rib_priority` in their Rack Setup Service (RSS) would
    have encountered this error. However, `rib_priority` is an uncommon
    configuration option and none of our customer's RSS configurations show
    this as being set.
    
    Given this information, it seems safe to assume that no customer has the
    `rib_priority` column set so the clamping logic implemented here should
    work well for customer installations.

--- schema/crdb/dbinit.sql
+++ schema/crdb/dbinit.sql
@@ -3148,7 +3148,7 @@ CREATE TABLE IF NOT EXISTS omicron.public.switch_port_settings_route_config (
     dst INET,
     gw INET,
     vid INT4,
-    local_pref INT8,
+    rib_priority INT2,
 
     /* TODO https://github.com/oxidecomputer/omicron/issues/3013 */
     PRIMARY KEY (port_settings_id, interface_name, dst, gw)
@@ -6209,7 +6209,7 @@ INSERT INTO omicron.public.db_metadata (
     version,
     target_version
 ) VALUES
-    (TRUE, NOW(), NOW(), '164.0.0', NULL)
+    (TRUE, NOW(), NOW(), '165.0.0', NULL)
 ON CONFLICT DO NOTHING;
 
 COMMIT;
---
+++ schema/crdb/route-config-rib-priority/up02.sql
@@ -0,0 +1,9 @@
+SET LOCAL disallow_full_table_scans = off;
+UPDATE omicron.public.switch_port_settings_route_config
+SET rib_priority = 
+    CASE 
+        WHEN local_pref > 255 THEN 255
+        WHEN local_pref < 0 THEN 0
+        ELSE local_pref::INT2
+    END;
+SET LOCAL disallow_full_table_scans = on;
