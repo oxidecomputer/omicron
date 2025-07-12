oxidecomputer/omicron

commit f3ab3387b640de27875502209a55211087e9b23d

Rename static route local_pref to rib_priority (#6693)
This is the omicron half of maghemite#359. This renames the `local_pref`
field of static routes to `rib_priority` to avoid overloading the name
of a standardized and well-known BGP attribute (Local Preference). This
change shrinks the size of the field from u32 -> u8 and unwraps the
Option<T> before shipping routes to mgd, aligning with the updated API.
This also brings omicron back into sync with maghemite.

Fixes: #6711

Signed-off-by: Trey Aspelund <trey@oxidecomputer.com>

---------

Signed-off-by: Trey Aspelund <trey@oxidecomputer.com>
Co-authored-by: Levon Tarver <levon@oxide.computer>

common/src/api/external/mod.rs
@@ -2607,8 +2607,8 @@ pub struct SwitchPortRouteConfig {
     /// over an 802.1Q tagged L2 segment.
     pub vlan_id: Option<u16>,
 
-    /// Local preference indicating priority within and across protocols.
-    pub local_pref: Option<u32>,
+    /// RIB Priority indicating priority within and across protocols.
+    pub rib_priority: Option<u8>,
 }
 
 /*

nexus/db-model/src/switch_port.rs
@@ -559,7 +559,8 @@ pub struct SwitchPortRouteConfig {
     pub dst: IpNetwork,
     pub gw: IpNetwork,
     pub vid: Option<SqlU16>,
-    pub local_pref: Option<SqlU32>,
+    #[diesel(column_name = local_pref)]
+    pub rib_priority: Option<SqlU8>,
 }
 
 impl SwitchPortRouteConfig {
@@ -569,9 +570,9 @@ impl SwitchPortRouteConfig {
         dst: IpNetwork,
         gw: IpNetwork,
         vid: Option<SqlU16>,
-        local_pref: Option<SqlU32>,
+        rib_priority: Option<SqlU8>,
     ) -> Self {
-        Self { port_settings_id, interface_name, dst, gw, vid, local_pref }
+        Self { port_settings_id, interface_name, dst, gw, vid, rib_priority }
     }
 }
 
@@ -583,7 +584,7 @@ impl Into<external::SwitchPortRouteConfig> for SwitchPortRouteConfig {
             dst: self.dst.into(),
             gw: self.gw.into(),
             vlan_id: self.vid.map(Into::into),
-            local_pref: self.local_pref.map(Into::into),
+            rib_priority: self.rib_priority.map(Into::into),
         }
     }
 }

nexus/types/src/external_api/params.rs
@@ -1791,7 +1791,7 @@ pub struct Route {
 
     /// Local preference for route. Higher preference indictes precedence
     /// within and across protocols.
-    pub local_pref: Option<u32>,
+    pub rib_priority: Option<u8>,
 }
 
 /// Select a BGP config by a name or id.

nexus/db-model/src/schema.rs
@@ -188,7 +188,7 @@ table! {
         dst -> Inet,
         gw -> Inet,
         vid -> Nullable<Int4>,
-        local_pref -> Nullable<Int8>,
+        local_pref -> Nullable<Int2>,
     }
 }
 

nexus/db-queries/src/db/datastore/switch_port.rs
@@ -1237,7 +1237,7 @@ async fn do_switch_port_settings_create(
                 route.dst.into(),
                 route.gw.into(),
                 route.vid.map(Into::into),
-                route.local_pref.map(Into::into),
+                route.rib_priority.map(Into::into),
             ));
         }
     }

nexus/tests/integration_tests/switch_port.rs
@@ -148,7 +148,7 @@ async fn test_port_settings_basic_crud(ctx: &ControlPlaneTestContext) {
                 dst: "1.2.3.0/24".parse().unwrap(),
                 gw: "1.2.3.4".parse().unwrap(),
                 vid: None,
-                local_pref: None,
+                rib_priority: None,
             }],
         },
     );

Cargo.toml
@@ -420,10 +420,8 @@ macaddr = { version = "1.0.1", features = ["serde_std"] }
 maplit = "1.0.2"
 mockall = "0.13"
 newtype_derive = "0.1.6"
-# mg-admin-client = { git = "https://github.com/oxidecomputer/maghemite", rev = "9e0fe45ca3862176dc31ad8cc83f605f8a7e1a42" }
-# ddm-admin-client = { git = "https://github.com/oxidecomputer/maghemite", rev = "9e0fe45ca3862176dc31ad8cc83f605f8a7e1a42" }
-mg-admin-client = { git = "https://github.com/oxidecomputer/maghemite", branch = "hyper-v1-no-merge" }
-ddm-admin-client = { git = "https://github.com/oxidecomputer/maghemite", branch = "hyper-v1-no-merge" }
+mg-admin-client = { git = "https://github.com/oxidecomputer/maghemite", rev = "056283eb02b6887fbf27f66a215662520f7c159c" }
+ddm-admin-client = { git = "https://github.com/oxidecomputer/maghemite", rev = "056283eb02b6887fbf27f66a215662520f7c159c" }
 multimap = "0.10.0"
 nexus-auth = { path = "nexus/auth" }
 nexus-client = { path = "clients/nexus-client" }

common/src/api/internal/shared.rs
@@ -306,9 +306,9 @@ pub struct RouteConfig {
     /// The VLAN id associated with this route.
     #[serde(default)]
     pub vlan_id: Option<u16>,
-    /// The local preference associated with this route.
+    /// The RIB priority (i.e. Admin Distance) associated with this route.
     #[serde(default)]
-    pub local_pref: Option<u32>,
+    pub rib_priority: Option<u8>,
 }
 
 #[derive(

commit b40fcbd32d4a392fe3e9bf2c7dbd731a8a789cc9

nexus: update `switch_port_settings_route_config` schema
I renamed the `local_pref` column to `rib_priority` to complete the
rename in https://github.com/oxidecomputer/omicron/pull/6693.

I also changed the type of the renamed column from `INT8` to
`INT2`, clamping existing values to `0` or `255`. This was missed
in https://github.com/oxidecomputer/omicron/pull/6693 and led to the
following error when reading the value from the database.

```
{"msg":"request completed","v":0,"name":"omicron-dev","level":30,"time":"2025-07-11T13:57:48.670343242Z","hostname":"ms-ox01","pid":3113,"uri":"/v1/system/networking/switch-port-settings","method":"POST","req_id":"12b35f5a-2255-4244-a5aa-f9c84032fb81","remote_addr":"127.0.0.1:41766","local_addr":"127.0.0.1:12220","component":"dropshot_external","name":"e6bff1ff-24fb-49dc-a54e-c6a350cd4d6c","error_message_external":"Internal Server Error","error_message_internal":"Unknown diesel error creating SwitchPortSettings called \"example\": Error deserializing field 'local_pref': Received more than 2 bytes while decoding an i16. Was an Integer expression accidentally marked as SmallInt?","latency_us":133766,"response_code":500}
```

The error occurred because the Rust type was `SqlU8` and the database
type was `INT8`. Reads would fail because `INT8` columns could not be
read into `SqlU8` types without loss of precision.

This was caught in
https://github.com/oxidecomputer/terraform-provider-oxide/pull/426
when implementing a Terraform provider resource for switch
port settings. It's likely that this has been broken since
https://github.com/oxidecomputer/omicron/pull/6693 and any user that
attempted to set `rib_priority` in their Rack Setup Service (RSS) would
have encountered this error. However, `rib_priority` is an uncommon
configuration option and none of our customer's RSS configurations show
this as being set.

Given this information, it seems safe to assume that no customer has the

common/src/api/external/mod.rs
@@ -2998,7 +2998,8 @@ pub struct SwitchPortRouteConfig {
     /// over an 802.1Q tagged L2 segment.
     pub vlan_id: Option<u16>,
 
-    /// RIB Priority indicating priority within and across protocols.
+    /// Route RIB priority. Higher priority indicates precedence within and across
+    /// protocols.
     pub rib_priority: Option<u8>,
 }
 

nexus/db-model/src/schema_versions.rs
@@ -16,7 +16,7 @@ use std::{collections::BTreeMap, sync::LazyLock};
 ///
 /// This must be updated when you change the database schema.  Refer to
 /// schema/crdb/README.adoc in the root of this repository for details.
-pub const SCHEMA_VERSION: Version = Version::new(161, 0, 0);
+pub const SCHEMA_VERSION: Version = Version::new(162, 0, 0);
 
 /// List of all past database schema versions, in *reverse* order
 ///
@@ -28,6 +28,7 @@ static KNOWN_VERSIONS: LazyLock<Vec<KnownVersion>> = LazyLock::new(|| {
         // |  leaving the first copy as an example for the next person.
         // v
         // KnownVersion::new(next_int, "unique-dirname-with-the-sql-files"),
+        KnownVersion::new(162, "route-config-rib-priority"),
         KnownVersion::new(161, "inv_cockroachdb_status"),
         KnownVersion::new(160, "tuf-trust-root"),
         KnownVersion::new(159, "sled-config-desired-host-phase-2"),

nexus/db-model/src/switch_port.rs
@@ -609,7 +609,6 @@ pub struct SwitchPortRouteConfig {
     pub dst: IpNetwork,
     pub gw: IpNetwork,
     pub vid: Option<SqlU16>,
-    #[diesel(column_name = local_pref)]
     pub rib_priority: Option<SqlU8>,
 }
 

nexus/db-schema/src/schema.rs
@@ -202,7 +202,7 @@ table! {
         dst -> Inet,
         gw -> Inet,
         vid -> Nullable<Int4>,
-        local_pref -> Nullable<Int2>,
+        rib_priority -> Nullable<Int2>,
     }
 }
 

nexus/types/src/external_api/params.rs
@@ -2003,8 +2003,8 @@ pub struct Route {
     /// VLAN id the gateway is reachable over.
     pub vid: Option<u16>,
 
-    /// Local preference for route. Higher preference indictes precedence
-    /// within and across protocols.
+    /// Route RIB priority. Higher priority indicates precedence within and across
+    /// protocols.
     pub rib_priority: Option<u8>,
 }
 

schema/crdb/dbinit.sql
@@ -3144,7 +3144,7 @@ CREATE TABLE IF NOT EXISTS omicron.public.switch_port_settings_route_config (
     dst INET,
     gw INET,
     vid INT4,
-    local_pref INT8,
+    rib_priority INT2,
 
     /* TODO https://github.com/oxidecomputer/omicron/issues/3013 */
     PRIMARY KEY (port_settings_id, interface_name, dst, gw)
@@ -6198,7 +6198,7 @@ INSERT INTO omicron.public.db_metadata (
     version,
     target_version
 ) VALUES
-    (TRUE, NOW(), NOW(), '161.0.0', NULL)
+    (TRUE, NOW(), NOW(), '162.0.0', NULL)
 ON CONFLICT DO NOTHING;
 
 COMMIT;

schema/crdb/route-config-rib-priority/up01.sql
@@ -1,0 +1,1 @@
+ALTER TABLE omicron.public.switch_port_settings_route_config ADD COLUMN rib_priority INT2;

schema/crdb/route-config-rib-priority/up02.sql
@@ -1,0 +1,7 @@
+UPDATE omicron.public.switch_port_settings_route_config
+SET rib_priority = 
+    CASE 
+        WHEN local_pref > 255 THEN 255
+        WHEN local_pref < 0 THEN 0
+        ELSE local_pref::INT2
+    END;

schema/crdb/route-config-rib-priority/up03.sql
@@ -1,0 +1,1 @@
+ALTER TABLE omicron.public.switch_port_settings_route_config DROP COLUMN local_pref;