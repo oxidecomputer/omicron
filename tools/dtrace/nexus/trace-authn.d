a68d33e166a3558e438107ee07452722a2e5e5c5
Benjamin Naecker <ben@oxide.computer>
2025-07-28 20:05:16 +0000

    Add examples to D scripts

--- tools/dtrace/nexus/trace-authn.d
+++ tools/dtrace/nexus/trace-authn.d
@@ -1,5 +1,17 @@
 #!/usr/sbin/dtrace -qZs
 
+/*
+ * This script prints the result of every authentication request. It
+ * includes the scheme, method, URI, duration, and the result of the
+ * authn attempt. Here is an example of the output:
+ * 
+ * scheme=spoof method=POST URI=/v1/system/ip-pools/default/silos result=Authenticated(Details { actor: Actor::SiloUser { silo_user_id: 001de000-05e4-4000-8000-000000004007, silo_id: 001de000-5110-4000-8000-000000000000, .. } }) duration=23961us
+ * scheme=spoof method=POST URI=/v1/projects result=Authenticated(Details { actor: Actor::SiloUser { silo_user_id: 001de000-05e4-4000-8000-000000004007, silo_id: 001de000-5110-4000-8000-000000000000, .. } }) duration=31087us
+ * scheme=spoof method=DELETE URI=/v1/disks/disky-mcdiskface?project=springfield-squidport result=Authenticated(Details { actor: Actor::SiloUser { silo_user_id: 001de000-05e4-4000-8000-000000004007, silo_id: 001de000-5110-4000-8000-000000000000, .. } }) duration=1033288us
+ * scheme=spoof method=POST URI=/v1/instances?project=carcosa result=Authenticated(Details { actor: Actor::SiloUser { silo_user_id: 001de000-05e4-4000-8000-000000004007, silo_id: 001de000-5110-4000-8000-000000000000, .. } }) duration=14631us
+ * scheme=spoof method=POST URI=/v1/disks?project=springfield-squidport result=Authenticated(Details { actor: Actor::SiloUser { silo_user_id: 001de000-05e4-4000-8000-000000004007, silo_id: 001de000-5110-4000-8000-000000000000, .. } }) duration=25946us
+ */
+
 #pragma D option strsize=4k
 
 nexus*:::authn-start
--- tools/dtrace/nexus/trace-authz.d
+++ tools/dtrace/nexus/trace-authz.d
@@ -1,5 +1,17 @@
 #!/usr/sbin/dtrace -qZs
 
+/*
+ * This script prints the result of every authorization request. It
+ * includes the actor, action, and resource, along with the duration
+ * and result of the authz attempt. Here is an example of the output:
+ *
+ * actor=Some(Actor::SiloUser { silo_user_id: 8b0bc366-958e-43af-a35c-8cb6867a6939, silo_id: 37ab44ad-213c-482d-acf4-98e6a978199d, .. }) action=Query resource=Database result=Ok(()) duration=1351us
+ * actor=Some(Actor::SiloUser { silo_user_id: 001de000-05e4-4000-8000-000000004007, silo_id: 001de000-5110-4000-8000-000000000000, .. }) action=Query resource=Database result=Ok(()) duration=657us
+ * actor=Some(Actor::SiloUser { silo_user_id: 001de000-05e4-4000-8000-000000004007, silo_id: 001de000-5110-4000-8000-000000000000, .. }) action=Query resource=Database result=Ok(()) duration=1640us
+ * actor=Some(Actor::UserBuiltin { user_builtin_id: 001de000-05e4-4000-8000-000000000002, .. }) action=Read resource=DnsConfig result=Ok(()) duration=12268us
+ *
+ */
+
 #pragma D option strsize=4k
 
 nexus*:::authz-start
