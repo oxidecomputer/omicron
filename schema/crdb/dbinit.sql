0bf5831079bd07407959261feb809621713d6961
John Gallagher <john@oxidecomputer.com>
2025-08-04 12:07:56 -0400

    database migration

--- schema/crdb/dbinit.sql
+++ schema/crdb/dbinit.sql
@@ -6524,7 +6524,7 @@ INSERT INTO omicron.public.db_metadata (
     version,
     target_version
 ) VALUES
-    (TRUE, NOW(), NOW(), '178.0.0', NULL)
+    (TRUE, NOW(), NOW(), '179.0.0', NULL)
 ON CONFLICT DO NOTHING;
 
 COMMIT;
