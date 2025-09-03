c79764437569737d4653f04cf90c44436af727d1
Benjamin Naecker <ben@oxide.computer>
2025-09-03 15:00:57 -0700

    Initial schema updates disallowing default IP Pool for the internal silo (#8964)
    - Add database constraints that ensure that we can't set a default pool
    for the Oxide internal silo. Add tests for this specifically.
    - This is part of #8948, but does not resolve it.

--- schema/crdb/dbinit.sql
+++ schema/crdb/dbinit.sql
@@ -2110,7 +2110,17 @@ CREATE TABLE IF NOT EXISTS omicron.public.ip_pool_resource (
 
     -- resource_type is redundant because resource IDs are globally unique, but
     -- logically it belongs here
-    PRIMARY KEY (ip_pool_id, resource_type, resource_id)
+    PRIMARY KEY (ip_pool_id, resource_type, resource_id),
+
+    -- Check that there are no default pools for the internal silo
+    CONSTRAINT internal_silo_has_no_default_pool CHECK (
+        NOT (
+            resource_type = 'silo' AND
+            resource_id = '001de000-5110-4000-8000-000000000001' AND
+            is_default
+        )
+    )
+
 );
 
 -- a given resource can only have one default ip pool
@@ -2123,6 +2133,7 @@ CREATE UNIQUE INDEX IF NOT EXISTS one_default_ip_pool_per_resource ON omicron.pu
 CREATE INDEX IF NOT EXISTS ip_pool_resource_id ON omicron.public.ip_pool_resource (
     resource_id
 );
+
 CREATE INDEX IF NOT EXISTS ip_pool_resource_ip_pool_id ON omicron.public.ip_pool_resource (
     ip_pool_id
 );
@@ -6602,7 +6613,7 @@ INSERT INTO omicron.public.db_metadata (
     version,
     target_version
 ) VALUES
-    (TRUE, NOW(), NOW(), '186.0.0', NULL)
+    (TRUE, NOW(), NOW(), '187.0.0', NULL)
 ON CONFLICT DO NOTHING;
 
 COMMIT;
