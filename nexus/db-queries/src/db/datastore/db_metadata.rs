a95c1c30d7759d6c6bf833a898ae0ff67eb0031b
Sean Klein <sean@oxidecomputer.com>
2025-09-16 14:00:04 -0700

    Add omdb command to display db_metadata_nexus_state

--- nexus/db-queries/src/db/datastore/db_metadata.rs
+++ nexus/db-queries/src/db/datastore/db_metadata.rs
@@ -300,6 +300,23 @@ impl DatastoreSetupAction {
 }
 
 impl DataStore {
+    /// Returns [`DbMetadataNexus`] records in any of the supplied states.
+    pub async fn get_db_metadata_nexus_in_state(
+        &self,
+        opctx: &OpContext,
+        states: &[DbMetadataNexusState],
+    ) -> Result<Vec<DbMetadataNexus>, Error> {
+        use nexus_db_schema::schema::db_metadata_nexus::dsl;
+
+        opctx.authorize(authz::Action::Read, &authz::FLEET).await?;
+
+        dsl::db_metadata_nexus
+            .filter(dsl::state.eq_any(states.to_vec()))
+            .load_async(&*self.pool_connection_authorized(&opctx).await?)
+            .await
+            .map_err(|e| public_error_from_diesel(e, ErrorHandler::Server))
+    }
+
     // Checks if the specified Nexus has access to the database.
     async fn check_nexus_access(
         &self,
