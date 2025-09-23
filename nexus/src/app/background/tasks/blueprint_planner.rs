2cb7a2c44deb3c71c7573f920d2ec35ed6b3e627
Rain <rain@oxide.computer>
2025-09-23 00:46:14 -0700

    [5/n] [reconfigurator-planning] gate adds/updates on zone image sources being known (#8921)
    Expand the set of gates for adds/updates to include the fact that zone
    image sources should be known. Add tests for this:
    
    * `cmds-mupdate-update-flow` contains the bulk of testing for this
    scenario.
    * I had to make tweaks to some tests, particularly to
    `cmds-target-release.txt`, in order to start running the test in earnest
    from the Artifact state rather than the InstallDataset state.

--- nexus/src/app/background/tasks/blueprint_planner.rs
+++ nexus/src/app/background/tasks/blueprint_planner.rs
@@ -359,7 +359,14 @@ mod test {
                 version: 1,
                 config: ReconfiguratorConfig {
                     planner_enabled: true,
-                    planner_config: PlannerConfig::default(),
+                    planner_config: PlannerConfig {
+                        // Set this config to true because we'd like to test
+                        // adding zones even if no target release is set. In the
+                        // future, we'll allow adding zones if no target release
+                        // has ever been set, in which case we can go back to
+                        // setting this field to false.
+                        add_zones_with_mupdate_override: true,
+                    },
                 },
                 time_modified: now_db_precision(),
             }),
