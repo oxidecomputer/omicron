887bc421f876a760a050771b20cf3272e463448c
Ryan Goodfellow <ryan.goodfellow@oxide.computer>
2025-09-29 19:42:31 -0700

    pick up dendrite #136, #137, #134, #140, maghemite #554 (#9086)

--- nexus/src/app/background/tasks/bfd.rs
+++ nexus/src/app/background/tasks/bfd.rs
@@ -14,6 +14,7 @@ use crate::app::background::BackgroundTask;
 use futures::FutureExt;
 use futures::future::BoxFuture;
 use internal_dns_resolver::Resolver;
+use mg_admin_client::ClientInfo;
 use mg_admin_client::types::{BfdPeerConfig, SessionMode};
 use nexus_db_model::{BfdMode, BfdSession};
 use nexus_db_queries::{context::OpContext, db::DataStore};
