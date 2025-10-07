44a947c2baa684130b02be40c158758003bfb5ad
Benjamin Naecker <ben@oxide.computer>
2025-10-07 21:54:32 +0000

    More typos

--- schema/crdb/add-ip-pool-reservation-type-column/up03.sql
+++ schema/crdb/add-ip-pool-reservation-type-column/up03.sql
@@ -1,4 +1,4 @@
 SET LOCAL disallow_full_table_scans = 'off';
 UPDATE ip_pool
-SET ip_pool_delgation_type = 'oxide_internal'
+SET reservation_type = 'oxide_internal'
 WHERE name = 'oxide-service-pool-v4' OR name = 'oxide-service-pool-v6';
