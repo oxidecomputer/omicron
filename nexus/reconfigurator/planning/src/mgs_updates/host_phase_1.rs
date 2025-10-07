9cfaf9e425ed545f7af6f7b6c08c3ec594514bed
karencfv <karencfv@users.noreply.github.com>
2025-10-07 13:37:51 +1300

    style nit

--- nexus/reconfigurator/planning/src/mgs_updates/host_phase_1.rs
+++ nexus/reconfigurator/planning/src/mgs_updates/host_phase_1.rs
@@ -430,22 +430,18 @@ pub(super) fn try_make_update(
     // Make sure the board we're targetting doesn't contain any zones that are
     // unsafe to shut down
     if unsafe_zone_boards.contains_key(baseboard_id) {
-        let zone_str = match unsafe_zone_boards.get(baseboard_id) {
-            Some(board) => {
-                let mut s = String::new();
-                for (zone_id, zone) in board {
-                    // TODO-K: Fix multiline here
-                    s.push_str(&format!("* {}: {}", zone_id, zone));
-                }
-                s
-            }
-            // All unsafe zones boards should contain which zones are unsafe
-            // in the unlikely case that they don't, we just pass an empty
-            // string as this is meant for informational purposes only. There
-            // is no need to bail out with a different error because of this
-            None => "".to_string(),
-        };
+        let mut zone_str = String::new();
 
+        // All unsafe zones boards should contain which zones are unsafe. In the
+        // unlikely case that they don't, we just pass an empty string as this
+        // is meant for informational purposes only. There is no need to bail
+        // out with a different error because of this
+        if let Some(board) = unsafe_zone_boards.get(baseboard_id) {
+            for (zone_id, zone) in board {
+                // TODO-K: Fix multiline here
+                zone_str.push_str(&format!("* {}: {}", zone_id, zone));
+            }
+        };
         return Err(FailedHostOsUpdateReason::UnsafeZoneFound(zone_str));
     }
 
