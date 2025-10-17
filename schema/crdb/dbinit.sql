6f411311509da6bfd45928a6a0275ed9f3071c57
Charlie Park <charlie@oxidecomputer.com>
2025-10-17 10:11:54 -0700

    Update version number in dbint.sql

--- schema/crdb/dbinit.sql
+++ schema/crdb/dbinit.sql
@@ -6794,7 +6794,7 @@ INSERT INTO omicron.public.db_metadata (
     version,
     target_version
 ) VALUES
-    (TRUE, NOW(), NOW(), '199.0.0', NULL)
+    (TRUE, NOW(), NOW(), '200.0.0', NULL)
 ON CONFLICT DO NOTHING;
 
 COMMIT;
