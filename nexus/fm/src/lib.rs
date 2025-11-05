d41781e0beec08311a08719d2995f58d7cedbee2
Eliza Weisman <eliza@elizas.website>
2025-11-05 09:20:46 -0800

    well here's something

--- nexus/fm/src/lib.rs
+++ nexus/fm/src/lib.rs
@@ -6,8 +6,37 @@
 
 use nexus_types::fm;
 use nexus_types::inventory;
+use omicron_uuid_kinds::OmicronZoneUuid;
+use omicron_uuid_kinds::SitrepUuid;
 
-pub struct DiagnosisInput<'a> {
+#[derive(Debug)]
+pub struct SitrepBuilder<'a> {
     inventory: &'a inventory::Collection,
     parent_sitrep: Option<&'a fm::Sitrep>,
+    comment: String,
+}
+
+impl<'a> SitrepBuilder<'a> {
+    pub fn new(
+        inventory: &'a inventory::Collection,
+        parent_sitrep: Option<&'a fm::Sitrep>,
+    ) -> Self {
+        SitrepBuilder { inventory, parent_sitrep, comment: String::new() }
+    }
+    
+    
+
+    pub fn build(self, creator_id: OmicronZoneUuid) -> fm::Sitrep {
+        fm::Sitrep {
+            metadata: fm::SitrepMetadata {
+                id: SitrepUuid::new_v4(),
+                parent_sitrep_id: self.parent_sitrep.map(|s| s.metadata.id),
+                inv_collection_id: self.inventory.id,
+                creator_id,
+                comment: self.comment,
+                time_created: chrono::Utc::now(),
+            },
+            // TODO(eliza): draw the rest of the owl...
+        }
+    }
 }
