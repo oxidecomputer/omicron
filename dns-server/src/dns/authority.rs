958f3743aaa32de197f7abaaf9bd61e7cce74cf7
David Pacheco <dap@oxidecomputer.com>
2025-11-13 10:36:40 -0800

    async_trait

--- dns-server/src/dns/authority.rs
+++ dns-server/src/dns/authority.rs
@@ -8,6 +8,7 @@
 //! Store-based DNS data with hickory-server's high-level server infrastructure.
 
 use crate::storage::{QueryError, Store};
+use async_trait::async_trait;
 use hickory_proto::rr::rdata::{NS, SRV};
 use hickory_proto::rr::{LowerName, Name, RData, Record, RecordType};
 use hickory_server::authority::{
@@ -17,9 +18,7 @@ use hickory_server::authority::{
 use hickory_server::server::RequestInfo;
 use internal_dns_types::config::{DnsRecord, Srv};
 use slog::{Logger, debug, warn};
-use std::future::Future;
 use std::io;
-use std::pin::Pin;
 use std::str::FromStr;
 
 /// Omicron's Authority implementation that wraps our Store
@@ -47,8 +46,6 @@ impl OmicronAuthority {
     }
 
     /// Convert a DnsRecord to a hickory Record
-    ///
-    /// This is essentially the same logic as the old dns_record_to_record function.
     fn dns_record_to_record(
         name: &Name,
         record: &DnsRecord,
@@ -151,10 +148,12 @@ impl LookupObject for OmicronLookup {
         self.records.is_empty()
     }
 
+    // XXX-dap why do this when it already impls Iterator?
     fn iter<'a>(&'a self) -> Box<dyn Iterator<Item = &'a Record> + Send + 'a> {
         Box::new(self.records.iter())
     }
 
+    // XXX-dap this seems like a weird thing to have
     fn take_additionals(&mut self) -> Option<Box<dyn LookupObject>> {
         // Return additional records if we have any
         self.additionals.take().map(|additionals| {
@@ -163,6 +162,7 @@ impl LookupObject for OmicronLookup {
     }
 }
 
+#[async_trait]
 impl Authority for OmicronAuthority {
     type Lookup = OmicronLookup;
 
@@ -175,198 +175,150 @@ impl Authority for OmicronAuthority {
         false
     }
 
-    fn update<'life0, 'life1, 'async_trait>(
-        &'life0 self,
-        _update: &'life1 MessageRequest,
-    ) -> Pin<
-        Box<
-            dyn Future<Output = Result<bool, hickory_proto::op::ResponseCode>>
-                + Send
-                + 'async_trait,
-        >,
-    >
-    where
-        'life0: 'async_trait,
-        'life1: 'async_trait,
-        Self: 'async_trait,
-    {
+    async fn update(
+        &self,
+        _update: &MessageRequest,
+    ) -> Result<bool, hickory_proto::op::ResponseCode> {
         // Dynamic DNS updates are handled via HTTP API, not DNS protocol
-        Box::pin(async { Err(hickory_proto::op::ResponseCode::NotImp) })
+        Err(hickory_proto::op::ResponseCode::NotImp)
     }
 
     fn origin(&self) -> &LowerName {
         &self.origin
     }
 
-    fn lookup<'life0, 'life1, 'async_trait>(
-        &'life0 self,
-        name: &'life1 LowerName,
+    async fn lookup(
+        &self,
+        name: &LowerName,
         rtype: RecordType,
         _lookup_options: LookupOptions,
-    ) -> Pin<
-        Box<
-            dyn Future<Output = LookupControlFlow<Self::Lookup>>
-                + Send
-                + 'async_trait,
-        >,
-    >
-    where
-        'life0: 'async_trait,
-        'life1: 'async_trait,
-        Self: 'async_trait,
-    {
+    ) -> LookupControlFlow<Self::Lookup> {
         let store = self.store.clone();
         let name = name.clone();
         let log = self.log.clone();
         let origin = self.origin.clone();
 
-        Box::pin(async move {
-            // Convert LowerName to Name
-            let query_name: Name = name.into();
+        // Convert LowerName to Name
+        let query_name: Name = name.into();
 
-            debug!(&log, "authority lookup";
-                "name" => ?query_name,
-                "rtype" => ?rtype,
-                "origin" => ?origin,
-            );
+        debug!(&log, "authority lookup";
+            "name" => ?query_name,
+            "rtype" => ?rtype,
+            "origin" => ?origin,
+        );
 
-            // Query the store using query_name directly
-            let answer = match store.query_name(&query_name) {
-                Ok(answer) => answer,
-                Err(QueryError::NoZone(zone)) => {
-                    debug!(&log, "no zone for query";
-                        "name" => ?query_name,
-                        "zone" => &zone,
-                    );
-                    // Not our zone - return empty
-                    return LookupControlFlow::Break(
-                        Ok(OmicronLookup::empty()),
-                    );
+        // Query the store using query_name directly
+        let answer = match store.query_name(&query_name) {
+            Ok(answer) => answer,
+            Err(QueryError::NoZone(zone)) => {
+                debug!(&log, "no zone for query";
+                    "name" => ?query_name,
+                    "zone" => &zone,
+                );
+                // Not our zone - return empty
+                return LookupControlFlow::Break(Ok(OmicronLookup::empty()));
+            }
+            Err(e) => {
+                warn!(&log, "query failed"; "error" => ?e);
+                return LookupControlFlow::Break(Err(LookupError::Io(
+                    io::Error::new(
+                        io::ErrorKind::Other,
+                        format!("query failed: {:#}", e),
+                    ),
+                )));
+            }
+        };
+
+        // Convert DnsRecords to hickory Records
+        let mut records: Vec<Record> = Vec::new();
+
+        // Add regular records
+        if let Some(ref dns_records) = answer.records {
+            for dns_record in dns_records {
+                match Self::dns_record_to_record(&query_name, dns_record) {
+                    Ok(record) => records.push(record),
+                    Err(e) => {
+                        warn!(&log, "failed to convert record"; "error" => ?e);
+                    }
                 }
+            }
+        }
+
+        // Handle SOA queries at the apex
+        if answer.name.is_none() && rtype == RecordType::SOA {
+            match store.soa_for(&answer) {
+                Ok(soa_record) => records.push(soa_record),
                 Err(e) => {
-                    warn!(&log, "query failed"; "error" => ?e);
-                    return LookupControlFlow::Break(Err(LookupError::Io(
-                        io::Error::new(
-                            io::ErrorKind::Other,
-                            format!("query failed: {:#}", e),
-                        ),
-                    )));
+                    warn!(&log, "failed to generate SOA"; "error" => ?e);
                 }
+            }
+        }
+
+        // Filter records by type
+        let filtered_records: Vec<Record> = records
+            .into_iter()
+            .filter(|record| match rtype {
+                RecordType::ANY => true,
+                RecordType::A => matches!(record.data(), RData::A(_)),
+                RecordType::AAAA => matches!(record.data(), RData::AAAA(_)),
+                RecordType::SRV => matches!(record.data(), RData::SRV(_)),
+                RecordType::NS => matches!(record.data(), RData::NS(_)),
+                RecordType::SOA => matches!(record.data(), RData::SOA(_)),
+                _ => false,
+            })
+            .collect();
+
+        // Handle additional records for SRV and NS
+        let mut additional_records: Vec<Record> = Vec::new();
+        for record in &filtered_records {
+            let target = match record.data() {
+                RData::SRV(srv) => Some(srv.target()),
+                RData::NS(ns) => Some(&ns.0),
+                _ => None,
             };
 
-            // Convert DnsRecords to hickory Records
-            let mut records: Vec<Record> = Vec::new();
-
-            // Add regular records
-            if let Some(ref dns_records) = answer.records {
-                for dns_record in dns_records {
-                    match Self::dns_record_to_record(&query_name, dns_record) {
-                        Ok(record) => records.push(record),
-                        Err(e) => {
-                            warn!(&log, "failed to convert record"; "error" => ?e);
-                        }
-                    }
-                }
-            }
-
-            // Handle SOA queries at the apex
-            if answer.name.is_none() && rtype == RecordType::SOA {
-                match store.soa_for(&answer) {
-                    Ok(soa_record) => records.push(soa_record),
-                    Err(e) => {
-                        warn!(&log, "failed to generate SOA"; "error" => ?e);
-                    }
-                }
-            }
-
-            // Filter records by type
-            let filtered_records: Vec<Record> = records
-                .into_iter()
-                .filter(|record| match rtype {
-                    RecordType::ANY => true,
-                    RecordType::A => matches!(record.data(), RData::A(_)),
-                    RecordType::AAAA => matches!(record.data(), RData::AAAA(_)),
-                    RecordType::SRV => matches!(record.data(), RData::SRV(_)),
-                    RecordType::NS => matches!(record.data(), RData::NS(_)),
-                    RecordType::SOA => matches!(record.data(), RData::SOA(_)),
-                    _ => false,
-                })
-                .collect();
-
-            // Handle additional records for SRV and NS
-            let mut additional_records: Vec<Record> = Vec::new();
-            for record in &filtered_records {
-                let target = match record.data() {
-                    RData::SRV(srv) => Some(srv.target()),
-                    RData::NS(ns) => Some(&ns.0),
-                    _ => None,
-                };
-
-                if let Some(target) = target {
-                    if let Ok(target_answer) = store.query_name(target) {
-                        if let Some(target_records) = target_answer.records {
-                            for target_record in &target_records {
-                                if let Ok(record) = Self::dns_record_to_record(
-                                    target,
-                                    target_record,
-                                ) {
-                                    additional_records.push(record);
-                                }
+            if let Some(target) = target {
+                if let Ok(target_answer) = store.query_name(target) {
+                    if let Some(target_records) = target_answer.records {
+                        for target_record in &target_records {
+                            if let Ok(record) =
+                                Self::dns_record_to_record(target, target_record)
+                            {
+                                additional_records.push(record);
                             }
                         }
                     }
                 }
             }
+        }
 
-            // Return answers and additional records separately
-            LookupControlFlow::Break(Ok(OmicronLookup::with_additionals(
-                filtered_records,
-                additional_records,
-            )))
-        })
+        // Return answers and additional records separately
+        LookupControlFlow::Break(Ok(OmicronLookup::with_additionals(
+            filtered_records,
+            additional_records,
+        )))
     }
 
-    fn search<'life0, 'life1, 'async_trait>(
-        &'life0 self,
-        request: RequestInfo<'life1>,
+    async fn search(
+        &self,
+        request: RequestInfo<'_>,
         lookup_options: LookupOptions,
-    ) -> Pin<
-        Box<
-            dyn Future<Output = LookupControlFlow<Self::Lookup>>
-                + Send
-                + 'async_trait,
-        >,
-    >
-    where
-        'life0: 'async_trait,
-        'life1: 'async_trait,
-        Self: 'async_trait,
-    {
+    ) -> LookupControlFlow<Self::Lookup> {
         // Delegate to lookup
-        Box::pin(self.lookup(
+        self.lookup(
             request.query.name(),
             request.query.query_type(),
             lookup_options,
-        ))
+        )
+        .await
     }
 
-    fn get_nsec_records<'life0, 'life1, 'async_trait>(
-        &'life0 self,
-        _name: &'life1 LowerName,
+    async fn get_nsec_records(
+        &self,
+        _name: &LowerName,
         _lookup_options: LookupOptions,
-    ) -> Pin<
-        Box<
-            dyn Future<Output = LookupControlFlow<Self::Lookup>>
-                + Send
-                + 'async_trait,
-        >,
-    >
-    where
-        'life0: 'async_trait,
-        'life1: 'async_trait,
-        Self: 'async_trait,
-    {
+    ) -> LookupControlFlow<Self::Lookup> {
         // DNSSEC not supported
-        Box::pin(async { LookupControlFlow::Break(Ok(OmicronLookup::empty())) })
+        LookupControlFlow::Break(Ok(OmicronLookup::empty()))
     }
 }
