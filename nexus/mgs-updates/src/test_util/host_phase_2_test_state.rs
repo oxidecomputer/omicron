fe50b8fff1295d9a773e1bbceb2084a7892f05d2
John Gallagher <john@oxidecomputer.com>
2025-11-17 13:08:48 -0500

    test fix

--- nexus/mgs-updates/src/test_util/host_phase_2_test_state.rs
+++ nexus/mgs-updates/src/test_util/host_phase_2_test_state.rs
@@ -127,7 +127,7 @@ impl HostPhase2TestContext {
             .version_policy(dropshot::VersionPolicy::Dynamic(Box::new(
                 dropshot::ClientSpecifiesVersionInHeader::new(
                     omicron_common::api::VERSION_HEADER,
-                    sled_agent_api::VERSION_MULTICAST_SUPPORT,
+                    sled_agent_api::latest_version(),
                 ),
             )))
             .start()
