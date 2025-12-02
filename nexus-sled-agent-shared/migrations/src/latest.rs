4d82c64f1e1d3dd259d50446423d35466f9e1cf4
Andrew J. Stone <andrew@oxidecomputer.com>
2025-12-02 00:57:58 +0000

    wip

--- nexus-sled-agent-shared/migrations/src/latest.rs
+++ nexus-sled-agent-shared/migrations/src/latest.rs
@@ -7,7 +7,22 @@
 use crate::v1;
 use crate::v10;
 
+// Initial types moved from `inventory.rs`
+pub type SledRole = v1::SledRole;
 pub type BootPartitionContents = v1::BootPartitionContents;
+pub type BootImageHeader = v1::BootImageHeader;
+pub type InventoryDisk = v1::InventoryDisk;
+pub type InventoryDataset = v1::InventoryDataset;
+pub type InventoryZpool = v1::InventoryZpool;
+pub type OrphanedDataset = v1::OrphanedDataset;
+pub type RemoveMupdateOverrideInventory = v1::RemoveMupdateOverrideInventory;
+pub type ConfigReconcilerInventoryResult = v1::ConfigReconcilerInventoryResult;
+pub type ZoneImageResolverInventory = v1::ZoneImageResolverInventory;
+pub type HostPhase2DesiredSlots = v1::HostPhase2DesiredSlots;
+pub type HostPhase2DesiredContents = v1::HostPhase2DesiredContents;
+pub type OmicronZoneDataset = v1::OmicronZoneDataset;
+
+// Types updated in v10
 pub type Inventory = v10::Inventory;
 pub type OmicronSledConfig = v10::OmicronSledConfig;
 pub type ConfigReconcilerInventory = v10::ConfigReconcilerInventory;
