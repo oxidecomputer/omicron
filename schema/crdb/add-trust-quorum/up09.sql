8ed5fc37c962fa223e3c35cd8c084ceac5c7807d
Andrew J. Stone <andrew@oxidecomputer.com>
2025-12-04 23:58:40 +0000

    Add CRDB schema changes required for trust quorum

---
+++ schema/crdb/add-trust-quorum/up09.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS omicron.public.trust_quorum_acked_prepare (
+    -- Foreign key into the rack table
+    -- Foreign key into the `trust_quorum_configuration` table along with `epoch`
+    rack_id UUID NOT NULL,
+
+    -- Foreign key into the `trust_quorum_configuration` table along with `rack_id`
+    epoch INT8 PRIMARY KEY,
+
+    -- Foreign key into the `hw_baseboard_id` table
+    hw_baseboard_id UUID NOT NULL
+);
