667e46fdd6b9635f6f0363eee6a5be97c0490160
Eliza Weisman <eliza@elizas.website>
2025-12-05 12:33:31 -0800

    use shared ereport parsing logic in test infra

--- nexus/fm/src/test_util.rs
+++ nexus/fm/src/test_util.rs
@@ -14,6 +14,7 @@ use omicron_uuid_kinds::EreporterRestartUuid;
 use omicron_uuid_kinds::OmicronZoneKind;
 use omicron_uuid_kinds::OmicronZoneUuid;
 use rand::rngs::StdRng;
+use slog::Logger;
 use typed_rng::TypedUuidRng;
 
 pub struct FmTest {
@@ -28,9 +29,13 @@ impl FmTest {
         let logctx = dev::test_setup_log(test_name);
         let example_system_builder =
             example::ExampleSystemBuilder::new(&logctx.log, test_name);
+        let reporters = SimReporters::new(
+            test_name,
+            logctx.log.new(slog::o!("component" => "sim-reporters")),
+        );
         Self {
             logctx,
-            reporters: SimReporters::new(test_name),
+            reporters,
             sitrep_rng: SitrepBuilderRng::from_seed(test_name),
             system_builder: example_system_builder,
         }
@@ -38,19 +43,20 @@ impl FmTest {
 }
 
 pub struct SimReporters {
+    log: slog::Logger,
     parent: StdRng,
     collector_id_rng: TypedUuidRng<OmicronZoneKind>,
 }
 
 impl SimReporters {
-    fn new(test_name: &str) -> Self {
+    fn new(test_name: &str, log: slog::Logger) -> Self {
         let mut parent = typed_rng::from_seed(test_name, "sim-reporters");
         // TODO(eliza): would be more realistic to pick something from the
         // example system's omicron zones, but these UUIDs are only used for
         // debugging purposes...
         let collector_id_rng =
             TypedUuidRng::from_parent_rng(&mut parent, "collector-ids");
-        Self { parent, collector_id_rng }
+        Self { parent, collector_id_rng, log }
     }
 
     pub fn reporter(&mut self, reporter: Reporter) -> SimReporter {
@@ -66,6 +72,7 @@ impl SimReporters {
             ena: Ena(0x1),
             restart_id_rng,
             collector_id,
+            log: self.log.new(slog::o!("reporter" => reporter.to_string())),
         }
     }
 }
@@ -80,6 +87,8 @@ pub struct SimReporter {
     // to each reporter...but the DEs don't actually care who collected the
     // ereport, and we just need something to put in there.
     collector_id: OmicronZoneUuid,
+
+    log: slog::Logger,
 }
 
 impl SimReporter {
@@ -98,10 +107,11 @@ impl SimReporter {
     pub fn mk_ereport(
         &mut self,
         now: chrono::DateTime<Utc>,
-        json: serde_json::Value,
+        json: serde_json::Map<String, serde_json::Value>,
     ) -> Ereport {
         self.ena.0 += 1;
         mk_ereport(
+            &self.log,
             self.reporter,
             EreportId { ena: self.ena, restart_id: self.restart_id },
             self.collector_id,
@@ -117,19 +127,23 @@ impl SimReporter {
 }
 
 pub fn mk_ereport(
+    log: &slog::Logger,
     reporter: Reporter,
     id: EreportId,
     collector_id: OmicronZoneUuid,
     time_collected: chrono::DateTime<Utc>,
-    json: serde_json::Value,
+    json: serde_json::Map<String, serde_json::Value>,
 ) -> Ereport {
-    let (serial_number, part_number) = match reporter {
+    let data = match reporter {
         Reporter::Sp { .. } => {
-            let part_number =
-                json["baseboard_part_number"].as_str().map(ToOwned::to_owned);
-            let serial_number =
-                json["baseboard_serial_number"].as_str().map(ToOwned::to_owned);
-            (serial_number, part_number)
+            let raw = ereport_types::Ereport { ena: id.ena, data: json };
+            EreportData::from_sp_ereport(
+                log,
+                id.restart_id,
+                raw,
+                time_collected,
+                collector_id,
+            )
         }
         Reporter::HostOs { .. } => {
             todo!(
@@ -139,19 +153,13 @@ pub fn mk_ereport(
             );
         }
     };
-    Ereport {
-        reporter,
-        data: EreportData {
-            id,
-            collector_id,
-            time_collected,
-            class: json["class"]
-                .as_str()
-                .or_else(|| json["k"].as_str())
-                .map(ToOwned::to_owned),
-            serial_number,
-            part_number,
-            report: json,
-        },
-    }
+    slog::info!(
+        &log,
+        "simulating an ereport: {}", data.id;
+        "ereport_id" => %data.id,
+        "ereport_class" => ?data.class,
+        "serial_number" => ?data.serial_number,
+        "part_number" => ?data.part_number,
+    );
+    Ereport { reporter, data }
 }
