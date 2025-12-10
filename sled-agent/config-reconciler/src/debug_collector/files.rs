11794475429c09988fd763330c389abe3cf4d11e
David Pacheco <dap@oxidecomputer.com>
2025-12-09 19:25:24 -0800

    WIP: big rework

--- sled-agent/config-reconciler/src/debug_collector/files.rs
+++ sled-agent/config-reconciler/src/debug_collector/files.rs
@@ -5,19 +5,9 @@
 //! Configuration and implementation for archiving ordinary files as debug data
 //! (e.g., log files)
 
-// XXX-dap next steps:
-// - add output config
-//   - directory
-//   - filename
-//   - both of these are dynamic, which makes it feel like this should be a
-//     trait after all?
-
 use camino::Utf8Path;
 use camino::Utf8PathBuf;
-use itertools::Itertools;
 use slog::Logger;
-use slog::debug;
-use std::sync::LazyLock;
 use tokio::fs::File;
 
 #[derive(Debug, Clone, Copy)]
@@ -26,170 +16,158 @@ pub struct ZoneInfo<'a> {
     root: &'a Utf8Path,
 }
 
-pub struct AllFilesConfig {
-    file_configs: Vec<DebugFileConfig>,
-}
-
-impl AllFilesConfig {
-    fn new(file_configs: Vec<DebugFileConfig>) -> AllFilesConfig {
-        AllFilesConfig { file_configs }
-    }
-
-    pub fn start_archive<'a>(
-        &'a self,
-        log: &'a Logger,
-        what: ArchiveWhat,
-        zones: &'a [ZoneInfo<'a>],
-    ) -> Archiver<'a> {
-        Archiver { log: log.clone(), what, zones, configs: self }
-    }
-}
-
 #[derive(Debug, Clone, Copy)]
 pub enum ArchiveWhat {
     ImmutableOnly,
     Everything,
 }
 
-struct DebugFileConfig {
-    name: String,
-    path_in_zone: Utf8PathBuf,
-    kind: DebugFileKind,
-    // XXX-dap trait instead?
-    // output_directory: Box<dyn Fn(&Utf8Path, &Utf8Path) -> Utf8PathBuf>,
-    // output_file: Box<dyn Fn(&Utf8Path, &Utf8Path) -> File>,
-    // XXX-dap cfg(test)
-    test_examples: Vec<&'static str>,
-    // XXX-dap output pattern
-}
-
-impl DebugFileConfig {
-    fn archive_steps(
-        &self,
-        zone: &ZoneInfo<'_>,
-        what: ArchiveWhat,
-    ) -> Vec<ArchiveStep> {
-        let mut rv = Vec::new();
-
-        match self.kind {
-            DebugFileKind::RotatedLog(base_pattern) => {
-                // Always add a step for rotated logs.
-                let zone_logs_path = zone.root.join(&self.path_in_zone);
-                // XXX-dap digits
-                let rotated_pattern =
-                    zone_logs_path.join(format!("{base_pattern}.*"));
-                rv.push(ArchiveStep {
-                    label: format!("rotated logs for {}", self.name),
-                    glob_pattern: rotated_pattern.to_string(),
-                    do_delete: true,
-                });
-
-                // If this is an "everything" archival, then add a step for the
-                // live logs, too.  But don't delete them.
-                let live_pattern = zone_logs_path.join(base_pattern);
-                rv.push(ArchiveStep {
-                    label: format!("live logs for {}", self.name),
-                    glob_pattern: live_pattern.to_string(),
-                    do_delete: false,
-                });
-            }
-        }
-
-        rv
-    }
+#[derive(Debug, Clone)]
+pub struct GlobGroup<'a> {
+    label: &'static str,
+    glob_pattern: String,
+    kind: DebugFileKind<'a>,
 }
 
 #[derive(Debug, Clone, Copy)]
-enum DebugFileKind {
-    RotatedLog(&'static str),
+pub enum DebugFileKind<'a> {
+    LogRotated(&'a ZoneInfo<'a>),
+    LogLive(&'a ZoneInfo<'a>),
+    Core,
 }
 
-enum ArchiveWhen {
-    Always,
-    NonPeriodicOnly,
+impl DebugFileKind<'_> {
+    fn should_delete_original(&self) -> bool {
+        match self {
+            DebugFileKind::LogRotated(_) | DebugFileKind::Core => true,
+            DebugFileKind::LogLive(zone_info) => false,
+        }
+    }
 }
 
-enum DeleteWhen {
-    Never,
-    AfterArchive,
-}
-
-static ARCHIVE_CONFIGS: LazyLock<AllFilesConfig> = LazyLock::new(|| {
-    AllFilesConfig::new(vec![
-        DebugFileConfig {
-            name: String::from("SMF log files"),
-            path_in_zone: Utf8PathBuf::from("var/svc/log"),
-            kind: DebugFileKind::RotatedLog("*.log"),
-            test_examples: vec![
-                // service with a live log and two rotated logs
-                "svc1.log",
-                "svc1.log.1",
-                "svc1.log.2",
-                // service with just a live log
-                "svc2.log",
-                // service with only rotated logs
-                "svc3.log.0",
-                "svc3.log.1",
-                // servie with many digits in the rotated log file name
-                "svc3.log.12345",
-            ],
-        },
-        DebugFileConfig {
-            name: String::from("syslog"),
-            path_in_zone: Utf8PathBuf::from("var/adm"),
-            kind: DebugFileKind::RotatedLog("messages"),
-            test_examples: vec!["messages", "messages.0", "messages.1"],
-        },
-    ])
-});
-
-pub struct Archiver<'a> {
-    log: Logger,
-    what: ArchiveWhat,
+pub fn plan_archive_zones<'a>(
     zones: &'a [ZoneInfo<'a>],
-    configs: &'a AllFilesConfig,
-}
+    what: ArchiveWhat,
+) -> impl Iterator<Item = GlobGroup<'a>> {
+    let mut rv = Vec::new();
 
-impl Archiver<'_> {
-    pub fn archive_all(self) -> Vec<anyhow::Error> {
-        let mut errors = Vec::new();
-        let mut archive_steps = self
-            .zones
-            .iter()
-            .cartesian_product(&self.configs.file_configs)
-            .flat_map(|(zone, file_config)| {
-                file_config.archive_steps(&zone, self.what)
-            });
+    for zone in zones {
+        let smf_log_dir = format!("{}/var/svc/log", zone.root);
+        rv.push(GlobGroup {
+            label: "rotated SMF logs",
+            // XXX-dap digits
+            glob_pattern: format!("{smf_log_dir}/*.log.*"),
+            kind: DebugFileKind::LogRotated(zone),
+        });
 
-        for step in archive_steps {
-            debug!(&self.log, "processing archive step"; &step);
-            if let Err(error) = self.archive_one(&step) {
-                // XXX-dap log
-                errors.push(error);
+        let syslog_dir = format!("{}/var/adm", zone.root);
+        rv.push(GlobGroup {
+            label: "rotated syslog",
+            // XXX-dap digits
+            glob_pattern: format!("{syslog_dir}/messages.*"),
+            kind: DebugFileKind::LogRotated(zone),
+        });
+
+        match what {
+            ArchiveWhat::ImmutableOnly => (),
+            ArchiveWhat::Everything => {
+                rv.push(GlobGroup {
+                    label: "live SMF logs",
+                    glob_pattern: format!("{smf_log_dir}/*.log"),
+                    kind: DebugFileKind::LogLive(zone),
+                });
+                rv.push(GlobGroup {
+                    label: "live syslog",
+                    glob_pattern: format!("{syslog_dir}/messages"),
+                    kind: DebugFileKind::LogLive(zone),
+                });
             }
         }
-
-        errors
     }
 
-    fn archive_one(&self, step: &ArchiveStep) -> Result<(), anyhow::Error> {
-        // XXX-dap this is where we do the thing
-        todo!();
+    rv.into_iter()
+}
+
+pub fn plan_archive_cores<'a>(
+    core_directories: &'a [&'a Utf8Path],
+) -> impl Iterator<Item = GlobGroup<'a>> {
+    core_directories.iter().map(|core_dir| GlobGroup {
+        label: "core files",
+        glob_pattern: format!("{core_dir}/*"),
+        kind: DebugFileKind::Core,
+    })
+}
+
+pub struct ArchiveStep<'a> {
+    source: Utf8PathBuf,
+    kind: DebugFileKind<'a>,
+}
+
+impl ArchiveStep<'_> {
+    fn destination(&self, debug_dir: &Utf8Path) -> Utf8PathBuf {
+        match self.kind {
+            DebugFileKind::LogRotated(zone_info) => todo!(),
+            DebugFileKind::LogLive(zone_info) => todo!(),
+            DebugFileKind::Core => todo!(),
+        }
     }
 }
 
-struct ArchiveStep {
-    label: String,
-    glob_pattern: String,
-    do_delete: bool,
+pub fn plan_archive<'a>(
+    globs: impl Iterator<Item = GlobGroup<'a>>,
+) -> Result<impl Iterator<Item = ArchiveStep<'a>>, glob::PatternError> {
+    // XXX-dap this really ought to work in a streaming way
+    let mut rv = Vec::new();
+
+    for glob_group in globs {
+        // XXX-dap warn instead of ignoring these errors with flatten
+        let files = glob::glob(&glob_group.glob_pattern)?.flatten();
+        for file in files {
+            // XXX-dap unwrap()
+            let path = Utf8PathBuf::try_from(file).unwrap();
+            rv.push(ArchiveStep { source: path, kind: glob_group.kind });
+        }
+    }
+
+    Ok(rv.into_iter())
 }
 
-impl slog::KV for ArchiveStep {
-    fn serialize(
-        &self,
-        record: &slog::Record<'_>,
-        serializer: &mut dyn slog::Serializer,
-    ) -> slog::Result {
-        todo!() // XXX-dap
+pub async fn archive_all<'a>(
+    log: &Logger,
+    debug_dir: &Utf8Path,
+    steps: impl Iterator<Item = ArchiveStep<'a>>,
+) {
+    // XXX-dap logging
+    for step in steps {
+        match archive_one(debug_dir, step).await {
+            Ok(()) => (),
+            Err(error) => {
+                // XXX-dap warning
+            }
+        }
     }
+}
+
+// XXX-dap copied from copy_sync_and_remove()
+pub async fn archive_one(
+    debug_dir: &Utf8Path,
+    step: ArchiveStep<'_>,
+) -> tokio::io::Result<()> {
+    let dest = step.destination(debug_dir);
+    let source = &step.source;
+    let mut dest_f = tokio::fs::File::create(&dest).await?;
+    let mut src_f = tokio::fs::File::open(&source).await?;
+
+    tokio::io::copy(&mut src_f, &mut dest_f).await?;
+
+    dest_f.sync_all().await?;
+
+    drop(src_f);
+    drop(dest_f);
+
+    if step.kind.should_delete_original() {
+        tokio::fs::remove_file(source).await?;
+    }
+
+    Ok(())
 }
