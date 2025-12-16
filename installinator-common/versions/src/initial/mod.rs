ae7aac4a53476f9edbb84185e66beed6ac92a7a4
Rain <rain@oxide.computer>
2025-12-16 19:40:24 +0000

    [spr] initial version
    Created using spr 1.3.6-beta.1

--- installinator-common/versions/src/initial/mod.rs
+++ installinator-common/versions/src/initial/mod.rs
@@ -2,13 +2,6 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-//! Common types shared by the installinator client and server.
+//! Version `INITIAL` of the Installinator API.
 
-mod block_size_writer;
-mod progress;
-mod raw_disk_writer;
 pub mod report;
-
-pub use block_size_writer::*;
-pub use progress::*;
-pub use raw_disk_writer::*;
