b79fdddbd0eb77e417f382a14e3ee325ba319ad4
Andrew J. Stone <andrew@oxidecomputer.com>
2025-12-18 00:53:06 +0000

    fix docs

--- nexus/db-model/src/inventory.rs
+++ nexus/db-model/src/inventory.rs
@@ -489,7 +489,7 @@ impl<'a> From<&'a Collection> for InvCollection {
     }
 }
 
-/// See [`nexus_types::inventory::BaseboardId`].
+/// See [`sled_agent_types::sled::BaseboardId`].
 #[derive(Queryable, Insertable, Clone, Debug, Selectable, PartialEq, Eq)]
 #[diesel(table_name = hw_baseboard_id)]
 pub struct HwBaseboardId {
