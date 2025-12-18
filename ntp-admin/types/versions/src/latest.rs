1ac7cde201ef1344966256ec81825e418b44d6f6
Rain <rain@oxide.computer>
2025-12-17 21:45:44 -0800

    [ntp-admin-types] reorganize per RFD 619 (#9528)

---
+++ ntp-admin/types/versions/src/latest.rs
@@ -0,0 +1,9 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+//! Re-exports of the latest versions of all published types.
+
+pub mod timesync {
+    pub use crate::v1::timesync::TimeSync;
+}
