f2669048091c7d2fab3adab7061778dc71923588
John Gallagher <john@oxidecomputer.com>
2025-12-19 10:28:39 -0500

    reword comment explaining impl Ledgerable

--- sled-agent/types/versions/src/add_dual_stack_shared_network_interfaces/inventory.rs
+++ sled-agent/types/versions/src/add_dual_stack_shared_network_interfaces/inventory.rs
@@ -115,10 +115,15 @@ pub struct OmicronSledConfig {
     pub host_phase_2: HostPhase2DesiredSlots,
 }
 
-// NOTE: Most trait impls live in the `impls` module of this crate, but we
-// ledger sled configs on disk, so have to be able to read them from the ledger
-// even for old versions. Therefore, we implement `Ledgerable` on this type
-// directly.
+// NOTE: Most trait impls live in the `impls` module of this crate and are only
+// implemented for the `latest` version of each type. However,
+// `OmicronSledConfig` is special: it's not only used in the sled-agent API
+// (which would only require trait impls on `latest`); it's also ledgered to
+// disk to support cold boot of the rack. In the ledgering case, we have to be
+// able to handle reading older versions, which means all the old versions we
+// support also need to implement `Ledgerable`. Therefore, we implement this
+// trait for this specific version (and do so for every other version of
+// `OmicronSledConfig` too).
 impl Ledgerable for OmicronSledConfig {
     fn is_newer_than(&self, other: &Self) -> bool {
         self.generation > other.generation
