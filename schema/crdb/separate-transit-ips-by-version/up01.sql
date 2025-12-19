fa77bcc389180bd244fef5411cda05ec277f31a1
Benjamin Naecker <ben@oxide.computer>
2025-12-19 05:01:11 +0000

    Fixups to ensure idempotent DB schema changes

--- schema/crdb/separate-transit-ips-by-version/up01.sql
+++ schema/crdb/separate-transit-ips-by-version/up01.sql
@@ -1,5 +1,5 @@
 /* Add the new column for IPv6 transit IPs. */
 ALTER TABLE
 omicron.public.network_interface
-ADD COLUMN transit_ips_v6
+ADD COLUMN IF NOT EXISTS transit_ips_v6
 INET[] NOT NULL DEFAULT ARRAY[];
