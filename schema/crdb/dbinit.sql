72db8af4015a8d6c01bac236f3f3172a02477f03
James MacMahon <james@oxide.computer>
2025-12-19 13:52:09 +0000

    also commit schema/crdb/dbinit.sql

--- schema/crdb/dbinit.sql
+++ schema/crdb/dbinit.sql
@@ -7478,7 +7478,7 @@ INSERT INTO omicron.public.db_metadata (
     version,
     target_version
 ) VALUES
-    (TRUE, NOW(), NOW(), '214.0.0', NULL)
+    (TRUE, NOW(), NOW(), '215.0.0', NULL)
 ON CONFLICT DO NOTHING;
 
 COMMIT;
