d0240bffd250f41eba118d9c5db0fa21c53c42e2
David Pacheco <dap@oxidecomputer.com>
2025-12-19 17:40:24 -0800

    split out test module

--- sled-agent/config-reconciler/src/debug_collector/file_archiver/mod.rs
+++ sled-agent/config-reconciler/src/debug_collector/file_archiver/mod.rs
@@ -6,8 +6,10 @@
 //! (e.g., log files)
 
 // XXX-dap current status:
-// - lots of cleanup to do
-// - split this into modules
+// - fix up imports
+// - try to move tests into submodules and fix up visibility
+// - clean up XXX-daps
+// - self-review and clean up
 
 use anyhow::Context;
 use anyhow::anyhow;
@@ -32,6 +34,8 @@ mod execution;
 mod filesystem;
 mod planning;
 mod rules;
+#[cfg(test)]
+mod test_helpers;
 
 pub use planning::ArchiveKind;
 pub use planning::ArchivePlanner;
@@ -58,369 +62,27 @@ impl TryFrom<String> for Filename {
 
 #[cfg(test)]
 mod test {
+    use super::test_helpers::*;
+
     use super::Filename;
-    use super::filesystem::FileLister;
     use super::planning::ArchiveFile;
     use super::planning::ArchiveKind;
-    use super::planning::ArchivePlan;
     use super::planning::ArchivePlanner;
     use super::planning::ArchiveStep;
     use super::rules::ALL_RULES;
     use super::rules::MAX_COLLIDING_FILENAMES;
     use super::rules::NameRotatedLogFile;
     use anyhow::Context;
-    use anyhow::anyhow;
-    use anyhow::bail;
     use camino::Utf8Path;
-    use camino::Utf8PathBuf;
     use camino_tempfile::Utf8TempDir;
     use chrono::DateTime;
     use chrono::Timelike;
     use chrono::Utc;
-    use iddqd::IdOrdItem;
-    use iddqd::IdOrdMap;
-    use iddqd::id_upcast;
     use omicron_test_utils::dev::test_setup_log;
-    use regex::Regex;
-    use slog::Logger;
     use slog::debug;
     use slog::info;
     use slog_error_chain::InlineErrorChain;
     use std::collections::BTreeSet;
-    use std::sync::LazyLock;
-    use std::sync::Mutex;
-    use strum::Display;
-    use strum::EnumDiscriminants;
-    use strum::EnumIter;
-    use strum::IntoDiscriminant;
-    use strum::IntoEnumIterator;
-
-    #[derive(Clone)]
-    struct TestFile {
-        path: Utf8PathBuf,
-        kind: TestFileKind,
-    }
-
-    impl IdOrdItem for TestFile {
-        type Key<'a> = &'a Utf8Path;
-
-        fn key(&self) -> Self::Key<'_> {
-            &self.path
-        }
-
-        id_upcast!();
-    }
-
-    #[derive(Clone, Debug, Display, EnumIter, EnumDiscriminants)]
-    #[strum_discriminants(derive(EnumIter, Ord, PartialOrd))]
-    enum TestFileKind {
-        KernelCrashDump {
-            cores_directory: String,
-        },
-        ProcessCoreDump {
-            cores_directory: String,
-        },
-        LogSmfRotated {
-            zone_name: String,
-            zone_root: String,
-        },
-        LogSmfLive {
-            zone_name: String,
-            zone_root: String,
-        },
-        LogSyslogRotated {
-            zone_name: String,
-            zone_root: String,
-        },
-        LogSyslogLive {
-            zone_name: String,
-            zone_root: String,
-        },
-        GlobalLogSmfRotated,
-        GlobalLogSmfLive,
-        GlobalLogSyslogRotated,
-        GlobalLogSyslogLive,
-        /// files we don't especially care about, but are in the test data to
-        /// ensure that they don't create a problem
-        Ignored,
-    }
-
-    impl TestFileKind {
-        fn cores_directory(&self) -> Option<&Utf8Path> {
-            match self {
-                TestFileKind::KernelCrashDump { cores_directory }
-                | TestFileKind::ProcessCoreDump { cores_directory } => {
-                    Some(Utf8Path::new(cores_directory))
-                }
-                TestFileKind::LogSmfRotated { .. }
-                | TestFileKind::LogSmfLive { .. }
-                | TestFileKind::LogSyslogRotated { .. }
-                | TestFileKind::LogSyslogLive { .. }
-                | TestFileKind::GlobalLogSmfRotated
-                | TestFileKind::GlobalLogSmfLive
-                | TestFileKind::GlobalLogSyslogRotated
-                | TestFileKind::GlobalLogSyslogLive
-                | TestFileKind::Ignored => None,
-            }
-        }
-
-        fn zone_info(&self) -> Option<(&str, &Utf8Path)> {
-            match self {
-                TestFileKind::KernelCrashDump { .. }
-                | TestFileKind::ProcessCoreDump { .. }
-                | TestFileKind::Ignored => None,
-                TestFileKind::LogSmfRotated { zone_name, zone_root }
-                | TestFileKind::LogSmfLive { zone_name, zone_root }
-                | TestFileKind::LogSyslogRotated { zone_name, zone_root }
-                | TestFileKind::LogSyslogLive { zone_name, zone_root } => {
-                    Some((zone_name, Utf8Path::new(zone_root)))
-                }
-                TestFileKind::GlobalLogSmfRotated
-                | TestFileKind::GlobalLogSmfLive
-                | TestFileKind::GlobalLogSyslogRotated
-                | TestFileKind::GlobalLogSyslogLive => {
-                    Some(("global", Utf8Path::new("/")))
-                }
-            }
-        }
-    }
-
-    static RE_CORES_DATASET: LazyLock<Regex> = LazyLock::new(|| {
-        Regex::new("^(/pool/int/[^/]+/crash)/[^/]+$").unwrap()
-    });
-
-    static RE_NONGLOBAL_ZONE: LazyLock<Regex> = LazyLock::new(|| {
-        Regex::new("^/pool/ext/[^/]+/crypt/zone/([^/]+)/root").unwrap()
-    });
-
-    impl TryFrom<&Utf8Path> for TestFileKind {
-        type Error = anyhow::Error;
-
-        fn try_from(value: &Utf8Path) -> Result<Self, Self::Error> {
-            let s = value.as_str();
-
-            if let Some(c) = RE_CORES_DATASET.captures(s) {
-                let (_, [cores_directory]) = c.extract();
-                let cores_directory = cores_directory.to_owned();
-                if s.ends_with("bounds") {
-                    Ok(TestFileKind::Ignored)
-                } else if s.contains("/vmdump.") {
-                    Ok(TestFileKind::KernelCrashDump { cores_directory })
-                } else if s.contains("/core.") {
-                    Ok(TestFileKind::ProcessCoreDump { cores_directory })
-                } else {
-                    Err(anyhow!("unknown cores dataset test file kind"))
-                }
-            } else if let Some(c) = RE_NONGLOBAL_ZONE.captures(s) {
-                let (zone_root, [zone_name]) = c.extract();
-                let zone_root = zone_root.to_owned();
-                let zone_name = zone_name.to_owned();
-                if s.ends_with("/messages") {
-                    Ok(TestFileKind::LogSyslogLive { zone_name, zone_root })
-                } else if s.contains("/messages.") {
-                    Ok(TestFileKind::LogSyslogRotated { zone_name, zone_root })
-                } else if s.contains("/var/svc/log") {
-                    if s.ends_with(".log") {
-                        Ok(TestFileKind::LogSmfLive { zone_name, zone_root })
-                    } else {
-                        Ok(TestFileKind::LogSmfRotated { zone_name, zone_root })
-                    }
-                } else {
-                    Err(anyhow!("unknown non-global zone test file kind"))
-                }
-            } else {
-                if s == "/var/adm/messages" {
-                    Ok(TestFileKind::GlobalLogSyslogLive)
-                } else if s.starts_with("/var/adm") && s.contains("/messages.")
-                {
-                    Ok(TestFileKind::GlobalLogSyslogRotated)
-                } else if s.starts_with("/var/svc/log") {
-                    if s.ends_with(".log") {
-                        Ok(TestFileKind::GlobalLogSmfLive)
-                    } else {
-                        Ok(TestFileKind::GlobalLogSmfRotated)
-                    }
-                } else {
-                    Err(anyhow!("unknown test file kind"))
-                }
-            }
-        }
-    }
-
-    fn load_real_paths() -> anyhow::Result<BTreeSet<Utf8PathBuf>> {
-        let path = "test-data/debug-files.txt";
-        std::fs::read_to_string(&path)
-            .with_context(|| format!("read {path:?}"))?
-            .lines()
-            .enumerate()
-            .map(|(i, l)| (i, l.trim()))
-            .filter(|(_i, l)| !l.is_empty() && !l.starts_with("#"))
-            .map(|(i, l)| {
-                Utf8PathBuf::try_from(l).map_err(|_err| {
-                    anyhow!("{path:?} line {}: non-UTF8 file path", i + 1)
-                })
-            })
-            .collect()
-    }
-
-    fn load_test_files() -> anyhow::Result<IdOrdMap<TestFile>> {
-        load_real_paths()?
-            .into_iter()
-            .map(|path| {
-                TestFileKind::try_from(path.as_ref())
-                    .context("may need to update load_test_files()?")
-                    .map(|kind| TestFile { path, kind })
-            })
-            .collect()
-    }
-
-    struct TestLister<'a> {
-        files: BTreeSet<&'a Utf8Path>,
-        last_listed: Mutex<Option<Utf8PathBuf>>,
-        injected_error: Option<&'a Utf8Path>,
-    }
-
-    impl<'a> TestLister<'a> {
-        fn new_for_test_data(files: &'a IdOrdMap<TestFile>) -> Self {
-            Self::new(files.iter().map(|test_file| test_file.path.as_path()))
-        }
-
-        fn empty() -> Self {
-            Self::new::<_, &'a str>(std::iter::empty())
-        }
-
-        fn new<I, P>(files: I) -> Self
-        where
-            I: IntoIterator<Item = &'a P>,
-            P: AsRef<Utf8Path> + ?Sized + 'a,
-        {
-            Self {
-                files: files.into_iter().map(|p| p.as_ref()).collect(),
-                last_listed: Mutex::new(None),
-                injected_error: None,
-            }
-        }
-
-        fn inject_error(&mut self, fail_path: &'a Utf8Path) {
-            self.injected_error = Some(fail_path);
-        }
-    }
-
-    impl FileLister for TestLister<'_> {
-        fn list_files(
-            &self,
-            path: &Utf8Path,
-        ) -> Vec<Result<Filename, anyhow::Error>> {
-            // Keep track of the last path that was listed.
-            *self.last_listed.lock().unwrap() = Some(path.to_owned());
-
-            // Inject any errors we've been configured to inject.
-            if let Some(fail_path) = self.injected_error {
-                if path == fail_path {
-                    return vec![Err(anyhow!(
-                        "injected error for {fail_path:?}"
-                    ))];
-                }
-            }
-
-            // Create a directory listing from the files in our test data.
-            self.files
-                .iter()
-                .filter_map(|file_path| {
-                    let directory =
-                        file_path.parent().expect("test file has a parent");
-                    (directory == path).then(|| {
-                        let filename = file_path
-                            .file_name()
-                            .expect("test file has a filename");
-                        Ok(Filename::try_from(filename.to_owned())
-                            .expect("filename has no slashes"))
-                    })
-                })
-                .collect()
-        }
-
-        fn file_mtime(
-            &self,
-            path: &Utf8Path,
-        ) -> Result<Option<DateTime<Utc>>, anyhow::Error> {
-            if let Some(fail_path) = self.injected_error {
-                if path == fail_path {
-                    bail!("injected error for {fail_path:?}");
-                }
-            }
-
-            Ok(Some("2025-12-12T16:51:00-07:00".parse().unwrap()))
-        }
-
-        fn file_exists(&self, path: &Utf8Path) -> Result<bool, anyhow::Error> {
-            Ok(self.files.contains(path))
-        }
-    }
-
-    /// Test that our test data includes all the kinds of things that we expect.
-    /// If you see this test failing, presumably you updated the test data and
-    /// you'll need to make sure it's still representative.
-    #[test]
-    fn test_test_data() {
-        // Load the test data and determine what kind of file each one is.
-        let files = load_test_files().unwrap();
-
-        // Create a set of all the kinds of test files that we have not seen so
-        // far.  We'll remove from this set as we find files of this kind.  Any
-        // kinds left over at the end are missing from our test data.
-        let mut all_kinds: BTreeSet<_> =
-            TestFileKindDiscriminants::iter().collect();
-        // We don't care about finding the "ignored" kind.
-        all_kinds.remove(&TestFileKindDiscriminants::Ignored);
-        for test_file in files {
-            println!("{} {}", test_file.kind, test_file.path);
-            all_kinds.remove(&test_file.kind.discriminant());
-        }
-
-        if !all_kinds.is_empty() {
-            panic!("missing file in test data for kinds: {:?}", all_kinds);
-        }
-    }
-
-    fn test_archive<'a>(
-        log: &Logger,
-        files: &IdOrdMap<TestFile>,
-        output_dir: &Utf8Path,
-        what: ArchiveKind,
-        lister: &'a TestLister,
-    ) -> ArchivePlan<'a> {
-        // Construct sources that correspond with the test data.
-        let cores_datasets: BTreeSet<_> = files
-            .iter()
-            .filter_map(|test_file| test_file.kind.cores_directory())
-            .collect();
-        let zone_infos: BTreeSet<_> = files
-            .iter()
-            .filter_map(|test_file| test_file.kind.zone_info())
-            .collect();
-
-        // Plan an archival pass.
-        let mut planner =
-            ArchivePlanner::new_with_lister(log, what, output_dir, lister);
-
-        for cores_dir in cores_datasets {
-            debug!(log, "including cores directory"; "cores_dir" => %cores_dir);
-            planner.include_cores_directory(cores_dir);
-        }
-
-        for (zone_name, zone_root) in zone_infos {
-            debug!(
-                log,
-                "including zone";
-                "zone_name" => zone_name,
-                "zone_root" => %zone_root,
-            );
-            planner.include_zone(zone_name, zone_root);
-        }
-
-        planner.into_plan()
-    }
 
     /// Fully tests archive planning with a bunch of real-world file paths
     #[test]
@@ -695,7 +357,7 @@ mod test {
                 continue;
             };
 
-            let last_listed = lister.last_listed.lock().unwrap();
+            let last_listed = lister.last_listed();
             let last = last_listed
                 .as_ref()
                 .expect("listed a directory before archiving any files");
