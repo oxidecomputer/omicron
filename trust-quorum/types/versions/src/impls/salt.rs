13d71f395844ebc281468e81c9874abf7f41aa7a
Andrew J. Stone <andrew@oxidecomputer.com>
2026-01-01 21:20:41 +0000

    fix comment

--- trust-quorum/types/versions/src/impls/salt.rs
+++ trust-quorum/types/versions/src/impls/salt.rs
@@ -4,9 +4,6 @@ use crate::v1::crypto::Salt;
 
 impl Salt {
     /// Generate a new random salt.
-    ///
-    /// This is a free function because `Salt` is defined in `trust-quorum-types` (for API versioning
-    /// akin to RFD 619).
     pub fn new() -> Salt {
         let mut rng = OsRng;
         let mut salt = [0u8; 32];
