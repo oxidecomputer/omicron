dbbecb2ddda246d3d9077149793a2c039c58ab86
Eliza Weisman <eliza@elizas.website>
2026-01-28 11:30:18 -0800

    apparently there is no ALTER COLUMN IF EXISTS in crdb

--- schema/crdb/read-only-crucible-disks/up2.sql
+++ schema/crdb/read-only-crucible-disks/up2.sql
@@ -1,7 +1,7 @@
--- Now that the column has been added, drop the default, as it was only used for
--- backfilling existing data.
+-- Now that the column has been added, drop the default, as it was only used
+-- for backfilling existing data.
 ALTER TABLE
     omicron.public.disk_type_crucible
-ALTER COLUMN IF EXISTS
+ALTER COLUMN
     read_only
 DROP DEFAULT;
