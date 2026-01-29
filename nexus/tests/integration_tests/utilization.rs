7fdffb96585f64f3197127d5ec6169a8763c0d21
Eliza Weisman <eliza@elizas.website>
2026-01-28 17:55:06 -0800

    rustfmt

--- nexus/tests/integration_tests/utilization.rs
+++ nexus/tests/integration_tests/utilization.rs
@@ -151,7 +151,6 @@ async fn test_utilization_view(cptestctx: &ControlPlaneTestContext) {
                     disk_source: params::DiskSource::Blank {
                         block_size: params::BlockSize::try_from(512).unwrap(),
                     },
-                    
                 },
             }))
             .expect_status(Some(StatusCode::CREATED)),
