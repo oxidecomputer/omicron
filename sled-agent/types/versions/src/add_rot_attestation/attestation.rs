47cdecd261c328364c5adaa05bdabdb18d17550c
Luqman Aden <luqman@oxide.computer>
2026-02-05 22:21:28 +0000

    Replace allocating hex::encode with BytesToHexDebug wrapper.

--- sled-agent/types/versions/src/add_rot_attestation/attestation.rs
+++ sled-agent/types/versions/src/add_rot_attestation/attestation.rs
@@ -6,23 +6,17 @@
 
 use std::fmt;
 
+use omicron_common::BytesToHexDebug;
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
 /// A Root of Trust (RoT) which provides measurments and signed attestations.
-#[derive(Deserialize, Serialize, JsonSchema)]
+#[derive(Deserialize, Serialize, JsonSchema, strum::Display)]
 #[serde(rename_all = "snake_case")]
+#[strum(serialize_all = "snake_case")]
 pub enum Rot {
     /// The per-sled RoT in an Oxide rack.
     Oxide,
-}
-
-impl fmt::Display for Rot {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self {
-            Rot::Oxide => write!(f, "oxide"),
-        }
-    }
 }
 
 /// Path parameters for RoT attestation requests.
@@ -44,7 +38,9 @@ pub struct Sha3_256Digest(
 
 impl fmt::Debug for Sha3_256Digest {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        f.debug_tuple("Sha3_256Digest").field(&hex::encode(self.0)).finish()
+        f.debug_tuple("Sha3_256Digest")
+            .field(&BytesToHexDebug(&self.0))
+            .finish()
     }
 }
 
@@ -89,7 +85,9 @@ pub struct Ed25519Signature(
 
 impl fmt::Debug for Ed25519Signature {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        f.debug_tuple("Ed25519Signature").field(&hex::encode(self.0)).finish()
+        f.debug_tuple("Ed25519Signature")
+            .field(&BytesToHexDebug(&self.0))
+            .finish()
     }
 }
 
