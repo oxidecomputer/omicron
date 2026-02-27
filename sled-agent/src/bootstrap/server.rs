385ecf79d7e9dbf131f38b90c3ab4752dd3b9519
Simon Ruggier <Simon.Ruggier@gmail.com>
2026-02-27 16:25:49 -0500

    Log the full error chain on `SledAgentServerStartError`s
    This commit updates handle_start_sled_agent_request to log the full
    error chain in case a SledAgentServerStartError is returned. Previously,
    one level of cause would be logged for the FailedLearnerInit variant,
    and CommitToLedger errors were logged without any information about the
    source.

--- sled-agent/src/bootstrap/server.rs
+++ sled-agent/src/bootstrap/server.rs
@@ -50,6 +50,7 @@ use sled_agent_types::sled::StartSledAgentRequest;
 use sled_hardware::underlay;
 use sled_storage::dataset::CONFIG_DATASET;
 use slog::Logger;
+use slog_error_chain::InlineErrorChain;
 use std::io;
 use std::net::SocketAddr;
 use std::net::SocketAddrV6;
@@ -347,7 +348,7 @@ pub enum SledAgentServerStartError {
     #[error("Failed to commit sled agent request to ledger")]
     CommitToLedger(#[from] ledger::Error),
 
-    #[error("Failed to initialize this lrtq node as a learner: {0}")]
+    #[error("Failed to initialize this lrtq node as a learner")]
     FailedLearnerInit(#[from] bootstore::NodeRequestError),
 }
 
@@ -643,8 +644,15 @@ impl Inner {
                     Err(err) => {
                         // This error is unrecoverable, and if returned we'd
                         // end up in maintenance mode anyway.
-                        error!(log, "Failed to start sled agent: {err:#}");
-                        panic!("Failed to start sled agent: {err:#}");
+                        error!(
+                            log,
+                            "Failed to start sled agent: {}",
+                            InlineErrorChain::new(&err)
+                        );
+                        panic!(
+                            "Failed to start sled agent: {}",
+                            InlineErrorChain::new(&err)
+                        );
                     }
                 };
                 _ = response_tx.send(response);
