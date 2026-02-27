c401a312ecb2c1deeb4dceb6e45751a5dc40153e
Simon Ruggier <Simon.Ruggier@gmail.com>
2026-02-27 17:23:01 -0500

    Transition omicron_sled_agent::instance_manager::Error to chain logging
    The main consumer of this error type seems to be
    omicron_sled_agent::sled_agent::Error, so this commit has the effect of
    fixing duplication in error logging that was introduced by a previous
    commit within the same PR.

--- sled-agent/src/sled_agent.rs
+++ sled-agent/src/sled_agent.rs
@@ -285,7 +285,11 @@ impl From<Error> for dropshot::HttpError {
                 e @ crate::instance_manager::Error::NoSuchVmm(_),
             ) => HttpError::for_not_found(
                 Some(NO_SUCH_INSTANCE.to_string()),
-                e.to_string(),
+                // NoSuchVmm has no source error, so it's currently not necessary to use a
+                // chain-logging adapter here, but if that changes in the future, the compiler
+                // won't enforce anything, and maybe there will be a lint for this problem, one
+                // day, so might as well construct the adapter.
+                InlineErrorChain::new(&e).to_string(),
             ),
             Error::ZoneBundle(ref inner) => match inner {
                 BundleError::NoStorage | BundleError::Unavailable { .. } => {
