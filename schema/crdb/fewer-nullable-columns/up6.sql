cd59ebc3b84c1b6aa3abda8584674bad4efccc21
Sean Klein <sean@oxidecomputer.com>
2026-03-06 14:33:37 -0800

    update, don't delete

--- schema/crdb/fewer-nullable-columns/up6.sql
+++ schema/crdb/fewer-nullable-columns/up6.sql
@@ -1,3 +1,3 @@
 SET LOCAL disallow_full_table_scans = 'off';
-DELETE FROM omicron.public.inv_collection_error WHERE message IS NULL;
+UPDATE omicron.public.inv_collection_error SET message = '' WHERE message IS NULL;
 ALTER TABLE omicron.public.inv_collection_error ALTER COLUMN message SET NOT NULL;
