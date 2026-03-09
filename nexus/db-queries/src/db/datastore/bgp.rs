5dd06ddca5a91240e4ce31f2050d2c785011655e
John Gallagher <john@oxidecomputer.com>
2026-03-09 11:41:03 -0400

    Use an enum instead of `TEXT` when storing `SwitchLocation` in CRDB (#9984)
    This changes three column names:
    
    * `loopback_address.switch_location` -> `loopback_address.switch_slot`
    * `switch_port.switch_location` -> `switch_port.switch_slot`
    * `bfd_session.switch` -> `bfd_session.switch_slot`
    
    This is the top of the yak-stack I built for myself while working on
    #9832. Closes #3594. Removes one instance of diesel <-> CRDB drift
    (`switch_port.switch_location`).

--- nexus/db-queries/src/db/datastore/bgp.rs
+++ nexus/db-queries/src/db/datastore/bgp.rs
@@ -13,7 +13,7 @@ use ipnetwork::IpNetwork;
 use nexus_db_errors::OptionalError;
 use nexus_db_errors::{ErrorHandler, public_error_from_diesel};
 use nexus_db_model::{
-    BgpPeerView, SwitchPortBgpPeerConfigAllowExport,
+    BgpPeerView, DbSwitchSlot, SwitchPortBgpPeerConfigAllowExport,
     SwitchPortBgpPeerConfigAllowImport, SwitchPortBgpPeerConfigCommunity,
 };
 use nexus_types::external_api::networking;
@@ -814,8 +814,9 @@ impl DataStore {
     ) -> ListResultVec<BgpPeerView> {
         use nexus_db_schema::schema::bgp_peer_view::dsl;
 
+        let switch = DbSwitchSlot::from(switch);
         let results = dsl::bgp_peer_view
-            .filter(dsl::switch_location.eq(switch.to_string()))
+            .filter(dsl::switch_slot.eq(switch))
             .filter(dsl::port_name.eq(port))
             .select(BgpPeerView::as_select())
             .load_async(&*self.pool_connection_authorized(opctx).await?)
