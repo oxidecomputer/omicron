a0d59cd1390b77f7777ba3430e26b6c667158b03
Eliza Weisman <eliza@elizas.website>
2026-03-19 14:05:20 -0700

    reticulating CTE

---
+++ nexus/db-queries/tests/output/ereporter_restart_insert.sql
@@ -0,0 +1,19 @@
+WITH
+  previous
+    AS (
+      SELECT
+        generation
+      FROM
+        omicron.public.ereporter_restart
+      WHERE
+        reporter_type = $1 AND slot_type = $2 AND slot = $3
+      ORDER BY
+        generation DESC
+      LIMIT
+        1
+    )
+INSERT
+INTO
+  omicron.public.ereporter_restart (id, generation, reporter_type, slot_type, slot, time_first_seen)
+VALUES
+  ($4, COALESCE((SELECT generation FROM previous) + 1, 0), $5, $6, $7, now())
