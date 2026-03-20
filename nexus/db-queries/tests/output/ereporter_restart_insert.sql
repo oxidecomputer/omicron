763956eb551929857e2c7304594bc02ae71fc24d
Eliza Weisman <eliza@elizas.website>
2026-03-20 14:18:49 -0700

    reticulate

--- nexus/db-queries/tests/output/ereporter_restart_insert.sql
+++ nexus/db-queries/tests/output/ereporter_restart_insert.sql
@@ -17,3 +17,7 @@ INTO
   omicron.public.ereporter_restart (id, generation, reporter_type, slot_type, slot, time_first_seen)
 VALUES
   ($4, COALESCE((SELECT generation FROM previous) + 1, 0), $5, $6, $7, now())
+ON CONFLICT
+  (generation, reporter_type, slot_type, slot)
+DO
+  NOTHING
