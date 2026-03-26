c99d78e124d9c06801ae55aa42e59f538b698f94
Eliza Weisman <eliza@elizas.website>
2026-03-26 09:23:40 -0700

    add migration

--- schema/crdb/dbinit.sql
+++ schema/crdb/dbinit.sql
@@ -8297,7 +8297,7 @@ INSERT INTO omicron.public.db_metadata (
     version,
     target_version
 ) VALUES
-    (TRUE, NOW(), NOW(), '244.0.0', NULL)
+    (TRUE, NOW(), NOW(), '245.0.0', NULL)
 ON CONFLICT DO NOTHING;
 
 COMMIT;
