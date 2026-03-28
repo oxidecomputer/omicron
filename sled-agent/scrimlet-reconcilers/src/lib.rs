37f505e2ac95065cd45066a71e8bc2aa4941a4bd
John Gallagher <john@oxidecomputer.com>
2026-03-27 22:06:49 -0400

    initial scrimlet-reconcilers framework

--- sled-agent/scrimlet-reconcilers/src/lib.rs
+++ sled-agent/scrimlet-reconcilers/src/lib.rs
@@ -1,14 +1,25 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+//! TODO: crate-level docs
+//!
+//! For now, see <https://github.com/oxidecomputer/omicron/issues/10167>.
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+mod dpd_reconciler;
+mod handle;
+mod reconciler_task;
+mod status;
+mod switch_zone_underlay_ip;
+
+pub use handle::ScrimletReconcilers;
+pub use handle::ScrimletReconcilersPrereqs;
+pub use status::ReconcilerActivationReason;
+pub use status::ReconcilerCurrentStatus;
+pub use status::ReconcilerInertReason;
+pub use status::ReconcilerRunningStatus;
+pub use status::ReconcilerStatus;
+pub use status::ReconciliationCompletedStatus;
+pub use status::ScrimletReconcilersStatus;
+pub use status::ScrimletStatus;
+pub use switch_zone_underlay_ip::ThisSledSwitchZoneUnderlayIpAddr;
