627760ef766bbaf1797747fc1d6546eb4f022cf0
David Crespo <david.crespo@oxidecomputer.com>
2026-04-01 11:19:14 -0400

    split migration file in two

--- schema/crdb/cleanup-orphaned-subnet-pool-silo-links/up01.sql
+++ schema/crdb/cleanup-orphaned-subnet-pool-silo-links/up01.sql
@@ -6,10 +6,3 @@ WHERE subnet_pool_id NOT IN (
     FROM omicron.public.subnet_pool
     WHERE time_deleted IS NULL
 );
-
-DELETE FROM omicron.public.subnet_pool_silo_link
-WHERE silo_id NOT IN (
-    SELECT id
-    FROM omicron.public.silo
-    WHERE time_deleted IS NULL
-);
