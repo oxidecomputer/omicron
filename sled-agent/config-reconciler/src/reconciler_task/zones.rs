96c54d20a6618552bc70f627fe635742c86719f6
Benjamin Naecker <ben@oxidecomputer.com>
2026-04-25 10:10:37 -0700

    Pin clients to v1 of the NTP admin API for now

--- sled-agent/config-reconciler/src/reconciler_task/zones.rs
+++ sled-agent/config-reconciler/src/reconciler_task/zones.rs
@@ -26,7 +26,7 @@ use illumos_utils::zone::Api as _;
 use illumos_utils::zone::DeleteAddressError;
 use illumos_utils::zone::OmicronZoneConfigExt;
 use illumos_utils::zone::Zones;
-use ntp_admin_client::types::TimeSync;
+use ntp_admin_client::v1::types::TimeSync;
 use omicron_common::address::Ipv6Subnet;
 use omicron_uuid_kinds::OmicronZoneUuid;
 use sled_agent_types::inventory::ConfigReconcilerInventoryResult;
@@ -77,7 +77,9 @@ pub enum TimeSyncError {
     #[error("multiple running NTP zones - this should never happen!")]
     MultipleRunningNtpZones,
     #[error("failed to communicate with NTP admin server")]
-    NtpAdmin(#[from] ntp_admin_client::Error<ntp_admin_client::types::Error>),
+    NtpAdmin(
+        #[from] ntp_admin_client::v1::Error<ntp_admin_client::v1::types::Error>,
+    ),
     #[error("failed to execute chronyc within NTP zone")]
     ExecuteChronyc(#[source] RunCommandError),
     #[error(
@@ -534,12 +536,12 @@ impl OmicronZones {
                 match self.timesync_status_from_ntp_zone(log).await {
                     Ok(timesync) => {
                         if !timesync.sync {
+                            // TODO-completeness: Include maximum error and
+                            // dispersion after we update to the new client.
                             warn!(
                                 log,
                                 "time is not yet synchronized";
                                 "correction_s" => timesync.correction,
-                                "max_error_s" => timesync.max_error,
-                                "root_dispersion_s" => timesync.root_dispersion,
                                 "stratum" => timesync.stratum,
                             );
                         }
@@ -587,7 +589,7 @@ impl OmicronZones {
             return Err(TimeSyncError::MultipleRunningNtpZones);
         }
 
-        let client = ntp_admin_client::Client::new(
+        let client = ntp_admin_client::v1::Client::new(
             &format!("http://{ntp_admin_address}"),
             log.clone(),
         );
--- sled-agent/src/rack_setup/service.rs
+++ sled-agent/src/rack_setup/service.rs
@@ -91,8 +91,8 @@ use nexus_types::deployment::{
     Blueprint, BlueprintZoneType, blueprint_zone_type,
 };
 use nexus_types::internal_api::params::ExternalPortDiscovery;
-use ntp_admin_client::ClientInfo as _;
-use ntp_admin_client::{
+use ntp_admin_client::v1::ClientInfo as _;
+use ntp_admin_client::v1::{
     Client as NtpAdminClient, Error as NtpAdminError, types::TimeSync,
 };
 use omicron_common::address::BOOTSTRAP_AGENT_HTTP_PORT;
@@ -210,7 +210,7 @@ pub enum SetupServiceError {
     NexusApi(#[from] NexusError<NexusTypes::Error>),
 
     #[error("Error making HTTP request to NTP Admin Server")]
-    NtpAdminApi(#[from] NtpAdminError<ntp_admin_client::types::Error>),
+    NtpAdminApi(#[from] NtpAdminError<ntp_admin_client::v1::types::Error>),
 
     #[error("Error contacting ddmd")]
     DdmError(#[from] DdmError),
