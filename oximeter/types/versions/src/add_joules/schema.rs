f243d6a397b8d148105a247e8eeeb375cadf8e16
John Gallagher <john@oxidecomputer.com>
2026-04-27 12:40:12 -0400

    new API version that includes joules

--- oximeter/types/versions/src/add_joules/schema.rs
+++ oximeter/types/versions/src/add_joules/schema.rs
@@ -22,6 +22,10 @@ use serde::Serialize;
 use std::collections::BTreeSet;
 use std::num::NonZeroU8;
 
+#[derive(Debug, thiserror::Error)]
+#[error("new Units value (`Joules`) cannot be represented in old API version")]
+pub struct UnitsConversionJoulesError;
+
 /// Measurement units for timeseries samples.
 #[derive(Clone, Copy, Debug, Deserialize, Serialize, JsonSchema, PartialEq)]
 #[serde(rename_all = "snake_case")]
@@ -43,6 +47,26 @@ pub enum Units {
     Joules,
 }
 
+impl TryFrom<Units> for crate::v1::schema::Units {
+    type Error = UnitsConversionJoulesError;
+
+    fn try_from(value: Units) -> Result<Self, Self::Error> {
+        match value {
+            Units::None => Ok(Self::None),
+            Units::Count => Ok(Self::Count),
+            Units::Bytes => Ok(Self::Bytes),
+            Units::Seconds => Ok(Self::Seconds),
+            Units::Nanoseconds => Ok(Self::Nanoseconds),
+            Units::Volts => Ok(Self::Volts),
+            Units::Amps => Ok(Self::Amps),
+            Units::Watts => Ok(Self::Watts),
+            Units::DegreesCelsius => Ok(Self::DegreesCelsius),
+            Units::Rpm => Ok(Self::Rpm),
+            Units::Joules => Err(UnitsConversionJoulesError),
+        }
+    }
+}
+
 /// The schema for a timeseries.
 ///
 /// This includes the name of the timeseries, as well as the datum type of its metric and the
@@ -57,4 +81,23 @@ pub struct TimeseriesSchema {
     pub authz_scope: AuthzScope,
     pub units: Units,
     pub created: DateTime<Utc>,
+}
+
+impl TryFrom<TimeseriesSchema> for crate::v1::schema::TimeseriesSchema {
+    type Error = UnitsConversionJoulesError;
+
+    fn try_from(value: TimeseriesSchema) -> Result<Self, Self::Error> {
+        let units = value.units.try_into()?;
+
+        Ok(Self {
+            timeseries_name: value.timeseries_name,
+            description: value.description,
+            field_schema: value.field_schema,
+            datum_type: value.datum_type,
+            version: value.version,
+            authz_scope: value.authz_scope,
+            units,
+            created: value.created,
+        })
+    }
 }
