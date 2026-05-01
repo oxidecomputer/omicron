85f0bc11e8297db677c01047a5cacd41b49d05a7
Benjamin Naecker <ben@oxidecomputer.com>
2026-05-01 08:48:23 -0700

    add note

--- ntp-admin/api/src/lib.rs
+++ ntp-admin/api/src/lib.rs
@@ -7,32 +7,17 @@ use dropshot_api_manager_types::api_versions;
 use ntp_admin_types_versions::{latest, v1};
 
 api_versions!([
-    // WHEN CHANGING THE API (part 1 of 2):
+    // Do not create new versions of this client-side versioned API.
+    // https://github.com/oxidecomputer/omicron/issues/9290
     //
-    // +- Pick a new semver and define it in the list below.  The list MUST
-    // |  remain sorted, which generally means that your version should go at
-    // |  the very top.
-    // |
-    // |  Duplicate this line, uncomment the *second* copy, update that copy for
-    // |  your new API version, and leave the first copy commented out as an
-    // |  example for the next person.
-    // v
-    // (next_int, IDENT),
+    // NOTE: Version 2 of this API was added to address
+    // //github.com/oxidecomputer/omicron/issues/7668, a bad failure mode in
+    // which CRDB panics early during control plane startup because the clocks
+    // are not synchronized well-enough. We're adding this as part of a
+    // two-phase rollout to get around #9290 for now.
     (2, ADD_MAX_ERROR_AND_OFFSET),
     (1, INITIAL),
 ]);
-
-// WHEN CHANGING THE API (part 2 of 2):
-//
-// The call to `api_versions!` above defines constants of type
-// `semver::Version` that you can use in your Dropshot API definition to specify
-// the version when a particular endpoint was added or removed.  For example, if
-// you used:
-//
-//     (2, ADD_FOOBAR)
-//
-// Then you could use `VERSION_ADD_FOOBAR` as the version in which endpoints
-// were added or removed.
 
 #[dropshot::api_description]
 pub trait NtpAdminApi {
