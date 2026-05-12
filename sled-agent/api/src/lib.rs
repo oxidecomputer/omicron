a2d3940c5aebc875c714c64a75f4383325b78d82
John Gallagher <john@oxidecomputer.com>
2026-05-12 09:41:01 -0400

    `sync_switch_configuration`: Don't allow dueling Nexuses based on a stale blueprint (#10336)
    This commit has two primary changes:
    
    * Change the bootstore's `SystemNetworkingConfig` type to have an extra
    generation number inside it attached to the service NAT entries. This is
    a copy of the blueprint's `external_networking_generation` number from
    the blueprint where the NAT entries were extracted. (This requires
    bumping the sled-agent API and all the extra machinery for changing the
    bootstore type; this is a little noisy but very mechanical.)
    * Change the `sync_switch_configuration` bg task to inspect this
    generation number and skip updating the bootstore if its
    currently-loaded blueprint has an older `external_networking_generation`
    (i.e., is stale).
    
    Fixes #10320.

--- sled-agent/api/src/lib.rs
+++ sled-agent/api/src/lib.rs
@@ -21,7 +21,7 @@ use omicron_common::api::internal::{
 };
 use sled_agent_types_versions::{
     latest, v1, v4, v6, v7, v9, v10, v11, v12, v14, v16, v17, v18, v20, v22,
-    v24, v25, v26, v28, v29, v30, v31, v33, v34,
+    v24, v25, v26, v28, v29, v30, v31, v33, v34, v39,
 };
 use sled_diagnostics::SledDiagnosticsQueryOutput;
 use slog_error_chain::InlineErrorChain;
@@ -38,6 +38,7 @@ api_versions!([
     // |  example for the next person.
     // v
     // (next_int, IDENT),
+    (39, BOOTSTORE_SERVICE_NAT_GENERATION),
     (38, RENAME_PORT_FEC_SPEED_TO_LINK_FEC_SPEED),
     (37, MODIFY_SVC_ENABLED_NOT_ONLINE_STATE),
     (36, DROPSHOT_FREEFORM_BODY_DESC),
@@ -937,7 +938,20 @@ pub trait SledAgentApi {
     #[endpoint {
         method = PUT,
         path = "/network-bootstore-config",
-        versions = VERSION_BOOTSTORE_SERVICE_NAT..,
+        versions = VERSION_BOOTSTORE_SERVICE_NAT_GENERATION..,
+        operation_id = "write_network_bootstore_config",
+    }]
+    async fn write_network_bootstore_config_v39(
+        rqctx: RequestContext<Self::Context>,
+        body: TypedBody<v39::system_networking::WriteNetworkConfigRequest>,
+    ) -> Result<HttpResponseUpdatedNoContent, HttpError>;
+
+    // As described above, this must not forward to newer versions; sled-agent
+    // must implement this by faithfully serializing the requested version.
+    #[endpoint {
+        method = PUT,
+        path = "/network-bootstore-config",
+        versions = VERSION_BOOTSTORE_SERVICE_NAT..VERSION_BOOTSTORE_SERVICE_NAT_GENERATION,
         operation_id = "write_network_bootstore_config",
     }]
     async fn write_network_bootstore_config_v33(
