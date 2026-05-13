176b9ffc9396fd476c2a7e8ff2e05a5b88a7585c
Benjamin Naecker <ben@oxidecomputer.com>
2026-05-12 17:09:09 -0700

    Add omdb command to query for timesync status

---
+++ dev-tools/omdb/src/bin/omdb/timesync.rs
@@ -0,0 +1,207 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+//! Check status of time-synchronization.
+
+use std::net::IpAddr;
+
+use crate::Omdb;
+use clap::Args;
+use clap::Subcommand;
+use internal_dns_types::names::ServiceName;
+use ntp_admin_client::Client;
+use ntp_admin_client::types::TimeSync;
+use slog::Logger;
+use slog::debug;
+use slog::error;
+use tabled::Table;
+use tabled::Tabled;
+
+/// Arguments for the timesync subcommand.
+#[derive(Debug, Args)]
+pub struct TimesyncArgs {
+    #[command(subcommand)]
+    command: TimesyncSubcommands,
+}
+
+/// Subcommands that query time synchronization status
+#[derive(Debug, Subcommand)]
+enum TimesyncSubcommands {
+    /// Check the status of timesync across all NTP servers in DNS.
+    Status,
+}
+
+impl TimesyncArgs {
+    /// Run the command.
+    pub async fn run_cmd(
+        &self,
+        omdb: &Omdb,
+        log: &Logger,
+    ) -> anyhow::Result<()> {
+        match &self.command {
+            TimesyncSubcommands::Status => {
+                self.timesync_status(omdb, log).await
+            }
+        }
+    }
+
+    async fn timesync_status(
+        &self,
+        omdb: &Omdb,
+        log: &Logger,
+    ) -> anyhow::Result<()> {
+        // First boundary NTP
+        let boundaries = match omdb
+            .dns_lookup_all(log.clone(), ServiceName::BoundaryNtp)
+            .await
+        {
+            Ok(s) => {
+                debug!(
+                    log,
+                    "looked up boundary NTP services";
+                    "n_records" => s.len()
+                );
+                s
+            }
+            Err(e) => {
+                error!(
+                    log,
+                    "failed to look up boundary NTP SRV records";
+                    "error" => e.to_string(),
+                );
+                vec![]
+            }
+        };
+
+        // Then internal
+        let internal = match omdb
+            .dns_lookup_all(log.clone(), ServiceName::InternalNtp)
+            .await
+        {
+            Ok(s) => {
+                debug!(
+                    log,
+                    "looked up internal NTP services";
+                    "n_records" => s.len()
+                );
+                s
+            }
+            Err(e) => {
+                error!(
+                    log,
+                    "failed to look up internal NTP SRV records";
+                    "error" => e.to_string(),
+                );
+                vec![]
+            }
+        };
+
+        // Fetch timesync from all of them.
+        let mut rows = Vec::with_capacity(boundaries.len() + internal.len());
+        let boundaries = boundaries.into_iter().zip(std::iter::repeat(true));
+        let internal = internal.into_iter().zip(std::iter::repeat(false));
+        for (sock, is_boundary) in boundaries.chain(internal) {
+            let url = format!("http:://{sock}");
+            let client = Client::new(&url, log.clone());
+            let result = match client.timesync().await.map(|t| t.into_inner()) {
+                Ok(ts) => {
+                    let TimeSync {
+                        correction,
+                        ip_addr,
+                        last_offset,
+                        max_error,
+                        ref_id,
+                        ref_time,
+                        rms_offset,
+                        root_delay,
+                        root_dispersion,
+                        stratum,
+                        sync,
+                    } = ts;
+                    TimesyncStatus {
+                        is_boundary,
+                        is_synchronized: sync,
+                        upstream_ip: ip_addr,
+                        ref_id,
+                        stratum,
+                        ref_time,
+                        last_offset,
+                        rms_offset,
+                        correction,
+                        root_delay,
+                        root_dispersion,
+                        max_error,
+                    }
+                }
+                Err(e) => {
+                    error!(
+                        log,
+                        "failed to collect timesync status";
+                        "address" => %sock,
+                        "is_boundary" => is_boundary,
+                        "error" => e.to_string(),
+                    );
+                    continue;
+                }
+            };
+            rows.push(result);
+        }
+        let table = Table::new(rows)
+            .with(tabled::settings::Style::empty())
+            .with(tabled::settings::Padding::new(0, 1, 0, 0))
+            .to_string();
+        println!("{table}");
+        Ok(())
+    }
+}
+
+#[derive(Debug, Tabled)]
+#[tabled(rename_all = "SCREAMING_SNAKE_CASE")]
+struct TimesyncStatus {
+    /// True if this is a boundary NTP server, false otherwise.
+    is_boundary: bool,
+    /// True if the NTP admin server reports that time is synchronized.
+    is_synchronized: bool,
+    /// IP address of the current upstream source server.
+    upstream_ip: IpAddr,
+    /// NTP reference ID (derived from the IP address)
+    #[tabled(display_with = "display_ref_id")]
+    ref_id: u32,
+    /// Our NTP server's stratum (the upstream stratum plus one).
+    stratum: u8,
+    /// The "true" NTP reference time that the NTP server thinks it is.
+    #[tabled(display_with = "display_secs_f64")]
+    ref_time: f64,
+    /// The offset between the local and upstream NTP clocks at the last
+    /// measurement.
+    #[tabled(display_with = "display_secs_f64")]
+    last_offset: f64,
+    /// Root-mean-square of the offsets between local and upstream NTP clocks.
+    #[tabled(display_with = "display_secs_f64")]
+    rms_offset: f64,
+    /// The current offset between this server's NTP time and the system clock
+    /// time.
+    #[tabled(display_with = "display_secs_f64")]
+    correction: f64,
+    /// The total delay along the network path from this server to the stratum 1
+    /// servers from which we're ultimately deriving our time.
+    #[tabled(display_with = "display_secs_f64")]
+    root_delay: f64,
+    /// The total accumulated error along the path from this server to the
+    /// stratum 1 server. This is due to system clock resolution, measurement
+    /// errors, network jitter, etc.
+    #[tabled(display_with = "display_secs_f64")]
+    root_dispersion: f64,
+    /// The absolute upper bound on the local clock's error from true NTP time.
+    #[tabled(display_with = "display_secs_f64")]
+    max_error: f64,
+}
+
+fn display_secs_f64(s: &f64) -> String {
+    format!("{s} s")
+}
+
+fn display_ref_id(x: &u32) -> String {
+    format!("{x:x}")
+}
