46443084c7c27db3749b2b05c3415fefda16e632
Eliza Weisman <eliza@elizas.website>
2026-06-01 11:07:06 -0700

    db table for sitrep analysis reports

--- nexus/db-model/src/fm.rs
+++ nexus/db-model/src/fm.rs
@@ -28,6 +28,8 @@ mod diagnosis_engine;
 pub use diagnosis_engine::*;
 mod support_bundle_request;
 pub use support_bundle_request::*;
+mod sitrep_analysis_report;
+pub use sitrep_analysis_report::*;
 
 #[derive(Queryable, Insertable, Clone, Debug, Selectable)]
 #[diesel(table_name = fm_sitrep)]
