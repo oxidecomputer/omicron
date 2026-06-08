767759a34fb780edf84e2a5c8b961ac94b92003d
James MacMahon <james@oxide.computer>
2026-06-08 21:34:57 +0000

    add data migration test for sled_resource_vmm_state

--- schema/crdb/sled-resource-vmm-state/up03.sql
+++ schema/crdb/sled-resource-vmm-state/up03.sql
@@ -1,4 +1,29 @@
-CREATE UNIQUE INDEX IF NOT EXISTS single_vmm_reservation_per_state ON omicron.public.sled_resource_vmm (
-    instance_id,
-    state
-) WHERE state != 'tombstoned';
+SET LOCAL disallow_full_table_scans = off;
+
+WITH
+ sled_resource_vmm_update AS (
+  SELECT
+    sled_resource_vmm.id,
+    CAST(
+      (CASE
+       WHEN sled_resource_vmm.id = instance.active_propolis_id THEN 'active'
+       WHEN sled_resource_vmm.id = instance.target_propolis_id THEN 'target'
+       ELSE 'tombstoned'
+       END) AS omicron.public.sled_resource_vmm_state
+    ) AS state
+  FROM
+    instance
+  RIGHT JOIN
+    sled_resource_vmm
+  ON
+    sled_resource_vmm.instance_id = instance.id
+ )
+UPDATE
+ sled_resource_vmm
+SET
+ state = sled_resource_vmm_update.state
+FROM
+ sled_resource_vmm_update
+WHERE
+ sled_resource_vmm.id = sled_resource_vmm_update.id
+;
