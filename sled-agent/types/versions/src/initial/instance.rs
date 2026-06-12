a39258d7fb7a778815b533c4f203209c11e7ebe2
iximeow <iximeow@oxide.computer>
2026-06-12 03:28:13 +0000

    Revert "sled-agent-types does not have a dep on nexus_types and I'm not adding one for docs."
    This reverts commit 9843c7e5fc36fdfa831df01b38a5e54d0980a351.

--- sled-agent/types/versions/src/initial/instance.rs
+++ sled-agent/types/versions/src/initial/instance.rs
@@ -246,7 +246,7 @@ pub struct VmmPutStateResponse {
 
 /// Requestable running state of an Instance.
 ///
-/// A subset of `nexus_types::external_api::instance::InstanceState`.
+/// A subset of [`nexus_types::external_api::instance::InstanceState`].
 #[derive(Copy, Clone, Debug, Deserialize, Serialize, JsonSchema)]
 #[serde(rename_all = "snake_case", tag = "type", content = "value")]
 pub enum VmmStateRequested {
