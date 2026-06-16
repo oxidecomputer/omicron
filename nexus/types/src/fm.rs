8b0886dbd0222992175f34f9ab7bab48b304b0ad
Eliza Weisman <eliza@elizas.website>
2026-06-16 10:14:32 -0700

    [fm] stash sitrep analysis reports in the db for retrieval by omdb (#10556)
    As described in #10505, the `fm_analysis` background task that produces
    a sitrep also emits two human-readable report types that contain
    debugging information describing the analysis step that produced the
    sitrep, and the inputs to that analysis step. Currently, these are
    included in the status object for the `fm_analysis` task, meaning that
    they can be viewed by `omdb nexus background-tasks status`. However,
    once a new analysis step has begun, they are no longer accessible. And,
    even if no new analysis has been performed yet, accessing them through
    `omdb nexus background-tasks status` requires tracking down the Nexus
    that actually produced a given sitrep.
    
    This branch extends this system to also store these reports in a
    CockroachDB table, so that they may be retrieved and displayed by
    `omdb`. Now, they are accessible as long as the sitrep that they
    describe remains in the database. These entries are children of the
    sitrep, so that they can be GCed when the sitrep is deleted. In
    addition, this branch adds an `omdb db sitrep analysis-reports`
    subcommand for retrieving and displaying these reports. We attempt to
    use the typed representation from `nexus_types::fm` when possible, but
    fall back to printing whatever JSON is stored when the value cannot be
    decoded as the typed representation (i.e. because it changed in an
    incompatible way). This way, we can always display historical reports
    from older Nexus versions, but we don't have to worry too much about
    versioning this debug-only data. We also include the Git SHA of the
    Nexus build that produced a report so that we can include it in the omdb
    output, which is especially helpful when the JSON can't deserialize as
    the current typed representation.
    
    Closes #10505

--- nexus/types/src/fm.rs
+++ nexus/types/src/fm.rs
@@ -12,7 +12,7 @@ pub mod ereport;
 pub use ereport::{Ereport, EreportId};
 pub mod case;
 pub use case::Case;
-pub(crate) mod json_display;
+pub mod json_display;
 
 use case::AlertRequest;
 use chrono::{DateTime, Utc};
@@ -67,6 +67,12 @@ impl Sitrep {
 
     pub fn parent_id(&self) -> Option<SitrepUuid> {
         self.metadata.parent_sitrep_id
+    }
+
+    /// Returns the inventory collection ID of the collection that was used as
+    /// input to this sitrep.
+    pub fn inv_id(&self) -> CollectionUuid {
+        self.metadata.inv_collection_id
     }
 
     /// Iterate over all the open cases in this sitrep.
