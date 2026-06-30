fb00570165b42737c426f5b91a6d86a33014b098
iliana etaoin <iliana@oxide.computer>
2026-06-30 04:47:25 +0000

    releng: better dynamic library verification

--- dev-tools/common/src/lib.rs
+++ dev-tools/common/src/lib.rs
@@ -3,5 +3,9 @@
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
 mod cargo_command;
+mod verify_libraries;
+mod xtask_config;
 
 pub use cargo_command::{CargoLocation, cargo_command};
+pub use verify_libraries::{LibraryError, verify_executable_libraries};
+pub use xtask_config::{LibraryConfig, XtaskConfig};
