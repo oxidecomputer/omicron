0e61155449f23abc432f6819a0303228b7a3f7c2
Rain <rain@oxide.computer>
2026-06-30 13:21:42 -0700

    [9/n] [sync-switch] bail on an incomplete bootstore config instead of silently dropping items (#10650)
    Previously, on encountering a transient DB issue, we would silently drop
    items from the bootstore config. That seems quite obviously bad and was
    part of the reason #10640 escalated a crdb bug into persistent bootstore
    corruption.
    
    Rework all this by:
    
    * collecting all the problems that occurred
    * returning a new error type if there were any problems
    
    **This is a functional change that is worth thinking very hard about.**
    Many but not all of the invariants here are maintained by the database
    layer. If there's persistent db corruption or a similar issue, we will
    no longer generate a bootstore config for the rack! I think this is
    overall the right thing to do, but it is quite possible there are latent
    issues that were being hidden by the looser error handling that this
    code previously had.
    
    The report is now logged by the background task, unless the task exits
    with one of the other early errors. (There's a much larger rework of the
    task that needs to be done, but I'm not doing that here since it'll
    collide with a lot of what @jgallagher is currently doing.)
    
    Depends on:
    
    * #10642
    * #10643
    * #10644
    * #10645
    * #10646
    * #10647
    * #10648
    * #10649

--- nexus/src/app/background/tasks/sync_switch_configuration.rs
+++ nexus/src/app/background/tasks/sync_switch_configuration.rs
@@ -54,11 +54,15 @@ use nexus_db_queries::{
 };
 use nexus_types::external_api::networking;
 use nexus_types::identity::{Asset, Resource};
+use nexus_types::internal_api::background::IncompleteBootstoreConfigReport;
+use nexus_types::internal_api::background::SwitchPortSettingsManagerStatus;
 use omicron_common::OMICRON_DPD_TAG;
 use omicron_common::{
     address::{Ipv6Subnet, get_sled_address},
     api::external::DataPageParams,
 };
+use omicron_uuid_kinds::GenericUuid;
+use omicron_uuid_kinds::RackUuid;
 use serde_json::json;
 use sled_agent_client::types::HostPortConfig;
 use sled_agent_types::early_networking::EarlyNetworkConfigEnvelope;
@@ -293,6 +297,8 @@ impl BackgroundTask for SwitchPortSettingsManager {
                     });
                 },
             };
+
+            let mut status = SwitchPortSettingsManagerStatus::default();
 
             // TODO: https://github.com/oxidecomputer/omicron/issues/3090
             // Here we're iterating over racks because that's technically the correct thing to do,
@@ -980,10 +986,31 @@ impl BackgroundTask for SwitchPortSettingsManager {
 
                 let rack_network_config =
                     match nexus_switch_config::build_rack_network_config(
-                        &log, input,
+                        input,
                     ) {
-                        Some(rack_network_config) => rack_network_config,
-                        None => continue,
+                        Ok(rack_network_config) => rack_network_config,
+                        Err(report) => {
+                            error!(
+                                log,
+                                "incomplete bootstore network config; \
+                                 skipping rack";
+                                "problems" => %report,
+                            );
+                            status.incomplete_bootstore_configs.push(
+                                IncompleteBootstoreConfigReport {
+                                    // TODO: `Rack` should use newtype-uuid.
+                                    rack_id: RackUuid::from_untyped_uuid(
+                                        rack.id(),
+                                    ),
+                                    problems: report
+                                        .problems
+                                        .iter()
+                                        .map(|problem| problem.to_string())
+                                        .collect(),
+                                },
+                            );
+                            continue;
+                        }
                     };
 
                 let (
@@ -1213,7 +1240,14 @@ impl BackgroundTask for SwitchPortSettingsManager {
                     }
                 }
             }
-            json!({})
+            // TODO: we have some early returns in this task. We should instead
+            // collect all problems and return them in the response.
+            //
+            // As part of that, we should move the body into an
+            // `activate_impl(...)` function which returns a
+            // `SwitchPortManagerStatus`. That'll force us to not do early
+            // returns.
+            json!(status)
         }
         .boxed()
     }
