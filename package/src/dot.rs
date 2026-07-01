7bda923e28f90b65acba3d12d8893762e06bb15b
David Crespo <david-crespo@users.noreply.github.com>
2026-07-01 14:04:08 -0500

    Add skywalking-eyes license scanner action (#10708)
    And fix em all.
    
    I am not touching the copyright lines, but if it were up to me either
    would either:
    
    * delete them all, or
    * take the year out and make it part of the license header that
    `license-eye` enforces.
    
    We're not using it consistently anyway. Claude says: "of 1993 .rs files,
    only 153 (~8%) carry the line".

--- package/src/dot.rs
+++ package/src/dot.rs
@@ -1,3 +1,7 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 //! Visualize the package manifest using Graphviz
 
 use anyhow::anyhow;
