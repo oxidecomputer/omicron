655cb0f0fc07e881252597fdafb46ffb1b73969b
David Crespo <david.crespo@oxidecomputer.com>
2026-07-01 09:27:29 -0500

    fix em

--- end-to-end-tests/src/bin/commtest.rs
+++ end-to-end-tests/src/bin/commtest.rs
@@ -1,3 +1,7 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 use anyhow::{Result, anyhow};
 use clap::{Parser, Subcommand};
 use end_to_end_tests::helpers::icmp::ping4_test_run;
