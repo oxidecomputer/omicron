3de07b0045dc591659706800635ec7f9ff1abff1
Eliza Weisman <eliza@elizas.website>
2026-07-06 15:13:14 -0700

    STORING clause on select_next_for_dispatch's index

--- schema/crdb/alert-list-matching-indexes/up02.sql
+++ schema/crdb/alert-list-matching-indexes/up02.sql
@@ -1,4 +1,18 @@
 CREATE INDEX IF NOT EXISTS lookup_alerts_by_time_dispatched
 ON omicron.public.alert (
     time_dispatched
+)
+-- Since this is the query that's actually used operationally (by
+-- `alert_select_next_for_dispatch`), make it a covering index to optimize the
+-- background task's performance. This will also make *some* of thte
+-- `alert_fetch_matching` queries that filter by time dispatched a bit nicer by
+-- eliminating some of the index joins.
+STORING (
+    time_created,
+    time_modified,
+    alert_class,
+    payload,
+    num_dispatched,
+    case_id,
+    alert_version
 );
