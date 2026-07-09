d6c7d6270af0d903673d8af438ae2b6a2a06f6f4
Eliza Weisman <eliza@elizas.website>
2026-07-09 12:26:44 -0700

    turns out that i was holding `check_if_exists` wrong

--- nexus/db-queries/src/db/datastore/instance.rs
+++ nexus/db-queries/src/db/datastore/instance.rs
@@ -1612,47 +1612,68 @@ impl DataStore {
                 "current_gen" => ?current_gen,
             );
 
-            (instance, did_lock) = diesel::update(dsl::instance)
-                .filter(dsl::time_deleted.is_null())
-                .filter(dsl::id.eq(instance_id))
-                // If the generation is the same as the captured generation when we
-                // read the instance record to check if it was not locked, we can
-                // lock this instance. This is because changing the `updater_id`
-                // field always increments the generation number. Therefore, we
-                // want the update query to succeed if and only if the
-                // generation number remains the same as the generation when we
-                // last fetched the instance. This query is used equivalently to
-                // an atomic compare-and-swap instruction in the implementation
-                // of a non-distributed, single-process mutex.
-                .filter(dsl::updater_gen.eq(current_gen))
-                .set((
-                    dsl::updater_gen.eq(locked_gen),
-                    dsl::updater_id.eq(Some(updater_id)),
-                ))
-                .check_if_exists::<Instance>(instance_id)
-                .execute_and_check(
-                    &*self.pool_connection_authorized(opctx).await?,
-                )
-                .await
-                .map(|r| {
-                    // If we successfully updated the instance record, we have
-                    // acquired the lock; otherwise, we haven't --- either because
-                    // our generation is stale, or because the instance is already locked.
-                    let locked = match r.status {
-                        UpdateStatus::Updated => true,
-                        UpdateStatus::NotUpdatedButExists => false,
-                    };
-                    (r.found, locked)
-                })
-                .map_err(|e| {
-                    public_error_from_diesel(
-                        e,
-                        ErrorHandler::NotFoundByLookup(
-                            ResourceType::Instance,
-                            LookupType::ById(instance_id),
-                        ),
+            let UpdateAndQueryResult { status, found } =
+                diesel::update(dsl::instance)
+                    .filter(dsl::time_deleted.is_null())
+                    .filter(dsl::id.eq(instance_id))
+                    // If the generation is the same as the captured generation
+                    // when we read the instance record to check if it was not
+                    // locked, we can lock this instance. This is because
+                    // changing the `updater_id` field always increments the
+                    // generation number. Therefore, we want the update query to
+                    // succeed if and only if the generation number remains the
+                    // same as the generation when we last fetched the instance.
+                    // This query is used equivalently to an atomic
+                    // compare-and-swap instruction in the implementation of a
+                    // non-distributed, single-process mutex.
+                    .filter(dsl::updater_gen.eq(current_gen))
+                    .set((
+                        dsl::updater_gen.eq(locked_gen),
+                        dsl::updater_id.eq(Some(updater_id)),
+                    ))
+                    .check_if_exists::<Instance>(instance_id)
+                    .execute_and_check(
+                        &*self.pool_connection_authorized(opctx).await?,
                     )
-                })?;
+                    .await
+                    .map_err(|e| {
+                        public_error_from_diesel(
+                            e,
+                            ErrorHandler::NotFoundByLookup(
+                                ResourceType::Instance,
+                                LookupType::ById(instance_id),
+                            ),
+                        )
+                    })?;
+
+            match status {
+                // If we successfully updated the instance record, we have
+                // acquired the lock.
+                UpdateStatus::Updated => {
+                    did_lock = true;
+                }
+                // If the instance record has been soft-deleted, return
+                // `NotFound` explicitly. `check_if_exists` does not know
+                // about soft-deletion, so it will still return that it
+                // `found` something even if its `time_deleted` is set.
+                UpdateStatus::NotUpdatedButExists
+                    if found.time_deleted().is_some() =>
+                {
+                    return Err(Error::not_found_by_id(
+                        ResourceType::Instance,
+                        &instance_id,
+                    )
+                    .into());
+                }
+                // Otherwise, the instance still exists but we haven't
+                // locked it --- either because our generation is stale, or
+                // because the instance is already locked.
+                UpdateStatus::NotUpdatedButExists => {
+                    did_lock = false;
+                }
+            };
+
+            instance = found;
         }
     }
 
