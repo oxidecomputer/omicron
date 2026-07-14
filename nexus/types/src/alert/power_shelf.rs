a867eb5084cb30c8e76a4bf788f352bd469ac937
Eliza Weisman <eliza@elizas.website>
2026-07-14 10:18:30 -0700

    doc comments for alert schemas

--- nexus/types/src/alert/power_shelf.rs
+++ nexus/types/src/alert/power_shelf.rs
@@ -12,6 +12,8 @@ use omicron_uuid_kinds::RackUuid;
 use serde::Deserialize;
 use uuid::Uuid;
 
+/// An alert indicating that a power supply unit (PSU) has been inserted into a
+/// power shelf.
 #[derive(Debug, Clone, Serialize, Deserialize, JsonSchema)]
 pub struct PsuInsertedV0 {
     pub power_shelf: PowerShelf,
@@ -24,6 +26,8 @@ impl AlertPayload for PsuInsertedV0 {
     const VERSION: u32 = 0;
 }
 
+/// An alert indicating that a power supply unit (PSU) has been removed from a
+/// power shelf.
 #[derive(Debug, Clone, Serialize, Deserialize, JsonSchema)]
 pub struct PsuRemovedV0 {
     pub power_shelf: PowerShelf,
@@ -36,22 +40,41 @@ impl AlertPayload for PsuRemovedV0 {
     const VERSION: u32 = 0;
 }
 
+/// Describes the power shelf involved in an alert.
 #[derive(Debug, Clone, Serialize, Deserialize, JsonSchema)]
 pub struct PowerShelf {
     // TODO(eliza): the PSC should probably have a UUID, but AFAICT, they don't
     // currently...
+    /// The ID of the rack containing the power shelf.
     #[schemars(with = "Uuid")]
     pub rack_id: RackUuid,
+    /// The physical shelf number of the power shelf involved in the alert.
+    ///
+    /// This will always be either 0 or 1.
     pub shelf: u8,
+    /// The baseboard FRU identity of the power shelf controller that was
+    /// installed in the power shelf at the time the event occurred.
     pub baseboard: Option<Baseboard>,
 }
 
+/// Describes a power supply unit (PSU) involved in an alert.
 #[derive(Debug, Clone, Serialize, Deserialize, JsonSchema)]
 pub struct Psu {
+    /// The slot number of the PSU within the power shelf.
+    ///
+    /// This will always be an integer between 0 and 5 (inclusive), since each
+    /// power shelf can contain up to 6 PSUs.
     pub slot: u8,
+    /// The FRU identity of the PSU, if it could be determined.
+    ///
+    /// This value may be NULL if the identity of the PSU could not be read due
+    /// to a hardware failure.
     pub identity: Option<PsuIdentity>,
 }
 
+/// Describes the FRU identity of a power supply unit (PSU).
+///
+/// Note that this is *not* in the same format as Oxide FRU identities.
 #[derive(Debug, Clone, Serialize, Deserialize, JsonSchema)]
 pub struct PsuIdentity {
     pub manufacturer: String,
