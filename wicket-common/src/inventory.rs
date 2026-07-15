f5e5099800b2b7d20a31edcf2930b09326f05980
Rain <rain@oxide.computer>
2026-07-15 15:37:41 -0700

    add Debug
    Created using spr 1.3.6-beta.1

--- wicket-common/src/inventory.rs
+++ wicket-common/src/inventory.rs
@@ -56,7 +56,7 @@ impl From<BTreeSet<BootstrapSledDescription>> for SledInventory {
 }
 
 // Inventory about sleds derived from MGS inventory and DDM
-#[derive(Default, Clone, PartialEq, Eq)]
+#[derive(Default, Clone, Debug, PartialEq, Eq)]
 pub struct SledInventory {
     pub sleds: BTreeSet<BootstrapSledDescription>,
 }
