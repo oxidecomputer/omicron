a396ca67cc763098a9461dd838adefcdcdfa109f
Karen Cárcamo <karencfv@users.noreply.github.com>
2026-07-16 23:08:31 -0700

    [saga-recovery] Mark sagas with non-transient errors as abandoned (#10602)
    Closes: https://github.com/oxidecomputer/omicron/issues/10581

--- nexus/db-model/src/schema_versions.rs
+++ nexus/db-model/src/schema_versions.rs
@@ -16,7 +16,7 @@ use std::{collections::BTreeMap, sync::LazyLock};
 ///
 /// This must be updated when you change the database schema.  Refer to
 /// schema/crdb/README.adoc in the root of this repository for details.
-pub const SCHEMA_VERSION: Version = Version::new(278, 0, 0);
+pub const SCHEMA_VERSION: Version = Version::new(279, 0, 0);
 
 /// List of all past database schema versions, in *reverse* order
 ///
@@ -28,7 +28,7 @@ pub static KNOWN_VERSIONS: LazyLock<Vec<KnownVersion>> = LazyLock::new(|| {
         // |  leaving the first copy as an example for the next person.
         // v
         // KnownVersion::new(next_int, "unique-dirname-with-the-sql-files"),
-        //
+        KnownVersion::new(279, "saga-abandonment-metadata"),
         KnownVersion::new(278, "psu-presence-alert-classes"),
         KnownVersion::new(277, "ereporter-restart-latest-ereport"),
         KnownVersion::new(276, "webhook-delivery-list-indices"),
