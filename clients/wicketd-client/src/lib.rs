fbc091a31b17d0473fb86de27b282c47f1cfb08a
iliana etaoin <iliana@oxide.computer>
2026-07-30 00:31:10 -0700

    [RFD 621 2/n] switch to Tufaceous v2 in Wicket and Installinator (#10755)

--- clients/wicketd-client/src/lib.rs
+++ clients/wicketd-client/src/lib.rs
@@ -38,7 +38,7 @@ progenitor::generate_api!(
     replace = {
         AbortUpdateOptions = wicket_common::rack_update::AbortUpdateOptions,
         AllowedSourceIps = omicron_common::api::internal::shared::AllowedSourceIps,
-        ArtifactId = omicron_common::update::ArtifactId,
+        ArtifactId = wicket_common::artifact::ArtifactId,
         Baseboard = sled_hardware_types::Baseboard,
         BaseboardId = sled_hardware_types::BaseboardId,
         BgpAuthKey = wicketd_commission_types_versions::latest::rack_setup::BgpAuthKey,
--- wicketd-api/src/lib.rs
+++ wicketd-api/src/lib.rs
@@ -11,7 +11,6 @@ use dropshot::RequestContext;
 use dropshot::StreamingBody;
 use dropshot::TypedBody;
 use gateway_client::types::IgnitionCommand;
-use omicron_common::update::ArtifactId;
 use omicron_uuid_kinds::RackInitUuid;
 use schemars::JsonSchema;
 use semver::Version;
@@ -22,7 +21,7 @@ use sled_hardware_types::BaseboardId;
 use std::collections::BTreeMap;
 use std::collections::BTreeSet;
 use std::net::Ipv6Addr;
-use tufaceous_artifact::ArtifactHashId;
+use wicket_common::artifact::ArtifactId;
 use wicket_common::inventory::RackV1Inventory;
 use wicket_common::inventory::SpIdentifier;
 use wicket_common::inventory::SpType;
@@ -449,14 +448,6 @@ pub enum GetInventoryResponse {
     Unavailable,
 }
 
-#[derive(Clone, Debug, JsonSchema, Serialize)]
-#[serde(rename_all = "snake_case")]
-pub struct InstallableArtifacts {
-    pub artifact_id: ArtifactId,
-    pub installable: Vec<ArtifactHashId>,
-    pub sign: Option<Vec<u8>>,
-}
-
 /// The response to a `get_artifacts` call: the system version, and the list of
 /// all artifacts currently held by wicketd.
 #[derive(Clone, Debug, JsonSchema, Serialize)]
@@ -464,18 +455,9 @@ pub struct InstallableArtifacts {
 pub struct GetArtifactsAndEventReportsResponse {
     pub system_version: Option<Version>,
 
-    /// Map of artifacts we ingested from the most-recently-uploaded TUF
-    /// repository to a list of artifacts we're serving over the bootstrap
-    /// network. In some cases the list of artifacts being served will have
-    /// length 1 (when we're serving the artifact directly); in other cases the
-    /// artifact in the TUF repo contains multiple nested artifacts inside it
-    /// (e.g., RoT artifacts contain both A and B images), and we serve the list
-    /// of extracted artifacts but not the original combination.
-    ///
-    /// Conceptually, this is a `BTreeMap<ArtifactId, Vec<ArtifactHashId>>`, but
-    /// JSON requires string keys for maps, so we give back a vec of pairs
-    /// instead.
-    pub artifacts: Vec<InstallableArtifacts>,
+    /// List of artifacts we ingested from the most-recently-uploaded TUF
+    /// repository.
+    pub artifacts: Vec<ArtifactId>,
 
     pub event_reports: BTreeMap<SpType, BTreeMap<u16, EventReport>>,
 }
