43bd59b98e56dac52cd3cab9aad81bf82ac7b20e
James MacMahon <james@oxide.computer>
2026-09-01 16:09:13 +0000

    pass doctest

--- sled-agent/types/versions/src/add_nexus_lockstep_port_to_inventory/inventory.rs
+++ sled-agent/types/versions/src/add_nexus_lockstep_port_to_inventory/inventory.rs
@@ -217,7 +217,7 @@ pub enum ConfigReconcilerInventoryStatus {
     ///
     /// This variant does not include the `OmicronSledConfig` used in the last
     /// attempt, because that's always available via
-    /// [`ConfigReconcilerInventory::last_reconciled_config`].
+    /// `ConfigReconcilerInventory::last_reconciled_config`.
     Idle { completed_at: DateTime<Utc>, ran_for: Duration },
 }
 
